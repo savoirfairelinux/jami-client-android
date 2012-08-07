565f67757eff2daf11d8b74835b542c7c44b0c19
Emeric Vigier <emeric.vigier@savoirfairelinux.com>
2012-08-07 14:01:16 -0400

    #14283: add Android.mk for libdbus-c++ tools

---
+++ jni/libdbus-c++-0.9.0-android/tools/Android.mk
@@ -0,0 +1,37 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+	xml.cpp xml2cpp.cpp \
+	generate_adaptor.cpp generate_proxy.cpp \
+	generator_utils.cpp
+
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../include \
+                    $(LOCAL_PATH)/../../dbus
+
+# do not cross-compile, host tool only is needed
+# FIXME Android NDK cannot build host tool, android sources needed...
+LOCAL_MODULE := dbusxx-xml2cpp
+LOCAL_CFLAGS += -Wall
+LOCAL_STATIC_LIBRARIES += libexpat
+LOCAL_LDFLAGS += -lexpat
+
+include $(BUILD_EXECUTABLE)
+
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := introspect.cpp
+
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../include
+#                    $(LOCAL_PATH)/../../dbus
+
+LOCAL_MODULE := dbusxx-introspect
+LOCAL_CFLAGS += -Wall
+
+LOCAL_STATIC_LIBRARIES += libdbus-c++-1
+LOCAL_LDFLAGS += -ldbus-c++-1
+
+include $(BUILD_EXECUTABLE)
+
