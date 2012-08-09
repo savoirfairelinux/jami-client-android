52e2d5f6251794b0492e0484520b68a3c3f21eef
Emeric Vigier <emeric.vigier@savoirfairelinux.com>
2012-08-09 17:29:06 -0400

    #14523: minimize Application.mk to detect dependency issues          fix libexpat and libssl dependencies

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,41 +1,39 @@
 APP_OPTIM := release
 
-APP_MODULES := libccgnu2
-APP_MODULES += libyaml
-APP_MODULES += libsamplerate
+#APP_MODULES := libccgnu2
+#APP_MODULES += libyaml
+#APP_MODULES += libsamplerate
 #APP_MODULES += libexpat_static
-APP_MODULES += libexpat_shared
-APP_MODULES += libccrtp1
-#APP_MODULES += libcutils-static
-#APP_MODULES += libcutils-shared
-APP_MODULES += libdbus
-APP_MODULES += libdbus-tools-common
+#APP_MODULES += libexpat_shared
+#APP_MODULES += libccrtp1
+#APP_MODULES += libdbus
+#APP_MODULES += libdbus-tools-common
 APP_MODULES += dbus-daemon
 APP_MODULES += dbus-monitor
 APP_MODULES += dbus-send
 
-APP_MODULES += libdbus-c++-1
-APP_MODULES += libdbus-glue
-APP_MODULES += libsiplink
-APP_MODULES += libconfig
+#APP_MODULES += libdbus-c++-1
+#APP_MODULES += libdbus-glue
+#APP_MODULES += libsiplink
+#APP_MODULES += libconfig
 
 #APP_MODULES += libcrypto
 #APP_MODULES += libssl
 #APP_MODULES += openssl
 
-APP_MODULES += libspeex
-APP_MODULES += libspeexresampler
-APP_MODULES += libopensl
-APP_MODULES += libsound
-APP_MODULES += libulaw
-APP_MODULES += libalaw
-APP_MODULES += libcodecfactory
-APP_MODULES += librtp
-APP_MODULES += libaudio
-APP_MODULES += libhistory
-APP_MODULES += libhooks
+#APP_MODULES += libspeex
+#APP_MODULES += libspeexresampler
+#APP_MODULES += libopensl
+#APP_MODULES += libsound
+#APP_MODULES += libulaw
+#APP_MODULES += libalaw
+#APP_MODULES += libcodecfactory
+#APP_MODULES += librtp
+#APP_MODULES += libaudio
+#APP_MODULES += libhistory
+#APP_MODULES += libhooks
 
-APP_MODULES += libsflphone
+#APP_MODULES += libsflphone
 APP_MODULES += sflphoned
 
 APP_STL := gnustl_shared
