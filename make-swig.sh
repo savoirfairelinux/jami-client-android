c30b71d4bb206c63610921bf33378b51cfef9338
Emeric Vigier <emeric.vigier@savoirfairelinux.com>
2012-09-12 14:51:47 -0400

    #15414: run swig directly from Android makefiles, handle deps         add script licence

--- make-swig.sh
+++ make-swig.sh
@@ -1,21 +1,46 @@
-#!/bin/bash - 
-#===============================================================================
+#!/bin/bash -
 #
-#          FILE:  make-swig.sh
-# 
-#         USAGE:  ./make-swig.sh 
-# 
-#   DESCRIPTION:  
+#  Copyright (C) 2004-2012 Savoir-Faire Linux Inc.
 #
-# 
-#        AUTHOR: Emeric Vigier (), emeric.vigier@savoirfairelinux.com
-#       COMPANY: Savoir-faire Linux
-#       CREATED: 2012-09-11 18:44:34 EDT
-#===============================================================================
+#  Author: Emeric Vigier <emeric.vigier@savoirfairelinux.com>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+#  Additional permission under GNU GPL version 3 section 7:
+#
+#  If you modify this program, or any covered work, by linking or
+#  combining it with the OpenSSL project's OpenSSL library (or a
+#  modified version of that library), containing parts covered by the
+#  terms of the OpenSSL or SSLeay licenses, Savoir-Faire Linux Inc.
+#  grants you additional permission to convey the resulting work.
+#  Corresponding Source for a non-source form of such a combination
+#  shall include the source code for the parts of OpenSSL used as well
+#  as that of the covered work.
+#
+
+# input: jni/sflphone/daemon/src/dbus/callmanager.i
+# output: sflphoneservice_loader.c
+#         callmanager_wrap.cpp
+#         sflphoneservice.java
+#         sflphoneserviceJNI.java
+#         ManagerImpl.java
 
 ROOT=`pwd`
 echo "in $ROOT"
 
+# FIXME
 echo "Generating callmanager_wrap.cpp..."
 swig -v -debug-tmused -c++ -java \
 -package com.savoirfairelinux.sflphone.client \
