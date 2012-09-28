9c966e375190fc93289918b0f194edd69dd4b196
Alexandre Savard <alexandre.savard@savoirfairelinux.com>
2012-09-28 14:41:28 -0400

    #16062: Add extend account preference entries

--- src/com/savoirfairelinux/sflphone/client/AccountManagementFragment.java
+++ src/com/savoirfairelinux/sflphone/client/AccountManagementFragment.java
@@ -59,11 +59,118 @@ public class AccountManagementFragment extends PreferenceFragment
     static final String DEFAULT_ACCOUNT_ID = "IP2IP";
     private ISipService service;
     HashMap<String, String> mAccountDetails = null;
-    ArrayList<String> mAccountList = null; 
+    ArrayList<String> mAccountList = null;
+    ArrayList<PreferenceEntry> basicDetailKeys = new ArrayList<PreferenceEntry>();
+    ArrayList<PreferenceEntry> advancedDetailKeys = new ArrayList<PreferenceEntry>();
+    Activity context = getActivity();
 
     public AccountManagementFragment(ISipService s)
     {
         service = s;
+
+        basicDetailKeys.add(new PreferenceEntry(ServiceConstants.CONFIG_ACCOUNT_TYPE,
+                            R.string.account_type_label));
+        basicDetailKeys.add(new PreferenceEntry(ServiceConstants.CONFIG_ACCOUNT_ALIAS,
+                            R.string.account_alias_label));
+        basicDetailKeys.add(new PreferenceEntry(ServiceConstants.CONFIG_ACCOUNT_ENABLE,
+                            R.string.account_enabled_label));
+        basicDetailKeys.add(new PreferenceEntry(ServiceConstants.CONFIG_ACCOUNT_HOSTNAME,
+                            R.string.account_hostname_label));
+        basicDetailKeys.add(new PreferenceEntry(ServiceConstants.CONFIG_ACCOUNT_USERNAME,
+                            R.string.account_username_label));
+        basicDetailKeys.add(new PreferenceEntry(ServiceConstants.CONFIG_ACCOUNT_ROUTESET,
+                            R.string.account_routeset_label));
+        basicDetailKeys.add(new PreferenceEntry(ServiceConstants.CONFIG_ACCOUNT_PASSWORD,
+                            R.string.account_password_label));
+        basicDetailKeys.add(new PreferenceEntry(ServiceConstants.CONFIG_ACCOUNT_REALM,
+                            R.string.account_realm_label));
+        basicDetailKeys.add(new PreferenceEntry(ServiceConstants.CONFIG_ACCOUNT_DEFAULT_REALM,
+                            R.string.account_useragent_label));
+        basicDetailKeys.add(new PreferenceEntry(ServiceConstants.CONFIG_ACCOUNT_USERAGENT,
+                            R.string.account_autoanswer_label));
+
+        advancedDetailKeys.add(new PreferenceEntry(ServiceConstants.CONFIG_ACCOUNT_REGISTRATION_EXPIRE,
+                            R.string.account_registration_exp_label));
+        advancedDetailKeys.add(new PreferenceEntry(ServiceConstants.CONFIG_ACCOUNT_REGISTRATION_STATUS,
+                            R.string.account_registration_status_label));
+        advancedDetailKeys.add(new PreferenceEntry(ServiceConstants.CONFIG_ACCOUNT_REGISTRATION_STATE_CODE,
+                            R.string.account_registration_code_label));
+        advancedDetailKeys.add(new PreferenceEntry(ServiceConstants.CONFIG_ACCOUNT_REGISTRATION_STATE_DESC,
+                            R.string.account_registration_state_label));
+        advancedDetailKeys.add(new PreferenceEntry(ServiceConstants.CONFIG_CREDENTIAL_NUMBER,
+                            R.string.account_credential_count_label));
+        advancedDetailKeys.add(new PreferenceEntry(ServiceConstants.CONFIG_ACCOUNT_DTMF_TYPE,
+                            R.string.account_config_dtmf_type_label));
+        advancedDetailKeys.add(new PreferenceEntry(ServiceConstants.CONFIG_RINGTONE_PATH,
+                            R.string.account_ringtone_path_label));
+        advancedDetailKeys.add(new PreferenceEntry(ServiceConstants.CONFIG_RINGTONE_ENABLED,
+                            R.string.account_ringtone_enabled_label));
+        advancedDetailKeys.add(new PreferenceEntry(ServiceConstants.CONFIG_KEEP_ALIVE_ENABLED,
+                            R.string.account_keep_alive_label));
+        advancedDetailKeys.add(new PreferenceEntry(ServiceConstants.CONFIG_ACCOUNT_AUTOANSWER,
+                            R.string.account_account_interface_label));
+        advancedDetailKeys.add(new PreferenceEntry(ServiceConstants.CONFIG_LOCAL_INTERFACE,
+                            R.string.account_local_interface_label));
+        advancedDetailKeys.add(new PreferenceEntry(ServiceConstants.CONFIG_INTERFACE,
+                            R.string.account_account_interface_label));
+        advancedDetailKeys.add(new PreferenceEntry(ServiceConstants.CONFIG_PUBLISHED_SAMEAS_LOCAL,
+                            R.string.account_published_same_as_local_label));
+        advancedDetailKeys.add(new PreferenceEntry(ServiceConstants.CONFIG_LOCAL_PORT,
+                            R.string.account_local_port_label));
+        advancedDetailKeys.add(new PreferenceEntry(ServiceConstants.CONFIG_PUBLISHED_PORT,
+                            R.string.account_published_port_label));
+        advancedDetailKeys.add(new PreferenceEntry(ServiceConstants.CONFIG_PUBLISHED_ADDRESS,
+                            R.string.account_published_address_label));
+        advancedDetailKeys.add(new PreferenceEntry(ServiceConstants.CONFIG_DISPLAY_NAME,
+                            R.string.account_displayname_label));
+        advancedDetailKeys.add(new PreferenceEntry(ServiceConstants.CONFIG_STUN_SERVER,
+                            R.string.account_stun_server_label));
+        advancedDetailKeys.add(new PreferenceEntry(ServiceConstants.CONFIG_STUN_ENABLE,
+                            R.string.account_stun_enable_label));
+        advancedDetailKeys.add(new PreferenceEntry(ServiceConstants.CONFIG_SRTP_ENABLE,
+                            R.string.account_srtp_enabled_label));
+        advancedDetailKeys.add(new PreferenceEntry(ServiceConstants.CONFIG_SRTP_KEY_EXCHANGE,
+                            R.string.account_srtp_exchange_label));
+        advancedDetailKeys.add(new PreferenceEntry(ServiceConstants.CONFIG_SRTP_ENCRYPTION_ALGO,
+                            R.string.account_encryption_algo_label));
+        advancedDetailKeys.add(new PreferenceEntry(ServiceConstants.CONFIG_SRTP_RTP_FALLBACK,
+                            R.string.account_srtp_fallback_label));
+        advancedDetailKeys.add(new PreferenceEntry(ServiceConstants.CONFIG_ZRTP_HELLO_HASH,
+                            R.string.account_hello_hash_enable_label));
+        advancedDetailKeys.add(new PreferenceEntry(ServiceConstants.CONFIG_ZRTP_DISPLAY_SAS,
+                            R.string.account_display_sas_label));
+        advancedDetailKeys.add(new PreferenceEntry(ServiceConstants.CONFIG_ZRTP_NOT_SUPP_WARNING,
+                            R.string.account_not_supported_warning_label));
+        advancedDetailKeys.add(new PreferenceEntry(ServiceConstants.CONFIG_ZRTP_DISPLAY_SAS_ONCE,
+                            R.string.account_display_sas_once_label));
+        advancedDetailKeys.add(new PreferenceEntry(ServiceConstants.CONFIG_TLS_LISTENER_PORT,
+                            R.string.account_listener_port_label));
+        advancedDetailKeys.add(new PreferenceEntry(ServiceConstants.CONFIG_TLS_ENABLE,
+                            R.string.account_tls_enabled_label));
+        advancedDetailKeys.add(new PreferenceEntry(ServiceConstants.CONFIG_TLS_CA_LIST_FILE,
+                            R.string.account_tls_certificate_list_label));
+        advancedDetailKeys.add(new PreferenceEntry(ServiceConstants.CONFIG_TLS_CERTIFICATE_FILE,
+                            R.string.account_tls_certificate_file_label));
+        advancedDetailKeys.add(new PreferenceEntry(ServiceConstants.CONFIG_TLS_PRIVATE_KEY_FILE,
+                            R.string.account_tls_private_key_file_label));
+        advancedDetailKeys.add(new PreferenceEntry(ServiceConstants.CONFIG_TLS_PASSWORD,
+                            R.string.account_tls_password_label));
+        advancedDetailKeys.add(new PreferenceEntry(ServiceConstants.CONFIG_TLS_METHOD,
+                            R.string.account_tls_method_label));
+        advancedDetailKeys.add(new PreferenceEntry(ServiceConstants.CONFIG_TLS_CIPHERS,
+                            R.string.account_tls_ciphers_label));
+        advancedDetailKeys.add(new PreferenceEntry(ServiceConstants.CONFIG_TLS_SERVER_NAME,
+                            R.string.account_tls_server_name_label));
+        advancedDetailKeys.add(new PreferenceEntry(ServiceConstants.CONFIG_TLS_VERIFY_SERVER,
+                            R.string.account_tls_verify_label));
+        advancedDetailKeys.add(new PreferenceEntry(ServiceConstants.CONFIG_TLS_VERIFY_CLIENT,
+                            R.string.account_tls_verify_client_label));
+        advancedDetailKeys.add(new PreferenceEntry(ServiceConstants.CONFIG_TLS_REQUIRE_CLIENT_CERTIFICATE,
+                            R.string.account_tls_require_client_certificat_label));
+        advancedDetailKeys.add(new PreferenceEntry(ServiceConstants.CONFIG_TLS_NEGOTIATION_TIMEOUT_SEC,
+                            R.string.account_tls_negotiation_timeout_sec));
+        advancedDetailKeys.add(new PreferenceEntry(ServiceConstants.CONFIG_TLS_NEGOTIATION_TIMEOUT_MSEC,
+                            R.string.account_tls_negotiation_timeout_msec));
     } 
 
     @Override
@@ -170,6 +277,31 @@ public class AccountManagementFragment extends PreferenceFragment
         accountPrefCat.setTitle(R.string.account_preferences);
         root.addPreference(accountPrefCat);
 
+        // 
+        for(PreferenceEntry entry : basicDetailKeys)
+        {
+            EditTextPreference accountAliasPref = new EditTextPreference(currentContext);
+            accountAliasPref.setDialogTitle(entry.mLabelId);
+            accountAliasPref.setPersistent(false);
+            accountAliasPref.setTitle(entry.mLabelId);
+            accountAliasPref.setSummary(CURRENT_VALUE + mAccountDetails.get(entry.mKey));
+            accountAliasPref.setOnPreferenceChangeListener(changeTextEditListener);
+            accountPrefCat.addPreference(accountAliasPref);
+        }
+
+        // 
+        for(PreferenceEntry entry : advancedDetailKeys)
+        {
+            EditTextPreference accountAliasPref = new EditTextPreference(currentContext);
+            accountAliasPref.setDialogTitle(entry.mLabelId);
+            accountAliasPref.setPersistent(false);
+            accountAliasPref.setTitle(entry.mLabelId);
+            accountAliasPref.setSummary(CURRENT_VALUE + mAccountDetails.get(entry.mKey));
+            accountAliasPref.setOnPreferenceChangeListener(changeTextEditListener);
+            accountPrefCat.addPreference(accountAliasPref);
+        }
+
+        /*
         // Alias
         EditTextPreference accountAliasPref = new EditTextPreference(currentContext);
         accountAliasPref.setDialogTitle(R.string.dialogtitle_account_alias_field);
@@ -250,7 +382,20 @@ public class AccountManagementFragment extends PreferenceFragment
         accountSrtpPref.setSummary(CURRENT_VALUE + mAccountDetails.get(ServiceConstants.CONFIG_SRTP_ENABLE));
         accountSrtpPref.setOnPreferenceChangeListener(changeTextEditListener);
         accountPrefCat.addPreference(accountSrtpPref);
+        */
 
         return root;
+    }
+
+    public static class PreferenceEntry
+    {
+        public String mKey;
+        public int mLabelId;
+
+        public PreferenceEntry(String key, int labelId)
+        {
+            mKey = key;
+            mLabelId = labelId;
+        }
     }
 }
