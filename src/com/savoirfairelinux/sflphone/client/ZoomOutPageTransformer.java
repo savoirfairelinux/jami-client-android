4e35fae649ffec407bc510380d643c4cafe38fa0
Alexandre Lision <alexandre.lision@savoirfairelinux.com>
2013-09-05 16:50:39 -0400

    * #24468: BUILD SUCCESSFUL

--- src/com/savoirfairelinux/sflphone/client/ZoomOutPageTransformer.java
+++ src/com/savoirfairelinux/sflphone/client/ZoomOutPageTransformer.java
@@ -5,7 +5,7 @@ import android.support.v4.view.ViewPager;
 
 public class ZoomOutPageTransformer implements ViewPager.PageTransformer {
     private final float scalingStart;
-    
+
     public ZoomOutPageTransformer(float scalingStart) {
         super();
         this.scalingStart = 1 - scalingStart;
@@ -13,15 +13,6 @@ public class ZoomOutPageTransformer implements ViewPager.PageTransformer {
 
     @Override
     public void transformPage(View page, float position) {
-
-        if (position >= 0) {
-            final int w = page.getWidth();
-            float scaleFactor = 1 - scalingStart * position;
-
-            page.setAlpha(1 - position);
-            page.setScaleX(scaleFactor);
-            page.setScaleY(scaleFactor);
-            page.setTranslationX(w * (1 - position) - w);
-        }
+        page.setRotationY(position * -30);
     }
 }
