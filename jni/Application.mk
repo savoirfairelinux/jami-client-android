849bdca85ee7af9c8ca2662dc3b428b4618e10e3
Alexandre Lision <alexandre.lision@savoirfairelinux.com>
2013-09-20 14:27:48 -0400

    * build process: clean up in ndk modules

--- jni/Application.mk
+++ jni/Application.mk
@@ -12,10 +12,7 @@ APP_MODULES += libexpat_static
 APP_MODULES += libexpat_shared
 APP_MODULES += libccrtp1
 APP_MODULES += libsndfile
-#APP_MODULES += libcutils-static
-#APP_MODULES += libcutils-shared
 
-# APP_MODULES += libsiplink
 # APP_MODULES += libconfig
 
 # APP_MODULES += libcrypto
@@ -24,8 +21,6 @@ APP_MODULES += libsndfile
 
 APP_MODULES += libspeex
 APP_MODULES += libspeexresampler
-# APP_MODULES += libopensl
-# APP_MODULES += libsound
 APP_MODULES += libcodec_ulaw
 APP_MODULES += libcodec_alaw
 APP_MODULES += libcodec_g722
@@ -33,12 +28,5 @@ APP_MODULES += libcodec_g722
 #APP_MODULES += libcodec_speex_nb
 #APP_MODULES += libcodec_speex_ub
 #APP_MODULES += libcodec_speex_wb
-#APP_MODULES += libcodecfactory
-# APP_MODULES += librtp
-# APP_MODULES += libaudio
-# APP_MODULES += libhistory
-
-# APP_MODULES += libhooks
 
 APP_MODULES += libsflphone
-# APP_MODULES += sflphoned
