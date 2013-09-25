ee37f7cf7ee27d8f6e4ff75627b1e540406ae6aa
Alexandre Lision <alexandre.lision@savoirfairelinux.com>
2013-09-25 17:36:39 -0400

    refactor: added animation as inner class of Activity

--- src/com/savoirfairelinux/sflphone/client/SFLPhoneHomeActivity.java
+++ src/com/savoirfairelinux/sflphone/client/SFLPhoneHomeActivity.java
@@ -582,5 +582,22 @@ public class SFLPhoneHomeActivity extends Activity implements DialingFragment.Ca
         intent.putExtra("conference", c);
         startActivityForResult(intent, REQUEST_CODE_CALL);
     }
+    
+    private class ZoomOutPageTransformer implements ViewPager.PageTransformer {
+        private static final float MIN_ALPHA = .6f;
+//        private final float scalingStart;
+
+        public ZoomOutPageTransformer(float scalingStart) {
+            super();
+//            this.scalingStart = 1 - scalingStart;
+        }
+
+        @Override
+        public void transformPage(View page, float position) {
+            // page.setRotationY(position * -30);
+            final float normalizedposition = Math.abs(Math.abs(position) - 1);
+            page.setAlpha(MIN_ALPHA + (1.f-MIN_ALPHA)*normalizedposition);
+        }
+    }
 
 }
