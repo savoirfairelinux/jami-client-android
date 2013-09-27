bf95155c0a62726cc2165fa267892224fe7829f4
Alexandre Lision <alexandre.lision@savoirfairelinux.com>
2013-09-27 16:27:52 -0400

    about: added link to website

--- src/com/savoirfairelinux/sflphone/fragments/AboutFragment.java
+++ src/com/savoirfairelinux/sflphone/fragments/AboutFragment.java
@@ -1,12 +1,15 @@
 package com.savoirfairelinux.sflphone.fragments;
 
-import com.savoirfairelinux.sflphone.R;
-
 import android.app.Fragment;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.savoirfairelinux.sflphone.R;
 
 public class AboutFragment extends Fragment {
     
@@ -15,6 +18,12 @@ public class AboutFragment extends Fragment {
         View inflatedView = inflater.inflate(R.layout.frag_about, parent, false);
 
         
+        
+        
+        TextView link = (TextView) inflatedView.findViewById(R.id.web_site);
+        String linkText = "<a href='http://sflphone.org/'>"+getResources().getString(R.string.web_site)+"</a>";
+        link.setText(Html.fromHtml(linkText));
+        link.setMovementMethod(LinkMovementMethod.getInstance());
         getActivity().getActionBar().setTitle(R.string.menu_item_about);
         return inflatedView;
     }
