savoirfairelinux/jami-client-android

commit 822ca2e91b70be39984e69c00a4c1f44ac39f9d1

* #40116: fix some warnings during ndk-build

jni/Android.mk
@@ -65,10 +65,10 @@ MY_LIBZRTPCPP=libzrtp/sources
 MY_JNI_WRAP := $(LOCAL_SRC_PATH)/client/android/callmanager_wrap.cpp
 
 
+# FIXME: It would be cool to call the swig script automatically
+#$(shell $(LOCAL_PATH)/../make-swig.sh)
 
-$(MY_JNI_WRAP): $(LOCAL_SRC_PATH)/client/android/jni_interface.i $(LOCAL_SRC_PATH)/client/android/sflphoneservice.c.template
-	@echo "in $(MY_JNI_WRAP) target"
-	./make-swig.sh
+	
 
 #LOCAL_CPPFLAGS += -std=c++11
 LOCAL_CPPFLAGS += -frtti
@@ -218,7 +218,7 @@ LOCAL_STATIC_LIBRARIES += 	pjsip \
 							libzrtpcpp \
 							libsndfile \
 							libccrtp1 \
-							libexpat_shared \
+							libexpat \
 							libspeexresampler \
 							libyaml
 							
@@ -232,16 +232,14 @@ include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 
-$(info $(LOCAL_CODECS_PATH))
-
 LOCAL_SRC_FILES := 	$(LOCAL_CODECS_PATH)/ulaw.cpp \
 					$(LOCAL_CODECS_PATH)/audiocodec.cpp
 
 # FIXME
 LOCAL_C_INCLUDES += $(LOCAL_CODECS_PATH)/.. \
-			$(LOCAL_CODECS_PATH)/../.. \
-			$(APP_PROJECT_PATH)/jni/$(MY_CCRTP)/src \
-			$(APP_PROJECT_PATH)/jni/$(MY_COMMONCPP)/inc
+					$(LOCAL_CODECS_PATH)/../.. \
+					$(APP_PROJECT_PATH)/jni/$(MY_CCRTP)/src \
+					$(APP_PROJECT_PATH)/jni/$(MY_COMMONCPP)/inc
 
 LOCAL_MODULE := libcodec_ulaw
 
@@ -451,11 +449,11 @@ LOCAL_SRC_FILES := $(LOCAL_CODECS_PATH)/speexcodec_wb.cpp \
 					$(LOCAL_CODECS_PATH)/audiocodec.cpp
 
 LOCAL_C_INCLUDES += $(LOCAL_SRC_PATH) \
-			$(MY_SPEEX)/include/speex \
-			$(MY_SPEEX)/include \
-			$(APP_PROJECT_PATH)/jni/$(MY_CCRTP)/src \
-			$(APP_PROJECT_PATH)/jni/$(MY_COMMONCPP)/inc \
-			$(APP_PROJECT_PATH)/jni/sflphone/daemon
+					$(MY_SPEEX)/include/speex \
+					$(MY_SPEEX)/include \
+					$(APP_PROJECT_PATH)/jni/$(MY_CCRTP)/src \
+					$(APP_PROJECT_PATH)/jni/$(MY_COMMONCPP)/inc \
+					$(APP_PROJECT_PATH)/jni/sflphone/daemon
 
 LOCAL_MODULE := libcodec_speex_wb
 

jni/libopus/Android.mk
@@ -13,7 +13,6 @@ LOCAL_CELT_SOURCES := 	$(MY_OPUS)/celt/bands.c \
 						$(MY_OPUS)/celt/laplace.c \
 						$(MY_OPUS)/celt/cwrs.c \
 						$(MY_OPUS)/celt/quant_bands.c \
-						$(MY_OPUS)/celt/opus_custom_demo.c \
 						$(MY_OPUS)/celt/pitch.c \
 						$(MY_OPUS)/celt/celt_decoder.c \
 						$(MY_OPUS)/celt/celt_encoder.c \
@@ -42,7 +41,6 @@ LOCAL_SILK_SOURCES :=  	$(MY_OPUS)/silk/CNG.c \
 						$(MY_OPUS)/silk/encode_pulses.c \
 						$(MY_OPUS)/silk/resampler_rom.c \
 						$(MY_OPUS)/silk/stereo_quant_pred.c \
-						$(MY_OPUS)/silk/CNG.c \
 						$(MY_OPUS)/silk/biquad_alt.c \
 						$(MY_OPUS)/silk/resampler_down2_3.c \
 						$(MY_OPUS)/silk/VAD.c \
@@ -101,38 +99,6 @@ LOCAL_SILK_SOURCES :=  	$(MY_OPUS)/silk/CNG.c \
 						$(MY_OPUS)/silk/pitch_est_tables.c \
 						$(MY_OPUS)/silk/resampler_private_IIR_FIR.c
 
-
-
-
-LOCAL_SILK_SOURCES_FIXED := $(MY_OPUS)/silk/fixed/LTP_analysis_filter_FIX.c \
-							$(MY_OPUS)/silk/fixed/find_pitch_lags_FIX.c \
-							$(MY_OPUS)/silk/fixed/solve_LS_FIX.c \
-							$(MY_OPUS)/silk/fixed/k2a_FIX.c \
-							$(MY_OPUS)/silk/fixed/regularize_correlations_FIX.c \
-							$(MY_OPUS)/silk/fixed/apply_sine_window_FIX.c \
-							$(MY_OPUS)/silk/fixed/corrMatrix_FIX.c \
-							$(MY_OPUS)/silk/fixed/process_gains_FIX.c \
-							$(MY_OPUS)/silk/fixed/prefilter_FIX.c \
-							$(MY_OPUS)/silk/fixed/autocorr_FIX.c \
-							$(MY_OPUS)/silk/fixed/find_LTP_FIX.c \
-							$(MY_OPUS)/silk/fixed/warped_autocorrelation_FIX.c \
-							$(MY_OPUS)/silk/fixed/schur_FIX.c \
-							$(MY_OPUS)/silk/fixed/burg_modified_FIX.c \
-							$(MY_OPUS)/silk/fixed/noise_shape_analysis_FIX.c \
-							$(MY_OPUS)/silk/fixed/residual_energy_FIX.c \
-							$(MY_OPUS)/silk/fixed/find_LPC_FIX.c \
-							$(MY_OPUS)/silk/fixed/encode_frame_FIX.c \
-							$(MY_OPUS)/silk/fixed/k2a_Q16_FIX.c \
-							$(MY_OPUS)/silk/fixed/LTP_analysis_filter_FIX.c \
-							$(MY_OPUS)/silk/fixed/find_pred_coefs_FIX.c \
-							$(MY_OPUS)/silk/fixed/schur64_FIX.c \
-							$(MY_OPUS)/silk/fixed/pitch_analysis_core_FIX.c \
-							$(MY_OPUS)/silk/fixed/vector_ops_FIX.c \
-							$(MY_OPUS)/silk/fixed/residual_energy16_FIX.c \
-							$(MY_OPUS)/silk/fixed/LTP_scale_ctrl_FIX.c \
-
-
-
 LOCAL_SILK_SOURCES_FLOAT := $(MY_OPUS)/silk/float/apply_sine_window_FLP.c \
 							$(MY_OPUS)/silk/float/inner_product_FLP.c \
 							$(MY_OPUS)/silk/float/warped_autocorrelation_FLP.c \
@@ -169,10 +135,8 @@ LOCAL_OPUS_SOURCES := 	$(MY_OPUS)/src/opus.c \
 						$(MY_OPUS)/src/mlp.c \
 						$(MY_OPUS)/src/mlp_data.c \
 						$(MY_OPUS)/src/analysis.c \
-						$(MY_OPUS)/src/repacketizer_demo.c \
 						$(MY_OPUS)/src/opus_decoder.c \
 						$(MY_OPUS)/src/repacketizer.c \
-						$(MY_OPUS)/src/opus_demo.c \
 						$(MY_OPUS)/src/opus_multistream.c \
 						$(MY_OPUS)/src/opus_encoder.c \
 						$(MY_OPUS)/src/opus_compare.c \
@@ -189,11 +153,10 @@ LOCAL_C_INCLUDES := $(MY_OPUS) \
 LOCAL_SRC_FILES := 	$(LOCAL_OPUS_SOURCES) \
 					$(LOCAL_CELT_SOURCES) \
 					$(LOCAL_SILK_SOURCES) \
-					$(LOCAL_SILK_SOURCES_FIXED) \
 					$(LOCAL_SILK_SOURCES_FLOAT)
 
 LOCAL_EXPORT_C_INCLUDES := $(MY_OPUS)/include
 
 LOCAL_MODULE := libopus
 
-include $(BUILD_STATIC_LIBRARY)
+include $(BUILD_SHARED_LIBRARY)

jni/libexpat/Android.mk
@@ -3,9 +3,6 @@ include $(CLEAR_VARS)
 
 MY_LIBEXPAT := libexpat/sources
 
-# We need to build this for both the device (as a shared library)
-# and the host (as a static library for tools to use).
-
 common_SRC_FILES := $(MY_LIBEXPAT)/xmlparse.c \
 					$(MY_LIBEXPAT)/xmlrole.c \
 					$(MY_LIBEXPAT)/xmltok.c
@@ -16,47 +13,10 @@ common_C_INCLUDES += $(MY_LIBEXPAT)
 
 common_COPY_HEADERS_TO := libexpat
 
-common_COPY_HEADERS := \
-	lib/expat.h \
-	lib/expat_external.h
-
-# For the device
-# =====================================================
-
-# Device static library
-include $(CLEAR_VARS)
-
-ifeq ($(TARGET_ARCH),arm)
-LOCAL_NDK_VERSION := 4
-LOCAL_SDK_VERSION := 8
-endif
-
 LOCAL_SRC_FILES := $(common_SRC_FILES)
 LOCAL_CFLAGS += $(common_CFLAGS)
 LOCAL_C_INCLUDES += $(common_C_INCLUDES)
 
-LOCAL_MODULE:= libexpat_static
-LOCAL_MODULE_FILENAME := libexpat
-LOCAL_MODULE_TAGS := optional
-
-include $(BUILD_STATIC_LIBRARY)
-
-# Device shared library
-include $(CLEAR_VARS)
-
-ifeq ($(TARGET_ARCH),arm)
-LOCAL_NDK_VERSION := 4
-LOCAL_SDK_VERSION := 8
-endif
-
-LOCAL_SRC_FILES := $(common_SRC_FILES)
-LOCAL_CFLAGS += $(common_CFLAGS)
-LOCAL_C_INCLUDES += $(common_C_INCLUDES)
-
-LOCAL_MODULE:= libexpat_shared
-LOCAL_MODULE_FILENAME := libexpat
-LOCAL_MODULE_TAGS := optional
-LOCAL_COPY_HEADERS_TO := $(common_COPY_HEADERS_TO)
-LOCAL_COPY_HEADERS := $(common_COPY_HEADERS)
+LOCAL_MODULE:= libexpat
 
 include $(BUILD_STATIC_LIBRARY)

commit 73d06a3cb59f4b8cd027d445c7cc8c54d2bc2739

* #40116: fix opus compilation
Bug introduced in rev 822ca2e9.

jni/Android.mk
@@ -361,7 +361,7 @@ LOCAL_C_INCLUDES += $(LOCAL_PATH)/.. \
 
 LOCAL_MODULE := libcodec_opus
 
-LOCAL_LDLIBS := -llog 
+LOCAL_LDLIBS := -llog
 				
 LOCAL_CPPFLAGS += $(NETWORKMANAGER) \
 				  -DCCPP_PREFIX \

jni/libopus/Android.mk
@@ -159,4 +159,4 @@ LOCAL_EXPORT_C_INCLUDES := $(MY_OPUS)/include
 
 LOCAL_MODULE := libopus
 
-include $(BUILD_SHARED_LIBRARY)
+include $(BUILD_STATIC_LIBRARY)