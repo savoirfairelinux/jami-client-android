8fd1532455809a9450981e346d7ff6a7b418d5b4
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2015-11-10 12:10:46 -0500

    preferences: add Settings page
    * Allow user to select system contact integration
    * Allow user to enable mobile connectivity
    
    Change-Id: Ieb161d97d5dc61b3664597def130dea6c7eefb5f

---
+++ ring-android/app/src/main/java/cx/ring/client/SettingsActivity.java
@@ -0,0 +1,30 @@
+package cx.ring.client;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+import cx.ring.fragments.SettingsFragment;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new SettingsFragment())
+                .commit();
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
