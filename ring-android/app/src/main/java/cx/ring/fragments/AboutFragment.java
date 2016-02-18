e36c2dc0d1c4590d978d5bf770611eee01d13c2a
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2016-02-18 11:22:10 -0500

    update about dialog
    Tuleap: #102
    Change-Id: Ifede6ef96f2e811a683461e776cd233e989baa13

--- ring-android/app/src/main/java/cx/ring/fragments/AboutFragment.java
+++ ring-android/app/src/main/java/cx/ring/fragments/AboutFragment.java
@@ -1,6 +1,24 @@
+/*
+ *  Copyright (C) 2015-2016 Savoir-faire Linux Inc.
+ *
+ *  Author: Adrien Béraud <adrien.beraud@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package cx.ring.fragments;
 
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.text.Html;
@@ -9,6 +27,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import cx.ring.BuildConfig;
 import cx.ring.R;
 import cx.ring.client.HomeActivity;
 
@@ -25,17 +45,15 @@ public class AboutFragment extends Fragment {
         View inflatedView = inflater.inflate(R.layout.frag_about, parent, false);
 
         TextView title = (TextView) inflatedView.findViewById(R.id.app_name);
-        try {
-            String versionName = getActivity().getPackageManager().getPackageInfo(getActivity().getPackageName(), 0).versionName;
-            title.setText(getString(R.string.app_name) + " for Android " + versionName);
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-        }
+        title.setText(getString(R.string.app_name) + " for Android " + BuildConfig.VERSION_NAME);
 
         TextView link = (TextView) inflatedView.findViewById(R.id.web_site);
         String linkText = "<a href='"+getString(R.string.app_website)+"'>" + getString(R.string.web_site) + "</a>";
         link.setText(Html.fromHtml(linkText));
         link.setMovementMethod(LinkMovementMethod.getInstance());
+
+        TextView licence = (TextView) inflatedView.findViewById(R.id.licence);
+        licence.setMovementMethod(LinkMovementMethod.getInstance());
 
         return inflatedView;
     }
