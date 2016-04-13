5698363b42b10933c66319bbf540da28d013c6a9
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2016-04-13 11:43:04 -0400

    text message acknowledgement of receipt
    This patch implements the text message acknowledgement of receipt.
    
    Change-Id: I40115d40905c860420addfe34cd846b30ab2532c
    Tuleap: #633

--- ring-android/app/src/main/java/cx/ring/client/ConversationActivity.java
+++ ring-android/app/src/main/java/cx/ring/client/ConversationActivity.java
@@ -29,7 +29,9 @@ import android.content.ServiceConnection;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.SystemClock;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
@@ -48,7 +50,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.AnimationUtils;
 import android.view.inputmethod.EditorInfo;
-import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -78,6 +79,7 @@ public class ConversationActivity extends AppCompatActivity {
 
     public static final Uri CONTENT_URI = Uri.withAppendedPath(LocalService.AUTHORITY_URI, "conversations");
     public static final int REQ_ADD_CONTACT = 42;
+    static final long REFRESH_INTERVAL_MS = 30 * 1000;
 
     private boolean mBound = false;
     private boolean mVisible = false;
@@ -95,6 +97,8 @@ public class ConversationActivity extends AppCompatActivity {
 
     private ConversationAdapter adapter = null;
     private NumberAdapter numberAdapter = null;
+
+    private final Handler refreshTaskHandler = new Handler();
 
     private enum ConversationMessageType {
         INCOMING_TEXT_MESSAGE(0),
@@ -154,7 +158,7 @@ public class ConversationActivity extends AppCompatActivity {
         return 0;
     }
 
-    private void refreshView() {
+    private void refreshView(long refreshed) {
         Pair<Conversation, SipUri> conv = getConversation(service, getIntent());
         conversation = conv.first;
         preferredNumber = conv.second;
@@ -179,7 +183,7 @@ public class ConversationActivity extends AppCompatActivity {
             });
         }
 
-        adapter.updateDataset(conversation.getHistory());
+        adapter.updateDataset(conversation.getHistory(), refreshed);
 
         if (conversation.getContact().getPhones().size() > 1) {
             numberSpinner.setVisibility(View.VISIBLE);
@@ -189,22 +193,10 @@ public class ConversationActivity extends AppCompatActivity {
                 preferredNumber = new SipUri(conversation.getLastNumberUsed(conversation.getLastAccountUsed()));
             }
             numberSpinner.setSelection(getIndex(numberSpinner, preferredNumber));
-            numberSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-                @Override
-                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                    msgEditTxt.setHint(getString(R.string.action_send_msg, ((CallContact.Phone) numberAdapter.getItem(position)).getNumber().getRawUriString()));
-                }
-
-                @Override
-                public void onNothingSelected(AdapterView<?> parent) {
-                }
-            });
         } else {
             numberSpinner.setVisibility(View.GONE);
             preferredNumber = conversation.getContact().getPhones().get(0).getNumber();
         }
-
-        msgEditTxt.setHint(getString(R.string.action_send_msg, preferredNumber.getRawUriString()));
 
         invalidateOptionsMenu();
     }
@@ -213,25 +205,28 @@ public class ConversationActivity extends AppCompatActivity {
         @Override
         public void onServiceConnected(ComponentName className, IBinder binder) {
             service = ((LocalService.LocalBinder) binder).getService();
-            registerReceiver(receiver, new IntentFilter(LocalService.ACTION_CONF_UPDATE));
 
             adapter = new ConversationAdapter(ConversationActivity.this, service.get40dpContactCache(), service.getThreadPool());
             if (histList != null)
                 histList.setAdapter(adapter);
 
-            refreshView();
+            refreshView(0);
+            IntentFilter filter = new IntentFilter(LocalService.ACTION_CONF_UPDATE);
+            registerReceiver(receiver, filter);
 
             mBound = true;
             if (mVisible && conversation != null && !conversation.mVisible) {
                 conversation.mVisible = true;
                 service.readConversation(conversation);
             }
+            refreshTaskHandler.postDelayed(refreshTask, REFRESH_INTERVAL_MS);
         }
 
         @Override
         public void onServiceDisconnected(ComponentName arg0) {
             Log.w(TAG, "ConversationActivity onServiceDisconnected " + arg0.getClassName());
             mBound = false;
+            refreshTaskHandler.removeCallbacks(refreshTask);
             if (conversation != null) {
                 conversation.mVisible = false;
             }
@@ -242,9 +237,25 @@ public class ConversationActivity extends AppCompatActivity {
         @Override
         public void onReceive(Context context, Intent intent) {
             Log.w(TAG, "onReceive " + intent.getAction() + " " + intent.getDataString());
-            refreshView();
+            refreshView(intent.getLongExtra(LocalService.ACTION_CONF_UPDATE_EXTRA_MSG, 0));
             if (adapter.getItemCount() > 0)
                 histList.smoothScrollToPosition(adapter.getItemCount() - 1);
+        }
+    };
+
+    private final Runnable refreshTask = new Runnable() {
+        private long lastRefresh = 0;
+
+        public void run() {
+            if (lastRefresh == 0)
+                lastRefresh = SystemClock.uptimeMillis();
+            else
+                lastRefresh += REFRESH_INTERVAL_MS;
+
+            Log.w(TAG, "refreshTask");
+            adapter.notifyDataSetChanged();
+
+            refreshTaskHandler.postAtTime(this, lastRefresh + REFRESH_INTERVAL_MS);
         }
     };
 
@@ -430,10 +441,14 @@ public class ConversationActivity extends AppCompatActivity {
             infos_fetcher = pool;
         }
 
-        public void updateDataset(final ArrayList<Conversation.ConversationElement> list) {
-            Log.i(TAG, "updateDataset " + list.size());
-            if (list.size() == texts.size())
+        public void updateDataset(final ArrayList<Conversation.ConversationElement> list, long rid) {
+            Log.i(TAG, "updateDataset " + list.size() + " " + rid);
+            if (list.size() == texts.size()) {
+                if (rid != 0) {
+                    notifyDataSetChanged();
+                }
                 return;
+            }
             int lastPos = texts.size();
             int newItmes = list.size() - lastPos;
             if (lastPos == 0 || newItmes < 0) {
@@ -491,18 +506,17 @@ public class ConversationActivity extends AppCompatActivity {
                 boolean sep = false;
                 boolean sep_same = false;
                 if (position > 0 && texts.get(position - 1).text != null) {
-                    TextMessage msg = texts.get(position - 1).text;
-                    if (msg.isIncoming() && txt.text.isIncoming() && msg.getNumber().equals(txt.text.getNumber()))
+                    TextMessage prev = texts.get(position - 1).text;
+                    if (prev.isIncoming() && txt.text.isIncoming() && prev.getNumber().equals(txt.text.getNumber()))
                         sep_same = true;
-                }
-                if (position > 0 && texts.get(position - 1).text != null && position < texts.size() - 1) {
-                    TextMessage msg = texts.get(position + 1).text;
-                    if (msg != null) {
-                        long diff = msg.getTimestamp() - txt.text.getTimestamp();
-                        if (diff > 30 * 1000)
-                            sep = true;
-                    } else {
-                        sep = true;
+                    sep = true;
+                    if (position < texts.size() - 1) {
+                        TextMessage next = texts.get(position + 1).text;
+                        if (next != null) {
+                            long diff = next.getTimestamp() - txt.text.getTimestamp();
+                            if (diff < 60 * 1000)
+                                sep = false;
+                        }
                     }
                 }
 
@@ -515,7 +529,7 @@ public class ConversationActivity extends AppCompatActivity {
                     if (bmp != null)
                         h.photo.setImageBitmap(bmp);
                     else {
-                        h.photo.setImageBitmap(memory_cache.get(-1l));
+                        h.photo.setImageBitmap(memory_cache.get(-1L));
                         final WeakReference<ViewHolder> wh = new WeakReference<>(h);
                         final ContactPictureTask.PictureLoadedCallback cb = new ContactPictureTask.PictureLoadedCallback() {
                             @Override
@@ -553,11 +567,22 @@ public class ConversationActivity extends AppCompatActivity {
                     }
                 }
                 h.msgTxt.setText(txt.text.getMessage());
-                if (sep) {
+                if (txt.text.getStatus() == TextMessage.Status.SENDING) {
                     h.msgDetailTxt.setVisibility(View.VISIBLE);
-                    h.msgDetailTxt.setText(DateUtils.getRelativeTimeSpanString(txt.text.getTimestamp(), new Date().getTime(), 0, 0));
+                    h.msgDetailTxt.setText(R.string.message_sending);
                 } else {
-                    h.msgDetailTxt.setVisibility(View.GONE);
+                    if (sep) {
+                        h.msgDetailTxt.setVisibility(View.VISIBLE);
+                        long now = new Date().getTime();
+                        if (now - txt.text.getTimestamp() < 60L * 1000L)
+                            h.msgDetailTxt.setText(R.string.time_just_now);
+                        else if (now - txt.text.getTimestamp() < 3600L * 1000L)
+                            h.msgDetailTxt.setText(DateUtils.getRelativeTimeSpanString(txt.text.getTimestamp(), now, 0, 0));
+                        else
+                            h.msgDetailTxt.setText(DateUtils.formatSameDayTime(txt.text.getTimestamp(), now, DateFormat.SHORT, DateFormat.SHORT));
+                    } else {
+                        h.msgDetailTxt.setVisibility(View.GONE);
+                    }
                 }
             } else {
                 h.cid = txt.call.getContactID();
