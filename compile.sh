ff20effd8fa3cdf61707618b969dc9d6e192a04f
Romain Bertozzi <romain.bertozzi@savoirfairelinux.com>
2016-04-25 10:16:09 -0400

    compile: Android NDK Fixup update (build on OSX)
    This patch integrate a fix in the compile.sh concerning the Android
    NDK.
    This patch comes from the Android VLC project, please see commit :
    https://code.videolan.org/videolan/vlc-android/commit/0604f3fe39bf49e03bd08b6ce18f5a6d8d934629
    It is associated with a daemon patch, please see 4092.
    https://gerrit-ring.savoirfairelinux.com/#/c/4092/
    
    Change-Id: I9d8671894517b0eb4e679587ef583283a8e4bcc1
    Tuleap: #534

--- compile.sh
+++ compile.sh
@@ -331,32 +331,16 @@ if [ ! -f config.h ]; then
     ${ANDROID_PATH}/configure.sh ${OPTS}
 fi
 
-# ANDROID NDK FIXUP (BLAME GOOGLE)
-config_undef ()
-{
-    previous_change=`stat -c "%y" config.h`
-    sed -i 's,#define '$1' 1,/\* #undef '$1' \*/,' config.h
-    # don't change modified date in order to don't trigger a full build
-    touch -d "$previous_change" config.h
-}
-
-# if config dependencies change, ./config.status --recheck
-# is run and overwrite previously hacked config.h. So call make Makefile here
-# and hack config.h after.
-
-make $MAKEFLAGS Makefile
-
-if [ ${ANDROID_ABI} = "x86" -a ${ANDROID_API} != "android-21" ] ; then
-    # NDK x86 libm.so has nanf symbol but no nanf definition, we don't known if
-    # intel devices has nanf. Assume they don't have it.
-    config_undef HAVE_NANF
-fi
 if [ ${ANDROID_API} = "android-21" ] ; then
     # android-21 has empty sys/shm.h headers that triggers shm detection but it
     # doesn't have any shm functions and/or symbols. */
-    config_undef HAVE_SYS_SHM_H
+    export ac_cv_header_sys_shm_h=no
 fi
-# END OF ANDROID NDK FIXUP
+if [ ${ANDROID_ABI} = "x86" -a ${ANDROID_API} != "android-21" ] ; then
+    # NDK x86 libm.so has nanf symbol but no nanf definition, we don't known if
+    # intel devices has nanf. Assume they don't have it.
+    export ac_cv_lib_m_nanf=no
+fi
 
 echo "Building dring ${MAKEFLAGS}"
 V=99 make $MAKEFLAGS
