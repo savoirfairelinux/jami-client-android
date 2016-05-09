25973548d15f81fbc12f52835e7db968914956ef
Romain Bertozzi <romain.bertozzi@savoirfairelinux.com>
2016-05-09 11:56:45 -0400

    smartlist: present/add contact UI/UX
    This patch introduces changes concerning the contacts management from
    the Smartlist.
    By clicking on the picture of the conversation, the user can now dis-
    play the contact's system datasheet.
    If the contact is unknown, the system presents an invitation to crea-
    te a known contact.
    
    Change-Id: I3755b84da9aa7d7f4cde226f0d918602f1a91e9b
    Tuleap: #666

--- ring-android/app/src/main/java/cx/ring/fragments/SmartListFragment.java
+++ ring-android/app/src/main/java/cx/ring/fragments/SmartListFragment.java
@@ -61,10 +61,12 @@ import cx.ring.client.HomeActivity;
 import cx.ring.client.QRCodeScannerActivity;
 import cx.ring.model.CallContact;
 import cx.ring.model.Conference;
+import cx.ring.model.Conversation;
 import cx.ring.service.LocalService;
 
 public class SmartListFragment extends Fragment implements SearchView.OnQueryTextListener,
-        HomeActivity.Refreshable {
+        HomeActivity.Refreshable,
+        SmartListAdapter.SmartListAdapterCallback {
     private static final String TAG = SmartListFragment.class.getSimpleName();
 
     private static final int USER_INPUT_DELAY = 300;
@@ -124,7 +126,7 @@ public class SmartListFragment extends Fragment implements SearchView.OnQueryTex
         if (mSmartListAdapter == null) {
             bindService(getActivity(), service);
         } else {
-            mSmartListAdapter.updateDataset(service.getConversations(),null);
+            mSmartListAdapter.updateDataset(service.getConversations(), null);
         }
 
         if (service.isConnected()) {
@@ -330,13 +332,14 @@ public class SmartListFragment extends Fragment implements SearchView.OnQueryTex
                 service.get40dpContactCache(),
                 service.getThreadPool());
 
-        mSmartListAdapter.updateDataset(service.getConversations(),null);
-
+        mSmartListAdapter.updateDataset(service.getConversations(), null);
+        mSmartListAdapter.setCallback(this);
         mList.setAdapter(mSmartListAdapter);
     }
 
     private void startConversation(CallContact c) {
         mSearchMenuItem.collapseActionView();
+
         Intent intent = new Intent()
                 .setClass(getActivity(), ConversationActivity.class)
                 .setAction(Intent.ACTION_VIEW)
@@ -508,4 +511,22 @@ public class SmartListFragment extends Fragment implements SearchView.OnQueryTex
             }, delay);
         }
     }
+
+    //region SmartlistAdapterCallback
+
+    @Override
+    public void pictureTapped(Conversation conversation) {
+        if (conversation == null) {
+            Log.d(TAG, "pictureTapped, conversation is null");
+            return;
+        }
+        if (conversation.getContact() != null) {
+            Activity activity = getActivity();
+            if (activity != null) {
+                conversation.getContact().displayContact(getActivity());
+            }
+        }
+    }
+
+    //endregion
 }
