c8158c95c9685889c6f78d7a1fd95a4d24b79cdd
Romain Bertozzi <romain.bertozzi@savoirfairelinux.com>
2016-08-01 15:29:08 -0400

    profile: gather parts and build vcard
    This patch gathers all the parts of a given profile. It builds the
    final vcard string from these parts.
    
    Change-Id: I853caa25005469986d76fad3e892de7d813f0cb0
    Tuleap: #856

---
+++ ring-android/app/src/main/java/cx/ring/utils/VCardUtils.java
@@ -0,0 +1,56 @@
+/*
+ *  Copyright (C) 2004-2016 Savoir-faire Linux Inc.
+ *
+ *  Author: Romain Bertozzi <romain.bertozzi@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+package cx.ring.utils;
+
+import java.util.Hashtable;
+
+import cx.ring.service.StringMap;
+
+public class VCardUtils {
+    public static final String VCARD_KEY_MIME_TYPE = "mimeType";
+    public static final String VCARD_KEY_PART = "part";
+    public static final String VCARD_KEY_OF = "of";
+
+    /**
+     * Parse the "stringmap" of the mime attributes to build a proper hashtable
+     * @param stringMap the mimetype as returned by the daemon
+     * @return a correct hashtable, null if invalid input
+     */
+    public static Hashtable<String, String> parseMimeAttributes(StringMap stringMap) {
+        if (stringMap == null) {
+            return null;
+        }
+        Hashtable<String, String> messageKeyValue = new Hashtable<>();
+        String origin = stringMap.keys().toString().replace("[","");
+        origin = origin.replace("]","");
+        String elements[] = origin.split(";");
+        if (elements.length < 2) {
+            return messageKeyValue;
+        }
+        messageKeyValue.put(VCARD_KEY_MIME_TYPE, elements[0]);
+        String pairs[] = elements[1].split(",");
+        for (String pair : pairs) {
+            String kv[] = pair.split("=");
+            messageKeyValue.put(kv[0].trim(), kv[1]);
+        }
+        return messageKeyValue;
+    }
+}
