2a558159a1235595dd036639fc1a3290a7cbaa7c
Thibault Wittemberg <thibault.wittemberg@savoirfairelinux.com>
2016-10-04 17:59:03 -0400

    WIP: account migration for multidevice
    Change-Id: Ia7bce09720fd0ad08a624aafde69e1be304c42ef

--- ring-android/app/src/main/java/cx/ring/client/AccountWizard.java
+++ ring-android/app/src/main/java/cx/ring/client/AccountWizard.java
@@ -21,6 +21,8 @@
 
 package cx.ring.client;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
@@ -28,12 +30,11 @@ import android.content.ServiceConnection;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.os.IBinder;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v13.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.MenuItem;
 
@@ -42,6 +43,7 @@ import java.util.Locale;
 
 import cx.ring.R;
 import cx.ring.fragments.AccountCreationFragment;
+import cx.ring.fragments.AccountMigrationFragment;
 import cx.ring.service.IDRingService;
 import cx.ring.service.LocalService;
 
@@ -76,8 +78,15 @@ public class AccountWizard extends AppCompatActivity implements LocalService.Cal
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
-        SectionsPagerAdapter mSectionsPagerAdapter = new SectionsPagerAdapter(AccountWizard.this, getSupportFragmentManager());
-        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        if (getIntent().getData() != null && !TextUtils.isEmpty(getIntent().getData().getLastPathSegment())) {
+            String accountId = getIntent().getData().getLastPathSegment();
+            SectionsPagerAdapter mSectionsPagerAdapter = new SectionsPagerAdapter(AccountWizard.this, getFragmentManager(), accountId);
+            mViewPager.setAdapter(mSectionsPagerAdapter);
+        } else {
+            SectionsPagerAdapter mSectionsPagerAdapter = new SectionsPagerAdapter(AccountWizard.this, getFragmentManager());
+            mViewPager.setAdapter(mSectionsPagerAdapter);
+        }
 
         if (!mBound) {
             Log.i(TAG, "onCreate: Binding service...");
@@ -106,24 +115,39 @@ public class AccountWizard extends AppCompatActivity implements LocalService.Cal
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-        case android.R.id.home:
-            finish();
-            return true;
-        default:
-            return super.onOptionsItemSelected(item);
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 
     public class SectionsPagerAdapter extends FragmentStatePagerAdapter {
         private final Context mContext;
         private final ArrayList<Fragment> fragments;
+        private final String mAccountId;
 
         public SectionsPagerAdapter(Context c, FragmentManager fm) {
+            this(c, fm, null);
+        }
+
+        public SectionsPagerAdapter(Context c, FragmentManager fm, String accountId) {
             super(fm);
             mContext = c;
             fragments = new ArrayList<>();
-            fragments.add(new AccountCreationFragment());
+            mAccountId = accountId;
 
+            if (TextUtils.isEmpty(mAccountId)) {
+                fragments.add(new AccountCreationFragment());
+            } else {
+                AccountMigrationFragment fragment = new AccountMigrationFragment();
+                // give the installation id to display
+                Bundle bundle = new Bundle();
+                bundle.putString(AccountMigrationFragment.ACCOUNT_ID, mAccountId);
+                fragment.setArguments(bundle);
+                fragments.add(fragment);
+            }
         }
 
         @Override
@@ -135,13 +159,17 @@ public class AccountWizard extends AppCompatActivity implements LocalService.Cal
             String name;
 
             switch (i) {
-            case 0:
-                name = AccountCreationFragment.class.getName();
-                break;
+                case 0:
+                    if (TextUtils.isEmpty(mAccountId)) {
+                        name = AccountCreationFragment.class.getName();
+                    } else {
+                        name = AccountMigrationFragment.class.getName();
+                    }
+                    break;
 
-            default:
-                Log.e(TAG, "getClassName: unknown fragment position " + i);
-                return null;
+                default:
+                    Log.e(TAG, "getClassName: unknown fragment position " + i);
+                    return null;
             }
 
             // Log.w(TAG, "getClassName: name=" + name);
@@ -156,11 +184,11 @@ public class AccountWizard extends AppCompatActivity implements LocalService.Cal
         @Override
         public CharSequence getPageTitle(int position) {
             switch (position) {
-            case 0:
-                return mContext.getString(R.string.title_section0).toUpperCase(Locale.getDefault());
-            default:
-                Log.e(TAG, "getPageTitle: unknown tab position " + position);
-                break;
+                case 0:
+                    return mContext.getString(R.string.title_section0).toUpperCase(Locale.getDefault());
+                default:
+                    Log.e(TAG, "getPageTitle: unknown tab position " + position);
+                    break;
             }
             return null;
         }
