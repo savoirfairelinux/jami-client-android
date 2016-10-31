439138d95d12330e31825b9e3dc441df57be5f1a
Aline Bonnet <aline.bonnet@savoirfairelinux.com>
2016-10-31 11:42:11 -0400

    video: fix the video display
    When accepting a video call from the notification bar, sometimes the video
    is not displayed. startCapture was called before the initialization of
    CallActivity and CallFragment.
    This commit forces the addition of a videoSurface.
    
    Change-Id: I9a7a370ca95bb8f87a66c7941bc88c180fc676b5
    Tuleap: #774

--- ring-android/app/src/main/java/cx/ring/fragments/CallFragment.java
+++ ring-android/app/src/main/java/cx/ring/fragments/CallFragment.java
@@ -843,6 +843,13 @@ public class CallFragment extends Fragment implements CallInterface, ContactDeta
 
         contactBubbleLayout.setVisibility(haveVideo ? View.GONE : View.VISIBLE);
         updateSecurityDisplay();
+
+        try {
+            mCallbacks.getRemoteService().setPreviewSettings();
+            mCallbacks.getRemoteService().videoPreviewSurfaceAdded();
+        } catch (RemoteException e) {
+            Log.e(TAG, "service not found ", e);
+        }
     }
 
     private void updateSecurityDisplay() {
@@ -949,10 +956,9 @@ public class CallFragment extends Fragment implements CallInterface, ContactDeta
 
         if (!vcard.getPhotos().isEmpty()) {
             Photo tmp = vcard.getPhotos().get(0);
-            if(tmp.getData() != null) {
+            if (tmp.getData() != null) {
                 contactBubbleView.setImageBitmap(CropImageUtils.cropImageToCircle(tmp.getData()));
-            }
-            else{
+            } else {
                 setDefaultPhoto();
             }
         } else {
