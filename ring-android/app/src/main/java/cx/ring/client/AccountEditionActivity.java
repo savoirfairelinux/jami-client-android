522ada9364b37d44bf7ea05d23e5bb55f974732e
Aline Bonnet <aline.bonnet@savoirfairelinux.com>
2016-11-01 16:21:49 -0400

    ui: disable backup feature
    The backup feature doesn't work with the multi-devices.
    This commit removes it.
    
    Change-Id: Ia6df1c6bca586c3b4d3c4321d2b293b7031884a2
    Tuleap: #1160

--- ring-android/app/src/main/java/cx/ring/client/AccountEditionActivity.java
+++ ring-android/app/src/main/java/cx/ring/client/AccountEditionActivity.java
@@ -272,9 +272,6 @@ public class AccountEditionActivity extends AppCompatActivity implements Account
                 AlertDialog deleteDialog = createDeleteDialog();
                 deleteDialog.show();
                 break;
-            case R.id.menuitem_backup:
-                startBackup();
-                break;
             default:
                 break;
         }
@@ -305,12 +302,6 @@ public class AccountEditionActivity extends AppCompatActivity implements Account
                     public void onClick(DialogInterface dialog, int whichButton) {
                         /* Terminate with no action */
                     }
-                })
-                .setNeutralButton(R.string.account_backup, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int whichButton) {
-                        startBackup();
-                    }
                 });
 
         AlertDialog alertDialog = builder.create();
@@ -336,160 +327,6 @@ public class AccountEditionActivity extends AppCompatActivity implements Account
             }
         }
         return error;
-    }
-
-    private void startBackup() {
-        boolean hasPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
-        if (hasPermission) {
-            showBackupDialog();
-        } else {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_WRITE_STORAGE);
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode) {
-            case REQUEST_WRITE_STORAGE: {
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    showBackupDialog();
-                } else {
-                    Toast.makeText(this, R.string.permission_write_denied, Toast.LENGTH_LONG).show();
-                }
-            }
-        }
-    }
-
-    private AlertDialog showBackupDialog() {
-        Activity ownerActivity = this;
-
-        AlertDialog.Builder builder = new AlertDialog.Builder(ownerActivity);
-        LayoutInflater inflater = getLayoutInflater();
-        ViewGroup v = (ViewGroup) inflater.inflate(R.layout.dialog_account_backup, null);
-        final TextView pwd = (TextView) v.findViewById(R.id.newpwd_txt);
-        final TextView pwdConfirm = (TextView) v.findViewById(R.id.newpwd_confirm_txt);
-        builder.setMessage(R.string.account_backup_message)
-                .setTitle(R.string.account_backup_title)
-                .setPositiveButton(R.string.account_backup, null)
-                .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int whichButton) {
-                /* Terminate with no action */
-                    }
-                }).setView(v);
-
-
-        final AlertDialog alertDialog = builder.create();
-        pwd.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                Log.i(TAG, "onEditorAction " + actionId + " " + (event == null ? null : event.toString()));
-                return actionId == EditorInfo.IME_ACTION_NEXT && checkPassword(v, null);
-            }
-        });
-        pwd.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if (!hasFocus) {
-                    checkPassword((TextView) v, null);
-                } else {
-                    alertDialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
-                }
-            }
-        });
-        pwdConfirm.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                Log.i(TAG, "onEditorAction " + actionId + " " + (event == null ? null : event.toString()));
-                if (actionId == EditorInfo.IME_ACTION_DONE && !checkPassword(pwd, v)) {
-                    alertDialog.getButton(DialogInterface.BUTTON_POSITIVE).callOnClick();
-                    return true;
-                }
-                return false;
-            }
-        });
-
-
-        alertDialog.setOwnerActivity(ownerActivity);
-        alertDialog.show();
-        alertDialog.getButton(DialogInterface.BUTTON_POSITIVE).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (!checkPassword(pwd, pwdConfirm)) {
-                    final String pwd_txt = pwd.getText().toString();
-                    alertDialog.dismiss();
-
-                    new BackupAccountTask().execute(pwd_txt);
-                }
-            }
-        });
-
-        return alertDialog;
-    }
-
-    public File getBackupStorageDir() {
-        // Get the directory for the user's public pictures directory.
-        String env = Environment.DIRECTORY_DOWNLOADS;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            env = Environment.DIRECTORY_DOCUMENTS;
-        }
-
-        File path = Environment.getExternalStoragePublicDirectory(env);
-
-        if (!path.mkdirs() && !path.isDirectory()) {
-            Log.w(TAG, "Directory " + path.getAbsolutePath() + " not created, using fallback");
-            // Fallback case, use Downloads
-            env = Environment.DIRECTORY_DOWNLOADS;
-            path = Environment.getExternalStoragePublicDirectory(env);
-
-            if (!path.mkdirs() && !path.isDirectory()) {
-                Log.e(TAG, "Fallback on " + path.getAbsolutePath() + " failed!");
-            }
-        }
-
-        return new File(path, getAccount().getAlias() + ".ring");
-    }
-
-    private class BackupAccountTask extends AsyncTask<String, Void, Integer> {
-        ProgressDialog backupDialog;
-        private String path;
-
-        @Override
-        protected void onPreExecute() {
-            backupDialog = ProgressDialog.show(AccountEditionActivity.this,
-                    getString(R.string.backup_dialog_title),
-                    getString(R.string.restore_backup_wait), true);
-            backupDialog.setCancelable(false);
-        }
-
-        protected Integer doInBackground(String... args) {
-            int ret = 1;
-            ArrayList<String> ids = new ArrayList<>(1);
-            ids.add(mAccSelected.getAccountID());
-            File filePath = getBackupStorageDir();
-            path = filePath.getAbsolutePath();
-            try {
-                ret = getRemoteService().backupAccounts(ids, path, args[0]);
-            } catch (RemoteException e) {
-                Log.e(TAG, "Error while backup account", e);
-            }
-            return ret;
-        }
-
-        protected void onPostExecute(Integer ret) {
-            if (backupDialog != null) {
-                backupDialog.dismiss();
-            }
-
-            Log.d(TAG, "Account backup to " + path + " returned " + ret);
-            if (ret == 0) {
-                Snackbar.make(findViewById(android.R.id.content), getString(R.string.account_backup_result, path), Snackbar.LENGTH_LONG).show();
-            } else
-                new AlertDialog.Builder(AccountEditionActivity.this).setTitle(R.string.backup_failed_dialog_title)
-                        .setMessage(R.string.backup_failed_dialog_msg)
-                        .setPositiveButton(android.R.string.ok, null).show();
-        }
     }
 
     @Override
