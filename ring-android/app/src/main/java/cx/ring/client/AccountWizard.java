savoirfairelinux/jami-client-android

commit 1347b40f2c18ec4d790c63ef8b00438289c8f05c

blockchain: add name registration
- Allow to register name during account creation
- New account settings allow to see registered name,
  if any, or to register a name.

Tuleap: #1157
Change-Id: Ib26cf3325efeee10db0a088f3dd5a9c12de149fb

ring-android/app/src/main/java/cx/ring/client/AccountWizard.java
@@ -21,40 +21,52 @@
 
 package cx.ring.client;
 
-import android.app.Fragment;
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.app.ProgressDialog;
 import android.content.ComponentName;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
 import android.content.res.Configuration;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.IBinder;
-import android.support.v13.app.FragmentStatePagerAdapter;
-import android.support.v4.view.ViewPager;
+import android.os.RemoteException;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.MenuItem;
+import android.widget.Toast;
 
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import cx.ring.R;
 import cx.ring.fragments.AccountCreationFragment;
 import cx.ring.fragments.AccountMigrationFragment;
+import cx.ring.fragments.RingAccountCreationFragment;
+import cx.ring.fragments.RingAccountLoginFragment;
+import cx.ring.model.account.Account;
+import cx.ring.model.account.AccountConfig;
+import cx.ring.model.account.ConfigKey;
 import cx.ring.service.IDRingService;
 import cx.ring.service.LocalService;
 
 public class AccountWizard extends AppCompatActivity implements LocalService.Callbacks {
     static final String TAG = AccountWizard.class.getName();
     private boolean mBound = false;
-    private LocalService service;
-
-    @BindView(R.id.pager)
-    ViewPager mViewPager;
+    private LocalService mService;
+    private boolean mCreatingAccount = false;
 
     @BindView(R.id.main_toolbar)
     Toolbar mToolbar;
@@ -63,7 +75,7 @@ public class AccountWizard extends AppCompatActivity implements LocalService.Cal
 
         @Override
         public void onServiceConnected(ComponentName className, IBinder binder) {
-            service = ((LocalService.LocalBinder) binder).getService();
+            mService = ((LocalService.LocalBinder) binder).getService();
             mBound = true;
         }
 
@@ -83,21 +95,13 @@ public class AccountWizard extends AppCompatActivity implements LocalService.Cal
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
 
-        if (getIntent().getData() != null && !TextUtils.isEmpty(getIntent().getData().getLastPathSegment())) {
-            String accountId = getIntent().getData().getLastPathSegment();
-            SectionsPagerAdapter mSectionsPagerAdapter = new SectionsPagerAdapter(getFragmentManager(), accountId);
-            mViewPager.setAdapter(mSectionsPagerAdapter);
-        } else {
-            SectionsPagerAdapter mSectionsPagerAdapter = new SectionsPagerAdapter(AccountWizard.this, getFragmentManager());
-            mViewPager.setAdapter(mSectionsPagerAdapter);
-        }
-
         if (!mBound) {
             Log.i(TAG, "onCreate: Binding service...");
             Intent intent = new Intent(this, LocalService.class);
             bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
         }
 
+        ringChoose();
     }
 
     @Override
@@ -119,11 +123,11 @@ public class AccountWizard extends AppCompatActivity implements LocalService.Cal
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-        case android.R.id.home:
-            checkAccountPresence();
-            return true;
-        default:
-            return super.onOptionsItemSelected(item);
+            case android.R.id.home:
+                checkAccountPresence();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 
@@ -132,63 +136,221 @@ public class AccountWizard extends AppCompatActivity implements LocalService.Cal
      * If not, exit the app
      */
     private void checkAccountPresence() {
-        if (mBound && !service.getAccounts().isEmpty()) {
-            finish();
+        if (mBound && !mService.getAccounts().isEmpty()) {
+            FragmentManager fm = getFragmentManager();
+            if (fm.getBackStackEntryCount() >= 1) {
+                fm.popBackStack();
+            } else {
+                finish();
+            }
         } else {
-            service.stopSelf();
+            mService.stopSelf();
             finishAffinity();
         }
     }
 
     @Override
     public void onBackPressed() {
-        checkAccountPresence();
+        FragmentManager fm = getFragmentManager();
+        if (fm.getBackStackEntryCount() >= 1) {
+            fm.popBackStack();
+            return;
+        }
+        super.onBackPressed();
     }
 
-    public class SectionsPagerAdapter extends FragmentStatePagerAdapter {
-        private final ArrayList<Fragment> fragments;
-        private final String mAccountId;
+    @SuppressWarnings("unchecked")
+    public void initAccountCreation(boolean isRingAccount, String newUsername, String pin, String password, String host) {
+        final String accountType = isRingAccount ? AccountConfig.ACCOUNT_TYPE_RING : AccountConfig.ACCOUNT_TYPE_SIP;
 
-        SectionsPagerAdapter(Context c, FragmentManager fm) {
-            this(fm, null);
-        }
-
-        SectionsPagerAdapter(FragmentManager fm, String accountId) {
-            super(fm);
-            fragments = new ArrayList<>();
-            mAccountId = accountId;
-
-            if (TextUtils.isEmpty(mAccountId)) {
-                fragments.add(new AccountCreationFragment());
-            } else {
-                AccountMigrationFragment fragment = new AccountMigrationFragment();
-                // give the installation id to display
-                Bundle bundle = new Bundle();
-                bundle.putString(AccountMigrationFragment.ACCOUNT_ID, mAccountId);
-                fragment.setArguments(bundle);
-                fragments.add(fragment);
+        try {
+            HashMap<String, String> accountDetails = (HashMap<String, String>) mService.getRemoteService().getAccountTemplate(accountType);
+            for (Map.Entry<String, String> e : accountDetails.entrySet()) {
+                Log.d(TAG, "Default account detail: " + e.getKey() + " -> " + e.getValue());
             }
+            //~ Checking the state of the Camera permission to enable Video or not.
+            boolean hasCameraPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
+            accountDetails.put(ConfigKey.VIDEO_ENABLED.key(), Boolean.toString(hasCameraPermission));
+
+            //~ Sipinfo is forced for any sipaccount since overrtp is not supported yet.
+            //~ This will have to be removed when it will be supported.
+            accountDetails.put(ConfigKey.ACCOUNT_DTMF_TYPE.key(), getString(R.string.account_sip_dtmf_type_sipinfo));
+
+            if (isRingAccount) {
+                accountDetails.put(ConfigKey.ACCOUNT_ALIAS.key(), "Ring");
+                accountDetails.put(ConfigKey.ACCOUNT_HOSTNAME.key(), "bootstrap.ring.cx");
+                if (newUsername != null && !newUsername.isEmpty()) {
+                    accountDetails.put(ConfigKey.ACCOUNT_REGISTERED_NAME.key(), newUsername);
+                }
+                if (password != null && !password.isEmpty()) {
+                    accountDetails.put(ConfigKey.ARCHIVE_PASSWORD.key(), password);
+                }
+                if (pin != null && !pin.isEmpty()) {
+                    accountDetails.put(ConfigKey.ARCHIVE_PIN.key(), pin);
+                }
+                // Enable UPNP by default for Ring accounts
+                accountDetails.put(ConfigKey.ACCOUNT_UPNP_ENABLE.key(), AccountConfig.TRUE_STR);
+                createNewAccount(accountDetails, newUsername);
+            } else {
+                accountDetails.put(ConfigKey.ACCOUNT_ALIAS.key(), newUsername);
+                if (!TextUtils.isEmpty(host)) {
+                    accountDetails.put(ConfigKey.ACCOUNT_HOSTNAME.key(), host);
+                    accountDetails.put(ConfigKey.ACCOUNT_USERNAME.key(), newUsername);
+                    accountDetails.put(ConfigKey.ACCOUNT_PASSWORD.key(), password);
+                }
+                createNewAccount(accountDetails, null);
+            }
+
+        } catch (RemoteException e) {
+            Toast.makeText(this, "Error creating account", Toast.LENGTH_SHORT).show();
+            Log.e(TAG, "Error creating account", e);
+        }
+
+    }
+
+    public void ringChoose() {
+        boolean migrate = getIntent().getData() != null && !TextUtils.isEmpty(getIntent().getData().getLastPathSegment());
+
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager
+                .beginTransaction()
+                .replace(R.id.fragment_container, migrate ? new AccountMigrationFragment() : new AccountCreationFragment())
+                .commit();
+    }
+
+    public void ringCreate(boolean switchFragment) {
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        if (!switchFragment) {
+            fragmentTransaction.addToBackStack(null);
+        }
+        fragmentTransaction
+                .replace(R.id.fragment_container, new RingAccountCreationFragment())
+                .commit();
+    }
+
+    public void ringLogin(boolean switchFragment) {
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        if (!switchFragment) {
+            fragmentTransaction.addToBackStack(null);
+        }
+        fragmentTransaction
+                .replace(R.id.fragment_container, new RingAccountLoginFragment())
+                .commit();
+    }
+
+    private class CreateAccountTask extends AsyncTask<HashMap<String, String>, Void, String> {
+        private ProgressDialog progress = null;
+        private final String username;
+        private final Context ctx;
+
+        CreateAccountTask(String registerUsername, Context c) {
+            Log.d(TAG, "CreateAccountTask ");
+            username = registerUsername;
+            ctx = c;
         }
 
         @Override
-        public Fragment getItem(int i) {
-            return fragments.get(i);
+        protected void onPreExecute() {
+            progress = new ProgressDialog(ctx);
+            progress.setTitle(R.string.dialog_wait_create);
+            progress.setMessage(ctx.getString(R.string.dialog_wait_create_details));
+            progress.setCancelable(false);
+            progress.setCanceledOnTouchOutside(false);
+            progress.show();
         }
 
+        @SafeVarargs
         @Override
-        public int getCount() {
-            return 1;
+        protected final String doInBackground(HashMap<String, String>... accs) {
+            final Account account = mService.createAccount(accs[0]);
+            account.stateListener = new Account.OnStateChangedListener() {
+                @Override
+                public void stateChanged(String state, int code) {
+                    Log.i(TAG, "stateListener -> stateChanged " + state + " " + code);
+                    if (!AccountConfig.STATE_INITIALIZING.contentEquals(state)) {
+                        account.stateListener = null;
+                        if (progress != null) {
+                            if (progress.isShowing()) {
+                                progress.dismiss();
+                            }
+                            progress = null;
+                        }
+                        //Intent resultIntent = new Intent();
+                        AlertDialog.Builder dialog = new AlertDialog.Builder(ctx);
+                        dialog.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                //do things
+                            }
+                        });
+                        boolean success = false;
+                        switch (state) {
+                            case AccountConfig.STATE_ERROR_GENERIC:
+                                dialog.setTitle(R.string.account_cannot_be_found_title)
+                                        .setMessage(R.string.account_cannot_be_found_message);
+                                break;
+                            case AccountConfig.STATE_ERROR_NETWORK:
+                                dialog.setTitle(R.string.account_no_network_title)
+                                        .setMessage(R.string.account_no_network_message);
+                                break;
+                            default:
+                                dialog.setTitle(R.string.account_device_added_title)
+                                        .setMessage(R.string.account_device_added_message);
+                                success = true;
+                                break;
+                        }
+                        AlertDialog alertDialog = dialog.show();
+                        if (success) {
+                            if (!TextUtils.isEmpty(username)) {
+                                Log.i(TAG, "Account created, registering " + username);
+                                mService.registerName(account, "", username, new LocalService.NameRegistrationCallback() {
+                                    @Override
+                                    public void onRegistered(String name) {
+                                        Log.i(TAG, "Account wizard, onRegistered " + name);
+                                    }
+
+                                    @Override
+                                    public void onError(String name, CharSequence err) {
+                                        Log.w(TAG, "Account wizard, onError " + name);
+                                    }
+                                });
+                            }
+
+                            alertDialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                                @Override
+                                public void onDismiss(DialogInterface dialogInterface) {
+                                    setResult(Activity.RESULT_OK, new Intent());
+                                    finish();
+                                }
+                            });
+                        }
+                    }
+                }
+            };
+            return account.getAccountID();
         }
+    }
+
+    private void createNewAccount(HashMap<String, String> accountDetails, String registerName) {
+        if (mCreatingAccount) {
+            return;
+        }
+
+        mCreatingAccount = true;
+
+        //noinspection unchecked
+        new CreateAccountTask(registerName, this).execute(accountDetails);
     }
 
     @Override
     public IDRingService getRemoteService() {
-        return service.getRemoteService();
+        return mService.getRemoteService();
     }
 
     @Override
     public LocalService getService() {
-        return service;
+        return mService;
     }
 
 }

ring-android/app/build.gradle
@@ -13,6 +13,7 @@ dependencies {
     compile 'com.android.support:cardview-v7:24.2.1'
     compile 'com.android.support:preference-v14:24.2.1'
     compile 'com.android.support:recyclerview-v7:24.2.1'
+    compile 'com.android.support:appcompat-v7:24.2.1'
     compile 'com.jayway.android.robotium:robotium-solo:5.4.1'
     compile 'com.astuetz:pagerslidingtabstrip:1.0.1'
 

ring-android/app/src/main/AndroidManifest.xml
@@ -49,6 +49,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     <uses-permission android:name="android.permission.WAKE_LOCK" />
     <uses-permission android:name="android.permission.CAMERA" />
     <uses-permission android:name="android.permission.CHANGE_WIFI_STATE" />
+    <uses-permission android:name="android.permission.READ_PHONE_STATE" />
 
     <uses-feature
         android:name="android.hardware.wifi"
@@ -67,22 +68,21 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
         android:allowBackup="true"
         android:icon="@drawable/ic_launcher"
         android:label="@string/app_name"
-        android:supportsRtl="true"
-        android:resizeableActivity="true">
+        android:resizeableActivity="true"
+        android:supportsRtl="true">
         <activity
             android:name=".client.HomeActivity"
+            android:configChanges="orientation|screenSize|screenLayout|smallestScreenSize"
             android:label="@string/title_activity_sflphone_home"
-            android:theme="@style/AppThemeBase"
-            android:windowSoftInputMode="adjustResize"
-            android:screenOrientation="fullUser"
             android:launchMode="singleTask"
-            android:configChanges="orientation|screenSize|screenLayout|smallestScreenSize">
+            android:screenOrientation="fullUser"
+            android:theme="@style/AppThemeBase"
+            android:windowSoftInputMode="adjustResize">
             <intent-filter>
                 <action android:name="android.intent.action.MAIN" />
 
                 <category android:name="android.intent.category.LAUNCHER" />
             </intent-filter>
-
             <intent-filter>
                 <action android:name="android.intent.action.DIAL" />
                 <action android:name="android.intent.action.VIEW" />
@@ -134,28 +134,27 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
                 <data android:scheme="ring" />
             </intent-filter>
-
         </activity>
         <activity
             android:name=".client.AccountWizard"
-            android:theme="@style/AppThemeBase"
+            android:configChanges="orientation|screenSize|screenLayout|smallestScreenSize"
             android:screenOrientation="fullUser"
-            android:configChanges="orientation|screenSize|screenLayout|smallestScreenSize">
+            android:theme="@style/AppThemeBase">
             <meta-data
                 android:name="android.support.PARENT_ACTIVITY"
                 android:value="cx.ring.client.AccountEditionActivity" />
         </activity>
         <activity
             android:name=".client.AccountEditionActivity"
+            android:configChanges="orientation|screenSize|screenLayout|smallestScreenSize"
             android:label="@string/app_name"
-            android:theme="@style/AppThemeBase"
             android:screenOrientation="fullUser"
-            android:configChanges="orientation|screenSize|screenLayout|smallestScreenSize"/>
+            android:theme="@style/AppThemeBase" />
         <activity
             android:name=".client.NewConversationActivity"
             android:label="@string/app_name"
-            android:theme="@style/AppThemeBase"
-            android:screenOrientation="fullUser"/>
+            android:screenOrientation="fullUser"
+            android:theme="@style/AppThemeBase" />
 
         <receiver android:name=".service.OutgoingCallHandler">
             <intent-filter>
@@ -172,12 +171,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
         <activity
             android:name=".client.CallActivity"
+            android:configChanges="orientation|screenSize|screenLayout|smallestScreenSize"
             android:label="@string/app_name"
             android:screenOrientation="fullUser"
-            android:configChanges="orientation|screenSize|screenLayout|smallestScreenSize"
             android:theme="@style/AppTheme.ActionBar.Transparent"
             android:windowSoftInputMode="adjustPan|stateHidden">
-
             <intent-filter>
                 <action android:name="android.intent.action.CALL" />
 
@@ -224,7 +222,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
                 <data android:scheme="ring" />
             </intent-filter>
-
             <intent-filter>
                 <action android:name="android.intent.action.CALL" />
                 <action android:name="android.intent.action.DIAL" />
@@ -238,19 +235,18 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
         </activity>
         <activity
             android:name=".client.ConversationActivity"
+            android:configChanges="orientation|screenSize|screenLayout|smallestScreenSize"
             android:label="@string/app_name"
             android:parentActivityName=".client.HomeActivity"
-            android:theme="@style/AppThemeBase"
-            android:windowSoftInputMode="adjustResize"
             android:screenOrientation="fullUser"
-            android:configChanges="orientation|screenSize|screenLayout|smallestScreenSize"
-            />
-        <activity android:name=".client.QRCodeScannerActivity"
+            android:theme="@style/AppThemeBase"
+            android:windowSoftInputMode="adjustResize" />
+        <activity
+            android:name=".client.QRCodeScannerActivity"
             android:screenOrientation="fullUser"
             android:stateNotNeeded="true"
             android:theme="@style/zxing_CaptureTheme"
-            android:windowSoftInputMode="stateAlwaysHidden">
-        </activity>
+            android:windowSoftInputMode="stateAlwaysHidden"></activity>
 
         <service
             android:name=".service.LocalService"

commit 0eb542818a432094f1c1d7cb19f8f6da0f2b0c54

migration: properly inject accountId
We broke the migration in commit 1347b40f2. The accountId was not
injected in the Fragment anymore. This commit fixes the issue.

Change-Id: Idbf06a1718dad6cb4139ad6cb42e31b47dbd655f
Tuleap: #1197

ring-android/app/src/main/java/cx/ring/client/AccountWizard.java
@@ -24,6 +24,7 @@ package cx.ring.client;
 import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.app.ProgressDialog;
@@ -211,10 +212,20 @@ public class AccountWizard extends AppCompatActivity implements LocalService.Cal
     public void ringChoose() {
         boolean migrate = getIntent().getData() != null && !TextUtils.isEmpty(getIntent().getData().getLastPathSegment());
 
+        Bundle args = new Bundle();
+        Fragment fragment;
+        if (migrate) {
+            args.putString(AccountMigrationFragment.ACCOUNT_ID, getIntent().getData().getLastPathSegment());
+            fragment = new AccountMigrationFragment();
+            fragment.setArguments(args);
+        } else {
+            fragment = new AccountCreationFragment();
+        }
+
         FragmentManager fragmentManager = getFragmentManager();
         fragmentManager
                 .beginTransaction()
-                .replace(R.id.fragment_container, migrate ? new AccountMigrationFragment() : new AccountCreationFragment())
+                .replace(R.id.fragment_container, fragment)
                 .commit();
     }
 