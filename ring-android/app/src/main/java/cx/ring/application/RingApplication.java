550308798c702dd82a115e87b8a2306cb85b75d1
Thibault Wittemberg <thibault.wittemberg@savoirfairelinux.com>
2016-11-08 10:55:54 -0500

    injection: Introducing dependency injection with Dagger2
    This will improve the separation of concerns. Dagger relies on Components
    and Modules that define the dependency tree. As soon as the tree is built,
    @Inject annotations can be used anywere.For instance, we will use Dagger
    to inject low level services into presenters, and presenters into Activities and Fragments.
    
    Change-Id: I85f6c1d9c5531ef414548755c97ac01a03017b7d
    Tuleap: #1217

---
+++ ring-android/app/src/main/java/cx/ring/application/RingApplication.java
@@ -0,0 +1,48 @@
+/*
+ *  Copyright (C) 2016 Savoir-faire Linux Inc.
+ *
+ *  Author: Thibault Wittemberg <thibault.wittemberg@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+package cx.ring.application;
+
+import android.app.Application;
+
+import cx.ring.dependencyinjection.DaggerRingInjectionComponent;
+import cx.ring.dependencyinjection.RingInjectionComponent;
+import cx.ring.dependencyinjection.RingInjectionModule;
+
+public class RingApplication extends Application {
+
+    private RingInjectionComponent mRingInjectionComponent;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // building injection dependency tree
+        mRingInjectionComponent = DaggerRingInjectionComponent.builder()
+                .ringInjectionModule(new RingInjectionModule(this))
+                .build();
+
+        // we can now inject in our self whatever modules define
+        mRingInjectionComponent.inject(this);
+    }
+
+    public RingInjectionComponent getRingInjectionComponent() {
+        return mRingInjectionComponent;
+    }
+}
