d7dc1b72e1fa30462c208986f171e291bc59deb9
Aline Bonnet <aline.bonnet@savoirfairelinux.com>
2016-11-09 11:20:39 -0500

    vcard: link one profile by account
    Until now, there was one vcard for all accounts. Now, we want one vcard for
    each account.
    This commit amends the vcard management: it is saved with accountID as name.
    In RingnavigationFragment, photo and name are refreshed when you change the
    selected account.
    
    Change-Id: Icd0a7f0186817b23991d5fb3414daf83fef37edb
    Tuleap: #1279

--- ring-android/app/src/main/java/cx/ring/navigation/RingNavigationFragment.java
+++ ring-android/app/src/main/java/cx/ring/navigation/RingNavigationFragment.java
@@ -174,7 +174,9 @@ public class RingNavigationFragment extends Fragment implements NavigationAdapte
 
     public interface OnNavigationSectionSelected {
         void onNavigationSectionSelected(Section position);
+
         void onAccountSelected();
+
         void onAddAccountSelected();
     }
 
@@ -242,8 +244,6 @@ public class RingNavigationFragment extends Fragment implements NavigationAdapte
         // dependency injection
         ((RingApplication) getActivity().getApplication()).getRingInjectionComponent().inject(this);
 
-        mVCardProfile = VCardUtils.loadLocalProfileFromDisk(getActivity().getFilesDir(), getString(R.string.unknown));
-
         updateUserView();
 
         setupNavigationMenu();
@@ -305,7 +305,8 @@ public class RingNavigationFragment extends Fragment implements NavigationAdapte
 
     public void updateUserView() {
         Log.d(TAG, "updateUserView");
-        mVCardProfile = VCardUtils.loadLocalProfileFromDisk(getActivity().getFilesDir(), getString(R.string.unknown));
+        String accountID = mStateService.getCurrentAccount() != null ? mStateService.getCurrentAccount().getAccountID() : null;
+        mVCardProfile = VCardUtils.loadLocalProfileFromDisk(getActivity().getFilesDir(), accountID, getString(R.string.unknown));
         if (!mVCardProfile.getPhotos().isEmpty()) {
             Photo tmp = mVCardProfile.getPhotos().get(0);
             mUserImage.setImageBitmap(BitmapUtils.cropImageToCircle(tmp.getData()));
@@ -426,7 +427,7 @@ public class RingNavigationFragment extends Fragment implements NavigationAdapte
                 }
 
                 mVCardProfile.removeProperties(RawProperty.class);
-                VCardUtils.saveLocalProfileToDisk(mVCardProfile, getActivity().getFilesDir());
+                VCardUtils.saveLocalProfileToDisk(mVCardProfile, mStateService.getCurrentAccount().getAccountID(), getActivity().getFilesDir());
                 updateUserView();
             }
         });
