a9571f70b040b55533cedc41dd050f750591b128
Alexandre Lision <alexandre.lision@savoirfairelinux.com>
2016-11-11 16:02:33 -0500

    drawer: improve layout
    Remove the account spinner. The account list is directly displayed in
    the drawer. The 'Add account' action is also available from the drawer.
    
    Tuleap: #1218
    Change-Id: Idf6fcaeef8202b26ac0c406e46fee2d8c9f1eb90

--- ring-android/app/src/main/java/cx/ring/client/HomeActivity.java
+++ ring-android/app/src/main/java/cx/ring/client/HomeActivity.java
@@ -473,8 +473,6 @@ public class HomeActivity extends AppCompatActivity implements LocalService.Call
             if (fContent == null) {
                 fContent = new SmartListFragment();
                 fragmentManager.beginTransaction().replace(R.id.main_frame, fContent, HOME_TAG).addToBackStack(HOME_TAG).commit();
-
-
             } else if (fContent instanceof Refreshable) {
                 fragmentManager.beginTransaction().replace(R.id.main_frame, fContent).addToBackStack(HOME_TAG).commit();
                 ((Refreshable) fContent).refresh();
@@ -486,7 +484,6 @@ public class HomeActivity extends AppCompatActivity implements LocalService.Call
         public void onServiceDisconnected(ComponentName className) {
             Log.d(TAG, "onServiceDisconnected " + className.getClassName());
             if (fNavigation != null) {
-                fNavigation.setCallbacks(null);
                 fNavigation = null;
             }
             mBound = false;
@@ -496,7 +493,9 @@ public class HomeActivity extends AppCompatActivity implements LocalService.Call
     // TODO: Remove this when low level services are ready
     public void onNavigationViewReady() {
             if (fNavigation != null) {
-                fNavigation.setCallbacks(service);
+                if (service != null) {
+                    fNavigation.updateAccounts(service.getAccounts());
+                }
                 fNavigation.setNavigationSectionSelectedListener(HomeActivity.this);
                 fNavigation.registerAccountSelectionListener((RingNavigationFragment.MenuHeaderAccountSelectionListener) fContent);
             }
@@ -593,6 +592,12 @@ public class HomeActivity extends AppCompatActivity implements LocalService.Call
             default:
                 break;
         }
+    }
+
+    @Override
+    public void onAddAccountSelected() {
+        mNavigationDrawer.closeDrawers();
+        startActivityForResult(new Intent(HomeActivity.this, AccountWizard.class), AccountsManagementFragment.ACCOUNT_CREATE_REQUEST);
     }
 
     private void goToShare() {
