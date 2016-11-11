402e2db19c152443ff9d2261ad368782ba434ce5
Alexandre Lision <alexandre.lision@savoirfairelinux.com>
2016-11-11 15:59:50 -0500

    drawer: use custom navigation component
    Android component NavigationView is not flexible enough
    
    Change-Id: I272c29b466022552b244edd65b725d0dc3ec29eb

--- ring-android/app/src/main/java/cx/ring/navigation/RingNavigationFragment.java
+++ ring-android/app/src/main/java/cx/ring/navigation/RingNavigationFragment.java
@@ -17,22 +17,25 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-package cx.ring.views;
+package cx.ring.navigation;
 
 import android.Manifest;
-import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.Fragment;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.content.res.ResourcesCompat;
-import android.util.AttributeSet;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -41,7 +44,6 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Spinner;
@@ -74,13 +76,20 @@ import ezvcard.property.FormattedName;
 import ezvcard.property.Photo;
 import ezvcard.property.RawProperty;
 
-public class MenuHeaderView extends FrameLayout {
-    private static final String TAG = MenuHeaderView.class.getSimpleName();
+public class RingNavigationFragment extends Fragment implements NavigationAdapter.OnNavigationItemClicked {
+    private static final String TAG = RingNavigationFragment.class.getSimpleName();
 
     private AccountSelectionAdapter mAccountAdapter;
 
     @Inject
     StateService mStateService;
+
+    @Inject
+    Context mContext;
+
+    /***************
+     * Header views
+     ***************/
 
     @BindView(R.id.account_selection)
     Spinner mSpinnerAccounts;
@@ -97,26 +106,89 @@ public class MenuHeaderView extends FrameLayout {
     private ImageView mProfilePhoto;
     private VCard mVCardProfile;
 
+    /**************
+     * Menu views
+     **************/
+
+    @BindView(R.id.drawer_menu)
+    RecyclerView mMenuView;
+
+    private NavigationAdapter mMenuAdapter;
+    private OnNavigationSectionSelected mSectionListener;
     private List<WeakReference<MenuHeaderAccountSelectionListener>> mListeners;
 
-    public MenuHeaderView(Context context, AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
-        initViews();
+    public void selectSection(Section manage) {
+        mMenuAdapter.setSelection(manage.position);
     }
 
-    public MenuHeaderView(Context context, AttributeSet attrs) {
-        super(context, attrs);
-        initViews();
+    @Override
+    public void onNavigationItemClicked(int position) {
+        if (mSectionListener != null) {
+            mSectionListener.onNavigationSectionSelected(Section.valueOf(position));
+        }
     }
 
-    public MenuHeaderView(Context context) {
-        super(context);
+    public interface OnNavigationSectionSelected {
+        void onNavigationSectionSelected(Section position);
+    }
 
-        if (context instanceof Activity) {
-            ((RingApplication)((Activity)context).getApplication()).getRingInjectionComponent().inject(this);
+
+    public interface MenuHeaderAccountSelectionListener {
+        void accountSelected(Account account);
+    }
+
+    public enum Section {
+        HOME(0),
+        MANAGE(1),
+        SETTINGS(2),
+        SHARE(3),
+        ABOUT(4);
+
+        int position;
+
+        Section(int pos) {
+            position = pos;
         }
 
-        initViews();
+        public static Section valueOf(int sec) {
+            for (Section s : Section.values()) {
+                if (s.position == sec) {
+                    return s;
+                }
+            }
+            return HOME;
+        }
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+
+        View inflatedView = inflater.inflate(R.layout.ring_navigation_view, container, false);
+
+        ButterKnife.bind(this, inflatedView);
+
+        // dependency injection
+        ((RingApplication) getActivity().getApplication()).getRingInjectionComponent().inject(this);
+
+        mAccountAdapter = new AccountSelectionAdapter(mContext, new ArrayList<Account>());
+        mSpinnerAccounts.setAdapter(mAccountAdapter);
+        mVCardProfile = VCardUtils.loadLocalProfileFromDisk(getActivity());
+
+        updateUserView();
+
+        mListeners = new ArrayList<>();
+
+        setupNavigationMenu();
+        return inflatedView;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        // TODO: Remove this when low level services are ready
+        ((HomeActivity) getActivity()).onNavigationViewReady();
     }
 
     public void setCallbacks(final LocalService service) {
@@ -156,23 +228,21 @@ public class MenuHeaderView extends FrameLayout {
     }
 
     public void updateUserView() {
-        LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         Log.d(TAG, "updateUserView");
-        if (null != inflater) {
-            mVCardProfile = VCardUtils.loadLocalProfileFromDisk(getContext());
-            if (!mVCardProfile.getPhotos().isEmpty()) {
-                Photo tmp = mVCardProfile.getPhotos().get(0);
-                mUserImage.setImageBitmap(CropImageUtils.cropImageToCircle(tmp.getData()));
-            } else {
-                mUserImage.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.ic_contact_picture, null));
-            }
-            mUserName.setText(mVCardProfile.getFormattedName().getValue());
-            Log.d(TAG, "User did change, updating user view.");
+        mVCardProfile = VCardUtils.loadLocalProfileFromDisk(mContext);
+        if (!mVCardProfile.getPhotos().isEmpty()) {
+            Photo tmp = mVCardProfile.getPhotos().get(0);
+            mUserImage.setImageBitmap(CropImageUtils.cropImageToCircle(tmp.getData()));
+        } else {
+            mUserImage.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.ic_contact_picture, null));
         }
+        mUserName.setText(mVCardProfile.getFormattedName().getValue());
+        Log.d(TAG, "User did change, updating user view.");
+
     }
 
     public void updatePhoto(Uri uriImage) {
-        Bitmap imageProfile = ContactDetailsTask.loadProfilePhotoFromUri(getContext(), uriImage);
+        Bitmap imageProfile = ContactDetailsTask.loadProfilePhotoFromUri(mContext, uriImage);
         updatePhoto(imageProfile);
     }
 
@@ -181,37 +251,39 @@ public class MenuHeaderView extends FrameLayout {
         mProfilePhoto.setImageBitmap(CropImageUtils.cropImageToCircle(image));
     }
 
-    private void initViews() {
-        final LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View inflatedView = inflater.inflate(R.layout.frag_menu_header, this);
+    @OnClick(R.id.addaccount_btn)
+    public void addNewAccount(View sender) {
+        mContext.startActivity(new Intent(mContext, AccountWizard.class));
+    }
 
-        ButterKnife.bind(this, inflatedView);
+    private void setupNavigationMenu() {
+        mMenuView.setHasFixedSize(true);
+        LinearLayoutManager mLayoutManager2 = new LinearLayoutManager(mContext);
+        mMenuView.setLayoutManager(mLayoutManager2);
 
-        mNewAccountBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                getContext().startActivity(new Intent(v.getContext(), AccountWizard.class));
-            }
-        });
+        ArrayList<NavigationItem> menu = new ArrayList<>();
+        menu.add(0, new NavigationItem(R.string.menu_item_home, R.drawable.ic_home_black));
+        menu.add(1, new NavigationItem(R.string.menu_item_accounts, R.drawable.ic_group_black));
+        menu.add(2, new NavigationItem(R.string.menu_item_settings, R.drawable.ic_settings_black));
+        menu.add(3, new NavigationItem(R.string.menu_item_share, R.drawable.ic_share_black));
+        menu.add(4, new NavigationItem(R.string.menu_item_about, R.drawable.ic_info_black));
 
-        mAccountAdapter = new AccountSelectionAdapter(inflater.getContext(), new ArrayList<Account>());
+        mMenuAdapter = new NavigationAdapter(menu);
+        mMenuView.setAdapter(mMenuAdapter);
+        mMenuAdapter.setOnNavigationItemClickedListener(this);
+    }
 
-        mSpinnerAccounts.setAdapter(mAccountAdapter);
-
-        mVCardProfile = VCardUtils.loadLocalProfileFromDisk(getContext());
-
-        updateUserView();
-
-        mListeners = new ArrayList<>();
+    public void setNavigationSectionSelectedListener(OnNavigationSectionSelected listener) {
+        mSectionListener = listener;
     }
 
     @OnClick(R.id.profile_container)
     public void profileContainerClicked() {
         Log.d(TAG, "Click on the edit profile");
-        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setTitle(R.string.profile);
 
-        LayoutInflater inflater = ((Activity) getContext()).getLayoutInflater();
+        LayoutInflater inflater = LayoutInflater.from(getActivity());
         ViewGroup view = (ViewGroup) inflater.inflate(R.layout.dialog_profile, null);
 
         final EditText editText = (EditText) view.findViewById(R.id.user_name);
@@ -220,16 +292,16 @@ public class MenuHeaderView extends FrameLayout {
         mProfilePhoto.setImageDrawable(mUserImage.getDrawable());
 
         ImageButton cameraView = (ImageButton) view.findViewById(R.id.camera);
-        cameraView.setOnClickListener(new OnClickListener() {
+        cameraView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                boolean hasPermission = ContextCompat.checkSelfPermission(getContext(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED &&
-                        ContextCompat.checkSelfPermission(getContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
+                boolean hasPermission = ContextCompat.checkSelfPermission(mContext, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED &&
+                        ContextCompat.checkSelfPermission(mContext, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
                 if (hasPermission) {
                     Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                    ((Activity) getContext()).startActivityForResult(intent, HomeActivity.REQUEST_CODE_PHOTO);
+                    getActivity().startActivityForResult(intent, HomeActivity.REQUEST_CODE_PHOTO);
                 } else {
-                    ActivityCompat.requestPermissions((Activity) getContext(),
+                    ActivityCompat.requestPermissions(getActivity(),
                             new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE},
                             HomeActivity.REQUEST_PERMISSION_CAMERA);
                 }
@@ -237,15 +309,15 @@ public class MenuHeaderView extends FrameLayout {
         });
 
         ImageButton gallery = (ImageButton) view.findViewById(R.id.gallery);
-        gallery.setOnClickListener(new OnClickListener() {
+        gallery.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                boolean hasPermission = ContextCompat.checkSelfPermission(getContext(), Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
+                boolean hasPermission = ContextCompat.checkSelfPermission(mContext, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
                 if (hasPermission) {
                     Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                    ((Activity) getContext()).startActivityForResult(intent, HomeActivity.REQUEST_CODE_GALLERY);
+                    getActivity().startActivityForResult(intent, HomeActivity.REQUEST_CODE_GALLERY);
                 } else {
-                    ActivityCompat.requestPermissions((Activity) getContext(),
+                    ActivityCompat.requestPermissions(getActivity(),
                             new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
                             HomeActivity.REQUEST_PERMISSION_READ_STORAGE);
                 }
@@ -266,7 +338,7 @@ public class MenuHeaderView extends FrameLayout {
             public void onClick(DialogInterface dialog, int which) {
                 String username = editText.getText().toString().trim();
                 if (username.isEmpty()) {
-                    username = getContext().getString(R.string.unknown);
+                    username = mContext.getString(R.string.unknown);
                 }
                 mVCardProfile.setFormattedName(new FormattedName(username));
 
@@ -279,7 +351,7 @@ public class MenuHeaderView extends FrameLayout {
                 }
 
                 mVCardProfile.removeProperties(RawProperty.class);
-                VCardUtils.saveLocalProfileToDisk(mVCardProfile, getContext());
+                VCardUtils.saveLocalProfileToDisk(mVCardProfile, mContext);
                 updateUserView();
             }
         });
@@ -292,6 +364,7 @@ public class MenuHeaderView extends FrameLayout {
     }
 
     public void updateAccounts(List<Account> accounts) {
+
         if (accounts.isEmpty()) {
             mNewAccountBtn.setVisibility(View.VISIBLE);
             mSpinnerAccounts.setVisibility(View.GONE);
@@ -311,7 +384,13 @@ public class MenuHeaderView extends FrameLayout {
         }
     }
 
-    public interface MenuHeaderAccountSelectionListener {
-        void accountSelected(Account account);
+    class NavigationItem {
+        int mResTitleId;
+        int mResImageId;
+
+        NavigationItem(int resTitle, int resId) {
+            mResTitleId = resTitle;
+            mResImageId = resId;
+        }
     }
 }
