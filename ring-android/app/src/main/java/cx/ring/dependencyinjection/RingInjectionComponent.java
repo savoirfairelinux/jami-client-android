8b948a3c62c489965a23b04f116ec56087912aeb
Thibault Wittemberg <thibault.wittemberg@savoirfairelinux.com>
2016-11-23 16:33:09 -0500

    mvp/injection: Extract Conf features from DaemonService
    The goal is to split the DaemonService into functional entities
    that both manage daemon communication and callbacks.
    
    These entities will notify the presenters when a callback is received.
    
    Call callbacks and Conf callbacks are wrapped by the DaemonCallbackHandler.
    
    Change-Id: If32890c23775fb482f56fc44e3910815fb928949
    Tuleap: #1332

--- ring-android/app/src/main/java/cx/ring/dependencyinjection/RingInjectionComponent.java
+++ ring-android/app/src/main/java/cx/ring/dependencyinjection/RingInjectionComponent.java
@@ -32,6 +32,7 @@ import cx.ring.service.DRingService;
 import cx.ring.service.LocalService;
 import cx.ring.service.VideoManagerCallback;
 import cx.ring.services.CallService;
+import cx.ring.services.ConferenceService;
 import cx.ring.services.DaemonServiceImpl;
 import cx.ring.services.HistoryServiceImpl;
 import cx.ring.services.SettingsServiceImpl;
@@ -65,6 +66,8 @@ public interface RingInjectionComponent {
     void inject(VideoManagerCallback callback);
 
     void inject(CallService service);
+
+    void inject(ConferenceService service);
 
     void inject(DaemonServiceImpl service);
 
