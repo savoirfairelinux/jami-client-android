48e63242078d77bdff1c74830102951d0f92d635
Alexandre Lision <alexandre.lision@savoirfairelinux.com>
2016-12-05 10:34:27 -0500

    androidTest
    Change-Id: Id332e3e4ca87480dc0231ac3cef2b35088afa4fb

---
+++ ring-android/app/src/androidTest/java/cx/ring/tests/AllTests.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (C) 2016 by Savoir-faire Linux
+ * Author : Alexandre Lision <alexandre.lision@savoirfairelinux.com>
+ * <p>
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * <p>
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package cx.ring.tests;
+
+import junit.framework.TestSuite;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import cx.ring.application.RingApplication;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({Test1.class, Test2.class})
+public class AllTests extends TestSuite {
+
+    static RingApplication mApp;
+
+    @BeforeClass
+    public static void setUp() {
+        System.out.println("setting up");
+        mApp = new RingApplication();
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        System.out.println("tearing down");
+    }
+
+}
