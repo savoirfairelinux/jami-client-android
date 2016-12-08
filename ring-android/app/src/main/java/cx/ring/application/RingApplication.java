ba9bfec457e7bbd0ca770850d396fe4eb491238b
Thibault Wittemberg <thibault.wittemberg@savoirfairelinux.com>
2016-12-08 16:52:14 -0500

    Remove video stuff from DRingService
    Change-Id: I42a99bdfce60c50938d1c9fe6f8bfa4953b1d7ca

--- ring-android/app/src/main/java/cx/ring/application/RingApplication.java
+++ ring-android/app/src/main/java/cx/ring/application/RingApplication.java
@@ -20,28 +20,81 @@
 package cx.ring.application;
 
 import android.app.Application;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
+import android.hardware.Camera;
+import android.media.AudioManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Handler;
+import android.os.IBinder;
 import android.os.Looper;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.WindowManager;
 
+import java.io.IOException;
+import java.lang.ref.WeakReference;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
 
 import javax.inject.Inject;
+import javax.inject.Named;
 
+import cx.ring.BuildConfig;
+import cx.ring.daemon.Callback;
+import cx.ring.daemon.ConfigurationCallback;
+import cx.ring.daemon.VideoCallback;
 import cx.ring.dependencyinjection.DaggerRingInjectionComponent;
 import cx.ring.dependencyinjection.PresenterInjectionModule;
 import cx.ring.dependencyinjection.RingInjectionComponent;
 import cx.ring.dependencyinjection.RingInjectionModule;
 import cx.ring.dependencyinjection.ServiceInjectionModule;
+import cx.ring.model.Settings;
+import cx.ring.service.CallManagerCallBack;
+import cx.ring.service.ConfigurationManagerCallback;
+import cx.ring.service.LocalService;
+import cx.ring.service.VideoManagerCallback;
+import cx.ring.services.AccountService;
+import cx.ring.services.CallService;
+import cx.ring.services.ConferenceService;
+import cx.ring.services.DaemonService;
+import cx.ring.services.HardwareService;
 import cx.ring.services.LogService;
-import cx.ring.utils.Log;
+import cx.ring.services.SettingsService;
 
 public class RingApplication extends Application {
 
     private final static String TAG = RingApplication.class.getName();
+    static public final String DRING_CONNECTION_CHANGED = BuildConfig.APPLICATION_ID + ".event.DRING_CONNECTION_CHANGE";
+    static public final String VIDEO_EVENT = BuildConfig.APPLICATION_ID + ".event.VIDEO_EVENT";
 
     private RingInjectionComponent mRingInjectionComponent;
     private Map<String, Boolean> mPermissionsBeingAsked;
+
+    // Android Specific callbacks handlers. They rely on low level services notifications
+    private ConfigurationManagerCallback mConfigurationCallback;
+    private CallManagerCallBack mCallManagerCallBack;
+    public VideoManagerCallback mVideoManagerCallback;
+
+    // true Daemon callbacks handlers. The notify the Android ones
+    private Callback mCallAndConferenceCallbackHandler;
+    private ConfigurationCallback mAccountCallbackHandler;
+    private VideoCallback mHardwareCallbackHandler;
+
+    public final Map<String, RingApplication.Shm> videoInputs = new HashMap<>();
+    static public WeakReference<SurfaceHolder> mCameraPreviewSurface = new WeakReference<>(null);
+    static public Map<String, WeakReference<SurfaceHolder>> videoSurfaces = Collections.synchronizedMap(new HashMap<String, WeakReference<SurfaceHolder>>());
+    private Camera previewCamera = null;
+    public RingApplication.VideoParams previewParams = null;
 
     /**
      * Handler to run tasks that needs to be on main thread (UI updates)
@@ -50,6 +103,41 @@ public class RingApplication extends Application {
 
     @Inject
     LogService mLogService;
+
+    @Inject
+    @Named("DaemonExecutor")
+    ExecutorService mExecutor;
+
+    @Inject
+    DaemonService mDaemonService;
+
+    @Inject
+    AccountService mAccountService;
+
+    @Inject
+    CallService mCallService;
+
+    @Inject
+    ConferenceService mConferenceService;
+
+    @Inject
+    HardwareService mHardwareService;
+
+    @Inject
+    SettingsService mSettingsService;
+
+    static private final IntentFilter RINGER_FILTER = new IntentFilter(AudioManager.RINGER_MODE_CHANGED_ACTION);
+    private final BroadcastReceiver ringerModeListener = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            ringerModeChanged(intent.getIntExtra(AudioManager.EXTRA_RINGER_MODE, AudioManager.RINGER_MODE_NORMAL));
+        }
+    };
+
+    private void ringerModeChanged(int newMode) {
+        boolean mute = newMode == AudioManager.RINGER_MODE_VIBRATE || newMode == AudioManager.RINGER_MODE_SILENT;
+        mCallService.muteRingTone(mute);
+    }
 
     private void setDefaultUncaughtExceptionHandler() {
         try {
@@ -65,6 +153,69 @@ public class RingApplication extends Application {
             android.util.Log.e(TAG, "Could not set the Default Uncaught Exception Handler", e);
         }
     }
+
+    private final ServiceConnection mConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName className, IBinder s) {
+            android.util.Log.d(TAG, "onServiceConnected " + className.getClassName());
+
+            // bootstrap Daemon
+            Future<Boolean> startResult = mExecutor.submit(new Callable<Boolean>() {
+                @Override
+                public Boolean call() throws Exception {
+                    // Android specific callbacks handlers (rely on pure Java low level Services callbacks handlers as they
+                    // observe them)
+                    mConfigurationCallback = new ConfigurationManagerCallback(getApplicationContext());
+                    mCallManagerCallBack = new CallManagerCallBack(getApplicationContext());
+                    mVideoManagerCallback = new VideoManagerCallback(RingApplication.this);
+
+                    // mCallAndConferenceCallbackHandler is a wrapper to handle CallCallbacks and ConferenceCallbacks
+                    mCallAndConferenceCallbackHandler = mDaemonService.getDaemonCallbackHandler(
+                            mCallService.getCallbackHandler(),
+                            mConferenceService.getCallbackHandler());
+                    mAccountCallbackHandler = mAccountService.getCallbackHandler();
+                    mHardwareCallbackHandler = mHardwareService.getCallbackHandler();
+
+                    // Android specific Low level Services observers
+                    mCallService.addObserver(mCallManagerCallBack);
+                    mConferenceService.addObserver(mCallManagerCallBack);
+                    mAccountService.addObserver(mConfigurationCallback);
+                    mHardwareService.addObserver(mVideoManagerCallback);
+
+                    mDaemonService.startDaemon(
+                            mCallAndConferenceCallbackHandler,
+                            mAccountCallbackHandler,
+                            mHardwareCallbackHandler);
+
+                    ringerModeChanged(((AudioManager) getSystemService(Context.AUDIO_SERVICE)).getRingerMode());
+                    registerReceiver(ringerModeListener, RINGER_FILTER);
+
+                    mVideoManagerCallback.init();
+
+                    return true;
+                }
+            });
+
+            try {
+                startResult.get();
+            } catch (Exception e) {
+                android.util.Log.e(TAG, "DRingService start failed", e);
+            }
+
+            Intent intent = new Intent(DRING_CONNECTION_CHANGED);
+            intent.putExtra("connected", mDaemonService.isStarted());
+            sendBroadcast(intent);
+
+            // daemon is loaded, we can ask for accounts
+            mAccountService.loadAccountsFromDaemon(isConnected());
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName className) {
+            android.util.Log.d(TAG, "onServiceDisconnected " + className.getClassName());
+        }
+    };
 
     @Override
     public void onCreate() {
@@ -83,15 +234,59 @@ public class RingApplication extends Application {
 
         // we can now inject in our self whatever modules define
         mRingInjectionComponent.inject(this);
-        // Injecting LogService into the app logger
-        // as it is a static class, the injection is done manually
-        Log.injectLogService(mLogService);
 
-        Log.d(TAG, "Dependency Injection finished");
+        // to bootstrap the daemon
+        Intent intent = new Intent(this, LocalService.class);
+        startService(intent);
+        bindService(intent, mConnection, BIND_AUTO_CREATE | BIND_IMPORTANT | BIND_ABOVE_CLIENT);
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+
+        // todo decide when to stop the daemon
+        /*Future<Boolean> stopResult = mExecutor.submit(new Callable<Boolean>() {
+            @Override
+            public Boolean call() throws Exception {
+                unregisterReceiver(ringerModeListener);
+                mDaemonService.stopDaemon();
+                mConfigurationCallback = null;
+                mCallManagerCallBack = null;
+                mVideoManagerCallback = null;
+                Intent intent = new Intent(DRING_CONNECTION_CHANGED);
+                intent.putExtra("connected", mDaemonService.isStarted());
+                sendBroadcast(intent);
+
+                return true;
+            }
+        });
+
+        try {
+            stopResult.get();
+        } catch (Exception e) {
+            android.util.Log.e(TAG, "DRingService stop failed", e);
+        }*/
     }
 
     public RingInjectionComponent getRingInjectionComponent() {
         return mRingInjectionComponent;
+    }
+
+    public boolean isConnected() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo ni = cm.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        android.util.Log.d(TAG, "ActiveNetworkInfo (Wifi): " + (ni == null ? "null" : ni.toString()));
+        boolean isWifiConn = ni != null && ni.isConnected();
+
+        ni = cm.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+        android.util.Log.d(TAG, "ActiveNetworkInfo (mobile): " + (ni == null ? "null" : ni.toString()));
+        boolean isMobileConn = ni != null && ni.isConnected();
+
+        Settings settings = mSettingsService.loadSettings();
+
+        return isWifiConn || (settings.isAllowMobileData() && isMobileConn);
     }
 
     public boolean canAskForPermission(String permission) {
@@ -109,5 +304,238 @@ public class RingApplication extends Application {
 
     public void permissionHasBeenAsked(String permission) {
         mPermissionsBeingAsked.remove(permission);
+    }
+
+    public void decodingStarted(String id, String shmPath, int width, int height, boolean isMixer) {
+        android.util.Log.i(TAG, "DRingService.decodingStarted() " + id + " " + width + "x" + height);
+        Shm shm = new Shm();
+        shm.id = id;
+        shm.path = shmPath;
+        shm.w = width;
+        shm.h = height;
+        shm.mixer = isMixer;
+        videoInputs.put(id, shm);
+        WeakReference<SurfaceHolder> weakSurfaceHolder = videoSurfaces.get(id);
+        if (weakSurfaceHolder != null) {
+            SurfaceHolder holder = weakSurfaceHolder.get();
+            if (holder != null) {
+                startVideo(shm, holder);
+            }
+        }
+    }
+
+    public void decodingStopped(String id) {
+        android.util.Log.i(TAG, "DRingService.decodingStopped() " + id);
+        Shm shm = videoInputs.remove(id);
+        if (shm != null) {
+            stopVideo(shm);
+        }
+    }
+
+    public void startVideo(Shm input, SurfaceHolder holder) {
+        android.util.Log.i(TAG, "DRingService.startVideo() " + input.id);
+
+        input.window = mHardwareService.startVideo(input.id, holder.getSurface(), input.w, input.h);
+
+        if (input.window == 0) {
+            android.util.Log.i(TAG, "DRingService.startVideo() no window ! " + input.id);
+            Intent intent = new Intent(VIDEO_EVENT);
+            intent.putExtra("start", true);
+            sendBroadcast(intent);
+            return;
+        }
+
+        Intent intent = new Intent(VIDEO_EVENT);
+        intent.putExtra("started", true);
+        intent.putExtra("call", input.id);
+        intent.putExtra("width", input.w);
+        intent.putExtra("height", input.h);
+        sendBroadcast(intent);
+    }
+
+    public void stopVideo(Shm input) {
+        android.util.Log.i(TAG, "DRingService.stopVideo() " + input.id);
+        if (input.window != 0) {
+            mHardwareService.stopVideo(input.id, input.window);
+            input.window = 0;
+        }
+
+        Intent intent = new Intent(VIDEO_EVENT);
+        intent.putExtra("started", false);
+        intent.putExtra("call", input.id);
+        sendBroadcast(intent);
+    }
+
+    static public int rotationToDegrees(int rotation) {
+        switch (rotation) {
+            case Surface.ROTATION_0:
+                return 0;
+            case Surface.ROTATION_90:
+                return 90;
+            case Surface.ROTATION_180:
+                return 180;
+            case Surface.ROTATION_270:
+                return 270;
+        }
+        return 0;
+    }
+
+    public void setVideoRotation(VideoParams videoParams, Camera.CameraInfo info) {
+        WindowManager windowManager = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
+        int rotation = rotationToDegrees(windowManager.getDefaultDisplay().getRotation());
+        if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+            videoParams.rotation = (info.orientation + rotation + 360) % 360;
+        } else {
+            videoParams.rotation = (info.orientation - rotation + 360) % 360;
+        }
+    }
+
+    public void setCameraDisplayOrientation(int camId, android.hardware.Camera camera) {
+        android.hardware.Camera.CameraInfo info =
+                new android.hardware.Camera.CameraInfo();
+        android.hardware.Camera.getCameraInfo(camId, info);
+        WindowManager windowManager = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
+        int rotation = rotationToDegrees(windowManager.getDefaultDisplay().getRotation());
+        int result;
+        if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+            result = (info.orientation + rotation) % 360;
+            result = (360 - result) % 360;  // compensate the mirror
+        } else {  // back-facing
+            result = (info.orientation - rotation + 360) % 360;
+        }
+        camera.setDisplayOrientation(result);
+    }
+
+    public void startCapture(final VideoParams videoParams) {
+        stopCapture();
+
+        SurfaceHolder surface = mCameraPreviewSurface.get();
+        if (surface == null) {
+            android.util.Log.w(TAG, "Can't start capture: no surface registered.");
+            previewParams = videoParams;
+            Intent intent = new Intent(VIDEO_EVENT);
+            intent.putExtra("start", true);
+            sendBroadcast(intent);
+            return;
+        }
+
+        if (videoParams == null) {
+            android.util.Log.w(TAG, "startCapture: no video parameters ");
+            return;
+        }
+        android.util.Log.d(TAG, "startCapture " + videoParams.id);
+
+        final Camera preview;
+        try {
+            preview = Camera.open(videoParams.id);
+            setCameraDisplayOrientation(videoParams.id, preview);
+        } catch (Exception e) {
+            android.util.Log.e(TAG, e.getMessage());
+            return;
+        }
+
+        try {
+            surface.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+            preview.setPreviewDisplay(surface);
+        } catch (IOException e) {
+            android.util.Log.e(TAG, e.getMessage());
+            return;
+        }
+
+        Camera.Parameters parameters = preview.getParameters();
+        parameters.setPreviewFormat(videoParams.format);
+        parameters.setPreviewSize(videoParams.width, videoParams.height);
+        for (int[] fps : parameters.getSupportedPreviewFpsRange()) {
+            if (videoParams.rate >= fps[Camera.Parameters.PREVIEW_FPS_MIN_INDEX] &&
+                    videoParams.rate <= fps[Camera.Parameters.PREVIEW_FPS_MAX_INDEX]) {
+                parameters.setPreviewFpsRange(fps[Camera.Parameters.PREVIEW_FPS_MIN_INDEX],
+                        fps[Camera.Parameters.PREVIEW_FPS_MAX_INDEX]);
+            }
+        }
+
+        try {
+            preview.setParameters(parameters);
+        } catch (RuntimeException e) {
+            android.util.Log.e(TAG, "Error while settings preview parameters", e);
+        }
+
+        preview.setPreviewCallback(new Camera.PreviewCallback() {
+            @Override
+            public void onPreviewFrame(byte[] data, Camera camera) {
+                mHardwareService.setVideoFrame(data, videoParams.width, videoParams.height, videoParams.rotation);
+            }
+        });
+        preview.setErrorCallback(new Camera.ErrorCallback() {
+            @Override
+            public void onError(int error, Camera cam) {
+                android.util.Log.w(TAG, "Camera onError " + error);
+                if (preview == cam) {
+                    stopCapture();
+                }
+            }
+        });
+        preview.startPreview();
+
+        previewCamera = preview;
+        previewParams = videoParams;
+
+        Intent intent = new Intent(VIDEO_EVENT);
+        intent.putExtra("camera", videoParams.id == 1);
+        intent.putExtra("started", true);
+        intent.putExtra("width", videoParams.rotWidth);
+        intent.putExtra("height", videoParams.rotHeight);
+        sendBroadcast(intent);
+    }
+
+    public void stopCapture() {
+        android.util.Log.d(TAG, "stopCapture " + previewCamera);
+        if (previewCamera != null) {
+            final Camera preview = previewCamera;
+            final VideoParams p = previewParams;
+            previewCamera = null;
+            preview.setPreviewCallback(null);
+            preview.setErrorCallback(null);
+            preview.stopPreview();
+            preview.release();
+
+            Intent intent = new Intent(VIDEO_EVENT);
+            intent.putExtra("camera", p.id == 1);
+            intent.putExtra("started", false);
+            intent.putExtra("width", p.width);
+            intent.putExtra("height", p.height);
+            sendBroadcast(intent);
+        }
+    }
+
+    static public class Shm {
+        String id;
+        String path;
+        int w, h;
+        boolean mixer;
+        public long window = 0;
+    }
+
+    static public class VideoParams {
+        public VideoParams(int id, int format, int width, int height, int rate) {
+            this.id = id;
+            this.format = format;
+            this.width = width;
+            this.height = height;
+            this.rate = rate;
+        }
+
+        public int id;
+        public int format;
+
+        // size as captured by Android
+        public int width;
+        public int height;
+
+        //size, rotated, as seen by the daemon
+        public int rotWidth;
+        public int rotHeight;
+
+        public int rate;
+        public int rotation;
     }
 }
--- ring-android/app/src/main/java/cx/ring/fragments/CallFragment.java
+++ ring-android/app/src/main/java/cx/ring/fragments/CallFragment.java
@@ -69,11 +69,14 @@ import java.lang.ref.WeakReference;
 import java.util.HashMap;
 import java.util.Locale;
 
+import javax.inject.Inject;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import cx.ring.R;
 import cx.ring.adapters.ContactDetailsTask;
+import cx.ring.application.RingApplication;
 import cx.ring.client.ConversationActivity;
 import cx.ring.client.HomeActivity;
 import cx.ring.interfaces.CallInterface;
@@ -82,12 +85,12 @@ import cx.ring.model.Conference;
 import cx.ring.model.SecureSipCall;
 import cx.ring.model.SipCall;
 import cx.ring.service.CallManagerCallBack;
-import cx.ring.service.DRingService;
 import cx.ring.service.IDRingService;
 import cx.ring.service.LocalService;
+import cx.ring.services.AccountService;
 import cx.ring.utils.ActionHelper;
-import cx.ring.utils.ContentUriHandler;
 import cx.ring.utils.BitmapUtils;
+import cx.ring.utils.ContentUriHandler;
 import cx.ring.utils.KeyboardVisibilityManager;
 import cx.ring.utils.VCardUtils;
 import ezvcard.VCard;
@@ -202,6 +205,9 @@ public class CallFragment extends Fragment implements CallInterface, ContactDeta
         Log.i(TAG, "onCreate");
         super.onCreate(savedBundle);
 
+        // dependency injection
+        ((RingApplication) getActivity().getApplication()).getRingInjectionComponent().inject(this);
+
         audioManager = (AudioManager) getActivity().getSystemService(Context.AUDIO_SERVICE);
 
         setHasOptionsMenu(true);
@@ -313,7 +319,7 @@ public class CallFragment extends Fragment implements CallInterface, ContactDeta
             String action = intent.getAction();
             if (action.contentEquals(LocalService.ACTION_CONF_UPDATE)) {
                 confUpdate();
-            } else if (action.contentEquals(DRingService.VIDEO_EVENT)) {
+            } else if (action.contentEquals(RingApplication.VIDEO_EVENT)) {
                 if (mVideoSurface == null)
                     return;
                 Conference conf = getConference();
@@ -478,8 +484,10 @@ public class CallFragment extends Fragment implements CallInterface, ContactDeta
             displayManager.unregisterDisplayListener(displayListener);
         }
 
-        DRingService.videoSurfaces.remove(c.getId());
-        DRingService.mCameraPreviewSurface.clear();
+        RingApplication application = (RingApplication) getActivity().getApplication();
+
+        application.videoSurfaces.remove(c.getId());
+        application.mCameraPreviewSurface.clear();
         try {
             IDRingService service = mCallbacks.getRemoteService();
             if (service != null) {
@@ -780,9 +788,10 @@ public class CallFragment extends Fragment implements CallInterface, ContactDeta
         mVideoSurface.getHolder().addCallback(new SurfaceHolder.Callback() {
             @Override
             public void surfaceCreated(SurfaceHolder holder) {
+                RingApplication application = (RingApplication) getActivity().getApplication();
                 contactBubbleLayout.setVisibility(View.GONE);
                 Conference c = getConference();
-                DRingService.videoSurfaces.put(c.getId(), new WeakReference<>(holder));
+                application.videoSurfaces.put(c.getId(), new WeakReference<>(holder));
                 try {
                     mCallbacks.getRemoteService().videoSurfaceAdded(c.getId());
                 } catch (RemoteException e) {
@@ -798,7 +807,8 @@ public class CallFragment extends Fragment implements CallInterface, ContactDeta
             @Override
             public void surfaceDestroyed(SurfaceHolder holder) {
                 Conference c = getConference();
-                DRingService.videoSurfaces.remove(c.getId());
+                RingApplication application = (RingApplication) getActivity().getApplication();
+                application.videoSurfaces.remove(c.getId());
                 try {
                     IDRingService service = mCallbacks.getRemoteService();
                     if (service != null)
@@ -829,7 +839,8 @@ public class CallFragment extends Fragment implements CallInterface, ContactDeta
         videoPreview.getHolder().addCallback(new SurfaceHolder.Callback() {
             @Override
             public void surfaceCreated(SurfaceHolder holder) {
-                DRingService.mCameraPreviewSurface = new WeakReference<>(holder);
+                RingApplication application = (RingApplication) getActivity().getApplication();
+                application.mCameraPreviewSurface = new WeakReference<>(holder);
                 try {
                     mCallbacks.getRemoteService().videoPreviewSurfaceAdded();
                 } catch (RemoteException e) {
@@ -844,8 +855,9 @@ public class CallFragment extends Fragment implements CallInterface, ContactDeta
 
             @Override
             public void surfaceDestroyed(SurfaceHolder holder) {
-                if (videoPreview != null && DRingService.mCameraPreviewSurface.get() == holder) {
-                    DRingService.mCameraPreviewSurface.clear();
+                RingApplication application = (RingApplication) getActivity().getApplication();
+                if (videoPreview != null && application.mCameraPreviewSurface.get() == holder) {
+                    application.mCameraPreviewSurface.clear();
                 }
                 try {
                     IDRingService service = mCallbacks.getRemoteService();
