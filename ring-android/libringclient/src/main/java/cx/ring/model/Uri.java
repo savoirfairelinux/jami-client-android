25e70aba0baa27fc05cc2af83086ac7859e9bc7b
Aline Bonnet <aline.bonnet@savoirfairelinux.com>
2017-01-17 12:01:54 -0500

    conversation: fix the logo in front of a username
    In the spinner, the sip icon was displayed in front of username. Now,
    the ring icon is displayed in front of a username. The sip icon is
    already used for a sip account.
    
    Change-Id: I47d494dd2d474d3d493047b5907d4048cbd87bc9
    Tuleap: #1458

--- ring-android/libringclient/src/main/java/cx/ring/model/Uri.java
+++ ring-android/libringclient/src/main/java/cx/ring/model/Uri.java
@@ -49,10 +49,12 @@ public class Uri implements Serializable {
     }
 
     public String getRawUriString() {
-        if (getHost() != null && RING_ID_PATTERN.matcher(getHost()).find()) {
-            return "ring:" + getHost();
-        } else if (getUsername() != null && RING_ID_PATTERN.matcher(getUsername()).find()) {
-            return "ring:" + getUsername();
+        if (isRingId()) {
+            if (getUsername() != null) {
+                return "ring:" + getUsername();
+            } else {
+                return "ring:" + getHost();
+            }
         }
 
         StringBuilder builder = new StringBuilder(64);
@@ -85,8 +87,8 @@ public class Uri implements Serializable {
     }
 
     public boolean isRingId() {
-        return getHost() != null && RING_ID_PATTERN.matcher(getHost()).find()
-                || getUsername() != null && RING_ID_PATTERN.matcher(getUsername()).find();
+        return (getHost() != null && RING_ID_PATTERN.matcher(getHost()).find())
+                || (getUsername() != null && RING_ID_PATTERN.matcher(getUsername()).find());
     }
 
     private void parseUri(String uri) {
