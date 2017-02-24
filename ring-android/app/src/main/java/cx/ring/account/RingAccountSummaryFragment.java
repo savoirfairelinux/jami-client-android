4acfd287e805f93c9e4984c891cc035bab05e4ce
Adrien Beraud <adrien.beraud@savoirfairelinux.com>
2017-02-24 18:05:51 +0100

    link device ui: cleanup
    * use elevation for bottom panel (4dp)
    * reduce layout complexity
    * use dark-theme password field
    * use proper error message method
    
    Change-Id: I5b1c16f035f6473cbf226c6b6f39f9e8d2f58dde
    Tuleap: #1494

--- ring-android/app/src/main/java/cx/ring/account/RingAccountSummaryFragment.java
+++ ring-android/app/src/main/java/cx/ring/account/RingAccountSummaryFragment.java
@@ -280,7 +280,7 @@ public class RingAccountSummaryFragment extends Fragment implements BackHandlerI
             @Override
             public void run() {
                 mWaitDialog.dismiss();
-                mRingPassword.setError(getString(R.string.account_export_end_decryption_message));
+                mPasswordLayout.setError(getString(R.string.account_export_end_decryption_message));
                 mRingPassword.setText("");
             }
         });
@@ -320,14 +320,13 @@ public class RingAccountSummaryFragment extends Fragment implements BackHandlerI
         return false;
     }
 
-
     @OnClick(R.id.btn_start_export)
     public void onClickStart() {
-        mRingPassword.setError(null);
+        mPasswordLayout.setError(null);
         if (mRingPassword.getText().length() == 0) {
-            mRingPassword.setError(getString(R.string.account_enter_password));
+            mPasswordLayout.setError(getString(R.string.account_enter_password));
         } else if (mRingPassword.getText().length() < 6) {
-            mRingPassword.setError(getString(R.string.error_password_char_count));
+            mPasswordLayout.setError(getString(R.string.error_password_char_count));
         } else {
             mRingAccountSummaryPresenter.startAccountExport(mRingPassword.getText().toString());
         }
