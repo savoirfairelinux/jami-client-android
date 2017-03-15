0587a96e4a14ab89946b189aae182ad254978d5c
Hadrien De Sousa <hadrien.desousa@savoirfairelinux.com>
2017-03-15 17:14:45 -0400

    SmartListFragment : add presenter
    Apply mvp model to SmartListFragment
    
    Change-Id: If01deab1e257c4688d090fb9f4abbb7f8164a7fc
    Tuleap: #1374

---
+++ ring-android/libringclient/src/main/java/cx/ring/smartlist/SmartListView.java
@@ -0,0 +1,34 @@
+package cx.ring.smartlist;
+
+import java.util.Collection;
+
+import cx.ring.model.CallContact;
+import cx.ring.model.Conversation;
+import cx.ring.mvp.GenericView;
+
+/**
+ * Created by hdsousa on 17-03-15.
+ */
+
+public interface SmartListView extends GenericView {
+
+    void displayNetworkErrorPanel();
+
+    void displayMobileDataPanel();
+
+    void displayNewContactRowWithName(String name, String address);
+
+    void displayChooseNumberDialog(CharSequence numbers[]);
+
+    void hideSearchRow();
+
+    void hideErrorPanel();
+
+    void updateView(final Collection<Conversation> list, String query);
+
+    void goToConversation(CallContact callContact);
+
+    void goToCallActivity(String rawUriNumber);
+
+    void goToQRActivity();
+}
