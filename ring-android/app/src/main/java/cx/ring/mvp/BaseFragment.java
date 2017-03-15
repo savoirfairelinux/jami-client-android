0bead901b64ba213784a9a1bebd229c23d604b5b
Hadrien De Sousa <hadrien.desousa@savoirfairelinux.com>
2017-03-15 17:14:45 -0400

    SmartListFragment : add presenter
    Apply mvp model to SmartListFragment
    
    Change-Id: If01deab1e257c4688d090fb9f4abbb7f8164a7fc
    Tuleap: #1374

---
+++ ring-android/app/src/main/java/cx/ring/mvp/BaseFragment.java
@@ -0,0 +1,51 @@
+/*
+ *  Copyright (C) 2017 Savoir-faire Linux Inc.
+ *
+ *  Author: Hadrien De Sousa <hadrien.desousa@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+package cx.ring.mvp;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.View;
+
+public abstract class BaseFragment<T extends RootPresenter> extends Fragment {
+
+    protected T presenter;
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        presenter = createPresenter();
+
+        presenter.bindView(this);
+        initPresenter(presenter);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        presenter.unbindView();
+    }
+
+    protected abstract T createPresenter();
+
+    protected void initPresenter(T presenter) {
+
+    }
+}
