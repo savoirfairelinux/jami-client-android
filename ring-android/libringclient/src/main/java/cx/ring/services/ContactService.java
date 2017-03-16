e7414d6ef84fec118d745aaf9a302a2c59e86703
Hadrien De Sousa <hadrien.desousa@savoirfairelinux.com>
2017-03-16 15:45:52 -0400

    SmartListFragment : add recyclerView
    Replace the actual listView with a recyclerView
    
    Tuleap: #1374
    Change-Id: I293336c3c221d683887e8068c8b05ea8d4c24fb3

--- ring-android/libringclient/src/main/java/cx/ring/services/ContactService.java
+++ ring-android/libringclient/src/main/java/cx/ring/services/ContactService.java
@@ -37,6 +37,7 @@ import cx.ring.model.Uri;
 import cx.ring.utils.FutureUtils;
 import cx.ring.utils.Log;
 import cx.ring.utils.Observable;
+import cx.ring.utils.Tuple;
 
 /**
  * This service handles the contacts
@@ -50,6 +51,9 @@ import cx.ring.utils.Observable;
  * - CONTACT_REMOVED
  */
 public abstract class ContactService extends Observable {
+
+    public static final String CONTACT_NAME_KEY = "CONTACT_NAME";
+    public static final String CONTACT_PHOTO_KEY = "CONTACT_PHOTO";
 
     private final static String TAG = ContactService.class.getName();
 
@@ -77,6 +81,10 @@ public abstract class ContactService extends Observable {
     protected abstract CallContact findContactBySipNumberFromSystem(String number);
 
     protected abstract CallContact findContactByNumberFromSystem(String number);
+
+    public abstract Tuple<String, String> loadContactDataFromSystem(CallContact callContact);
+
+    public abstract Tuple<String, byte[]> loadContactData(CallContact callContact);
 
     public ContactService() {
         mContactList = new HashMap<>();
