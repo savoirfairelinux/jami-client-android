9cd2ac9a5570065935980283c80f068032d4100c
Hadrien De Sousa <hadrien.desousa@savoirfairelinux.com>
2017-03-16 15:45:52 -0400

    SmartListFragment : add recyclerView
    Replace the actual listView with a recyclerView
    
    Tuleap: #1374
    Change-Id: I293336c3c221d683887e8068c8b05ea8d4c24fb3

---
+++ ring-android/libringclient/src/main/java/cx/ring/smartlist/SmartListViewModel.java
@@ -0,0 +1,124 @@
+/*
+ *  Copyright (C) 2016 Savoir-faire Linux Inc.
+ *
+ *  Author: Hadrien De Sousa <hadrien.desousa@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+package cx.ring.smartlist;
+
+import java.util.Map;
+
+import cx.ring.model.Conversation;
+import cx.ring.model.HistoryEntry;
+import cx.ring.services.ContactService;
+
+public class SmartListViewModel {
+
+    private int uuid;
+    private String contactName;
+    private HistoryEntry lastInteraction;
+    private String photoUri;
+    private long lastInteractionTime;
+    private boolean hasUnreadTextMessage;
+    private boolean hasOngoingCall;
+
+    public SmartListViewModel(Conversation conversation, Map<String, String> contactDetails) {
+        this.uuid = conversation.getUuid();
+        setData(conversation, contactDetails);
+    }
+
+    public void update(Conversation conversation, Map<String, String> contactDetails) {
+        setData(conversation, contactDetails);
+    }
+
+    private void setData(Conversation conversation, Map<String, String> contactDetails) {
+        this.contactName = contactDetails.get(ContactService.CONTACT_NAME_KEY);
+        this.photoUri = contactDetails.get(ContactService.CONTACT_PHOTO_KEY);
+
+        for (HistoryEntry historyEntry : conversation.getHistory().values()) {
+            long lastTextTimestamp = historyEntry.getTextMessages().isEmpty() ? 0 : historyEntry.getTextMessages().lastEntry().getKey();
+            long lastCallTimestamp = historyEntry.getCalls().isEmpty() ? 0 : historyEntry.getCalls().lastEntry().getKey();
+            if (lastTextTimestamp > 0 && lastTextTimestamp > lastCallTimestamp) {
+                this.lastInteraction = historyEntry;
+                break;
+            }
+            if (lastCallTimestamp > 0) {
+                this.lastInteraction = historyEntry;
+                break;
+            }
+        }
+
+        this.lastInteractionTime = conversation.getLastInteraction().getTime();
+        this.hasUnreadTextMessage = conversation.hasUnreadTextMessages();
+        this.hasOngoingCall = conversation.hasCurrentCall();
+    }
+
+    public String getContactName() {
+        return contactName;
+    }
+
+    public void setContactName(String contactName) {
+        this.contactName = contactName;
+    }
+
+    public HistoryEntry getLastInteraction() {
+        return lastInteraction;
+    }
+
+    public void setLastInteraction(HistoryEntry lastInteraction) {
+        this.lastInteraction = lastInteraction;
+    }
+
+    public String getPhotoUri() {
+        return photoUri;
+    }
+
+    public void setPhotoUri(String photoUri) {
+        this.photoUri = photoUri;
+    }
+
+    public long getLastInteractionTime() {
+        return lastInteractionTime;
+    }
+
+    public void setLastInteractionTime(long lastInteractionTime) {
+        this.lastInteractionTime = lastInteractionTime;
+    }
+
+    public boolean hasUnreadTextMessage() {
+        return hasUnreadTextMessage;
+    }
+
+    public void setHasUnreadTextMessage(boolean hasUnreadTextMessage) {
+        this.hasUnreadTextMessage = hasUnreadTextMessage;
+    }
+
+    public boolean hasOngoingCall() {
+        return hasOngoingCall;
+    }
+
+    public void setHasOngoingCall(boolean hasOngoingCall) {
+        this.hasOngoingCall = hasOngoingCall;
+    }
+
+    public int getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(int uuid) {
+        this.uuid = uuid;
+    }
+}
