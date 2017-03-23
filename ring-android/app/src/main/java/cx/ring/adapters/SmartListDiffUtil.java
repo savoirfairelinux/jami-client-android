e3afa90e1957faf5a690510cfa8d816ca96b767c
Hadrien De Sousa <hadrien.desousa@savoirfairelinux.com>
2017-03-23 16:21:44 -0400

    Add animation to SmartListFragment
    Use DiffUtils to manage change in SmartListFragment
    adapter
    
    Tuleap: #1374
    Change-Id: I0872c9178c7fd9062d57287e61c4a2a650c7c393

---
+++ ring-android/app/src/main/java/cx/ring/adapters/SmartListDiffUtil.java
@@ -0,0 +1,57 @@
+/*
+ *  Copyright (C) 2016 Savoir-faire Linux Inc.
+ *
+ *  Author: Hadrien De Sousa <hadrien.desousa@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+package cx.ring.adapters;
+
+import android.support.v7.util.DiffUtil;
+
+import java.util.List;
+
+import cx.ring.smartlist.SmartListViewModel;
+
+public class SmartListDiffUtil extends DiffUtil.Callback {
+
+    private List<SmartListViewModel> mOldList;
+    private List<SmartListViewModel> mNewList;
+
+    public SmartListDiffUtil(List<SmartListViewModel> oldList, List<SmartListViewModel> newList) {
+        this.mOldList = oldList;
+        this.mNewList = newList;
+    }
+
+    @Override
+    public int getOldListSize() {
+        return mOldList.size();
+    }
+
+    @Override
+    public int getNewListSize() {
+        return mNewList.size();
+    }
+
+    @Override
+    public boolean areItemsTheSame(int oldItemPosition, int newItemPosition) {
+        return mOldList.get(oldItemPosition).getUuid() == mNewList.get(newItemPosition).getUuid();
+    }
+
+    @Override
+    public boolean areContentsTheSame(int oldItemPosition, int newItemPosition) {
+        return mOldList.get(oldItemPosition).equals(mNewList.get(newItemPosition));
+    }
+}
