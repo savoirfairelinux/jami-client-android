6d0f88366ccb3dc383102ea1427de2d24b80bb77
Aline Bonnet <aline.bonnet@savoirfairelinux.com>
2017-04-06 15:04:49 -0400

    database: add conversation table
    We want display one history by account without loading all
    conversations. For it, this commit creates a new database to save the
    conversations ID according to the account ID and the contact ID.
    
    Change-Id: I4e82c20e8fe3219f735166a7a5c11a0139076452
    Tuleap: #1496

--- ring-android/libringclient/src/main/java/cx/ring/facades/ConversationFacade.java
+++ ring-android/libringclient/src/main/java/cx/ring/facades/ConversationFacade.java
@@ -254,7 +254,8 @@ public class ConversationFacade extends Observable implements Observer<ServiceEv
     public void sendTextMessage(String account, Uri to, String txt) {
         long id = mCallService.sendAccountTextMessage(account, to.getRawUriString(), txt);
         Log.i(TAG, "sendAccountTextMessage " + txt + " got id " + id);
-        TextMessage message = new TextMessage(false, txt, to, null, account);
+        long conversationID = mHistoryService.getConversationID(account, to.getRawUriString());
+        TextMessage message = new TextMessage(false, txt, to, null, account, conversationID);
         message.setID(id);
         message.read();
         mHistoryService.insertNewTextMessage(message);
@@ -263,7 +264,8 @@ public class ConversationFacade extends Observable implements Observer<ServiceEv
     public void sendTextMessage(Conference conf, String txt) {
         mCallService.sendTextMessage(conf.getId(), txt);
         SipCall call = conf.getParticipants().get(0);
-        TextMessage message = new TextMessage(false, txt, call.getNumberUri(), conf.getId(), call.getAccount());
+        long conversationID = mHistoryService.getConversationID(call.getAccount(), call.getNumber());
+        TextMessage message = new TextMessage(false, txt, call.getNumberUri(), conf.getId(), call.getAccount(), conversationID);
         message.read();
         mHistoryService.insertNewTextMessage(message);
     }
@@ -583,6 +585,9 @@ public class ConversationFacade extends Observable implements Observer<ServiceEv
                         if (call.getTimestampEnd() == 0) {
                             call.setTimestampEnd(System.currentTimeMillis());
                         }
+
+                        long conversationID = mHistoryService.getConversationID(call.getAccount(), call.getNumber());
+                        call.setConversationID(conversationID);
 
                         mHistoryService.insertNewEntry(new Conference(call));
                         conference.removeParticipant(call);
