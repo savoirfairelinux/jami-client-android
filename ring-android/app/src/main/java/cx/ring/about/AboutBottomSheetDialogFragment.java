fb184b1c20f464bdcc8a324cb83b652e1b09cb3e
Aline Bonnet <aline.bonnet@savoirfairelinux.com>
2017-04-26 14:39:26 -0400

    about: add missing elements
    This commit adds two missing elements on the about screen:
    - a short description of Ring
    - the authors and artists list
    
    Change-Id: Id141bfa77378449b08d165a7b32688fe34f45f19
    Tuleap: #1574

---
+++ ring-android/app/src/main/java/cx/ring/about/AboutBottomSheetDialogFragment.java
@@ -0,0 +1,41 @@
+package cx.ring.about;
+
+import android.app.Dialog;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.design.widget.BottomSheetDialogFragment;
+import android.support.design.widget.CoordinatorLayout;
+import android.view.View;
+
+import cx.ring.R;
+
+public class AboutBottomSheetDialogFragment extends BottomSheetDialogFragment {
+
+    private BottomSheetBehavior.BottomSheetCallback mCallback = new BottomSheetBehavior.BottomSheetCallback() {
+        @Override
+        public void onStateChanged(@NonNull View bottomSheet, int newState) {
+            if (newState == BottomSheetBehavior.STATE_HIDDEN) {
+                dismiss();
+            }
+        }
+
+        @Override
+        public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+
+        }
+    };
+
+    @Override
+    public void setupDialog(Dialog dialog, int style) {
+        super.setupDialog(dialog, style);
+        View contentView = View.inflate(getContext(), R.layout.dialog_about, null);
+        dialog.setContentView(contentView);
+
+        CoordinatorLayout.LayoutParams params = (CoordinatorLayout.LayoutParams) ((View) contentView.getParent()).getLayoutParams();
+        CoordinatorLayout.Behavior behavior = params.getBehavior();
+
+        if (behavior != null && behavior instanceof BottomSheetBehavior) {
+            ((BottomSheetBehavior) behavior).setBottomSheetCallback(mCallback);
+        }
+    }
+}
