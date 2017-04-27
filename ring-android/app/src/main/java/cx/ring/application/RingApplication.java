fe569e84812c1a14fe64ea9a08747dc0ae5ff240
Alexandre Lision <alexandre.lision@savoirfairelinux.com>
2017-04-27 18:09:42 -0400

    network: display actual connectivity state
    There was a visual desynchronisation between the actual network state
    and what was visible to the user. We now refresh the connectivity state
    display of the smart list every time an acocunt state changes
    
    The LocalService BroadcastReceiver has been moved to the DRingService.
    Most of its actions have been removed since they have been reimplemented
    by low level services.
    
    SharedPreferenceService has been renamed to SettingsService since
    SharedPreferences are an Android notion.
    
    Change-Id: Ic002d8ccb6bf55c0fcaa1ca381b7a35a7d5b465f
    Tuleap: #1568

--- ring-android/app/src/main/java/cx/ring/application/RingApplication.java
+++ ring-android/app/src/main/java/cx/ring/application/RingApplication.java
@@ -70,7 +70,7 @@ import cx.ring.services.DaemonService;
 import cx.ring.services.DeviceRuntimeService;
 import cx.ring.services.HardwareService;
 import cx.ring.services.PresenceService;
-import cx.ring.services.SharedPreferencesService;
+import cx.ring.services.SettingsService;
 import cx.ring.utils.FutureUtils;
 import cx.ring.utils.Log;
 
@@ -126,7 +126,7 @@ public class RingApplication extends Application {
     HardwareService mHardwareService;
 
     @Inject
-    SharedPreferencesService mSharedPreferencesService;
+    SettingsService mSettingsService;
 
     @Inject
     DeviceRuntimeService mDeviceRuntimeService;
@@ -243,7 +243,7 @@ public class RingApplication extends Application {
         sendBroadcast(intent);
 
         // load accounts from Daemon
-        mAccountService.loadAccountsFromDaemon(mSharedPreferencesService.isConnectedWifiAndMobile());
+        mAccountService.loadAccountsFromDaemon(mSettingsService.isConnectedWifiAndMobile());
     }
 
     public void restartVideo() {
