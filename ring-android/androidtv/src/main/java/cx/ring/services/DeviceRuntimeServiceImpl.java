10ff410360afa4b3f0e11f3a5e44b02221bd899e
Alexandre Lision <alexandre.lision@savoirfairelinux.com>
2017-05-08 16:13:59 -0400

    androidtv: initial load of daemon
    Duplicate necessary code from app module to load and run the Ring
    daemon. UI must be recreated for each feature.
    build-daemon.sh has been amended to also copy libring.so to androidtv
    module
    
    Change-Id: I389e6327972bac0c2dc19d21e85dd5ef867b1cfc
    Reviewed-by: Aline Bonnet <aline.bonnet@savoirfairelinux.com>

---
+++ ring-android/androidtv/src/main/java/cx/ring/services/DeviceRuntimeServiceImpl.java
@@ -0,0 +1,181 @@
+/*
+ *  Copyright (C) 2016 Savoir-faire Linux Inc.
+ *
+ *  Author: Thibault Wittemberg <thibault.wittemberg@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+package cx.ring.services;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.media.AudioManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Handler;
+import android.provider.ContactsContract;
+import android.support.v4.content.ContextCompat;
+
+import java.io.File;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import cx.ring.utils.Log;
+import cx.ring.utils.MediaManager;
+import cx.ring.utils.NetworkUtils;
+
+public class DeviceRuntimeServiceImpl extends DeviceRuntimeService {
+
+    private static final String TAG = DeviceRuntimeServiceImpl.class.getName();
+    private static final String[] PROFILE_PROJECTION = new String[]{ContactsContract.Profile._ID,
+            ContactsContract.Profile.DISPLAY_NAME_PRIMARY,
+            ContactsContract.Profile.PHOTO_ID};
+
+    @Inject
+    @Named("DaemonExecutor")
+    ExecutorService mExecutor;
+
+    @Inject
+    protected Context mContext;
+
+    @Inject
+    protected MediaManager mediaManager;
+
+    private long mDaemonThreadId = -1;
+
+    @Override
+    public void loadNativeLibrary() {
+        Future<Boolean> result = mExecutor.submit(new Callable<Boolean>() {
+            @Override
+            public Boolean call() throws Exception {
+                try {
+                    mDaemonThreadId = Thread.currentThread().getId();
+                    System.loadLibrary("ring");
+                    return true;
+                } catch (Exception e) {
+                    Log.e(TAG, "Could not load Ring library", e);
+                    return false;
+                }
+            }
+        });
+
+        try {
+            boolean loaded = result.get();
+            Log.i(TAG, "Ring library has been successfully loaded");
+        } catch (Exception e) {
+            Log.e(TAG, "Could not load Ring library", e);
+        }
+    }
+
+    @Override
+    public void updateAudioState(final boolean isRinging) {
+        Handler mainHandler = new Handler(mContext.getMainLooper());
+
+        mainHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                mediaManager.obtainAudioFocus(isRinging);
+                if (isRinging) {
+                    mediaManager.audioManager.setMode(AudioManager.MODE_RINGTONE);
+                    mediaManager.startRing(null);
+                } else {
+                    mediaManager.stopRing();
+                    mediaManager.audioManager.setMode(AudioManager.MODE_IN_COMMUNICATION);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void closeAudioState() {
+        mediaManager.stopRing();
+        mediaManager.abandonAudioFocus();
+    }
+
+    @Override
+    public File provideFilesDir() {
+        return mContext.getFilesDir();
+    }
+
+    @Override
+    public boolean isConnectedWifi() {
+        NetworkInfo info = NetworkUtils.getNetworkInfo(mContext);
+        return (info != null && info.isConnected() && info.getType() == ConnectivityManager.TYPE_WIFI);
+    }
+
+    @Override
+    public boolean isConnectedMobile() {
+        NetworkInfo info = NetworkUtils.getNetworkInfo(mContext);
+        return (info != null && info.isConnected() && info.getType() == ConnectivityManager.TYPE_MOBILE);
+    }
+
+    @Override
+    public long provideDaemonThreadId() {
+        return mDaemonThreadId;
+    }
+
+    @Override
+    public boolean hasVideoPermission() {
+        return checkPermission(Manifest.permission.CAMERA);
+    }
+
+    @Override
+    public boolean hasAudioPermission() {
+        return checkPermission(Manifest.permission.RECORD_AUDIO);
+    }
+
+    @Override
+    public boolean hasContactPermission() {
+        return checkPermission(Manifest.permission.READ_CONTACTS);
+    }
+
+    @Override
+    public boolean hasCallLogPermission() {
+        return checkPermission(Manifest.permission.WRITE_CALL_LOG);
+    }
+
+    @Override
+    public boolean hasPhotoPermission() {
+        return checkPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+    }
+
+    @Override
+    public boolean hasGalleryPermission() {
+        return checkPermission(Manifest.permission.READ_EXTERNAL_STORAGE);
+    }
+
+    @Override
+    public String getProfileName() {
+        Cursor mProfileCursor = mContext.getContentResolver().query(ContactsContract.Profile.CONTENT_URI, PROFILE_PROJECTION, null, null, null);
+        if (mProfileCursor != null) {
+            if (mProfileCursor.moveToFirst()) {
+                mProfileCursor.close();
+                return mProfileCursor.getString(mProfileCursor.getColumnIndex(ContactsContract.Profile.DISPLAY_NAME_PRIMARY));
+            }
+            mProfileCursor.close();
+        }
+        return null;
+    }
+
+    private boolean checkPermission(String permission) {
+        return ContextCompat.checkSelfPermission(mContext, permission) == PackageManager.PERMISSION_GRANTED;
+    }
+}
---
+++ ring-android/androidtv/src/main/java/cx/ring/utils/NetworkUtils.java
@@ -0,0 +1,105 @@
+/*
+ *  Copyright (C) 2017 Savoir-faire Linux Inc.
+ *
+ *  Author: Hadrien Desousa <hadrien.desousa@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+package cx.ring.utils;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.telephony.TelephonyManager;
+
+public final class NetworkUtils {
+    /**
+     * Get the network info
+     *
+     * @param context
+     * @return
+     */
+    public static NetworkInfo getNetworkInfo(Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        return connectivityManager.getActiveNetworkInfo();
+    }
+
+    /**
+     * Check if there is fast connectivity
+     *
+     * @param context
+     * @return
+     */
+    public static boolean isConnectedFast(Context context) {
+        NetworkInfo info = NetworkUtils.getNetworkInfo(context);
+        return (info != null && info.isConnected() && NetworkUtils.isConnectionFast(info.getType(), info.getSubtype()));
+    }
+
+    /**
+     * Check if the connection is fast
+     *
+     * @param type
+     * @param subType
+     * @return
+     */
+    public static boolean isConnectionFast(int type, int subType) {
+        if (type == ConnectivityManager.TYPE_WIFI) {
+            return true;
+        } else if (type == ConnectivityManager.TYPE_MOBILE) {
+            switch (subType) {
+                case TelephonyManager.NETWORK_TYPE_1xRTT:
+                    return false; // ~ 50-100 kbps
+                case TelephonyManager.NETWORK_TYPE_CDMA:
+                    return false; // ~ 14-64 kbps
+                case TelephonyManager.NETWORK_TYPE_EDGE:
+                    return false; // ~ 50-100 kbps
+                case TelephonyManager.NETWORK_TYPE_EVDO_0:
+                    return true; // ~ 400-1000 kbps
+                case TelephonyManager.NETWORK_TYPE_EVDO_A:
+                    return true; // ~ 600-1400 kbps
+                case TelephonyManager.NETWORK_TYPE_GPRS:
+                    return false; // ~ 100 kbps
+                case TelephonyManager.NETWORK_TYPE_HSDPA:
+                    return true; // ~ 2-14 Mbps
+                case TelephonyManager.NETWORK_TYPE_HSPA:
+                    return true; // ~ 700-1700 kbps
+                case TelephonyManager.NETWORK_TYPE_HSUPA:
+                    return true; // ~ 1-23 Mbps
+                case TelephonyManager.NETWORK_TYPE_UMTS:
+                    return true; // ~ 400-7000 kbps
+            /*
+             * Above API level 7, make sure to set android:targetSdkVersion
+			 * to appropriate level to use these
+			 */
+                case TelephonyManager.NETWORK_TYPE_EHRPD: // API level 11
+                    return true; // ~ 1-2 Mbps
+                case TelephonyManager.NETWORK_TYPE_EVDO_B: // API level 9
+                    return true; // ~ 5 Mbps
+                case TelephonyManager.NETWORK_TYPE_HSPAP: // API level 13
+                    return true; // ~ 10-20 Mbps
+                case TelephonyManager.NETWORK_TYPE_IDEN: // API level 8
+                    return false; // ~25 kbps
+                case TelephonyManager.NETWORK_TYPE_LTE: // API level 11
+                    return true; // ~ 10+ Mbps
+                // Unknown
+                case TelephonyManager.NETWORK_TYPE_UNKNOWN:
+                default:
+                    return false;
+            }
+        } else {
+            return false;
+        }
+    }
+}
