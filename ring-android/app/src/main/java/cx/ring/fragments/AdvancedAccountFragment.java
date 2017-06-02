e740a4abc3ad7f3ff176707aa7b7a0088150865b
Hadrien De Sousa <hadrien.desousa@savoirfairelinux.com>
2017-06-02 10:57:08 -0400

    mvp: refactor AdvancedAccountPresenter
    Apply MVP to AdvancedAccountPresenter
    
    Change-Id: I30b56af8bc86169e4e0103c6edb3a4dcf9858864
    Tuleap: #1372

--- ring-android/app/src/main/java/cx/ring/fragments/AdvancedAccountFragment.java
+++ ring-android/app/src/main/java/cx/ring/fragments/AdvancedAccountFragment.java
@@ -20,67 +20,39 @@
 package cx.ring.fragments;
 
 import android.os.Bundle;
-import android.support.v14.preference.PreferenceFragment;
 import android.support.v7.preference.EditTextPreference;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.TwoStatePreference;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.inputmethod.EditorInfo;
 
-import java.net.NetworkInterface;
-import java.net.SocketException;
 import java.util.ArrayList;
-import java.util.Enumeration;
-
-import javax.inject.Inject;
 
 import cx.ring.R;
 import cx.ring.account.AccountEditionActivity;
 import cx.ring.application.RingApplication;
-import cx.ring.facades.ConversationFacade;
-import cx.ring.model.Account;
 import cx.ring.model.AccountConfig;
 import cx.ring.model.ConfigKey;
-import cx.ring.services.AccountService;
+import cx.ring.mvp.BasePreferenceFragment;
 import cx.ring.views.EditTextIntegerPreference;
 import cx.ring.views.EditTextPreferenceDialog;
 import cx.ring.views.PasswordPreference;
 
-public class AdvancedAccountFragment extends PreferenceFragment {
+public class AdvancedAccountFragment extends BasePreferenceFragment<AdvancedAccountPresenter> implements AdvancedAccountView, Preference.OnPreferenceChangeListener {
 
     private static final String TAG = AdvancedAccountFragment.class.getSimpleName();
     private static final String DIALOG_FRAGMENT_TAG = "android.support.v14.preference.PreferenceFragment.DIALOG";
 
-    @Inject
-    protected ConversationFacade mConversationFacade;
-
-    @Inject
-    protected AccountService mAccountService;
-
-    private String mAccountID;
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        if (getArguments() == null || getArguments().getString(AccountEditionActivity.ACCOUNT_ID_KEY) == null) {
-            return;
-        }
-        mAccountID = getArguments().getString(AccountEditionActivity.ACCOUNT_ID_KEY);
-        Account account = mAccountService.getAccount(mAccountID);
-        if (account != null) {
-            setPreferenceDetails(account.getConfig());
-            setPreferenceListener(account.getConfig(), changeAdvancedPreferenceListener);
-        }
-    }
-
     @Override
     public void onCreatePreferences(Bundle bundle, String s) {
         ((RingApplication) getActivity().getApplication()).getRingInjectionComponent().inject(this);
+        super.onCreatePreferences(bundle, s);
 
         // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.account_advanced_prefs);
+
+        presenter.init(getArguments().getString(AccountEditionActivity.ACCOUNT_ID_KEY));
     }
 
     @Override
@@ -101,97 +73,48 @@ public class AdvancedAccountFragment extends PreferenceFragment {
         }
     }
 
-    private void setPreferenceDetails(AccountConfig details) {
-        for (ConfigKey confKey : details.getKeys()) {
+    @Override
+    public void initView(AccountConfig config, ArrayList<CharSequence> networkInterfaces) {
+        for (ConfigKey confKey : config.getKeys()) {
             Preference pref = findPreference(confKey.key());
             if (pref != null) {
+                pref.setOnPreferenceChangeListener(this);
                 if (confKey == ConfigKey.LOCAL_INTERFACE) {
-                    String val = details.get(confKey);
-                    ArrayList<CharSequence> entries = getNetworkInterfaces();
-                    CharSequence[] display = entries.toArray(new CharSequence[entries.size()]);
+                    String val = config.get(confKey);
+                    CharSequence[] display = networkInterfaces.toArray(new CharSequence[networkInterfaces.size()]);
                     ListPreference listPref = (ListPreference) pref;
                     listPref.setEntries(display);
                     listPref.setEntryValues(display);
                     listPref.setSummary(val);
                     listPref.setValue(val);
                 } else if (!confKey.isTwoState()) {
-                    String val = details.get(confKey);
+                    String val = config.get(confKey);
                     pref.setSummary(val);
                     if (pref instanceof EditTextPreference) {
                         ((EditTextPreference) pref).setText(val);
                     }
                 } else {
-                    ((TwoStatePreference) pref).setChecked(details.getBool(confKey));
+                    ((TwoStatePreference) pref).setChecked(config.getBool(confKey));
                 }
             }
         }
     }
 
-    private void setPreferenceListener(AccountConfig details, Preference.OnPreferenceChangeListener listener) {
-        for (ConfigKey confKey : details.getKeys()) {
-            Preference pref = findPreference(confKey.key());
-            if (pref != null) {
-                pref.setOnPreferenceChangeListener(listener);
-            }
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        final ConfigKey key = ConfigKey.fromString(preference.getKey());
+
+        presenter.preferenceChanged(key, newValue);
+
+        if (preference instanceof TwoStatePreference) {
+            presenter.twoStatePreferenceChanged(key, newValue);
+        } else if (preference instanceof PasswordPreference) {
+            presenter.passwordPreferenceChanged(key, newValue);
+            preference.setSummary(TextUtils.isEmpty(newValue.toString()) ? "" : "******");
+        } else {
+            presenter.preferenceChanged(key, newValue);
+            preference.setSummary(newValue.toString());
         }
+        return true;
     }
-
-    private ArrayList<CharSequence> getNetworkInterfaces() {
-        ArrayList<CharSequence> result = new ArrayList<>();
-        result.add("default");
-        try {
-
-            for (Enumeration<NetworkInterface> list = NetworkInterface.getNetworkInterfaces(); list.hasMoreElements(); ) {
-                NetworkInterface i = list.nextElement();
-                if (i.isUp()) {
-                    result.add(i.getDisplayName());
-                }
-            }
-        } catch (SocketException e) {
-            Log.e(TAG, "Error enumerating interfaces: ", e);
-        }
-        return result;
-    }
-
-    Preference.OnPreferenceChangeListener changeAdvancedPreferenceListener = new Preference.OnPreferenceChangeListener() {
-        @Override
-        public boolean onPreferenceChange(Preference preference, Object newValue) {
-            final Account account = mAccountService.getAccount(mAccountID);
-            final ConfigKey key = ConfigKey.fromString(preference.getKey());
-            Log.i(TAG, "Changing " + preference.getKey() + " value: " + newValue);
-
-            if (preference instanceof TwoStatePreference) {
-                if (key != null && key.equals(ConfigKey.DHT_PUBLIC_IN)) {
-                    mConversationFacade.clearConversations();
-                }
-                account.setDetail(key, newValue.toString());
-            } else if (preference instanceof PasswordPreference) {
-                account.setDetail(key, newValue.toString());
-                preference.setSummary(TextUtils.isEmpty(newValue.toString()) ? "" : "******");
-            } else {
-                if (key == ConfigKey.AUDIO_PORT_MAX || key == ConfigKey.AUDIO_PORT_MIN) {
-                    newValue = adjustRtpRange(Integer.valueOf((String) newValue));
-                }
-                preference.setSummary(newValue.toString());
-                account.setDetail(key, newValue.toString());
-            }
-
-            mAccountService.setCredentials(mAccountID, account.getCredentialsHashMapList());
-            mAccountService.setAccountDetails(mAccountID, account.getDetails());
-            return true;
-        }
-    };
-
-    private String adjustRtpRange(int newValue) {
-        if (newValue < 1024) {
-            return "1024";
-        }
-
-        if (newValue > 65534) {
-            return "65534";
-        }
-
-        return String.valueOf(newValue);
-    }
-
 }
