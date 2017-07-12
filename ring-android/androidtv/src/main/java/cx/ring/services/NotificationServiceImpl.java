7e933edf7e1da1c7db72f0a2897e7c295421f6ce
Loïc Siret <loic.siret@savoirfairelinux.com>
2017-07-12 12:09:14 -0400

    Android TV: Adding call reception
    Change-Id: Ia3f96554446565d0429e59e5d55c2a23075c8db5

--- ring-android/androidtv/src/main/java/cx/ring/services/NotificationServiceImpl.java
+++ ring-android/androidtv/src/main/java/cx/ring/services/NotificationServiceImpl.java
@@ -20,22 +20,33 @@
 
 package cx.ring.services;
 
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
 
 import java.util.HashMap;
+import java.util.Random;
 import java.util.TreeMap;
 
 import javax.inject.Inject;
 
 import cx.ring.BuildConfig;
+import cx.ring.R;
 import cx.ring.model.Account;
 import cx.ring.model.CallContact;
 import cx.ring.model.Conference;
 import cx.ring.model.Conversation;
 import cx.ring.model.ServiceEvent;
+import cx.ring.model.SipCall;
 import cx.ring.model.TextMessage;
+import cx.ring.service.CallManagerCallBack;
+import cx.ring.service.DRingService;
+import cx.ring.utils.BitmapUtils;
 import cx.ring.utils.Observable;
 import cx.ring.utils.Observer;
 
@@ -59,6 +70,9 @@ public class NotificationServiceImpl extends NotificationService implements Obse
     @Inject
     protected PreferencesService mPreferencesService;
 
+    @Inject
+    DeviceRuntimeService mDeviceRuntimeService;
+
     private NotificationManagerCompat notificationManager;
 
     private HashMap<Integer, NotificationCompat.Builder> mNotificationBuilders;
@@ -78,8 +92,92 @@ public class NotificationServiceImpl extends NotificationService implements Obse
 
     @Override
     public void showCallNotification(Conference conference) {
+        if (conference.getParticipants().isEmpty()) {
+            return;
+        }
 
+        SipCall call = conference.getParticipants().get(0);
+        CallContact contact = call.getContact();
+        final int notificationId = call.getCallId().hashCode();
+        notificationManager.cancel(notificationId);
+
+        PendingIntent gotoIntent = PendingIntent.getService(mContext,
+                new Random().nextInt(),
+                new Intent(DRingService.ACTION_CALL_VIEW)
+                        .setClass(mContext, DRingService.class)
+                        .putExtra(KEY_CALL_ID, call.getCallId()), 0);
+        NotificationCompat.Builder messageNotificationBuilder = new NotificationCompat.Builder(mContext);
+
+        if (conference.isOnGoing()) {
+            messageNotificationBuilder.setContentTitle(mContext.getString(R.string.notif_current_call_title, contact.getRingUsername()))
+                    .setContentText(mContext.getText(R.string.notif_current_call))
+                    .setContentIntent(gotoIntent)
+                    .addAction(R.drawable.ic_call_end_white, mContext.getText(R.string.action_call_hangup),
+                            PendingIntent.getService(mContext, new Random().nextInt(),
+                                    new Intent(DRingService.ACTION_CALL_END)
+                                            .setClass(mContext, DRingService.class)
+                                            .putExtra(KEY_CALL_ID, call.getCallId()),
+                                    PendingIntent.FLAG_ONE_SHOT));
+        } else if (conference.isRinging()) {
+            if (conference.isIncoming()) {
+                Bundle extras = new Bundle();
+                extras.putBoolean(CallManagerCallBack.INCOMING_CALL, true);
+                messageNotificationBuilder.setContentTitle(mContext.getString(R.string.notif_incoming_call_title, contact.getRingUsername()))
+                        .setPriority(NotificationCompat.PRIORITY_MAX)
+                        .setContentText(mContext.getText(R.string.notif_incoming_call))
+                        .setContentIntent(gotoIntent)
+                        .setFullScreenIntent(gotoIntent, true)
+                        .addAction(R.drawable.ic_call_end_white, mContext.getText(R.string.action_call_decline),
+                                PendingIntent.getService(mContext, new Random().nextInt(),
+                                        new Intent(DRingService.ACTION_CALL_REFUSE)
+                                                .setClass(mContext, DRingService.class)
+                                                .putExtra(KEY_CALL_ID, call.getCallId()),
+                                        PendingIntent.FLAG_ONE_SHOT))
+                        .addAction(R.drawable.ic_action_accept, mContext.getText(R.string.action_call_accept),
+                                PendingIntent.getService(mContext, new Random().nextInt(),
+                                        new Intent(DRingService.ACTION_CALL_ACCEPT)
+                                                .setClass(mContext, DRingService.class)
+                                                .putExtra(KEY_CALL_ID, call.getCallId()),
+                                        PendingIntent.FLAG_ONE_SHOT))
+                        .addExtras(extras);
+            } else {
+                messageNotificationBuilder.setContentTitle(mContext.getString(R.string.notif_outgoing_call_title, contact.getRingUsername()))
+                        .setContentText(mContext.getText(R.string.notif_outgoing_call))
+                        .setContentIntent(gotoIntent)
+                        .addAction(R.drawable.ic_call_end_white, mContext.getText(R.string.action_call_hangup),
+                                PendingIntent.getService(mContext, new Random().nextInt(),
+                                        new Intent(DRingService.ACTION_CALL_END)
+                                                .setClass(mContext, DRingService.class)
+                                                .putExtra(KEY_CALL_ID, call.getCallId()),
+                                        PendingIntent.FLAG_ONE_SHOT));
+            }
+        } else {
+            return;
+        }
+
+        messageNotificationBuilder.setOngoing(true)
+                .setCategory(NotificationCompat.CATEGORY_CALL)
+                .setSmallIcon(R.drawable.ic_ring_logo_white);
+
+        if (contact.getPhoto() != null) {
+            Resources res = mContext.getResources();
+            int height = (int) res.getDimension(android.R.dimen.notification_large_icon_height);
+            int width = (int) res.getDimension(android.R.dimen.notification_large_icon_width);
+            Bitmap bmp = BitmapUtils.bytesToBitmap(contact.getPhoto());
+            if (bmp != null) {
+                messageNotificationBuilder.setLargeIcon(Bitmap.createScaledBitmap(bmp, width, height, false));
+            }
+        }
+
+        notificationManager.notify(notificationId, messageNotificationBuilder.build());
+        mNotificationBuilders.put(notificationId, messageNotificationBuilder);
+
+        String[] split = contact.getDisplayName().split(":");
+        if (split.length > 1) {
+            mAccountService.lookupAddress("", "", split[1]);
+        }
     }
+
 
     @Override
     public void showTextNotification(CallContact contact, Conversation conversation, TreeMap<Long, TextMessage> texts) {
@@ -88,7 +186,8 @@ public class NotificationServiceImpl extends NotificationService implements Obse
 
     @Override
     public void cancelCallNotification(int notificationId) {
-
+        notificationManager.cancel(notificationId);
+        mNotificationBuilders.remove(notificationId);
     }
 
     @Override
