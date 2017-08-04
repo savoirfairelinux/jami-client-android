575d8e33f06c8c48d8619891db475064f49ee6e9
Loïc Siret <loic.siret@savoirfairelinux.com>
2017-08-04 11:15:52 -0400

    AndroidTV : Wizard enhancements
    Adding a more AndroidTV wizard.
    Adding Link Account features.
    
    Change-Id: I04e8aae05504694f0b517b28ca51f0c18fe2b87a

---
+++ ring-android/androidtv/src/main/java/cx/ring/account/ProfileCreationFragment.java
@@ -0,0 +1,93 @@
+package cx.ring.account;
+
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.v17.leanback.widget.GuidanceStylist;
+import android.support.v17.leanback.widget.GuidedAction;
+import android.view.View;
+
+import java.util.List;
+
+import cx.ring.R;
+import cx.ring.application.RingTVApplication;
+import cx.ring.fragments.RingGuidedStepFragment;
+
+public class ProfileCreationFragment extends RingGuidedStepFragment<ProfileCreationPresenter>
+        implements ProfileCreationView {
+
+    private static final int USER_NAME = 1;
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        ((RingTVApplication) getActivity().getApplication()).getAndroidTVInjectionComponent().inject(this);
+
+        super.onViewCreated(view, savedInstanceState);
+        presenter.initPresenter();
+    }
+
+    @Override
+    public GuidanceStylist.Guidance onCreateGuidance(Bundle savedInstanceState) {
+        String title = getString(R.string.account_create_title);
+        String breadcrumb = "";
+        String description = getString(R.string.profile_message_warning);
+        Drawable icon = getActivity().getDrawable(R.drawable.ic_contact_picture);
+        return new GuidanceStylist.Guidance(title, description, breadcrumb, icon);
+    }
+
+    @Override
+    public int onProvideTheme() {
+        return R.style.Theme_Ring_Leanback_GuidedStep_First;
+    }
+
+    @Override
+    public void onCreateActions(List<GuidedAction> actions, Bundle savedInstanceState) {
+        String desc = getString(R.string.account_creation_profile);
+        String editdesc = getString(R.string.profile_name_hint);
+        addEditTextAction(actions, USER_NAME, desc, editdesc, "");
+    }
+
+    @Override
+    public void onGuidedActionClicked(GuidedAction action) {
+        presenter.nextClick();
+    }
+
+    @Override
+    public void displayProfileName(String profileName) {
+        findActionById(USER_NAME).setEditDescription(profileName);
+        notifyActionChanged(findActionPositionById(USER_NAME));
+    }
+
+    @Override
+    public void goToGallery() {
+        //not implemented on TV
+    }
+
+    @Override
+    public void goToPhotoCapture() {
+        //not implemented on TV
+    }
+
+    @Override
+    public void askStoragePermission() {
+        //not implemented on TV
+    }
+
+    @Override
+    public void askPhotoPermission() {
+        //not implemented on TV
+    }
+
+    @Override
+    public void goToNext() {
+        GuidedAction editText = findActionById(USER_NAME);
+        ((AccountWizard) getActivity()).profileNext(editText.getEditDescription().toString(), null);
+    }
+
+    @Override
+    public void goToLast() {
+
+    }
+
+
+
+}
