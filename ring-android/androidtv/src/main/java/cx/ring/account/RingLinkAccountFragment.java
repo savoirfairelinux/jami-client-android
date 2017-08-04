06605efa860ef6e9a1aae620dba9262772c54a56
Loïc Siret <loic.siret@savoirfairelinux.com>
2017-08-04 11:15:52 -0400

    AndroidTV : Wizard enhancements
    Adding a more AndroidTV wizard.
    Adding Link Account features.
    
    Change-Id: I04e8aae05504694f0b517b28ca51f0c18fe2b87a

---
+++ ring-android/androidtv/src/main/java/cx/ring/account/RingLinkAccountFragment.java
@@ -0,0 +1,101 @@
+package cx.ring.account;
+
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.v17.leanback.widget.GuidanceStylist;
+import android.support.v17.leanback.widget.GuidedAction;
+import android.view.View;
+
+import java.util.List;
+
+import cx.ring.R;
+import cx.ring.application.RingTVApplication;
+import cx.ring.fragments.RingGuidedStepFragment;
+import cx.ring.utils.StringUtils;
+
+public class RingLinkAccountFragment extends RingGuidedStepFragment<RingLinkAccountPresenter>
+        implements RingLinkAccountView {
+    private static final int PASSWORD = 0;
+    private static final int PIN = 1;
+    private static final int LINK = 2;
+
+    public RingLinkAccountFragment() {
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        ((RingTVApplication) getActivity().getApplication()).getAndroidTVInjectionComponent().inject(this);
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public GuidanceStylist.Guidance onCreateGuidance(Bundle savedInstanceState) {
+        String title = getString(R.string.account_link_title);
+        String breadcrumb = "";
+        String description = "";
+
+        Drawable icon = getActivity().getDrawable(R.drawable.ic_main_icon);
+        return new GuidanceStylist.Guidance(title, description, breadcrumb, icon);
+    }
+
+    @Override
+    public void onCreateActions(List<GuidedAction> actions, Bundle savedInstanceState) {
+        addMultilineActions(actions, getString(R.string.help_password_enter), "");
+        addPasswordtAction(actions, PASSWORD, getString(R.string.account_enter_password), "", "");
+        addMultilineActions(actions, getString(R.string.help_pin_enter), "");
+        addPasswordtAction(actions, PIN, getString(R.string.account_link_prompt_pin), "", "");
+        addDisabledAction(actions, LINK, getString(R.string.account_link_title), "");
+    }
+
+    @Override
+    public int onProvideTheme() {
+        return R.style.Theme_Ring_Leanback_GuidedStep_First;
+    }
+
+    @Override
+    public void onGuidedActionClicked(GuidedAction action) {
+        if (action.getId() == LINK) {
+            presenter.linkClicked();
+        }
+    }
+
+    @Override
+    public void enableLinkButton(boolean enable) {
+        findActionById(LINK).setEnabled(enable);
+
+        notifyActionChanged(findActionPositionById(LINK));
+    }
+
+    @Override
+    public void goToLast() {
+        //no op on tv
+    }
+
+    @Override
+    public void createAccount() {
+        ((AccountWizard) getActivity()).createAccount(
+                null,
+                presenter.getPin(),
+                presenter.getPassword());
+
+    }
+
+    @Override
+    public long onGuidedActionEditedAndProceed(GuidedAction action) {
+        String password = action.getEditDescription().toString();
+        if (password.length() > 0) {
+            action.setDescription(StringUtils.toPassword(password));
+        } else {
+            action.setDescription(getString(R.string.account_enter_password));
+        }
+        if (action.getId() == PASSWORD) {
+            notifyActionChanged(findActionPositionById(PASSWORD));
+            presenter.passwordChanged(password);
+        } else if (action.getId() == PIN) {
+            notifyActionChanged(findActionPositionById(PIN));
+            presenter.pinChanged(action.getEditDescription().toString());
+        }
+        return super.onGuidedActionEditedAndProceed(action);
+    }
+
+}
