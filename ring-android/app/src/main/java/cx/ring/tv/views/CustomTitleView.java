53e4131f0c5ed6e47919b0009a732290ec6f63a9
Loïc Siret <loic.siret@savoirfairelinux.com>
2017-08-14 11:31:16 -0400

    AndroidTV: Display Username
    Change-Id: I052796ce5cdf37a284ba5cddf1b8cbd15f6ef65b

---
+++ ring-android/app/src/main/java/cx/ring/tv/views/CustomTitleView.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package cx.ring.tv.views;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.v17.leanback.widget.TitleViewAdapter;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import cx.ring.R;
+
+/**
+ * Custom title view to be used in {@link android.support.v17.leanback.app.BrowseFragment}.
+ */
+public class CustomTitleView extends RelativeLayout implements TitleViewAdapter.Provider {
+    private final TextView mTitleView;
+    private final View mLogoView;
+    private final View mSearchOrbView;
+
+    private final TitleViewAdapter mTitleViewAdapter = new TitleViewAdapter() {
+        @Override
+        public View getSearchAffordanceView() {
+            return mSearchOrbView;
+        }
+
+        @Override
+        public void setTitle(CharSequence titleText) {
+            CustomTitleView.this.setTitle(titleText);
+        }
+
+        @Override
+        public void setBadgeDrawable(Drawable drawable) {
+            //NOOP
+        }
+
+        @Override
+        public void setOnSearchClickedListener(OnClickListener listener) {
+            mSearchOrbView.setOnClickListener(listener);
+        }
+
+        @Override
+        public void updateComponentsVisibility(int flags) {
+            int visibility = (flags & SEARCH_VIEW_VISIBLE) == SEARCH_VIEW_VISIBLE
+                    ? View.VISIBLE : View.INVISIBLE;
+            mSearchOrbView.setVisibility(visibility);
+        }
+
+    };
+
+    public CustomTitleView(Context context) {
+        this(context, null);
+    }
+
+    public CustomTitleView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public CustomTitleView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        View root = LayoutInflater.from(context).inflate(R.layout.tv_titleview, this);
+        mTitleView = (TextView) root.findViewById(R.id.title_text);
+        mLogoView = root.findViewById(R.id.title_badge);
+        mSearchOrbView = root.findViewById(R.id.title_orb);
+    }
+
+    public void setTitle(CharSequence title) {
+        if (title != null) {
+            mTitleView.setText(title);
+            mTitleView.setVisibility(View.VISIBLE);
+            mLogoView.setVisibility(View.VISIBLE);
+        }
+    }
+
+    @Override
+    public TitleViewAdapter getTitleViewAdapter() {
+        return mTitleViewAdapter;
+    }
+}
