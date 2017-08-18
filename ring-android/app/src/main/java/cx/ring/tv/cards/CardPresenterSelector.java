ae3a07f7c7303bd3aa26b89faa6760f83320c454
Alexandre Lision <alexandre.lision@savoirfairelinux.com>
2017-08-18 17:22:29 -0400

    AndroidTV: about section
    This patch adds a PresenterSelector to centralise the display of cards
    It also adds a new row for about things
    
    Change-Id: I777fbbb52265efdfcfa8ef2c903205c2b67e37a8

---
+++ ring-android/app/src/main/java/cx/ring/tv/cards/CardPresenterSelector.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ */
+
+package cx.ring.tv.cards;
+
+import android.content.Context;
+import android.support.v17.leanback.widget.Presenter;
+import android.support.v17.leanback.widget.PresenterSelector;
+
+import java.util.HashMap;
+
+import cx.ring.R;
+import cx.ring.tv.cards.about.AboutCardPresenter;
+import cx.ring.tv.cards.contacts.ContactCardPresenter;
+
+/**
+ * This PresenterSelector will decide what Presenter to use depending on a given card's type.
+ */
+public class CardPresenterSelector extends PresenterSelector {
+
+    private final Context mContext;
+    private final HashMap<Card.Type, Presenter> presenters = new HashMap<Card.Type, Presenter>();
+
+    public CardPresenterSelector(Context context) {
+        mContext = context;
+    }
+
+    @Override
+    public Presenter getPresenter(Object item) {
+        if (!(item instanceof Card)) throw new RuntimeException(
+                String.format("The PresenterSelector only supports data items of type '%s'",
+                        Card.class.getName()));
+        Card card = (Card) item;
+        Presenter presenter = presenters.get(card.getType());
+        if (presenter == null) {
+            switch (card.getType()) {
+                case ICON:
+                    presenter = new AboutCardPresenter(mContext);
+                    break;
+                case SEARCH_RESULT:
+                    presenter = new ContactCardPresenter(mContext,R.style.SearchtCardTheme);
+                    break;
+                default:
+                    presenter = new ContactCardPresenter(mContext, R.style.DefaultCardTheme);
+                    break;
+            }
+        }
+        presenters.put(card.getType(), presenter);
+        return presenter;
+    }
+
+}
