698804298fc756f20cfd5120b33e6c2cc645603a
Alexandre Lision <alexandre.lision@savoirfairelinux.com>
2017-08-18 17:22:29 -0400

    AndroidTV: about section
    This patch adds a PresenterSelector to centralise the display of cards
    It also adds a new row for about things
    
    Change-Id: I777fbbb52265efdfcfa8ef2c903205c2b67e37a8

---
+++ ring-android/app/src/main/java/cx/ring/tv/cards/CardRow.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2015 The Android Open Source Project
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License
+ *  is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ *  or implied. See the License for the specific language governing permissions and limitations under
+ *  the License.
+ */
+
+package cx.ring.tv.cards;
+
+import java.util.List;
+
+/**
+ * This class represents a row of cards. In a real world application you might want to store more
+ * data than in this example.
+ */
+public class CardRow {
+
+    // default is a list of cards
+    public static final int TYPE_DEFAULT = 0;
+    // section header
+    public static final int TYPE_SECTION_HEADER = 1;
+    // divider
+    public static final int TYPE_DIVIDER = 2;
+
+    private int mType = TYPE_DEFAULT;
+    // Used to determine whether the row shall use shadows when displaying its cards or not.
+    private boolean mShadow = true;
+    private String mTitle;
+    private List<Card> mCards;
+
+    public int getType() {
+        return mType;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public boolean useShadow() {
+        return mShadow;
+    }
+
+    public List<Card> getCards() {
+        return mCards;
+    }
+
+    public void setType(int pType) {
+        mType = pType;
+    }
+
+    public void setShadow(boolean pShadow) {
+        mShadow = pShadow;
+    }
+
+    public void setTitle(String pTitle) {
+        mTitle = pTitle;
+    }
+
+    public void setCards(List<Card> pCards) {
+        mCards = pCards;
+    }
+
+    public CardRow(int pType, boolean pShadow, String pTitle, List<Card> pCards) {
+        mType = pType;
+        mShadow = pShadow;
+        mTitle = pTitle;
+        mCards = pCards;
+    }
+}
---
+++ ring-android/app/src/main/java/cx/ring/tv/cards/contacts/ContactCard.java
@@ -0,0 +1,55 @@
+/*
+ *  Copyright (C) 2017 Savoir-faire Linux Inc.
+ *
+ *  Author: Loïc Siret <loic.siret@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+package cx.ring.tv.cards.contacts;
+
+import cx.ring.model.CallContact;
+import cx.ring.tv.cards.Card;
+
+public class ContactCard extends Card {
+    CallContact mCallContact = null;
+    private byte[] mPhoto = null;
+    public ContactCard(CallContact pCallContact, Type type) {
+        mCallContact = pCallContact;
+        setId(pCallContact.getId());
+        setTitle(pCallContact.getDisplayName());
+        setDescription(pCallContact.getRingUsername());
+        if (pCallContact.getPhoto() != null) {
+            mPhoto = pCallContact.getPhoto();
+        }
+        setType(type);
+    }
+
+    public ContactCard(CallContact pCallContact) {
+        this(pCallContact, Type.DEFAULT);
+    }
+
+    @Override
+    public Type getType() {
+        return Type.DEFAULT;
+    }
+
+    public CallContact getCallContact() {
+        return mCallContact;
+    }
+
+    public byte[] getPhoto() {
+        return mPhoto;
+    }
+}
