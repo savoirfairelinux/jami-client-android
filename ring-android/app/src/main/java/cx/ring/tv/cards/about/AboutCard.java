8c4ea6cc358e0f3b3e163f40bddc5fd2bae8c91c
Loïc Siret <loic.siret@savoirfairelinux.com>
2017-08-28 15:45:07 -0400

    AndroidTV: about section
    This patch adds a PresenterSelector to centralise the display of cards
    It also adds a new row for about things
    
    Change-Id: I777fbbb52265efdfcfa8ef2c903205c2b67e37a8

---
+++ ring-android/app/src/main/java/cx/ring/tv/cards/about/AboutCard.java
@@ -0,0 +1,37 @@
+/*
+ *  Copyright (C) 2017 Savoir-faire Linux Inc.
+ *
+ *  Author: Loïc Siret <loic.siret@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+package cx.ring.tv.cards.about;
+
+import cx.ring.tv.cards.Card;
+
+public class AboutCard extends Card {
+    @Override
+    public Type getType() {
+        return Type.VERSION;
+    }
+
+    public AboutCard(Type pType, String name, String description, String imageId){
+        setType(pType);
+        setTitle(name);
+        setDescription(description);
+        setLocalImageResource(imageId);
+    }
+
+}
