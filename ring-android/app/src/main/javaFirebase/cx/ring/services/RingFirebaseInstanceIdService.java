8d84652b98f78d345588aa5907bb5757c640dc3f
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2018-01-13 20:23:34 -0500

    push notifications: add Firebase support
    Change-Id: I578e8101600a9582c345fb5593eab3f4ac3de28a

---
+++ ring-android/app/src/main/javaFirebase/cx/ring/services/RingFirebaseInstanceIdService.java
@@ -0,0 +1,54 @@
+/*
+ *  Copyright (C) 2018 Savoir-faire Linux Inc.
+ *
+ *  Authors: Adrien Béraud <adrien.beraud@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+package cx.ring.services;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+import javax.inject.Inject;
+
+import cx.ring.application.RingApplication;
+import dagger.Lazy;
+
+public class RingFirebaseInstanceIdService extends FirebaseInstanceIdService {
+    private static final String TAG = RingFirebaseInstanceIdService.class.getSimpleName();
+
+    public RingFirebaseInstanceIdService() {
+        onTokenRefresh();
+    }
+
+    @Inject
+    protected AccountService mAccountService;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        ((RingApplication) getApplication()).getRingInjectionComponent().inject(this);
+    }
+
+    @Override
+    public void onTokenRefresh() {
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.w(TAG, "Refreshed token: " + refreshedToken);
+        mAccountService.setPushNotificationToken(FirebaseInstanceId.getInstance().getToken());
+    }
+
+}
