2802bfd535a7f1ad03889cdea27e80b5486d25f7
Pierre Duchemin <pierre.duchemin@savoirfairelinux.com>
2018-02-21 16:41:32 -0500

    update copyrights
    Change-Id: Ie0b07cb9e4263d8552a63e2a5751397c04f6ff0e

--- ring-android/app/src/main/java/cx/ring/service/DRingService.java
+++ ring-android/app/src/main/java/cx/ring/service/DRingService.java
@@ -1,27 +1,25 @@
 /*
-  Copyright (C) 2010-2012 Regis Montoya (aka r3gis - www.r3gis.fr)
-  Copyright (C) 2004-2017 Savoir-faire Linux Inc.
-  <p>
-  Author: Regis Montoya <r3gis.3R@gmail.com>
-  Author: Emeric Vigier <emeric.vigier@savoirfairelinux.com>
-  Alexandre Lision <alexandre.lision@savoirfairelinux.com>
-  Adrien Béraud <adrien.beraud@savoirfairelinux.com>
-  <p>
-  This program is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
-  If you own a pjsip commercial license you can also redistribute it
-  and/or modify it under the terms of the GNU Lesser General Public License
-  as an android library.
-  <p>
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-  <p>
-  You should have received a copy of the GNU General Public License
-  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * Copyright (C) 2010-2012 Regis Montoya (aka r3gis - www.r3gis.fr)
+ * Copyright (C) 2004-2018 Savoir-faire Linux Inc.
+ *
+ * Author: Regis Montoya <r3gis.3R@gmail.com>
+ * Author: Emeric Vigier <emeric.vigier@savoirfairelinux.com>
+ * Author: Alexandre Lision <alexandre.lision@savoirfairelinux.com>
+ * Author: Adrien Béraud <adrien.beraud@savoirfairelinux.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 package cx.ring.service;
 
