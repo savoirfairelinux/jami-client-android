1f9bfce97d73dafec0a8a24ee5da2a914e22c5eb
Pierre Duchemin <pierre.duchemin@savoirfairelinux.com>
2018-02-21 16:41:32 -0500

    update copyrights
    Change-Id: Ie0b07cb9e4263d8552a63e2a5751397c04f6ff0e

--- ring-android/app/src/main/java/cx/ring/account/ConfirmRevocationDialog.java
+++ ring-android/app/src/main/java/cx/ring/account/ConfirmRevocationDialog.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2017 Savoir-faire Linux Inc.
+ *  Copyright (C) 2004-2018 Savoir-faire Linux Inc.
  *
  *  Author: Alexandre Lision <alexandre.lision@savoirfairelinux.com>
  *          Adrien Beraud <adrien.beraud@savoirfairelinux.com>
