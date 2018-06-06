febfefa3fafa162542e3667dadb37ce30dc19a80
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2018-06-05 23:06:50 -0400

    accountservice: add getLastMessages()
    Change-Id: I4d62d167fe079a1b1791e24c1fd880275a60dda5

--- ring-android/libringclient/src/main/java/cx/ring/utils/SwigNativeConverter.java
+++ ring-android/libringclient/src/main/java/cx/ring/utils/SwigNativeConverter.java
@@ -24,13 +24,15 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import cx.ring.daemon.Message;
+import cx.ring.daemon.MessageVect;
 import cx.ring.daemon.StringMap;
 import cx.ring.daemon.StringVect;
 import cx.ring.daemon.VectMap;
 
 public class SwigNativeConverter {
 
-    public static VectMap convertFromNativeToSwig(List creds) {
+    public static VectMap toSwig(List creds) {
         ArrayList<HashMap<String, String>> todecode = (ArrayList<HashMap<String, String>>) creds;
         VectMap toReturn = new VectMap();
 
@@ -40,9 +42,18 @@ public class SwigNativeConverter {
         return toReturn;
     }
 
-    public static ArrayList<String> convertSwigToNative(StringVect vector) {
+    public static ArrayList<String> toJava(StringVect vector) {
         ArrayList<String> toReturn = new ArrayList<>();
         toReturn.addAll(vector);
         return toReturn;
     }
+
+    public static ArrayList<Message> toJava(MessageVect vector) {
+        int size = (int)vector.size();
+        ArrayList<Message> toReturn = new ArrayList<>(size);
+        for (int i = 0; i < size; i++)
+            toReturn.add(vector.get(i));
+        return toReturn;
+    }
+
 }
