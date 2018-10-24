edfe309f879af8449b23f33116fbd6b61827eacf
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2018-10-24 13:42:53 -0400

    connectivity: trigger connectivity changed less frequently
    Change-Id: I220ef52e6b9ca07e3ad8c646882a6f49b8eafd08

--- ring-android/app/src/main/java/cx/ring/service/DRingService.java
+++ ring-android/app/src/main/java/cx/ring/service/DRingService.java
@@ -517,15 +517,7 @@ public class DRingService extends Service {
     private final Runnable mConnectivityChecker = new Runnable() {
         @Override
         public void run() {
-            mAccountService.setAccountsActive(mPreferencesService.hasNetworkConnected());
-
-            for (Account account : mAccountService.getAccounts()) {
-                if (account.isActive() && !account.isRegistered()) {
-                    // Account is active, but still not connected. Execute connectivityChanged
-                    mHardwareService.connectivityChanged();
-                    break;
-                }
-            }
+            updateConnectivityState();
         }
     };
 
@@ -642,7 +634,14 @@ public class DRingService extends Service {
     private void updateConnectivityState() {
         if (mDaemonService.isStarted()) {
             mAccountService.setAccountsActive(mPreferencesService.hasNetworkConnected());
-            mHardwareService.connectivityChanged();
+
+            for (Account account : mAccountService.getAccounts()) {
+                if (account.isActive() && !account.isRegistered()) {
+                    // Account is active, but still not connected. Execute connectivityChanged
+                    mHardwareService.connectivityChanged();
+                    break;
+                }
+            }
         }
     }
 
