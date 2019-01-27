47f9ac2bc5f55ed6881ff9ed704be07d8f224382
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2019-01-27 01:50:12 -0500

    (DNM) video: increase resolution
    Change-Id: Iacea33a036f0f64cb524c596e8498a21490c97b4

--- ring-android/app/src/main/java/cx/ring/services/HardwareServiceImpl.java
+++ ring-android/app/src/main/java/cx/ring/services/HardwareServiceImpl.java
@@ -351,7 +351,7 @@ public class HardwareServiceImpl extends HardwareService implements AudioManager
         final boolean useLargerSize = Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && Build.SUPPORTED_64_BIT_ABIS.length > 0;
         final boolean useHD = DeviceUtils.isTv(mContext) || mPreferenceService.getSettings().isHD();
         //int MIN_WIDTH = useLargerSize ? (useHD ? VIDEO_WIDTH_HD : VIDEO_WIDTH) : VIDEO_WIDTH_MIN;
-        final Point minVideoSize = useLargerSize ? (useHD ? VIDEO_SIZE_HD : VIDEO_SIZE_DEFAULT) : VIDEO_SIZE_LOW;
+        final Point minVideoSize = useLargerSize ? (useHD ? VIDEO_SIZE_FULL_HD : VIDEO_SIZE_HD) : VIDEO_SIZE_LOW;
 
         cameraService.getCameraInfo(camId, formats, sizes, rates, minVideoSize);
     }
