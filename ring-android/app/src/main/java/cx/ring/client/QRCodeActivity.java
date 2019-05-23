4cc8c592431cb635ec70d097f21dc2b0e2e6cede
Rayan Osseiran <rayan.osseiran@savoirfairelinux.com>
2019-05-23 10:58:25 -0400

    qr code: group scanning and sharing into one page
    The QR scanning page is now grouped with the share page via a tabbed layout.
    
    Change-Id: I2e4ed3415d86777a780a7137d97f16e9d971834f
    Gitlab: #575

---
+++ ring-android/app/src/main/java/cx/ring/client/QRCodeActivity.java
@@ -0,0 +1,118 @@
+/*
+ *  Copyright (C) 2004-2019 Savoir-faire Linux Inc.
+ *
+ *  Authors: Adrien Béraud <adrien.beraud@savoirfairelinux.com>
+ *           Rayan Osseiran <rayan.osseiran@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+package cx.ring.client;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.tabs.TabLayout;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
+
+import cx.ring.R;
+import cx.ring.fragments.ConversationFragment;
+import cx.ring.share.ScanFragment;
+import cx.ring.share.ShareFragment;
+
+public class QRCodeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qrcode);
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
+
+        Toolbar toolbar = findViewById(R.id.qrToolbar);
+
+        setSupportActionBar(toolbar);
+        ActionBar supportActionBar = getSupportActionBar();
+
+
+        if (supportActionBar != null) {
+            supportActionBar.setDisplayHomeAsUpEnabled(true);
+            supportActionBar.setDisplayShowHomeEnabled(true);
+
+        }
+
+        toolbar.setNavigationOnClickListener((View v) ->
+                finish());
+    }
+
+
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        @StringRes
+        private final int[] TAB_TITLES = new int[]{R.string.tab_code, R.string.tab_scan};
+        private final Context mContext;
+
+        public SectionsPagerAdapter(Context context, FragmentManager fm) {
+            super(fm);
+            mContext = context;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return new ShareFragment();
+                case 1:
+                    return new ScanFragment();
+                default:
+                    return null;
+            }
+
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mContext.getResources().getString(TAB_TITLES[position]);
+        }
+
+        @Override
+        public int getCount() {
+            // Show 2 total pages.
+            return 2;
+        }
+    }
+
+    public void startConversationTablet(Bundle bundle) {
+        ConversationFragment fConversation = new ConversationFragment();
+        fConversation.setArguments(bundle);
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.conversation_container, fConversation, ConversationFragment.class.getName())
+                .commit();
+    }
+}
