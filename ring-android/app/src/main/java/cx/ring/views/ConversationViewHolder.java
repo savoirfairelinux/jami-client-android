9eab542f2bfd85e1548f48b59ea57e90d183dd29
Rayan Osseiran <rayan.osseiran@savoirfairelinux.com>
2019-05-29 13:44:55 -0400

    messaging: delete and cancel messages
    Allows the ability to delete message and call history. Implements cancel messages but not functional.
    
    Change-Id: Ic81fd4534868d1ddad186ffd8ba5e93dcea0c934
    Gitlab: #576

--- ring-android/app/src/main/java/cx/ring/views/ConversationViewHolder.java
+++ ring-android/app/src/main/java/cx/ring/views/ConversationViewHolder.java
@@ -28,7 +28,9 @@ import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import cx.ring.R;
@@ -52,6 +54,7 @@ public class ConversationViewHolder extends RecyclerView.ViewHolder {
     public Surface surface = null;
     public long mCid = -1;
     public UiUpdater updater;
+    public LinearLayout mCallInfoLayout;
 
     public ConversationViewHolder(ViewGroup v, ConversationAdapter.MessageType type) {
         super(v);
@@ -67,6 +70,8 @@ public class ConversationViewHolder extends RecyclerView.ViewHolder {
             mHistTxt = v.findViewById(R.id.call_hist_txt);
             mHistDetailTxt = v.findViewById(R.id.call_details_txt);
             mPhoto = v.findViewById(R.id.call_icon);
+            mCallInfoLayout = v.findViewById(R.id.callInfoLayout);
+
         } else if (type == ConversationAdapter.MessageType.FILE_TRANSFER) {
             mMsgTxt = v.findViewById(R.id.call_hist_filename);
             mMsgDetailTxt = v.findViewById(R.id.file_details_txt);
