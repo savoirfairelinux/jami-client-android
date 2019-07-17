83b5618455aeecde0f51dfe14863881cd13a37e4
Rayan Osseiran <rayan.osseiran@savoirfairelinux.com>
2019-07-17 16:40:50 -0400

    calls: fix/enable bluetooth support
    > Cleans up audio routing system
    > Restarting audio layer prevents "no audio to play" error from daemon
    > Fixes random hangup during bluetooth calls
    
    Change-Id: I71067e68d2a5ff2befc8be7dceefc21793d98120

--- ring-android/libringclient/src/main/java/cx/ring/call/CallPresenter.java
+++ ring-android/libringclient/src/main/java/cx/ring/call/CallPresenter.java
@@ -121,7 +121,7 @@ public class CallPresenter extends RootPresenter<CallView> {
         mCompositeDisposable.add(mHardwareService
                 .getBluetoothEvents()
                 .subscribe(event -> {
-                    if (!event.connected && mSipCall != null) {
+                    if (!event.connected && mSipCall == null) {
                         hangupCall();
                     }
                 }));
@@ -209,6 +209,7 @@ public class CallPresenter extends RootPresenter<CallView> {
     }
 
     public void speakerClick(boolean checked) {
+        mCallService.restartAudioLayer();
         mHardwareService.toggleSpeakerphone(checked);
     }
 
@@ -306,6 +307,7 @@ public class CallPresenter extends RootPresenter<CallView> {
             timeUpdateTask = null;
         }
         mSipCall = null;
+        mHardwareService.closeAudioState();
         CallView view = getView();
         if (view != null)
             view.finish();
@@ -328,7 +330,7 @@ public class CallPresenter extends RootPresenter<CallView> {
         view.updateMenu();
         if (call.isOnGoing()) {
             mOnGoingCall = true;
-            mHardwareService.toggleSpeakerphone(!mAudioOnly);
+            mCallService.restartAudioLayer();
             view.initNormalStateDisplay(mAudioOnly, mHardwareService.isSpeakerPhoneOn(), isMicrophoneMuted());
             view.updateMenu();
             if (!mAudioOnly) {
