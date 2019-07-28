34a6fa264c395a734cced8cef9373662290e8ef0
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2019-07-28 16:02:59 -0400

    audio: add rx subject for audio state
    Change-Id: Ia148c724fdeb1921acfe888c7e6aa3054d3667fb

--- ring-android/app/src/main/java/cx/ring/services/HardwareServiceImpl.java
+++ ring-android/app/src/main/java/cx/ring/services/HardwareServiceImpl.java
@@ -293,6 +293,7 @@ public class HardwareServiceImpl extends HardwareService implements AudioManager
         mAudioManager.setSpeakerphoneOn(false);
         mBluetoothWrapper.setBluetoothOn(true);
         mAudioManager.setMode(oldMode);
+        audioStateSubject.onNext(new AudioState(AudioOutput.BLUETOOTH, mBluetoothWrapper.getDeviceName()));
     }
 
     /**
@@ -307,6 +308,7 @@ public class HardwareServiceImpl extends HardwareService implements AudioManager
             mAudioManager.setMode(oldMode);
         }
         mAudioManager.setSpeakerphoneOn(true);
+        audioStateSubject.onNext(new AudioState(AudioOutput.SPEAKERS));
     }
 
     /**
@@ -316,6 +318,7 @@ public class HardwareServiceImpl extends HardwareService implements AudioManager
         if (mBluetoothWrapper != null)
             mBluetoothWrapper.setBluetoothOn(false);
         mAudioManager.setSpeakerphoneOn(false);
+        audioStateSubject.onNext(new AudioState(AudioOutput.INTERNAL));
     }
 
     @Override
