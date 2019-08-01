e491d1150e4da5410bb702ba5240eea76675e8f5
Rayan Osseiran <rayan.osseiran@savoirfairelinux.com>
2019-08-01 14:43:02 -0400

    database: (wip) migration of android client to new schema
    - Combined previous patches as they will now be merged together
    - Includes account splitting
    - Migration is functional
    
    Change-Id: Ic830eecd9f9d194bcf122d5845709ff5dcefeb0b

---
+++ ring-android/libringclient/src/main/java/cx/ring/model/ConversationHistory.java
@@ -0,0 +1,62 @@
+package cx.ring.model;
+
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+@DatabaseTable(tableName = ConversationHistory.TABLE_NAME)
+public class ConversationHistory {
+
+    public static final String TABLE_NAME = "conversations";
+    public static final String COLUMN_CONVERSATION_ID = "id";
+    public static final String COLUMN_PARTICIPANT = "participant";
+    public static final String COLUMN_EXTRA_DATA = "extra_data";
+
+    @DatabaseField(generatedId = true , columnName = COLUMN_CONVERSATION_ID, canBeNull = false)
+    Integer mId;
+    @DatabaseField(columnName = COLUMN_PARTICIPANT, index = true)
+    String mParticipant;
+    @DatabaseField(columnName = COLUMN_EXTRA_DATA)
+    String mExtraData;
+
+    String account;
+
+
+    /* Needed by ORMLite */
+    public ConversationHistory() {
+    }
+
+    public ConversationHistory(Conversation conversation) {
+        mId = conversation.getId();
+        mParticipant = conversation.getParticipant();
+    }
+
+    public ConversationHistory(Integer id, String participant) {
+        mId = id;
+        mParticipant = participant;
+    }
+
+    public ConversationHistory(String participant) {
+        mParticipant = participant;
+    }
+
+    public Integer getId() {
+        return mId;
+    }
+
+    public void setId(Integer id) {
+        mId = id;
+    }
+
+    public String getParticipant() {
+        return mParticipant;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+}
--- ring-android/libringclient/src/main/java/cx/ring/model/SipCall.java
+++ ring-android/libringclient/src/main/java/cx/ring/model/SipCall.java
@@ -1,36 +1,18 @@
-/*
- *  Copyright (C) 2004-2019 Savoir-faire Linux Inc.
- *
- *  Author: Alexandre Lision <alexandre.lision@savoirfairelinux.com>
- *          Alexandre Savard <alexandre.savard@gmail.com>
- *          Adrien Béraud <adrien.beraud@savoirfairelinux.com>
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 package cx.ring.model;
 
+
+import com.google.gson.JsonObject;
+
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
-import cx.ring.daemon.StringMap;
-import cx.ring.daemon.StringVect;
 import cx.ring.utils.ProfileChunk;
 import cx.ring.utils.VCardUtils;
 import ezvcard.Ezvcard;
 import ezvcard.VCard;
 
-public class SipCall {
+public class SipCall extends Interaction {
 
     public final static String KEY_ACCOUNT_ID = "ACCOUNTID";
     public final static String KEY_AUDIO_ONLY = "AUDIO_ONLY";
@@ -42,65 +24,99 @@ public class SipCall {
     public final static String KEY_VIDEO_MUTED = "VIDEO_MUTED";
     public final static String KEY_AUDIO_CODEC = "AUDIO_CODEC";
     public final static String KEY_VIDEO_CODEC = "VIDEO_CODEC";
+    public final static String KEY_DURATION = "duration";
 
-    private final String mCallID;
-    private final String mAccount;
-    private CallContact mContact = null;
-    private Uri mNumber = null;
     private boolean isPeerHolding = false;
     private boolean isAudioMuted = false;
     private boolean isVideoMuted = false;
     private boolean isRecording = false;
     private boolean isAudioOnly = false;
-    private long timestampStart = 0;
+
+
+    private CallStatus mCallStatus = CallStatus.NONE;
+
     private long timestampEnd = 0;
     private boolean missed = true;
     private String mAudioCodec;
     private String mVideoCodec;
-
-    private int mCallType;
-    private State mCallState = State.NONE;
+    private String mContactNumber;
 
     private ProfileChunk mProfileChunk = null;
 
-
-    public SipCall(String id, String account, Uri number, int direction) {
-        mCallID = id;
+    public SipCall(String daemonId, String author, String account, ConversationHistory conversation, CallContact contact, int direction) {
+        mDaemonId = daemonId == null ? null : Long.parseLong(daemonId);
+        mAuthor = direction == Direction.INCOMING ? author : null;
         mAccount = account;
-        mNumber = number;
-        mCallType = direction;
+        mConversation = conversation;
+        mIsIncoming = direction == Direction.INCOMING;
+        mTimestamp = System.currentTimeMillis();
+        mType = InteractionType.CALL.toString();
+        mContact = contact;
+        mIsRead = 1;
     }
 
-    public SipCall(String id, String account, String number, int direction) {
-        this(id, account, new Uri(number), direction);
+    public SipCall(Interaction interaction) {
+        mId = interaction.getId();
+        mAuthor = interaction.getAuthor();
+        mConversation = interaction.getConversation();
+        mIsIncoming = mAuthor != null;
+        mTimestamp = interaction.getTimestamp();
+        mType = InteractionType.CALL.toString();
+        mDaemonId = interaction.getDaemonId();
+        mIsRead = interaction.isRead() ? 1 : 0;
+        mAccount = interaction.getAccount();
+        mExtraFlag = fromJson(interaction.getExtraFlag());
+        missed = getDuration() == 0;
+        mIsRead = 1;
+        mContact = interaction.getContact();
     }
 
-    /**
-     * *********************
-     * Constructors
-     * *********************
-     */
+    public SipCall(String daemonId, String account, String contactNumber, int direction) {
+        mDaemonId = daemonId == null ? null : Long.parseLong(daemonId);
+        mIsIncoming = direction == Direction.INCOMING;
+        mAccount = account;
+        mAuthor = direction == Direction.INCOMING ? contactNumber : null;
+        mContactNumber = contactNumber;
+        mTimestamp = System.currentTimeMillis();
+        mType = InteractionType.CALL.toString();
+        mIsRead = 1;
+    }
 
-    public SipCall(String callId, Map<String, String> call_details) {
-        this(callId,
-                call_details.get(KEY_ACCOUNT_ID),
-                call_details.get(KEY_PEER_NUMBER),
-                Integer.parseInt(call_details.get(KEY_CALL_TYPE)));
-        mCallState = stateFromString(call_details.get(KEY_CALL_STATE));
+    public SipCall(String daemonId, Map<String, String> call_details) {
+        this(daemonId, call_details.get(KEY_ACCOUNT_ID), call_details.get(KEY_PEER_NUMBER), Integer.parseInt(call_details.get(KEY_CALL_TYPE)));
+        setCallState(CallStatus.fromString(call_details.get(KEY_CALL_STATE)));
         setDetails(call_details);
     }
 
-    public String getRecordPath() {
-        return "";
-    }
-
-    public int getCallType() {
-        return mCallType;
-    }
-
-
-    public State getCallState() {
-        return mCallState;
+    public static CallStatus stateFromString(String state) {
+        switch (state) {
+            case "SEARCHING":
+                return CallStatus.SEARCHING;
+            case "CONNECTING":
+                return CallStatus.CONNECTING;
+            case "INCOMING":
+            case "RINGING":
+                return CallStatus.RINGING;
+            case "CURRENT":
+                return CallStatus.CURRENT;
+            case "HUNGUP":
+                return CallStatus.HUNGUP;
+            case "BUSY":
+                return CallStatus.BUSY;
+            case "FAILURE":
+                return CallStatus.FAILURE;
+            case "HOLD":
+                return CallStatus.HOLD;
+            case "UNHOLD":
+                return CallStatus.UNHOLD;
+            case "INACTIVE":
+                return CallStatus.INACTIVE;
+            case "OVER":
+                return CallStatus.OVER;
+            case "NONE":
+            default:
+                return CallStatus.NONE;
+        }
     }
 
     public void setDetails(Map<String, String> details) {
@@ -112,9 +128,51 @@ public class SipCall {
         mVideoCodec = details.get(KEY_VIDEO_CODEC);
     }
 
-    public long getDuration() {
-        return isMissed() ? 0 : timestampEnd - timestampStart;
+    public String getContactNumber() {
+        return mContactNumber;
     }
+
+    public Long getDuration() {
+        return toJson(mExtraFlag).get(KEY_DURATION) == null ? 0 : toJson(mExtraFlag).get(KEY_DURATION).getAsLong();
+    }
+
+    public void setDuration(Long value) {
+        JsonObject jsonObject = getExtraFlag();
+        jsonObject.addProperty(KEY_DURATION, value);
+        mExtraFlag = fromJson(jsonObject);
+    }
+
+    public String getDurationString() {
+        Long mDuration = getDuration() / 1000;
+        if (mDuration < 60) {
+            return String.format(Locale.getDefault(), "%02d secs", mDuration);
+        }
+
+        if (mDuration < 3600) {
+            return String.format(Locale.getDefault(), "%02d mins %02d secs", (mDuration % 3600) / 60, (mDuration % 60));
+        }
+
+        return String.format(Locale.getDefault(), "%d h %02d mins %02d secs", mDuration / 3600, (mDuration % 3600) / 60, (mDuration % 60));
+    }
+
+    public long getTimestampEnd() {
+        return timestampEnd;
+    }
+
+    public void setTimestampEnd(long timestampEnd) {
+        this.timestampEnd = timestampEnd;
+        if (timestampEnd != 0 && !isMissed())
+            setDuration(timestampEnd - mTimestamp);
+    }
+
+    public boolean isMissed() {
+        return missed;
+    }
+
+    public boolean isAudioOnly() {
+        return isAudioOnly;
+    }
+
 
     public void muteVideo(boolean mute) {
         isVideoMuted = mute;
@@ -127,150 +185,37 @@ public class SipCall {
     public String getAudioCodec() {
         return mAudioCodec;
     }
-    public interface Direction {
-        int INCOMING = 0;
-        int OUTGOING = 1;
-    }
 
-    public enum State {
-        NONE(0),
-        SEARCHING(1),
-        CONNECTING(2),
-        RINGING(3),
-        CURRENT(4),
-        HUNGUP(5),
-        BUSY(6),
-        FAILURE(7),
-        HOLD(8),
-        UNHOLD(9),
-        INACTIVE(10),
-        OVER(11);
-
-        private final int value;
-        State(int value){
-            this.value = value;
-        }
-    }
-
-    public String getCallId() {
-        return mCallID;
-    }
-
-    public long getTimestampStart() {
-        return timestampStart;
-    }
-
-    public void setTimestampStart(long timestampStart) {
-        this.timestampStart = timestampStart;
-    }
-
-    public long getTimestampEnd() {
-        return timestampEnd;
-    }
-
-    public void setTimestampEnd(long timestampEnd) {
-        this.timestampEnd = timestampEnd;
-    }
-
-    public String getAccount() {
-        return mAccount;
-    }
-
-    public void setCallState(State callState) {
-        mCallState = callState;
-        if (mCallState == State.CURRENT)
+    public void setCallState(CallStatus callStatus) {
+        mCallStatus = callStatus;
+        if (callStatus == CallStatus.CURRENT) {
             missed = false;
-    }
-
-    public boolean isMissed() {
-        return missed;
-    }
-
-    public void setContact(CallContact c) {
-        mContact = c;
-    }
-
-    public CallContact getContact() {
-        return mContact;
-    }
-
-    public void setNumber(String n) {
-        mNumber = new Uri(n);
-    }
-
-    public void setNumber(Uri n) {
-        mNumber = n;
-    }
-
-    public boolean isAudioOnly() {
-        return isAudioOnly;
-    }
-
-    public String getNumber() {
-        return mNumber.getUriString();
-    }
-
-    public Uri getNumberUri() {
-        return mNumber;
-    }
-
-    public static State stateFromString(String state) {
-        switch (state) {
-            case "SEARCHING":
-                return State.SEARCHING;
-            case "CONNECTING":
-                return State.CONNECTING;
-            case "INCOMING":
-            case "RINGING":
-                return State.RINGING;
-            case "CURRENT":
-                return State.CURRENT;
-            case "HUNGUP":
-                return State.HUNGUP;
-            case "BUSY":
-                return State.BUSY;
-            case "FAILURE":
-                return State.FAILURE;
-            case "HOLD":
-                return State.HOLD;
-            case "UNHOLD":
-                return State.UNHOLD;
-            case "INACTIVE":
-                return State.INACTIVE;
-            case "OVER":
-                return State.OVER;
-            case "NONE":
-            default:
-                return State.NONE;
+            mStatus = InteractionStatus.SUCCEEDED.toString();
+        } else if (isRinging() || isOnGoing()) {
+            mStatus = InteractionStatus.SUCCEEDED.toString();
+        } else if (mCallStatus == CallStatus.FAILURE) {
+            mStatus = InteractionStatus.FAILED.toString();
         }
     }
 
-    /**
-     * Compare sip calls based on call ID
-     */
-    @Override
-    public boolean equals(Object c) {
-        return c instanceof SipCall && ((SipCall) c).mCallID.contentEquals((mCallID));
+    public CallStatus getCallStatus() {
+        return mCallStatus;
     }
 
-    public boolean isOutGoing() {
-        return mCallType == Direction.OUTGOING;
+    public void setTimestamp(long timestamp) {
+        mTimestamp = timestamp;
     }
 
     public boolean isRinging() {
-        return mCallState == State.CONNECTING || mCallState == State.RINGING || mCallState == State.NONE || mCallState == State.SEARCHING;
-    }
-
-    public boolean isIncoming() {
-        return mCallType == Direction.INCOMING;
+        return mCallStatus == CallStatus.CONNECTING || mCallStatus == CallStatus.RINGING || mCallStatus == CallStatus.NONE || mCallStatus == CallStatus.SEARCHING;
     }
 
     public boolean isOnGoing() {
-        return mCallState == State.CURRENT || mCallState == State.HOLD || mCallState == State.UNHOLD;
+        return mCallStatus == CallStatus.CURRENT || mCallStatus == CallStatus.HOLD || mCallStatus == CallStatus.UNHOLD;
     }
 
-    public boolean isCurrent() {
-        return mCallState == State.CURRENT;
+    public void setIsIncoming(int direction) {
+        mIsIncoming = (direction == Direction.INCOMING);
     }
 
     public VCard appendToVCard(Map<String, String> messages) {
@@ -294,5 +239,35 @@ public class SipCall {
         }
         return null;
     }
+
+    public enum CallStatus {
+        NONE,
+        SEARCHING,
+        CONNECTING,
+        RINGING,
+        CURRENT,
+        HUNGUP,
+        BUSY,
+        FAILURE,
+        HOLD,
+        UNHOLD,
+        INACTIVE,
+        OVER;
+
+        static CallStatus fromString(String str) {
+            for (CallStatus status : values()) {
+                if (status.name().equals(str)) {
+                    return status;
+                }
+            }
+            return NONE;
+        }
+    }
+
+    public interface Direction {
+        int INCOMING = 0;
+        int OUTGOING = 1;
+    }
+
 
 }
