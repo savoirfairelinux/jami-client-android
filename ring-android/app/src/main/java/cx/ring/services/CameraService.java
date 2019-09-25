4dcb511d4bd59361d66bbb799c4c3e080bc4af53
ayounes <amine.younes-bouacida@savoirfairelinux.com>
2019-09-25 13:22:06 -0400

    call: add a button to load/unload a plugin
    + Add buttons and the appropriate RingService calls to load/unload a plugin (portrait and land modes)
    + Use a shared stl libc++_shared.so, this way jami and the plugins use the same stl
    
    Change-Id: Ia0857c9a6059f911b8bc28b7a345e1924864741a

--- ring-android/app/src/main/java/cx/ring/services/CameraService.java
+++ ring-android/app/src/main/java/cx/ring/services/CameraService.java
@@ -736,8 +736,10 @@ public class CameraService {
                 tmpReader = ImageReader.newInstance(videoParams.width, videoParams.height, ImageFormat.YUV_420_888, 8);
                 tmpReader.setOnImageAvailableListener(r -> {
                     Image image = r.acquireLatestImage();
-                    if (image != null)
+                    if (image != null) {
                         RingserviceJNI.captureVideoFrame(image, videoParams.rotation);
+                        image.close();
+                    }
                 }, handler);
                 targets.add(tmpReader.getSurface());
             }
