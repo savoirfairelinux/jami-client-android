4a4f27d4956c79efbc3bc9309a329ea080a69f65
amine younes <amine.younes-bouacida@savoirfairelinux.com>
2019-10-25 11:04:54 -0400

    feat(Settings): display the list of available plugins + Search for plugins in the plugins folder in internal storage + Displays the list of plugins in a RecyclerView
    Change-Id: I5093f5f7c261a08640081d806fa2f62b22372da6

--- ring-android/app/src/main/java/cx/ring/settings/PluginsListSettingsFragment.java
+++ ring-android/app/src/main/java/cx/ring/settings/PluginsListSettingsFragment.java
@@ -5,50 +5,66 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import cx.ring.R;
 import cx.ring.utils.Log;
-
-import static cx.ring.utils.AndroidFileUtils.assetTree;
 
 public class PluginsListSettingsFragment extends Fragment {
 
     public static final String TAG = PluginsListSettingsFragment.class.getSimpleName();
     private Context mContext;
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         mContext = requireActivity();
         View pluginsSettingsList = inflater.inflate(R.layout.frag_plugins_list_settings, container, false);
-        TextView psl = pluginsSettingsList.findViewById(R.id.plugins_list);
-        listPlugins(psl);
-        assetTree(mContext.getAssets(),"","plugins",0);
+        recyclerView = (RecyclerView) pluginsSettingsList.findViewById(R.id.plugins_list);
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(mContext);
+        recyclerView.setLayoutManager(layoutManager);
+
+        // specify an adapter (see also next example)
+
+        mAdapter = new PluginsListAdapter(listPlugins());
+        recyclerView.setAdapter(mAdapter);
+
         return pluginsSettingsList;
     }
 
 
-    private void listPlugins(TextView display){
+    private List<String> listPlugins(){
+        List<String> pluginsList = new ArrayList<>();
         File pluginsDir = new File(mContext.getFilesDir(),"plugins");
         Log.i(TAG, "Plugins dir: " + pluginsDir.getAbsolutePath());
         if(pluginsDir.isDirectory()) {
             File[] pluginsFolders = pluginsDir.listFiles();
             if(pluginsFolders != null) {
                 for(File pluginFolder : pluginsFolders) {
-                    Log.i(TAG, "    Plugin: "+ pluginFolder.getName());
                     if(pluginFolder.isDirectory()){
-                        display.append("    Plugin: "+ pluginFolder.getName() + " ABS PATH: " +
-                                pluginFolder.getAbsolutePath());
+                        pluginsList.add(pluginFolder.getName());
                     }
                 }
             }
         }
+        return pluginsList;
     }
 }
