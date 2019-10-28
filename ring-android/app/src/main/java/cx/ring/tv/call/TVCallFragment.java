e35ee13793e250a85bc03914f437922b736ca8d8
amine younes <amine.younes-bouacida@savoirfairelinux.com>
2019-10-28 12:09:53 -0400

    videoCall: add UI to interact with video plugins
    + Add a menu item to show available plugins
    + Add a buttons carousel for video plugins
    
    Change-Id: Iac01e3014bea595a7ded2dcea20b27b2cba36e8b

--- ring-android/app/src/main/java/cx/ring/tv/call/TVCallFragment.java
+++ ring-android/app/src/main/java/cx/ring/tv/call/TVCallFragment.java
@@ -467,7 +467,8 @@ public class TVCallFragment extends BaseSupportFragment<CallPresenter> implement
     }
 
     @Override
-    public void initMenu(boolean isSpeakerOn, boolean displayFlip, boolean canDial, boolean onGoingCall) {
+    public void initMenu(boolean isSpeakerOn, boolean displayFlip, boolean canDial,
+                         boolean showPluginBtn, boolean onGoingCall) {
 
     }
 
