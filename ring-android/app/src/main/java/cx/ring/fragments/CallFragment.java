8108747ce7b12bd23b6581249923ef0fb278b0c9
ayounes <amine.younes-bouacida@savoirfairelinux.com>
2019-11-08 15:26:05 -0500

    plugins: use toggle instead of load/unload
    Change-Id: Ida781ad32bd4f348f8613ad0aedd0547360d611a

--- ring-android/app/src/main/java/cx/ring/fragments/CallFragment.java
+++ ring-android/app/src/main/java/cx/ring/fragments/CallFragment.java
@@ -81,8 +81,10 @@ import androidx.fragment.app.FragmentActivity;
 import com.rodolfonavalon.shaperipplelibrary.model.Circle;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 import java.util.Random;
 
 import javax.inject.Inject;
@@ -157,8 +159,9 @@ public class CallFragment extends BaseSupportFragment<CallPresenter> implements 
 
     private ConfParticipantAdapter confAdapter = null;
     private boolean mConferenceMode = false;
-    
+    private boolean pluginsModeFirst = true;
     private List<PluginDetails> videoPluginsDetails;
+    private Map<String, Boolean> videoPluginsStatus;
     private int previousPluginPosition = -1;
     private RecyclerPicker rp;
     private boolean pluginsMode = false;
@@ -1131,6 +1134,8 @@ public class CallFragment extends BaseSupportFragment<CallPresenter> implements 
         presenter.unloadPlugin(path);
     }
 
+    public void togglePlugin(String path, boolean toggle) { presenter.togglePlugin(path, toggle);}
+
     @Override
     public void positiveMediaButtonClicked() {
         presenter.positiveButtonClicked();
@@ -1166,13 +1171,18 @@ public class CallFragment extends BaseSupportFragment<CallPresenter> implements 
 
     public void displayVideoPluginsCarousel() {
         pluginsMode  = !pluginsMode;
+        Context context = requireActivity();
+
+        // Create videoPluginsDetails and videoPluginsStatus in a lazy manner
+        if(pluginsModeFirst) {
+            videoPluginsDetails = PluginUtils.listPlugins(context);
+            videoPluginsStatus = new HashMap<>();
+            pluginsModeFirst = false;
+        }
 
         if(pluginsMode) {
-            Context context = requireActivity();
             binding.recyclerPicker.setVisibility(View.VISIBLE);
             displayHangupButton(false);
-
-            videoPluginsDetails = PluginUtils.listPlugins(context);
             List<Drawable> videoPluginsItems = new ArrayList<>();
 
             // Search for plugin icons
@@ -1189,13 +1199,15 @@ public class CallFragment extends BaseSupportFragment<CallPresenter> implements 
 
         } else {
             if(previousPluginPosition != -1) {
-                PluginDetails previouspluginDetails = videoPluginsDetails.get(previousPluginPosition);
-                unloadPlugin(previouspluginDetails.getRootPath()+"/lib"+previouspluginDetails.getName()+".so");
+                PluginDetails previouspluginDetails = videoPluginsDetails.
+                        get(previousPluginPosition);
+
+                togglePlugin(previouspluginDetails.getRootPath()+"/lib"+
+                        previouspluginDetails.getName()+".so", false);
             }
             binding.recyclerPicker.setVisibility(View.GONE);
             displayHangupButton(true);
         }
-
     }
 
     @Override
@@ -1207,12 +1219,20 @@ public class CallFragment extends BaseSupportFragment<CallPresenter> implements 
              */
             if(previousPluginPosition != -1) {
                 PluginDetails previouspluginDetails = videoPluginsDetails.get(previousPluginPosition);
-                unloadPlugin(previouspluginDetails.getRootPath() + "/lib" + previouspluginDetails.getName() + ".so");
+                togglePlugin(previouspluginDetails.getRootPath() + "/lib" + previouspluginDetails.getName() + ".so", false);
             }
 
             previousPluginPosition = position;
             PluginDetails pluginDetails = videoPluginsDetails.get(position);
-            loadPlugin(pluginDetails.getRootPath()+"/lib"+pluginDetails.getName()+".so");
+            Boolean loaded = videoPluginsStatus.get(pluginDetails.getName());
+            String pluginSoPath = pluginDetails.getRootPath()+"/lib"+pluginDetails.getName()+".so";
+            if(loaded != null) {
+                togglePlugin(pluginSoPath, true);
+            } else {
+                loadPlugin(pluginSoPath);
+                videoPluginsStatus.put(pluginDetails.getName(), true);
+            }
+
         }
     }
 }
