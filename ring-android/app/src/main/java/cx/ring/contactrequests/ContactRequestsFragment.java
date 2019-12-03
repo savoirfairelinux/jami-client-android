66f46b5d22b143b2f4227809dd22985001b6b20c
Andreas Traczyk <andreas.traczyk@savoirfairelinux.com>
2019-12-03 15:45:04 -0500

    avatars: encorporate presence within avatardrawable
    Change-Id: I3ec7cef66a18d892b603442db14220be42c6c6fb
    Gitlab: #655

--- ring-android/app/src/main/java/cx/ring/contactrequests/ContactRequestsFragment.java
+++ ring-android/app/src/main/java/cx/ring/contactrequests/ContactRequestsFragment.java
@@ -26,6 +26,8 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
