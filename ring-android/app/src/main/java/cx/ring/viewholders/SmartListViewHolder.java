9876e80dcd1cc773533ebe015294039c31b09511
Andreas Traczyk <andreas.traczyk@savoirfairelinux.com>
2019-12-17 12:03:21 -0500

    smartlist: migrate from Butterknife to data-bindings
    Change-Id: I5930cd0f1a0daf0965139c8a4d3ebbe3b9a18950

--- ring-android/app/src/main/java/cx/ring/viewholders/SmartListViewHolder.java
+++ ring-android/app/src/main/java/cx/ring/viewholders/SmartListViewHolder.java
@@ -19,33 +19,29 @@
  */
 package cx.ring.viewholders;
 
-import android.graphics.drawable.Drawable;
-import androidx.recyclerview.widget.RecyclerView;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
 import cx.ring.R;
+import cx.ring.databinding.ItemSmartlistBinding;
+import cx.ring.model.CallContact;
+import cx.ring.model.ContactEvent;
+import cx.ring.model.Interaction;
+import cx.ring.model.SipCall;
 import cx.ring.smartlist.SmartListViewModel;
+import cx.ring.views.AvatarDrawable;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.text.format.DateUtils;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class SmartListViewHolder extends RecyclerView.ViewHolder {
+    public ItemSmartlistBinding binding;
 
-    @BindView(R.id.conv_participant)
-    public TextView convParticipants;
-    @BindView(R.id.conv_last_item)
-    public TextView convStatus;
-    @BindView(R.id.conv_last_time)
-    public TextView convTime;
-    @BindView(R.id.photo)
-    public ImageView photo;
-
-    public Drawable picture = null;
-
-    public SmartListViewHolder(View itemView) {
-        super(itemView);
-        ButterKnife.bind(this, itemView);
+    public SmartListViewHolder(@NonNull ItemSmartlistBinding b) {
+        super(b.getRoot());
+        binding = b;
     }
 
     public void bind(final SmartListListeners clickListener, final SmartListViewModel smartListViewModel) {
@@ -54,11 +50,78 @@ public class SmartListViewHolder extends RecyclerView.ViewHolder {
             clickListener.onItemLongClick(smartListViewModel);
             return true;
         });
+
+        CallContact contact = smartListViewModel.getContact();
+
+        binding.convParticipant.setText(smartListViewModel.getContactName());
+
+        long lastInteraction = smartListViewModel.getLastInteractionTime();
+        String lastInteractionStr = lastInteraction == 0 ?
+                "" : DateUtils.getRelativeTimeSpanString(lastInteraction, System.currentTimeMillis(), 0L, DateUtils.FORMAT_ABBREV_ALL).toString();
+
+        binding.convLastTime.setText(lastInteractionStr);
+        if (smartListViewModel.hasOngoingCall()) {
+            binding.convLastItem.setText(itemView.getContext().getString(R.string.ongoing_call));
+        } else if (smartListViewModel.getLastEvent() != null) {
+            binding.convLastItem.setText(getLastEventSummary(smartListViewModel.getLastEvent(), itemView.getContext()));
+        } else {
+            binding.convLastItem.setVisibility(View.GONE);
+        }
+
+        if (smartListViewModel.hasUnreadTextMessage()) {
+            binding.convParticipant.setTypeface(null, Typeface.BOLD);
+            binding.convLastTime.setTypeface(null, Typeface.BOLD);
+            binding.convLastItem.setTypeface(null, Typeface.BOLD);
+        } else {
+            binding.convParticipant.setTypeface(null, Typeface.NORMAL);
+            binding.convLastTime.setTypeface(null, Typeface.NORMAL);
+            binding.convLastItem.setTypeface(null, Typeface.NORMAL);
+        }
+
+        binding.photo.setImageDrawable(
+                new AvatarDrawable.Builder()
+                        .withContact(contact)
+                        .withCircleCrop(true)
+                        .build(binding.photo.getContext()));
+
+    }
+
+    private String getLastEventSummary(Interaction e, Context context) {
+        if (e.getType() == (Interaction.InteractionType.TEXT)) {
+            if (e.isIncoming()) {
+                return e.getBody();
+            } else {
+                return context.getText(R.string.you_txt_prefix) + " " + e.getBody();
+            }
+        } else if (e.getType() == (Interaction.InteractionType.CALL)) {
+            SipCall call = (SipCall) e;
+            if (call.isMissed())
+                return call.isIncoming() ?
+                        context.getString(R.string.notif_missed_incoming_call) :
+                        context.getString(R.string.notif_missed_outgoing_call);
+            else
+                return call.isIncoming() ?
+                        String.format(context.getString(R.string.hist_in_call), call.getDurationString()) :
+                        String.format(context.getString(R.string.hist_out_call), call.getDurationString());
+        } else if (e.getType() == (Interaction.InteractionType.CONTACT)) {
+            ContactEvent contactEvent = (ContactEvent) e;
+            if (contactEvent.event == ContactEvent.Event.ADDED) {
+                return context.getString(R.string.hist_contact_added);
+            } else if (contactEvent.event == ContactEvent.Event.INCOMING_REQUEST) {
+                return context.getString(R.string.hist_invitation_received);
+            }
+        } else if (e.getType() == (Interaction.InteractionType.DATA_TRANSFER)) {
+            if (!e.isIncoming()) {
+                return context.getString(R.string.hist_file_sent);
+            } else {
+                return context.getString(R.string.hist_file_received);
+            }
+        }
+        return null;
     }
 
     public interface SmartListListeners {
         void onItemClick(SmartListViewModel smartListViewModel);
-
         void onItemLongClick(SmartListViewModel smartListViewModel);
     }
 
