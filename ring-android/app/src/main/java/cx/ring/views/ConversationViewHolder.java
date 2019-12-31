2e2bd43fe9ed3fe4a1448582ca83ed2503dee279
Andreas Traczyk <andreas.traczyk@savoirfairelinux.com>
2019-12-31 15:14:29 -0500

    (WIP) conversations: ui/ux touch-ups
    Change-Id: I067f275686bb28aeb2b8bca38f840df92f3297b1

--- ring-android/app/src/main/java/cx/ring/views/ConversationViewHolder.java
+++ ring-android/app/src/main/java/cx/ring/views/ConversationViewHolder.java
@@ -40,6 +40,7 @@ import io.reactivex.disposables.Disposable;
 public class ConversationViewHolder extends RecyclerView.ViewHolder {
     public TextView mMsgTxt;
     public TextView mMsgDetailTxt;
+    public TextView mMsgDetailTxtPerm;
     public ImageView mPhoto;
     public TextView mHistTxt;
     public TextView mHistDetailTxt;
@@ -62,10 +63,12 @@ public class ConversationViewHolder extends RecyclerView.ViewHolder {
         if (type == ConversationAdapter.MessageType.INCOMING_TEXT_MESSAGE) {
             mMsgTxt = v.findViewById(R.id.msg_txt);
             mMsgDetailTxt = v.findViewById(R.id.msg_details_txt);
+            mMsgDetailTxtPerm = v.findViewById(R.id.msg_details_txt_perm);
             mPhoto = v.findViewById(R.id.photo);
         } else if (type == ConversationAdapter.MessageType.OUTGOING_TEXT_MESSAGE) {
             mMsgTxt = v.findViewById(R.id.msg_txt);
             mMsgDetailTxt = v.findViewById(R.id.msg_details_txt);
+            mMsgDetailTxtPerm = v.findViewById(R.id.msg_details_txt_perm);
             mPhoto = v.findViewById(R.id.status_icon);
         } else if (type == ConversationAdapter.MessageType.CALL_INFORMATION) {
             mHistTxt = v.findViewById(R.id.call_hist_txt);
