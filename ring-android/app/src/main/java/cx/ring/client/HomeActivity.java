8415bc3b9d341242dc5ff5bbfae2d951625208ca
ayounes <amine.younes-bouacida@savoirfairelinux.com>
2020-01-13 15:14:08 -0500

    plugins: change the plugin loading lifecycle
    Change-Id: I9ad31cdae2f4cf9fcf146c5772ec2ba7f14c9134

--- ring-android/app/src/main/java/cx/ring/client/HomeActivity.java
+++ ring-android/app/src/main/java/cx/ring/client/HomeActivity.java
@@ -45,6 +45,8 @@ import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
 import butterknife.BindView;
@@ -62,6 +64,7 @@ import cx.ring.interfaces.Colorable;
 import cx.ring.model.Account;
 import cx.ring.model.AccountConfig;
 import cx.ring.navigation.HomeNavigationFragment;
+import cx.ring.plugins.PluginUtils;
 import cx.ring.service.DRingService;
 import cx.ring.services.AccountService;
 import cx.ring.services.NotificationService;
@@ -231,6 +234,14 @@ public class HomeActivity extends AppCompatActivity implements HomeNavigationFra
         if (mAccountWithPendingrequests != null) {
             presentTrustRequestFragment(mAccountWithPendingrequests);
             mAccountWithPendingrequests = null;
+        }
+
+        List<PluginDetails> plugins = PluginUtils.listPlugins(this);
+
+        for(PluginDetails pluginDetails : plugins) {
+            if(pluginDetails.isEnabled()) {
+                PluginUtils.loadPlugin(pluginDetails.getRootPath());
+            }
         }
     }
 
