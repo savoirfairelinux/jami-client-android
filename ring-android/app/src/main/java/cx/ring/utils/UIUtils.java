fa9d124d87551e94c94acb6fd5d109a93b009984
Pierre Duchemin <pierre.duchemin@savoirfairelinux.com>
2020-04-24 14:04:15 -0400

    call: hide preview feature
    Change-Id: I8f22a67840d5f7ec4649a248894e86562af89f17

---
+++ ring-android/app/src/main/java/cx/ring/utils/UIUtils.java
@@ -0,0 +1,13 @@
+package cx.ring.utils;
+
+import android.content.res.Resources;
+
+public class UIUtils {
+    public static int dpToPx(int dp) {
+        return (int) (dp * Resources.getSystem().getDisplayMetrics().density);
+    }
+
+    public static int pxToDp(int px) {
+        return (int) (px / Resources.getSystem().getDisplayMetrics().density);
+    }
+}
