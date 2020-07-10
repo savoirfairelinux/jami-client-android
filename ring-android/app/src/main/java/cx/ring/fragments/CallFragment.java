dd8bcb90813b3aca217fc85af8efbcd49ad3d9b7
agsantos <aline.gondimsantos@savoirfairelinux.com>
2020-07-10 12:01:36 -0400

    disable hardwareacceleration once mediahandler is toggled
    Change-Id: I8ab845df0ade6875e2c1fbcc017ab3593f6b29b7

--- ring-android/app/src/main/java/cx/ring/fragments/CallFragment.java
+++ ring-android/app/src/main/java/cx/ring/fragments/CallFragment.java
@@ -1429,7 +1429,8 @@ public class CallFragment extends BaseSupportFragment<CallPresenter> implements 
                 }
                 if (position > 0) {
                     String callMediaId = callMediaHandlers.get(position-1);
-                    toggleCallMediaHandler(callMediaId, true);
+                    presenter.startPlugin();
+                    presenter.toggleCallMediaHandler(true);
                 }
             }
 
@@ -1443,6 +1444,7 @@ public class CallFragment extends BaseSupportFragment<CallPresenter> implements 
                 toggleCallMediaHandler(callMediaId, false);
                 rp.scrollToPosition(previousPluginPosition);
             }
+            presenter.stopPlugin();
             binding.recyclerPicker.setVisibility(View.GONE);
             movePreview(false);
             displayHangupButton(true);
