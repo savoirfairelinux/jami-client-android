a3d244886b2afba8d8c79b893e421cb01f2c1550
Amirhossein <amirhossein.naghshzan@savoirfairelinux.com>
2020-07-17 20:29:12 -0400

    HomeActivity: fix settings toolbar bug
    Change-Id: I2be5bdb506c0e8e4e5dc880e30de173103ef358a

--- ring-android/app/src/main/java/cx/ring/client/HomeActivity.java
+++ ring-android/app/src/main/java/cx/ring/client/HomeActivity.java
@@ -352,7 +352,9 @@ public class HomeActivity extends AppCompatActivity implements BottomNavigationV
                 .subscribe(accounts -> {
                     mAccountAdapter = new ToolbarSpinnerAdapter(HomeActivity.this, R.layout.item_toolbar_spinner, accounts);
                     binding.spinnerToolbar.setAdapter(mAccountAdapter);
-                    showProfileInfo();
+                    if (fContent instanceof SmartListFragment) {
+                        showProfileInfo();
+                    }
                 }, e ->  Log.e(TAG, "Error loading account list !", e)));
 
         mDisposable.add((mAccountService
