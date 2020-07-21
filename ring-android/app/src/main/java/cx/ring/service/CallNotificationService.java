05c2fbd1c1884287a370b29095b19fa7fda76aa6
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2020-07-21 15:29:06 -0400

    CallService: use notification queue
    Change-Id: I10c8c64c2f12f2cc3fe7a1087c2286f65cc212d6

--- ring-android/app/src/main/java/cx/ring/service/CallNotificationService.java
+++ ring-android/app/src/main/java/cx/ring/service/CallNotificationService.java
@@ -2,6 +2,7 @@
  *  Copyright (C) 2004-2019 Savoir-faire Linux Inc.
  *
  *  Author: Rayan Osseiran <rayan.osseiran@savoirfairelinux.com>
+ *  Author: Adrien Béraud <adrien.beraud@savoirfairelinux.com>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -27,7 +28,6 @@ import android.os.Build;
 import android.os.IBinder;
 
 import androidx.annotation.Nullable;
-import androidx.core.app.NotificationManagerCompat;
 
 import javax.inject.Inject;
 
@@ -35,46 +35,35 @@ import cx.ring.application.JamiApplication;
 import cx.ring.services.NotificationService;
 
 public class CallNotificationService extends Service {
-
-    private boolean isFirst = true;
+    public static final String ACTION_START = "START";
+    public static final String ACTION_STOP = "STOP";
     private static final int NOTIF_CALL_ID = 1001;
-    private NotificationManagerCompat notificationManager;
 
     @Inject
     NotificationService mNotificationService;
 
     @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        super.onStartCommand(intent, flags, startId);
-        Notification notification = (Notification) mNotificationService.showCallNotification(intent.getIntExtra(NotificationService.KEY_NOTIFICATION_ID, -1));
-        if (notification == null) {
-            if (isFirst)
-                stopSelf();
-            return START_NOT_STICKY;
-        }
-
-        if (isFirst) {
-            isFirst = false;
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q)
-                startForeground(NOTIF_CALL_ID, notification, ServiceInfo.FOREGROUND_SERVICE_TYPE_PHONE_CALL | ServiceInfo.FOREGROUND_SERVICE_TYPE_MEDIA_PROJECTION);
-            else
-                startForeground(NOTIF_CALL_ID, notification);
-        } else {
-            notificationManager.notify(NOTIF_CALL_ID, notification);
-        }
-        return START_NOT_STICKY;
-    }
-
-    @Override
     public void onCreate() {
         ((JamiApplication) getApplication()).getInjectionComponent().inject(this);
-        notificationManager = NotificationManagerCompat.from(this);
         super.onCreate();
     }
 
     @Override
-    public void onDestroy() {
-        super.onDestroy();
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        super.onStartCommand(intent, flags, startId);
+        if (ACTION_START.equals(intent.getAction())) {
+            Notification notification = (Notification) mNotificationService.showCallNotification(intent.getIntExtra(NotificationService.KEY_NOTIFICATION_ID, -1));
+            if (notification != null) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q)
+                    startForeground(NOTIF_CALL_ID, notification, ServiceInfo.FOREGROUND_SERVICE_TYPE_PHONE_CALL | ServiceInfo.FOREGROUND_SERVICE_TYPE_MEDIA_PROJECTION);
+                else
+                    startForeground(NOTIF_CALL_ID, notification);
+            }
+        } else if (ACTION_STOP.equals(intent.getAction())) {
+            stopForeground(true);
+            stopSelf();
+        }
+        return START_NOT_STICKY;
     }
 
     @Nullable
