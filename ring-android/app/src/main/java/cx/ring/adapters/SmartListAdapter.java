8392c0ce8a9886f75aed37016ee4506572146dec
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2020-07-31 13:30:50 -0400

    smartlist: cleanup
    Change-Id: I9b3d5f37815cbd1c3ee0e771507f1d93d63d2a72

--- ring-android/app/src/main/java/cx/ring/adapters/SmartListAdapter.java
+++ ring-android/app/src/main/java/cx/ring/adapters/SmartListAdapter.java
@@ -31,8 +31,6 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.DiffUtil;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.ListUpdateCallback;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
