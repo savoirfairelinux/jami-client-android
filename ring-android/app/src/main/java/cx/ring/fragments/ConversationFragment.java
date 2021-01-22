1bf77ad462c408ea80d5edd804aee262edf451e6
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2021-01-22 16:56:45 -0500

    conversation: add emoji preference
    Change-Id: Ie47a58d18a81754bf5a7dca0771bd4293ee2d3fa

--- ring-android/app/src/main/java/cx/ring/fragments/ConversationFragment.java
+++ ring-android/app/src/main/java/cx/ring/fragments/ConversationFragment.java
@@ -134,6 +134,7 @@ public class ConversationFragment extends BaseSupportFragment<ConversationPresen
     public static final String KEY_PREFERENCE_PENDING_MESSAGE = "pendingMessage";
     public static final String KEY_PREFERENCE_CONVERSATION_COLOR = "color";
     public static final String KEY_PREFERENCE_CONVERSATION_LAST_READ = "lastRead";
+    public static final String KEY_PREFERENCE_CONVERSATION_SYMBOL = "symbol";
     public static final String EXTRA_SHOW_MAP = "showMap";
 
     private static final int REQUEST_CODE_FILE_PICKER = 1000;
@@ -369,6 +370,11 @@ public class ConversationFragment extends BaseSupportFragment<ConversationPresen
         if (activity != null)
             activity.setColor(color);
         mAdapter.setPrimaryColor(color);
+    }
+
+    @Override
+    public void setConversationSymbol(CharSequence symbol) {
+        binding.emojiSend.setText(symbol);
     }
 
     @Override
@@ -863,8 +869,8 @@ public class ConversationFragment extends BaseSupportFragment<ConversationPresen
             mPreferences = requireActivity().getSharedPreferences(path.getAccountId() + "_" + uri.getUri(), Context.MODE_PRIVATE);
             mPreferences.registerOnSharedPreferenceChangeListener(this);
             presenter.setConversationColor(mPreferences.getInt(KEY_PREFERENCE_CONVERSATION_COLOR, getResources().getColor(R.color.color_primary_light)));
+            presenter.setConversationSymbol(mPreferences.getString(KEY_PREFERENCE_CONVERSATION_SYMBOL, getResources().getText(R.string.conversation_default_emoji).toString()));
             mLastRead = mPreferences.getString(KEY_PREFERENCE_CONVERSATION_LAST_READ, null);
-            Log.w(TAG, "Loaded last read " + mLastRead);
         } catch (Exception e) {
             Log.e(TAG, "Can't load conversation preferences");
         }
@@ -904,6 +910,9 @@ public class ConversationFragment extends BaseSupportFragment<ConversationPresen
         switch (key) {
             case KEY_PREFERENCE_CONVERSATION_COLOR:
                 presenter.setConversationColor(prefs.getInt(KEY_PREFERENCE_CONVERSATION_COLOR, getResources().getColor(R.color.color_primary_light)));
+                break;
+            case KEY_PREFERENCE_CONVERSATION_SYMBOL:
+                presenter.setConversationSymbol(prefs.getString(KEY_PREFERENCE_CONVERSATION_SYMBOL, getResources().getText(R.string.conversation_default_emoji).toString()));
                 break;
         }
     }
