13eafe6ae84913cb74b38d6f1185c758d867e4ef
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2021-01-27 09:57:24 -0500

    update copyright to 2021
    Change-Id: I6a68fc7605b528698a451de03914d203de34012d

--- ring-android/app/src/main/java/cx/ring/application/JamiApplication.java
+++ ring-android/app/src/main/java/cx/ring/application/JamiApplication.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2016-2020 Savoir-faire Linux Inc.
+ *  Copyright (C) 2016-2021 Savoir-faire Linux Inc.
  *
  *  Author: Thibault Wittemberg <thibault.wittemberg@savoirfairelinux.com>
  *
