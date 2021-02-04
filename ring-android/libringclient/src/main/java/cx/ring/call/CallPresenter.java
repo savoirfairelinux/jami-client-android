1b633756997692baf968562bd62f39f7c2b257a8
Amirhossein <amirhossein.naghshzan@savoirfairelinux.com>
2021-02-04 13:03:04 -0500

    call: show remote recording indicator
    Change-Id: Iddccbc6743ef38c9a3d5eb5906fdca7dea20bde6

--- ring-android/libringclient/src/main/java/cx/ring/call/CallPresenter.java
+++ ring-android/libringclient/src/main/java/cx/ring/call/CallPresenter.java
@@ -39,7 +39,6 @@ import cx.ring.services.CallService;
 import cx.ring.services.ContactService;
 import cx.ring.services.DeviceRuntimeService;
 import cx.ring.services.HardwareService;
-import cx.ring.services.PreferencesService;
 import cx.ring.utils.Log;
 import cx.ring.utils.StringUtils;
 import io.reactivex.Maybe;
@@ -229,11 +228,18 @@ public class CallPresenter extends RootPresenter<CallView> {
     }
 
     private void showConference(Observable<Conference> conference) {
+        conference = conference
+                .distinctUntilChanged();
         mCompositeDisposable.add(conference
-                .distinctUntilChanged()
                 .switchMap(Conference::getParticipantInfo)
                 .observeOn(mUiScheduler)
                 .subscribe(info -> getView().updateConfInfo(info),
+                        e -> Log.e(TAG, "Error with initIncoming, action view flow: ", e)));
+
+        mCompositeDisposable.add(conference
+                .switchMap(Conference::getParticipantRecording)
+                .observeOn(mUiScheduler)
+                .subscribe(contacts -> getView().updateParticipantRecording(contacts),
                         e -> Log.e(TAG, "Error with initIncoming, action view flow: ", e)));
     }
 
