183aab808214bd6b76d92443ee88f3b2607245b3
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2021-02-05 17:35:24 -0500

    class rename
    Change-Id: I4177581e1a97aff514161c0a778f24cc7972409a

--- ring-android/app/src/main/java/cx/ring/contactrequests/BlockListAdapter.java
+++ ring-android/app/src/main/java/cx/ring/contactrequests/BlockListAdapter.java
@@ -28,35 +28,35 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 import cx.ring.R;
-import net.jami.model.CallContact;
+import net.jami.model.Contact;
 
-public class BlackListAdapter extends RecyclerView.Adapter<BlackListViewHolder> {
+public class BlockListAdapter extends RecyclerView.Adapter<BlockListViewHolder> {
 
-    private final BlackListViewHolder.BlackListListeners mListener;
-    private final ArrayList<CallContact> mBlacklisted;
+    private final BlockListViewHolder.BlockListListeners mListener;
+    private final ArrayList<Contact> mBlacklisted;
 
-    public BlackListAdapter(Collection<CallContact> viewModels, BlackListViewHolder.BlackListListeners listener) {
+    public BlockListAdapter(Collection<Contact> viewModels, BlockListViewHolder.BlockListListeners listener) {
         mBlacklisted = new ArrayList<>(viewModels);
         mListener = listener;
     }
 
-    public void replaceAll(Collection<CallContact> viewModels) {
+    public void replaceAll(Collection<Contact> viewModels) {
         mBlacklisted.clear();
         mBlacklisted.addAll(viewModels);
         notifyDataSetChanged();
     }
 
     @Override
-    public BlackListViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public BlockListViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View holderView = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.item_contact_blacklist, parent, false);
 
-        return new BlackListViewHolder(holderView);
+        return new BlockListViewHolder(holderView);
     }
 
     @Override
-    public void onBindViewHolder(BlackListViewHolder holder, int position) {
-        final CallContact contact = mBlacklisted.get(position);
+    public void onBindViewHolder(BlockListViewHolder holder, int position) {
+        final Contact contact = mBlacklisted.get(position);
         holder.bind(mListener, contact);
     }
 
