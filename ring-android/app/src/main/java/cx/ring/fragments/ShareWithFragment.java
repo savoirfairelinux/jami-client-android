a3f913cac6e51ecc1b8063186e043d52ce94dd7e
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2021-02-10 17:13:54 -0500

    swarm: update model, conversations, add creation wizard
    Change-Id: I212e550d145a90a7d6dc63416849aadd1ac7573f

--- ring-android/app/src/main/java/cx/ring/fragments/ShareWithFragment.java
+++ ring-android/app/src/main/java/cx/ring/fragments/ShareWithFragment.java
@@ -146,7 +146,7 @@ public class ShareWithFragment extends Fragment {
                         intent.putExtra(Intent.EXTRA_TEXT, previewText.getText().toString());
                     }
                     intent.putExtra(ConversationFragment.KEY_ACCOUNT_ID, smartListViewModel.getAccountId());
-                    intent.putExtra(ConversationFragment.KEY_CONTACT_RING_ID, smartListViewModel.getContact().getPrimaryNumber());
+                    intent.putExtra(ConversationFragment.KEY_CONTACT_RING_ID, smartListViewModel.getUri().getUri());
                     intent.setClass(requireActivity(), ConversationActivity.class);
                     startActivity(intent);
                 }
