bc6f7ad2db008bb5ba2287f8eb3bdbb92b58d8d3
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2021-03-05 15:23:29 -0500

    sharewith: cleanup
    Change-Id: Ie253fab6909fc7de95ad894c7e602eb140fb8ebb

--- ring-android/app/src/main/java/cx/ring/client/ShareActivity.java
+++ ring-android/app/src/main/java/cx/ring/client/ShareActivity.java
@@ -1,14 +1,24 @@
 package cx.ring.client;
 
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import cx.ring.R;
+import cx.ring.utils.ConversationPath;
 
 public class ShareActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+        Bundle extra = intent.getExtras();
+        if (ConversationPath.fromBundle(extra) != null) {
+            intent.setClass(this, ConversationActivity.class);
+            startActivity(intent);
+            finishAndRemoveTask();
+            return;
+        }
         setContentView(R.layout.activity_share);
     }
 
