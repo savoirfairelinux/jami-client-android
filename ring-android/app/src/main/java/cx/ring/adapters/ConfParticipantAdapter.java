3cef16b9c2bf2c9d2b24a23fe2cf5e7881290a8d
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2021-05-20 16:32:52 -0400

    call: use participant info for conference layout
    Change-Id: Iebbd60510d2b3812476a2456d6cef24995fff873

--- ring-android/app/src/main/java/cx/ring/adapters/ConfParticipantAdapter.java
+++ ring-android/app/src/main/java/cx/ring/adapters/ConfParticipantAdapter.java
@@ -1,3 +1,22 @@
+/*
+ *  Copyright (C) 2004-2021 Savoir-faire Linux Inc.
+ *
+ *  Author: Adrien Béraud <adrien.beraud@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
 package cx.ring.adapters;
 
 import android.content.Context;
@@ -11,16 +30,18 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
+import cx.ring.fragments.CallFragment;
 import cx.ring.views.AvatarFactory;
 import cx.ring.databinding.ItemConferenceParticipantBinding;
-import cx.ring.fragments.CallFragment;
+
+import net.jami.model.Conference;
 import net.jami.model.Contact;
 import net.jami.model.Call;
 import cx.ring.views.ParticipantView;
 
 public class ConfParticipantAdapter extends RecyclerView.Adapter<ParticipantView> {
     protected final ConfParticipantAdapter.ConfParticipantSelected onSelectedCallback;
-    private List<Call> calls = null;
+    private List<Conference.ParticipantInfo> calls = null;
 
     public ConfParticipantAdapter(@NonNull ConfParticipantSelected cb) {
         onSelectedCallback = cb;
@@ -34,22 +55,23 @@ public class ConfParticipantAdapter extends RecyclerView.Adapter<ParticipantView
 
     @Override
     public void onBindViewHolder(@NonNull ParticipantView holder, int position) {
-        final Call call = calls.get(position);
-        final Contact contact = call.getContact();
+        final Conference.ParticipantInfo info = calls.get(position);
+        final Contact contact = info.contact;
+
         final Context context = holder.itemView.getContext();
-        Call.CallStatus status = call.getCallStatus();
-        if (status == Call.CallStatus.CURRENT)  {
+        if (info.call != null && info.call.getCallStatus() != Call.CallStatus.CURRENT)  {
+            holder.binding.displayName.setText(String.format("%s\n%s", contact.getDisplayName(), context.getText(CallFragment.callStateToHumanState(info.call.getCallStatus()))));
+            holder.binding.photo.setAlpha(.5f);
+        } else {
             holder.binding.displayName.setText(contact.getDisplayName());
             holder.binding.photo.setAlpha(1f);
-        } else {
-            holder.binding.displayName.setText(String.format("%s\n%s", contact.getDisplayName(), context.getText(CallFragment.callStateToHumanState(status))));
-            holder.binding.photo.setAlpha(.5f);
         }
+
         if (holder.disposable != null)
             holder.disposable.dispose();
         holder.disposable = AvatarFactory.getAvatar(context, contact)
                 .subscribe(holder.binding.photo::setImageDrawable);
-        holder.itemView.setOnClickListener(view -> onSelectedCallback.onParticipantSelected(view, call));
+        holder.itemView.setOnClickListener(view -> onSelectedCallback.onParticipantSelected(view, info));
     }
 
     @Override
@@ -57,8 +79,8 @@ public class ConfParticipantAdapter extends RecyclerView.Adapter<ParticipantView
         return calls == null ? 0 : calls.size();
     }
 
-    public void updateFromCalls(@NonNull final List<Call> contacts) {
-        final List<Call> oldCalls = calls;
+    public void updateFromCalls(@NonNull final List<Conference.ParticipantInfo> contacts) {
+        final List<Conference.ParticipantInfo> oldCalls = calls;
         calls = contacts;
         if (oldCalls != null) {
             DiffUtil.calculateDiff(new DiffUtil.Callback() {
@@ -88,6 +110,6 @@ public class ConfParticipantAdapter extends RecyclerView.Adapter<ParticipantView
     }
 
     public interface ConfParticipantSelected {
-        void onParticipantSelected(View view, Call contact);
+        void onParticipantSelected(View view, Conference.ParticipantInfo contact);
     }
 }
