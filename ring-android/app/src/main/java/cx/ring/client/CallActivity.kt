7e69e1a6b6d7a8d6506d9d4ae874e3cb6fbdc50c
Maxime Callet <maxime.callet@savoirfairelinux.com>
2021-09-30 14:01:24 -0400

    Call: Implement new Android Api and update edge to edge behavior
    Change-Id: I8a6f7204da3a8e60b2c7f5f75a6c9d0f86af86a6

--- ring-android/app/src/main/java/cx/ring/client/CallActivity.kt
+++ ring-android/app/src/main/java/cx/ring/client/CallActivity.kt
@@ -32,6 +32,9 @@ import android.view.KeyEvent
 import android.view.View
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import cx.ring.BuildConfig
 import cx.ring.R
 import cx.ring.application.JamiApplication
@@ -41,6 +44,7 @@ import cx.ring.utils.KeyboardVisibilityManager
 import cx.ring.utils.MediaButtonsHelper
 import dagger.hilt.android.AndroidEntryPoint
 import net.jami.services.NotificationService
+import net.jami.utils.Log
 
 @AndroidEntryPoint
 class CallActivity : AppCompatActivity() {
@@ -114,6 +118,7 @@ class CallActivity : AppCompatActivity() {
     }
 
     private val onNoInteraction = Runnable {
+        Log.w(TAG, "DEBUG -------- HidesystemUi from onNoInteraction")
         if (!dimmed) {
             dimmed = true
             hideSystemUI()
@@ -147,14 +152,14 @@ class CallActivity : AppCompatActivity() {
 
     private fun hideSystemUI() {
         KeyboardVisibilityManager.hideKeyboard(this)
+        Log.w(TAG, "DEBUG -------- hideSystemUI ")
+
         mMainView?.let { mainView ->
-            mainView.systemUiVisibility =
-                (View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LOW_PROFILE
-                        or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
-                        or View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
-                        or View.SYSTEM_UI_FLAG_IMMERSIVE)
+            WindowCompat.setDecorFitsSystemWindows(window, false)
+            WindowInsetsControllerCompat(window, mainView).let { controller ->
+                controller.hide(WindowInsetsCompat.Type.systemBars())
+                controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            }
             val callFragment = callFragment
             if (callFragment != null && !callFragment.isChoosePluginMode) {
                 callFragment.toggleVideoPluginsCarousel(false)
@@ -163,13 +168,14 @@ class CallActivity : AppCompatActivity() {
         }
     }
 
+
+
     fun showSystemUI() {
+        Log.w(TAG, "DEBUG -------- showSystemUI ")
+
         mMainView?.let { mainView ->
-            mainView.systemUiVisibility =
-                (View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LOW_PROFILE
-                        or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        or View.SYSTEM_UI_FLAG_IMMERSIVE)
+            WindowCompat.setDecorFitsSystemWindows(window, true)
+            WindowInsetsControllerCompat(window, mainView).show(WindowInsetsCompat.Type.systemBars())
             callFragment?.toggleVideoPluginsCarousel(true)
             restartNoInteractionTimer()
         }
