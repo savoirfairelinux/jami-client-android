e0c2990185f16acb0d9b671a22d2ccae7564684c
Maxime Callet <maxime.callet@savoirfairelinux.com>
2021-10-13 15:12:35 -0400

    Call: implement new ui
    Change-Id: I8c688835d42117e9fb237e3ba21b9399d9c1783f

--- ring-android/app/src/main/java/cx/ring/views/AutoFitTextureView.java
+++ ring-android/app/src/main/java/cx/ring/views/AutoFitTextureView.java
@@ -31,7 +31,7 @@ public class AutoFitTextureView extends TextureView {
 
     public AutoFitTextureView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-        mSize = Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 192, context.getResources().getDisplayMetrics()));
+        mSize = Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 150, context.getResources().getDisplayMetrics()));
     }
 
     @Override
