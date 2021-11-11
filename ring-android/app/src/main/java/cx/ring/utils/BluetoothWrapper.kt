7000c035e94cfd9b7e033f6e1c772938c3cf378e
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2021-11-11 15:36:54 -0500

    WIP adapt for Android 12
    Change-Id: I8aa5b3893251f2cb5a147f9af51cd16c71274073

--- ring-android/app/src/main/java/cx/ring/utils/BluetoothWrapper.kt
+++ ring-android/app/src/main/java/cx/ring/utils/BluetoothWrapper.kt
@@ -52,29 +52,30 @@ class BluetoothWrapper(private val mContext: Context, private val btChangesListe
     private val mHeadsetProfileReceiver: BroadcastReceiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context, intent: Intent) {
             val action = intent.action
-            if (BluetoothHeadset.ACTION_CONNECTION_STATE_CHANGED == action) {
-                val status = intent.getIntExtra(BluetoothHeadset.EXTRA_STATE, BluetoothHeadset.STATE_DISCONNECTED)
-                Log.d(TAG, "BluetoothHeadset.ACTION_CONNECTION_STATE_CHANGED $status")
-            }
-            if (BluetoothHeadset.ACTION_AUDIO_STATE_CHANGED == action) {
-                val status = intent.getIntExtra(BluetoothHeadset.EXTRA_STATE, BluetoothHeadset.STATE_AUDIO_DISCONNECTED)
-                Log.d(TAG, "BluetoothHeadset.ACTION_AUDIO_STATE_CHANGED $status")
-                synchronized(this) {
-                    if (headsetAdapter != null && status == BluetoothHeadset.STATE_AUDIO_CONNECTED) {
-                        val devices = headsetAdapter!!.connectedDevices
-                        connectedDevices = devices
-                        if (DBG) {
-                            for (device in devices)
-                                Log.d(TAG, "BluetoothDevice " + device.name + " " + device + " " + device.bondState + " connected: " + headsetAdapter!!.isAudioConnected(device))
-                        }
-                    } else if (status == BluetoothHeadset.STATE_AUDIO_DISCONNECTED) {
-                        connectedDevices = null
-                    }
+            try {
+                if (BluetoothHeadset.ACTION_CONNECTION_STATE_CHANGED == action) {
+                    val status = intent.getIntExtra(BluetoothHeadset.EXTRA_STATE, BluetoothHeadset.STATE_DISCONNECTED)
+                    Log.d(TAG, "BluetoothHeadset.ACTION_CONNECTION_STATE_CHANGED $status")
                 }
-            } else if (BluetoothHeadset.ACTION_VENDOR_SPECIFIC_HEADSET_EVENT == action) {
-                val cmdType = intent.getIntExtra(BluetoothHeadset.EXTRA_VENDOR_SPECIFIC_HEADSET_EVENT_CMD_TYPE, -1)
-                val cmd = intent.getStringExtra(BluetoothHeadset.EXTRA_VENDOR_SPECIFIC_HEADSET_EVENT_CMD)
-                Log.d(TAG, "BluetoothHeadset.ACTION_VENDOR_SPECIFIC_HEADSET_EVENT $cmdType $cmd")
+                if (BluetoothHeadset.ACTION_AUDIO_STATE_CHANGED == action) {
+                    val status = intent.getIntExtra(BluetoothHeadset.EXTRA_STATE, BluetoothHeadset.STATE_AUDIO_DISCONNECTED)
+                    Log.d(TAG, "BluetoothHeadset.ACTION_AUDIO_STATE_CHANGED $status")
+                        synchronized(this) {
+                            if (headsetAdapter != null && status == BluetoothHeadset.STATE_AUDIO_CONNECTED) {
+                                val devices = headsetAdapter!!.connectedDevices
+                                connectedDevices = devices
+                            } else if (status == BluetoothHeadset.STATE_AUDIO_DISCONNECTED) {
+                                connectedDevices = null
+                            }
+                        }
+
+                } else if (BluetoothHeadset.ACTION_VENDOR_SPECIFIC_HEADSET_EVENT == action) {
+                    val cmdType = intent.getIntExtra(BluetoothHeadset.EXTRA_VENDOR_SPECIFIC_HEADSET_EVENT_CMD_TYPE, -1)
+                    val cmd = intent.getStringExtra(BluetoothHeadset.EXTRA_VENDOR_SPECIFIC_HEADSET_EVENT_CMD)
+                    Log.d(TAG, "BluetoothHeadset.ACTION_VENDOR_SPECIFIC_HEADSET_EVENT $cmdType $cmd")
+                }
+            } catch (e: SecurityException) {
+                Log.d(TAG, "Error receiving Bluetooth state", e)
             }
         }
     }
@@ -106,35 +107,34 @@ class BluetoothWrapper(private val mContext: Context, private val btChangesListe
         }
     }
     val isBTHeadsetConnected: Boolean
-        get() = bluetoothAdapter != null && bluetoothAdapter.getProfileConnectionState(BluetoothProfile.HEADSET) == BluetoothAdapter.STATE_CONNECTED
+        get() = try {
+            bluetoothAdapter?.getProfileConnectionState(BluetoothProfile.HEADSET) == BluetoothAdapter.STATE_CONNECTED
+        } catch (e: SecurityException) {
+            false
+        }
 
     fun canBluetooth(): Boolean {
         // Detect if any bluetooth a device is available for call
-        if (bluetoothAdapter == null) {
-            // Device does not support Bluetooth
-            return false
-        }
-        var hasConnectedDevice = false
-        //If bluetooth is on
-        if (bluetoothAdapter.isEnabled) {
+        try {
+            if (bluetoothAdapter == null || !bluetoothAdapter.isEnabled || !audioManager.isBluetoothScoAvailableOffCall) {
+                return false
+            }
             //We get all bounded bluetooth devices
             // bounded is not enough, should search for connected devices....
             val pairedDevices = bluetoothAdapter.bondedDevices
             for (device in pairedDevices) {
-                val bluetoothClass = device.bluetoothClass
-                if (bluetoothClass != null) {
+                device.bluetoothClass?.let { bluetoothClass ->
                     val deviceClass = bluetoothClass.deviceClass
                     if (bluetoothClass.hasService(BluetoothClass.Service.TELEPHONY) || deviceClass == BluetoothClass.Device.AUDIO_VIDEO_WEARABLE_HEADSET || deviceClass == BluetoothClass.Device.AUDIO_VIDEO_CAR_AUDIO || deviceClass == BluetoothClass.Device.AUDIO_VIDEO_HANDSFREE) {
                         //And if any can be used as a audio handset
-                        hasConnectedDevice = true
-                        break
+                        return true
                     }
                 }
             }
+        } catch (e: SecurityException) {
+            Log.w(TAG, "Can't get bluetooth status", e)
         }
-        val retVal = hasConnectedDevice && audioManager.isBluetoothScoAvailableOffCall
-        Log.d(TAG, "canBluetooth: Can I do BT ? $retVal")
-        return retVal
+        return false
     }
 
     fun setBluetoothOn(on: Boolean) {
@@ -167,13 +167,6 @@ class BluetoothWrapper(private val mContext: Context, private val btChangesListe
                 Log.d(TAG, "onServiceConnected $profile $proxy")
                 if (profile == BluetoothProfile.HEADSET) {
                     headsetAdapter = proxy as BluetoothHeadset
-                    if (DBG) {
-                        val devices = headsetAdapter!!.connectedDevices
-                        Log.d(TAG, "Bluetooth Headset profile connected: ${devices.size} devices.")
-                        for (device in devices) {
-                            Log.d(TAG, "BluetoothDevice ${device.name} $device ${device.bondState} connected: ${headsetAdapter!!.isAudioConnected(device)}")
-                        }
-                    }
                 }
             }
 
@@ -207,19 +200,23 @@ class BluetoothWrapper(private val mContext: Context, private val btChangesListe
     @get:Synchronized
     val deviceName: String?
         get() {
-            connectedDevices?.takeIf { it.isNotEmpty() }?.let { devices ->
-                // Use first bounded device with audio connected
-                for (device in devices) {
-                    if (device.bondState == BluetoothDevice.BOND_BONDED && headsetAdapter!!.isAudioConnected(device)) {
-                        return device.name
+            try {
+                connectedDevices?.takeIf { it.isNotEmpty() }?.let { devices ->
+                    // Use first bounded device with audio connected
+                    for (device in devices) {
+                        if (device.bondState == BluetoothDevice.BOND_BONDED && headsetAdapter!!.isAudioConnected(device)) {
+                            return device.name
+                        }
+                    }
+                    // Fallback on first bounded device
+                    for (device in devices) {
+                        if (device.bondState == BluetoothDevice.BOND_BONDED) {
+                            return device.name
+                        }
                     }
                 }
-                // Fallback on first bounded device
-                for (device in devices) {
-                    if (device.bondState == BluetoothDevice.BOND_BONDED) {
-                        return device.name
-                    }
-                }
+            } catch (e: SecurityException) {
+                Log.w(TAG, "Failed to get bluetooth device name", e)
             }
             return null
         }
