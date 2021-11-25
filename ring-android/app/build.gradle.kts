69d4a18e53a146359eefed727741fc4281d58d10
Maxime Callet <maxime.callet@savoirfairelinux.com>
2021-11-25 14:16:17 -0500

    build: convert gradle to Kotlin script
    Change-Id: I0ae31705d9cea205a3263895c654504c0845730d

---
+++ ring-android/app/build.gradle.kts
@@ -0,0 +1,141 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    kotlin("kapt")
+}
+
+apply(plugin = "dagger.hilt.android.plugin")
+
+val kotlin_version: String by rootProject.extra
+val hilt_version: String by rootProject.extra
+val archs: CharSequence by project
+val buildFirebase = project.hasProperty("buildFirebase") || gradle.startParameter.taskRequests.toString().contains("Firebase")
+
+android {
+    compileSdk = 31
+    buildToolsVersion = "30.0.3"
+    ndkVersion = "23.0.7599858"
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 31
+        versionCode = 322
+        versionName = "20211104-01"
+    }
+    sourceSets {
+        getByName("main") {
+            jniLibs.srcDir( "src/main/libs")
+        }
+    }
+
+    buildTypes {
+        getByName("debug") {
+            isDebuggable = true
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+
+            packagingOptions{
+                doNotStrip("*/armeabi/*.so")
+                doNotStrip("*/armeabi-v7a/*.so")
+                doNotStrip("*/arm64-v8a/*.so")
+                doNotStrip("*/x86/*.so")
+                doNotStrip("*/x86_64/*.so")
+            }
+        }
+        getByName("release") {
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    buildFeatures {
+        viewBinding =true
+        dataBinding = true
+    }
+    flavorDimensions ("push")
+    productFlavors {
+        create("noPush") {
+            dimension = "push"
+        }
+        create("withFirebase") {
+            dimension = "push"
+        }
+    }
+    signingConfigs {
+        create("config") {
+            keyAlias = "ring"
+            storeFile = file("../keystore.bin")
+        }
+    }
+    lint {
+        disable("MissingTranslation")
+    }
+    splits {
+        abi {
+            isEnable = true
+            reset()
+            val sp = archs.split(",")
+            include("armeabi-v7a, arm64-v8a, x86_64")
+            isUniversalApk = true
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    implementation (project(":libjamiclient"))
+    implementation ("org.jetbrains.kotlin:kotlin-stdlib:$kotlin_version")
+    implementation ("androidx.core:core-ktx:1.7.0")
+    implementation ("androidx.appcompat:appcompat:1.4.0-rc01")
+    implementation ("androidx.constraintlayout:constraintlayout:2.1.1")
+    implementation ("androidx.legacy:legacy-support-core-utils:1.0.0")
+    implementation ("androidx.cardview:cardview:1.0.0")
+    implementation ("androidx.preference:preference-ktx:1.1.1")
+    implementation( "androidx.recyclerview:recyclerview:1.2.1")
+    implementation( "androidx.leanback:leanback:1.2.0-alpha01")
+    implementation ("androidx.leanback:leanback-preference:1.2.0-alpha01")
+    implementation ("androidx.tvprovider:tvprovider:1.1.0-alpha01")
+    implementation ("androidx.media:media:1.4.3")
+    implementation ("androidx.percentlayout:percentlayout:1.0.0")
+    implementation ("com.google.android.material:material:1.5.0-alpha05")
+    implementation ("com.google.android.flexbox:flexbox:3.0.0")
+    implementation ("org.osmdroid:osmdroid-android:6.1.11")
+    implementation ("androidx.sharetarget:sharetarget:1.2.0-rc01")
+
+    // ORM
+    implementation ("com.j256.ormlite:ormlite-android:5.6")
+
+    // Barcode scanning
+    implementation("com.journeyapps:zxing-android-embedded:4.3.0") { isTransitive = false }
+    implementation ("com.google.zxing:core:3.3.3")
+
+    // Dagger dependency injection
+    implementation("com.google.dagger:hilt-android:$hilt_version")
+    kapt("com.google.dagger:hilt-android-compiler:$hilt_version")
+
+    // Glide
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
+    kapt ("com.github.bumptech.glide:compiler:4.12.0")
+
+    // RxAndroid
+    implementation ("io.reactivex.rxjava3:rxandroid:3.0.0")
+    implementation ("io.reactivex.rxjava3:rxjava:3.1.2")
+
+    implementation ("com.jsibbold:zoomage:1.3.1")
+    implementation ("com.googlecode.ez-vcard:ez-vcard:0.11.3")
+
+    implementation("com.google.firebase:firebase-messaging:23.0.0") {
+        exclude(group= "com.google.firebase", module= "firebase-core")
+        exclude(group= "com.google.firebase", module= "firebase-analytics")
+        exclude(group= "com.google.firebase", module= "firebase-measurement-connector")
+    }
+}
+
+if (buildFirebase) {
+    println ("apply plugin " + buildFirebase)
+    apply(plugin = "com.google.gms.google-services")
+}
---
+++ ring-android/libjamiclient/build.gradle.kts
@@ -0,0 +1,34 @@
+val kotlin_version: String by rootProject.extra
+val hilt_version: String by rootProject.extra
+
+plugins {
+    id("kotlin")
+    id("java")
+    kotlin("kapt")
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    // VCard parsing
+    implementation ("com.googlecode.ez-vcard:ez-vcard:0.11.3")
+    // QRCode encoding
+    implementation ("com.google.zxing:core:3.4.1")
+    // dependency injection
+    implementation( "javax.inject:javax.inject:1")
+    // ORM
+    implementation ("com.j256.ormlite:ormlite-core:5.6")
+    // Required -- JUnit 4 framework
+    testImplementation ("junit:junit:4.13.2")
+    // RxJava
+    implementation ("io.reactivex.rxjava3:rxjava:3.1.2")
+    // gson
+    implementation ("com.google.code.gson:gson:2.8.9")
+    api("com.google.dagger:dagger:$hilt_version")
+    kapt("com.google.dagger:dagger-compiler:$hilt_version")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
