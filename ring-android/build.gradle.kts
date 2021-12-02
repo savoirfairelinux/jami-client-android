2176437239889851828d5d2915643aa505a0981a
Maxime Callet <maxime.callet@savoirfairelinux.com>
2021-12-02 17:22:30 -0500

    build: convert gradle to Kotlin script
    Change-Id: I0ae31705d9cea205a3263895c654504c0845730d

---
+++ ring-android/build.gradle.kts
@@ -0,0 +1,24 @@
+buildscript {
+    repositories {
+        google()
+        maven { url = uri( "https://maven.google.com") }
+        mavenCentral()
+    }
+
+    val kotlin_version by extra { "1.6.0" }
+    val hilt_version by extra { "2.40.3" }
+
+    dependencies {
+        classpath ("com.android.tools.build:gradle:7.0.3")
+        classpath ("com.google.gms:google-services:4.3.10")
+        classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
+        classpath ("com.google.dagger:hilt-android-gradle-plugin:$hilt_version")
+    }
+}
+allprojects {
+    repositories {
+        google()
+        maven { url = uri("https://maven.google.com") }
+        mavenCentral()
+    }
+}
