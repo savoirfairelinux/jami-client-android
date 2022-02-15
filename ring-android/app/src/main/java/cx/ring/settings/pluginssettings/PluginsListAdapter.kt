f7a713a8e339c07e4f2d383623b15295b8a301ac
Maxime Callet <maximecallet@savoirfairelinux.com>
2022-02-15 18:10:37 -0500

    Plugins: refacto old version
    Change-Id: Ie65e296530149e7333d30ea0cf95770970c547ab

--- ring-android/app/src/main/java/cx/ring/settings/pluginssettings/PluginsListAdapter.kt
+++ ring-android/app/src/main/java/cx/ring/settings/pluginssettings/PluginsListAdapter.kt
@@ -1,27 +1,23 @@
 package cx.ring.settings.pluginssettings
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.CheckBox
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import cx.ring.R
-import cx.ring.settings.pluginssettings.PluginsListAdapter.PluginViewHolder
+import cx.ring.viewholders.PluginSettingViewHolder
 
 class PluginsListAdapter(private var mList: List<PluginDetails>, private val listener: PluginListItemListener, private val accountId: String? = "") :
-    RecyclerView.Adapter<PluginViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PluginViewHolder {
+    RecyclerView.Adapter<PluginSettingViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PluginSettingViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.frag_plugins_list_item, parent, false)
-        return PluginViewHolder(view, listener)
+        return PluginSettingViewHolder(view, listener)
     }
 
-    override fun onBindViewHolder(holder: PluginViewHolder, position: Int) {
+    override fun onBindViewHolder(holderSetting: PluginSettingViewHolder, position: Int) {
         for (item in mList) {
             item.accountId = accountId
         }
-        holder.setDetails(mList[position])
+        holderSetting.setDetails(mList[position])
     }
 
     override fun getItemCount(): Int {
@@ -43,45 +39,6 @@ class PluginsListAdapter(private var mList: List<PluginDetails>, private val lis
         }
         mList = listPlugins
         notifyDataSetChanged()
-    }
-
-    class PluginViewHolder(itemView: View, listener: PluginListItemListener) : RecyclerView.ViewHolder(itemView) {
-        private val pluginIcon: ImageView = itemView.findViewById(R.id.plugin_item_icon)
-        private val pluginNameTextView: TextView = itemView.findViewById(R.id.plugin_item_name)
-        private val pluginItemEnableCheckbox: CheckBox = itemView.findViewById(R.id.plugin_item_enable_checkbox)
-        private var details: PluginDetails? = null
-
-        fun setDetails(details: PluginDetails) {
-            this.details = details
-            update(details)
-        }
-
-        // update the viewHolder view
-        fun update(details: PluginDetails) {
-            pluginNameTextView.text = details.name
-            if (details.accountId!!.isEmpty())
-                pluginItemEnableCheckbox.isChecked = details.isEnabled
-            else
-                pluginItemEnableCheckbox.visibility = View.GONE
-
-            // Set the plugin icon
-            val icon = details.icon
-            if (icon != null) {
-                pluginIcon.setImageDrawable(icon)
-            }
-        }
-
-        init {
-            itemView.setOnClickListener {
-                details?.let { details -> listener.onPluginItemClicked(details) }
-            }
-            pluginItemEnableCheckbox.setOnClickListener {
-                details?.let { details ->
-                    details.isEnabled = !details.isEnabled
-                    listener.onPluginEnabled(details)
-                }
-            }
-        }
     }
 
     interface PluginListItemListener {
---
+++ ring-android/app/src/main/java/cx/ring/viewholders/PluginViewHolder.kt
@@ -0,0 +1,49 @@
+package cx.ring.viewholders
+
+import android.view.View
+import android.widget.CheckBox
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import cx.ring.R
+import cx.ring.settings.pluginssettings.PluginDetails
+import cx.ring.adapters.PluginsAdapter.PluginListItemListener
+
+class PluginViewHolder(itemView: View, listener: PluginListItemListener) : RecyclerView.ViewHolder(itemView) {
+    private val pluginIcon: ImageView = itemView.findViewById(R.id.plugin_item_icon)
+    private val pluginNameTextView: TextView = itemView.findViewById(R.id.plugin_item_name)
+    private val pluginItemEnableCheckbox: CheckBox = itemView.findViewById(R.id.plugin_item_enable_checkbox)
+    private var details: PluginDetails? = null
+
+    fun setDetails(details: PluginDetails) {
+        this.details = details
+        update(details)
+    }
+
+    // update the viewHolder view
+    fun update(details: PluginDetails) {
+        pluginNameTextView.text = details.name
+        if (details.accountId!!.isEmpty())
+            pluginItemEnableCheckbox.isChecked = details.isEnabled
+        else
+            pluginItemEnableCheckbox.visibility = View.GONE
+
+        // Set the plugin icon
+        val icon = details.icon
+        if (icon != null) {
+            pluginIcon.setImageDrawable(icon)
+        }
+    }
+
+    init {
+        itemView.setOnClickListener {
+            details?.let { details -> listener.onPluginItemClicked(details) }
+        }
+        pluginItemEnableCheckbox.setOnClickListener {
+            details?.let { details ->
+                details.isEnabled = !details.isEnabled
+                listener.onPluginEnabled(details)
+            }
+        }
+    }
+}
