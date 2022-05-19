367a87e3fb6e7fbeda55c4700802d70a18d1dda8
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2022-05-19 16:13:56 -0400

    call: support video sink per media
    Change-Id: I698258e43f934462e7db6f8bb62866a93c7d3c71

---
+++ ring-android/app/src/main/java/cx/ring/utils/ScaleManager.kt
@@ -0,0 +1,121 @@
+package cx.ring.utils
+
+import android.graphics.Matrix
+import android.util.Size
+import kotlin.math.max
+import kotlin.math.min
+
+enum class PivotPoint {
+    LEFT_TOP, LEFT_CENTER, LEFT_BOTTOM, CENTER_TOP, CENTER, CENTER_BOTTOM, RIGHT_TOP, RIGHT_CENTER, RIGHT_BOTTOM
+}
+enum class ScalableType {
+    NONE, FIT_XY, FIT_START, FIT_CENTER, FIT_END, LEFT_TOP, LEFT_CENTER, LEFT_BOTTOM, CENTER_TOP, CENTER, CENTER_BOTTOM, RIGHT_TOP, RIGHT_CENTER, RIGHT_BOTTOM, LEFT_TOP_CROP, LEFT_CENTER_CROP, LEFT_BOTTOM_CROP, CENTER_TOP_CROP, CENTER_CROP, CENTER_BOTTOM_CROP, RIGHT_TOP_CROP, RIGHT_CENTER_CROP, RIGHT_BOTTOM_CROP, START_INSIDE, CENTER_INSIDE, END_INSIDE
+}
+
+class ScaleManager(private val viewSize: Size, private val videoSize: Size) {
+    fun getScaleMatrix(scalableType: ScalableType): Matrix = when (scalableType) {
+        ScalableType.NONE -> noScale
+        ScalableType.FIT_XY -> fitXY()
+        ScalableType.FIT_CENTER -> fitCenter()
+        ScalableType.FIT_START -> fitStart()
+        ScalableType.FIT_END -> fitEnd()
+        ScalableType.LEFT_TOP -> getOriginalScale(PivotPoint.LEFT_TOP)
+        ScalableType.LEFT_CENTER -> getOriginalScale(PivotPoint.LEFT_CENTER)
+        ScalableType.LEFT_BOTTOM -> getOriginalScale(PivotPoint.LEFT_BOTTOM)
+        ScalableType.CENTER_TOP -> getOriginalScale(PivotPoint.CENTER_TOP)
+        ScalableType.CENTER -> getOriginalScale(PivotPoint.CENTER)
+        ScalableType.CENTER_BOTTOM -> getOriginalScale(PivotPoint.CENTER_BOTTOM)
+        ScalableType.RIGHT_TOP -> getOriginalScale(PivotPoint.RIGHT_TOP)
+        ScalableType.RIGHT_CENTER -> getOriginalScale(PivotPoint.RIGHT_CENTER)
+        ScalableType.RIGHT_BOTTOM -> getOriginalScale(PivotPoint.RIGHT_BOTTOM)
+        ScalableType.LEFT_TOP_CROP -> getCropScale(PivotPoint.LEFT_TOP)
+        ScalableType.LEFT_CENTER_CROP -> getCropScale(PivotPoint.LEFT_CENTER)
+        ScalableType.LEFT_BOTTOM_CROP -> getCropScale(PivotPoint.LEFT_BOTTOM)
+        ScalableType.CENTER_TOP_CROP -> getCropScale(PivotPoint.CENTER_TOP)
+        ScalableType.CENTER_CROP -> getCropScale(PivotPoint.CENTER)
+        ScalableType.CENTER_BOTTOM_CROP -> getCropScale(PivotPoint.CENTER_BOTTOM)
+        ScalableType.RIGHT_TOP_CROP -> getCropScale(PivotPoint.RIGHT_TOP)
+        ScalableType.RIGHT_CENTER_CROP -> getCropScale(PivotPoint.RIGHT_CENTER)
+        ScalableType.RIGHT_BOTTOM_CROP -> getCropScale(PivotPoint.RIGHT_BOTTOM)
+        ScalableType.START_INSIDE -> startInside()
+        ScalableType.CENTER_INSIDE -> centerInside()
+        ScalableType.END_INSIDE -> endInside()
+    }
+
+    private fun getMatrix(sx: Float, sy: Float, px: Float, py: Float) = Matrix().apply {
+        setScale(sx, sy, px, py)
+    }
+
+    private fun getMatrix(sx: Float, sy: Float, pivotPoint: PivotPoint): Matrix = when (pivotPoint) {
+        PivotPoint.LEFT_TOP -> getMatrix(sx, sy, 0f, 0f)
+        PivotPoint.LEFT_CENTER -> getMatrix(sx, sy, 0f, viewSize.height / 2f)
+        PivotPoint.LEFT_BOTTOM -> getMatrix(sx, sy, 0f, viewSize.height.toFloat())
+        PivotPoint.CENTER_TOP -> getMatrix(sx, sy, viewSize.width / 2f, 0f)
+        PivotPoint.CENTER -> getMatrix(sx, sy, viewSize.width / 2f, viewSize.height / 2f)
+        PivotPoint.CENTER_BOTTOM -> getMatrix(sx, sy, viewSize.width / 2f, viewSize.height.toFloat())
+        PivotPoint.RIGHT_TOP -> getMatrix(sx, sy, viewSize.width.toFloat(), 0f)
+        PivotPoint.RIGHT_CENTER -> getMatrix(sx, sy, viewSize.width.toFloat(), viewSize.height / 2f)
+        PivotPoint.RIGHT_BOTTOM -> getMatrix(sx, sy, viewSize.width.toFloat(), viewSize.height.toFloat())
+    }
+
+    private val noScale: Matrix
+        get() {
+            val sx = videoSize.width / viewSize.width.toFloat()
+            val sy = videoSize.height / viewSize.height.toFloat()
+            return getMatrix(sx, sy, PivotPoint.LEFT_TOP)
+        }
+
+    private fun getFitScale(pivotPoint: PivotPoint): Matrix {
+        var sx = viewSize.width.toFloat() / videoSize.width
+        var sy = viewSize.height.toFloat() / videoSize.height
+        val minScale = min(sx, sy)
+        sx = minScale / sx
+        sy = minScale / sy
+        return getMatrix(sx, sy, pivotPoint)
+    }
+
+    private fun fitXY(): Matrix = getMatrix(1f, 1f, PivotPoint.LEFT_TOP)
+
+    private fun fitStart(): Matrix = getFitScale(PivotPoint.LEFT_TOP)
+
+    private fun fitCenter(): Matrix = getFitScale(PivotPoint.CENTER)
+
+    private fun fitEnd(): Matrix = getFitScale(PivotPoint.RIGHT_BOTTOM)
+
+    private fun getOriginalScale(pivotPoint: PivotPoint): Matrix {
+        val sx = videoSize.width / viewSize.width.toFloat()
+        val sy = videoSize.height / viewSize.height.toFloat()
+        return getMatrix(sx, sy, pivotPoint)
+    }
+
+    private fun getCropScale(pivotPoint: PivotPoint): Matrix {
+        val sx = viewSize.width.toFloat() / videoSize.width
+        val sy = viewSize.height.toFloat() / videoSize.height
+        val maxScale = max(sx, sy)
+        return getMatrix(maxScale / sx, maxScale / sy, pivotPoint)
+    }
+
+    private fun startInside(): Matrix = if (videoSize.height <= viewSize.width && videoSize.height <= viewSize.height) {
+        // video is smaller than view size
+        getOriginalScale(PivotPoint.LEFT_TOP)
+    } else {
+        // either of width or height of the video is larger than view size
+        fitStart()
+    }
+
+    private fun centerInside(): Matrix = if (videoSize.height <= viewSize.width && videoSize.height <= viewSize.height) {
+        // video is smaller than view size
+        getOriginalScale(PivotPoint.CENTER)
+    } else {
+        // either of width or height of the video is larger than view size
+        fitCenter()
+    }
+
+    private fun endInside(): Matrix = if (videoSize.height <= viewSize.width && videoSize.height <= viewSize.height) {
+        // video is smaller than view size
+        getOriginalScale(PivotPoint.RIGHT_BOTTOM)
+    } else {
+        // either of width or height of the video is larger than view size
+        fitEnd()
+    }
+}
---
+++ ring-android/app/src/main/java/cx/ring/views/VideoSinkView.kt
@@ -0,0 +1,188 @@
+package cx.ring.views
+
+import android.content.Context
+import android.content.res.TypedArray
+import android.graphics.SurfaceTexture
+import android.util.AttributeSet
+import android.util.Log
+import android.util.Size
+import android.view.Surface
+import kotlin.jvm.JvmOverloads
+import android.view.TextureView
+import androidx.core.view.isVisible
+import cx.ring.R
+import cx.ring.application.JamiApplication
+import cx.ring.utils.ScalableType
+import cx.ring.utils.ScaleManager
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import net.jami.daemon.JamiServiceJNI
+import net.jami.services.HardwareService
+
+/**
+ * A [TextureView] that can be adjusted to a specified aspect ratio.
+ */
+class VideoSinkView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyle: Int = 0, defStyleRes: Int = 0)
+    : TextureView(context, attrs, defStyle, defStyleRes), TextureView.SurfaceTextureListener {
+    private var ratioWidth = 0
+    private var ratioHeight = 0
+    private var sinkId: String? = null
+    private var nativeWindow: Long = -1
+    private var surface: Surface? = null
+    private var fitToContent: Boolean = true
+
+    private val disposableBag = CompositeDisposable()
+
+    val hardwareService: HardwareService? = JamiApplication.instance?.hardwareService
+
+    init {
+        val a:TypedArray = context.obtainStyledAttributes(attrs, R.styleable.VideoSinkView, defStyle, defStyleRes)
+        fitToContent = a.getBoolean(R.styleable.VideoSinkView_fitToContent, true)
+        setAspectRatio(a.getDimensionPixelSize(R.styleable.VideoSinkView_ratioWidth, 0),
+            a.getDimensionPixelSize(R.styleable.VideoSinkView_ratioHeight, 0))
+        a.recycle()
+    }
+
+    override fun onAttachedToWindow() {
+        Log.w(TAG, "onAttachedToWindow")
+        if (!isInEditMode) {
+            surfaceTextureListener = this
+        }
+        super.onAttachedToWindow()
+    }
+
+    override fun onDetachedFromWindow() {
+        Log.w(TAG, "onDetachedFromWindow")
+        stopSink()
+        super.onDetachedFromWindow()
+        surfaceTextureListener = null
+    }
+
+    /**
+     * Sets the aspect ratio for this view. The size of the view will be measured based on the ratio
+     * calculated from the parameters. Note that the actual sizes of parameters don't matter, that
+     * is, calling setAspectRatio(2, 3) and setAspectRatio(4, 6) make the same result.
+     *
+     * @param width  Relative horizontal size
+     * @param height Relative vertical size
+     */
+    private fun setAspectRatio(width: Int, height: Int) {
+        require(!(width < 0 || height < 0)) { "Size cannot be negative." }
+        val empty = width == 0 || height == 0
+        if (!empty && ratioWidth != width || ratioHeight != height) {
+            Log.w(TAG, "setAspectRatio: $sinkId $width $height")
+            ratioWidth = width
+            ratioHeight = height
+            requestLayout()
+            if (!fitToContent) {
+                configureTransform(width, height)
+            }
+        }
+        isVisible = !empty
+    }
+
+    fun setFitToContent(fit: Boolean) {
+        if (fitToContent != fit) {
+            fitToContent = fit
+            requestLayout()
+            if (nativeWindow != -1L)
+                configureTransform(ratioWidth, ratioHeight)
+        }
+    }
+
+    fun setSinkId(id: String?) {
+        if (sinkId != id) {
+            stopSink()
+            Log.w(TAG, "setSinkId: $id")
+            sinkId = id
+            if (id != null)
+                startSink(id)
+        }
+    }
+
+    private fun startSink(id: String) {
+        Log.w(TAG, "startSink: $id")
+        val nw = nativeWindow
+        if (nw != -1L) {
+            disposableBag.add(hardwareService!!.connectSink(id, nw)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe { size -> setAspectRatio(size.first, size.second) })
+        } else {
+            disposableBag.add(hardwareService!!.getSinkSize(id)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe { c -> setAspectRatio(c.first, c.second) })
+        }
+    }
+
+    private fun stopSink() {
+        Log.w(TAG, "stopSink: $sinkId")
+        disposableBag.clear()
+    }
+
+    private fun configureTransform(textureWidth: Int, textureHeight: Int) {
+        if (!fitToContent) {
+            setTransform(ScaleManager(Size(width, height), Size(textureWidth, textureHeight)).getScaleMatrix(ScalableType.CENTER_CROP))
+        } else {
+            setTransform(null)
+        }
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        val width = MeasureSpec.getSize(widthMeasureSpec)
+        val height = MeasureSpec.getSize(heightMeasureSpec)
+        if (0 == ratioWidth || 0 == ratioHeight) {
+            setMeasuredDimension(0, 0)
+        } else {
+            if (fitToContent) {
+                val ra = height * ratioWidth
+                val rb = width * ratioHeight
+                if (rb < ra) {
+                    setMeasuredDimension(width, rb / ratioWidth)
+                } else {
+                    setMeasuredDimension(ra / ratioHeight, height)
+                }
+            } else {
+                setMeasuredDimension(width, height)
+            }
+        }
+    }
+
+    override fun onSurfaceTextureAvailable(s: SurfaceTexture, width: Int, height: Int) {
+        if (surface == null) {
+            surface = Surface(s)
+            nativeWindow = JamiServiceJNI.acquireNativeWindow(surface)
+            JamiServiceJNI.setNativeWindowGeometry(nativeWindow, ratioWidth, ratioHeight)
+            configureTransform(ratioWidth, ratioHeight)
+        }
+        sinkId?.let { startSink(it) }
+    }
+
+    override fun onSurfaceTextureSizeChanged(s: SurfaceTexture, width: Int, height: Int) {
+        Log.w(TAG, "onSurfaceTextureSizeChanged $sinkId $width $height")
+        configureTransform(ratioWidth, ratioHeight)
+        if (surface != null) {
+            JamiServiceJNI.setNativeWindowGeometry(nativeWindow, ratioWidth, ratioHeight)
+        }
+    }
+
+    override fun onSurfaceTextureDestroyed(surfaceTexture: SurfaceTexture): Boolean {
+        Log.w(TAG, "onSurfaceTextureDestroyed")
+        stopSink()
+        surface?.let { s ->
+            JamiServiceJNI.releaseNativeWindow(nativeWindow)
+            s.release()
+            surface = null
+            nativeWindow = -1
+        }
+        sinkId?.let { startSink(it) }
+        return true
+    }
+
+    override fun onSurfaceTextureUpdated(surface: SurfaceTexture) {
+    }
+
+    companion object {
+        val TAG = VideoSinkView::class.simpleName!!
+    }
+}
