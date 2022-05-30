f8c9b65357e6f44b783dc9f0a27ee29e24df7b2d
Amirhossein <amirhossein.naghshzan@savoirfairelinux.com>
2022-05-30 14:56:00 -0400

    ContactPickerFragment: enable creating swarm groups
    Change-Id: Ia4dd19c34b86d5e9d09a6633e879de1b8e93eac5

---
+++ ring-android/app/src/main/java/cx/ring/adapters/ContactPickerAdapter.kt
@@ -0,0 +1,75 @@
+/*
+ *  Copyright (C) 2004-2022 Savoir-faire Linux Inc.
+ *
+ *  Author: AmirHossein Naghshzan <amirhossein.naghshzan@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+package cx.ring.adapters
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import cx.ring.databinding.ItemSmartlistBinding
+import cx.ring.viewholders.ContactPickerViewHolder
+import cx.ring.viewholders.ContactPickerViewHolder.ContactPickerListeners
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import net.jami.smartlist.ConversationItemViewModel
+
+class ContactPickerAdapter(
+    private var conversations: List<ConversationItemViewModel>?,
+    private val listener: ContactPickerListeners,
+    private val disposable: CompositeDisposable
+) : RecyclerView.Adapter<ContactPickerViewHolder>() {
+    private var recyclerView: RecyclerView? = null
+    private var itemCount: Int = 0
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactPickerViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        return ContactPickerViewHolder(ItemSmartlistBinding.inflate(layoutInflater, parent, false), disposable)
+    }
+
+    override fun onViewRecycled(holder: ContactPickerViewHolder) {
+        super.onViewRecycled(holder)
+        holder.unbind()
+    }
+
+    override fun onBindViewHolder(holder: ContactPickerViewHolder, position: Int) {
+        conversations!![position].let { conversation ->
+            return holder.bind(listener, conversation)
+        }
+    }
+
+    override fun getItemCount(): Int = itemCount
+
+    override fun onAttachedToRecyclerView(recyclerView: RecyclerView) {
+        super.onAttachedToRecyclerView(recyclerView)
+        this.recyclerView = recyclerView
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun update(conversations: List<ConversationItemViewModel>) {
+        this.conversations = conversations
+        itemCount = conversations.size
+        notifyDataSetChanged()
+    }
+
+    fun update(conversation: ConversationItemViewModel) {
+        val index = conversations!!.indexOf(conversation)
+        notifyItemChanged(index)
+    }
+
+}
