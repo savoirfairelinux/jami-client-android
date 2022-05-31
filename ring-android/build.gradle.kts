724c915014cc6d80a6c786db8d0ca2b2b9439080
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2022-05-31 15:48:44 -0400

    gradle: update configuration
    Change-Id: I4fe2144b101f881b455a09c7ba5ebcc223823d90

--- ring-android/build.gradle.kts
+++ ring-android/build.gradle.kts
@@ -6,10 +6,10 @@ buildscript {
     }
 
     val kotlin_version by extra { "1.6.21" }
-    val hilt_version by extra { "2.41" }
+    val hilt_version by extra { "2.42" }
 
     dependencies {
-        classpath ("com.android.tools.build:gradle:7.1.3")
+        classpath ("com.android.tools.build:gradle:7.2.1")
         classpath ("com.google.gms:google-services:4.3.10")
         classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
         classpath ("com.google.dagger:hilt-android-gradle-plugin:$hilt_version")
