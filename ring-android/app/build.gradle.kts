5e185d1cdd520e5cc23a17f489deb922d2ea9bf2
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2022-08-26 10:25:54 -0400

    build: update version to 20220826-01 (341)
    Disable android auto due to reject on play store
    
    Change-Id: I3f4c1a294059effd853438b339505b7ca3ea2699

--- ring-android/app/build.gradle.kts
+++ ring-android/app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
     defaultConfig {
         minSdk = 24
         targetSdk = 32
-        versionCode = 340
-        versionName = "20220825-01"
+        versionCode = 341
+        versionName = "20220826-01"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     sourceSets {
