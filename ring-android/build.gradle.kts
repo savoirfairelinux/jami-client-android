c03eb2240ad536be593f4460b7d798bf13b3e885
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2022-09-21 15:32:45 -0400

    build: update gradle and tools
    Change-Id: I3a898a00680f48cb256eac22cf3c7326629771bb

--- ring-android/build.gradle.kts
+++ ring-android/build.gradle.kts
@@ -9,7 +9,7 @@ buildscript {
     val hilt_version by extra { "2.43.2" }
 
     dependencies {
-        classpath ("com.android.tools.build:gradle:7.2.1")
+        classpath ("com.android.tools.build:gradle:8.0.0-alpha01")
         classpath ("com.google.gms:google-services:4.3.13")
         classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
         classpath ("com.google.dagger:hilt-android-gradle-plugin:$hilt_version")
