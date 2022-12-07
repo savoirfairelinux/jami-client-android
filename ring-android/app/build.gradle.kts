2a8109361fc84de25afd3640cfeeb6e9096ebc44
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2022-12-07 16:36:48 -0500

    update version to 20221207-01 (352)
    Change-Id: I60a682b5a20395da2807eadc82ef9ce926ccb45b

--- ring-android/app/build.gradle.kts
+++ ring-android/app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
     defaultConfig {
         minSdk = 24
         targetSdk = 33
-        versionCode = 351
-        versionName = "20221206-01"
+        versionCode = 352
+        versionName = "20221207-01"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     sourceSets {
