1759339487c317e4db1d420ba0c881b5c9040fe1
Amirhossein <amirhossein.naghshzan@savoirfairelinux.com>
2022-12-27 14:19:35 -0500

    ConversationFragment: enable calling group swarms
    Change-Id: I9d7aca009663b18d61856306efa1169bfff840df

--- jami-android/libjamiclient/src/main/kotlin/net/jami/services/AccountService.kt
+++ jami-android/libjamiclient/src/main/kotlin/net/jami/services/AccountService.kt
@@ -1261,10 +1261,12 @@ class AccountService(
                     Interaction(conversation, Interaction.InteractionType.INVALID)
                 }
             }
-            "application/call-history+json" ->
-                Call(null, account.accountId, authorUri.rawUriString, if (contact.isUser) Call.Direction.OUTGOING else Call.Direction.INCOMING,timestamp).apply {
-                    message["duration"]?.let { d -> duration = d.toLong() }
-                }
+            "application/call-history+json" -> {
+                    Call(null, account.accountId, authorUri.rawUriString, if (contact.isUser) Call.Direction.OUTGOING else Call.Direction.INCOMING, timestamp).apply {
+                        message["duration"]?.let { d -> duration = d.toLong() }
+                        message["confId"]?.let { c -> confId = c }
+                    }
+            }
             "application/update-profile" -> Interaction(conversation, Interaction.InteractionType.INVALID)
             "merge" -> Interaction(conversation, Interaction.InteractionType.INVALID)
             else -> Interaction(conversation, Interaction.InteractionType.INVALID)
