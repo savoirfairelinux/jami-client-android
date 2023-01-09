0b1498d0263e5a7634b921b11c6f445ca0799d7e
Adrien Beraud <adrien.beraud@savoirfairelinux.com>
2023-01-09 18:44:21 -0500

    add daemon submodule
    Change-Id: I701b0915d66059cd2f865b2f5b9e6b2a54b1801c

--- ring-android/app/build.gradle.kts
+++ ring-android/app/build.gradle.kts
@@ -79,7 +79,7 @@ android {
     namespace = "cx.ring"
     externalNativeBuild {
         cmake {
-            path = file("../../../daemon/CMakeLists.txt")
+            path = file("../../daemon/CMakeLists.txt")
             version = "3.22.1"
         }
     }
