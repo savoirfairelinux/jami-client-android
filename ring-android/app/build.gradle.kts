72cbc236d0e63b3131629ed49558bf77e75ec8db
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2023-01-13 15:08:15 -0500

    update version to 20230113-01 (357)
    Change-Id: Ied4d1e9b5207bea32d84857ca01cfa9f2cb27782

--- ring-android/app/build.gradle.kts
+++ ring-android/app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     defaultConfig {
         minSdk = 24
         targetSdk = 33
-        versionCode = 356
-        versionName = "20221225-01"
+        versionCode = 357
+        versionName = "20230113-01"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         externalNativeBuild {
             cmake {
