726c9f619ba8e458efd6e96ea8180869b0ae9d58
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2023-01-14 17:02:28 -0500

    gradle: update
    Change-Id: Idf55018d1ac88f6429788fc1976db8b8ac58a3ed

--- ring-android/build.gradle.kts
+++ ring-android/build.gradle.kts
@@ -6,10 +6,10 @@ buildscript {
     }
 
     val kotlin_version by extra { "1.8.0" }
-    val hilt_version by extra { "2.44" }
+    val hilt_version by extra { "2.44.2" }
 
     dependencies {
-        classpath ("com.android.tools.build:gradle:7.3.1")
+        classpath ("com.android.tools.build:gradle:7.4.0")
         classpath ("com.google.gms:google-services:4.3.14")
         classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
         classpath ("com.google.dagger:hilt-android-gradle-plugin:$hilt_version")
