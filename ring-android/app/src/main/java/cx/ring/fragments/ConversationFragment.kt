be33e4021e35e05f8ae13c85dab4d105ff807b93
alussier-cullen <alexander.lussier-cullen@savoirfairelinux.com>
2023-01-16 16:41:28 -0500

    conversation: add scroll to linked message
    Change-Id: If811dcf87bc2dbcec2fef444d00f4e74c3bb10ea

--- ring-android/app/src/main/java/cx/ring/fragments/ConversationFragment.kt
+++ ring-android/app/src/main/java/cx/ring/fragments/ConversationFragment.kt
@@ -862,13 +862,14 @@ class ConversationFragment : BaseSupportFragment<ConversationPresenter, Conversa
     }
 
     override fun initPresenter(presenter: ConversationPresenter) {
-        val path = ConversationPath.fromBundle(arguments)
-        mIsBubble = requireArguments().getBoolean(NotificationServiceImpl.EXTRA_BUBBLE)
+        val args = requireArguments()
+        val path = ConversationPath.fromBundle(args) ?: return
+        val selection = args.getString(EXTRA_SELECTION) ?: "ae356a99760e677e082bb94d813b68d7298eeaf4" // TODO remove test id
+        mIsBubble = args.getBoolean(NotificationServiceImpl.EXTRA_BUBBLE)
         Log.w(TAG, "initPresenter $path")
-        if (path == null) return
         val uri = path.conversationUri
         mAdapter = ConversationAdapter(this, presenter)
-        presenter.init(uri, path.accountId)
+        presenter.init(uri, path.accountId, selection = selection)
         try {
             mPreferences = getConversationPreferences(requireContext(), path.accountId, uri).also { preferences ->
                 preferences.registerOnSharedPreferenceChangeListener(this)
@@ -1238,6 +1239,7 @@ class ConversationFragment : BaseSupportFragment<ConversationPresenter, Conversa
         const val KEY_PREFERENCE_CONVERSATION_LAST_READ = "lastRead"
         const val KEY_PREFERENCE_CONVERSATION_SYMBOL = "symbol"
         const val EXTRA_SHOW_MAP = "showMap"
+        const val EXTRA_SELECTION = "messageId"
         private const val REQUEST_CODE_FILE_PICKER = 1000
         private const val REQUEST_PERMISSION_CAMERA = 1001
         private const val REQUEST_CODE_TAKE_PICTURE = 1002
