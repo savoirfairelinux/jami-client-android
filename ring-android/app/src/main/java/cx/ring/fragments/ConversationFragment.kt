e0c20ed813abb9209792f9a3cee10980edce6a72
alussier-cullen <alexander.lussier-cullen@savoirfairelinux.com>
2023-01-17 16:39:45 -0500

    conversation: add scroll to search result
    Change-Id: I96855946ac19b2b35904a6bfb20d8bef952e2da7

--- ring-android/app/src/main/java/cx/ring/fragments/ConversationFragment.kt
+++ ring-android/app/src/main/java/cx/ring/fragments/ConversationFragment.kt
@@ -808,7 +808,7 @@ class ConversationFragment : BaseSupportFragment<ConversationPresenter, Conversa
             }
             override fun onMenuItemActionExpand(item: MenuItem): Boolean {
                 val binding = binding ?: return false
-                mSearchAdapter = ConversationAdapter(this@ConversationFragment, presenter)
+                mSearchAdapter = ConversationAdapter(this@ConversationFragment, presenter, true)
                 presenter.startSearch()
                 currentBottomView?.isVisible = false
                 binding.histList.adapter = mSearchAdapter
@@ -1227,6 +1227,13 @@ class ConversationFragment : BaseSupportFragment<ConversationPresenter, Conversa
 
     override fun hideErrorPanel() {
         binding?.errorMsgPane?.visibility = View.GONE
+    }
+
+    override fun goToSearchMessage(messageId: String) {
+        binding?.toolbar?.menu?.findItem(R.id.conv_search)?.collapseActionView()
+        binding?.histList?.doOnNextLayout {
+            presenter.scrollToMessage(messageId)
+        }
     }
 
     companion object {
