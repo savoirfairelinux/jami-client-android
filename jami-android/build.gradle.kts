0d52eb87effafb382617a19c0de19b179f74fbeb
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2023-02-10 14:25:02 -0500

    gradle: update dependencies
    Change-Id: Ibbc9ba0b188bb576cd3dd8c5c6a663797d08be0d

--- jami-android/build.gradle.kts
+++ jami-android/build.gradle.kts
@@ -6,11 +6,11 @@ buildscript {
     }
 
     val kotlin_version by extra { "1.8.0" }
-    val hilt_version by extra { "2.44.2" }
+    val hilt_version by extra { "2.45" }
 
     dependencies {
         classpath ("com.android.tools.build:gradle:7.4.1")
-        classpath ("com.google.gms:google-services:4.3.14")
+        classpath ("com.google.gms:google-services:4.3.15")
         classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
         classpath ("com.google.dagger:hilt-android-gradle-plugin:$hilt_version")
     }
