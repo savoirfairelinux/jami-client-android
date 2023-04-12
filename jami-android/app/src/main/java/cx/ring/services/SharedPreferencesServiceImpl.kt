410703195eadafca598573eb0bffc2efc0d75dcc
Pierre Nicolas <pierre.nicolas@savoirfairelinux.com>
2023-04-12 13:39:51 -0400

    logging: preserve logging on accross reboots/restarts
    GitLab: #1227
    Change-Id: I94305a1e8b5aa22f9221fd45d7f89e2cf1586dec

--- jami-android/app/src/main/java/cx/ring/services/SharedPreferencesServiceImpl.kt
+++ jami-android/app/src/main/java/cx/ring/services/SharedPreferencesServiceImpl.kt
@@ -118,6 +118,10 @@ class SharedPreferencesServiceImpl(private val context: Context, accountService:
     override val bitrate: Int
         get() = videoPreferences.getString(PREF_BITRATE, context.getString(R.string.video_bitrate_default))!!.toInt()
 
+    override var isLogActive: Boolean
+        get() = preferences.getBoolean(PREF_LOG_IS_ACTIVE, false)
+        set(enabled) = preferences.edit().putBoolean(PREF_LOG_IS_ACTIVE, enabled).apply()
+
     override val isHardwareAccelerationEnabled: Boolean
         get() = videoPreferences.getBoolean(PREF_HW_ENCODING, true)
 
@@ -170,6 +174,7 @@ class SharedPreferencesServiceImpl(private val context: Context, accountService:
         const val PREF_DARK_MODE = "darkMode"
         private const val PREF_ACCEPT_IN_MAX_SIZE = "acceptIncomingFilesMaxSize"
         const val PREF_PLUGINS = "plugins"
+        private const val PREF_LOG_IS_ACTIVE = "preference_log_is_active"
 
         fun getConversationPreferences(context: Context, accountId: String, conversationUri: Uri): SharedPreferences {
             return context.getSharedPreferences(accountId + "_" + conversationUri.uri, Context.MODE_PRIVATE)
