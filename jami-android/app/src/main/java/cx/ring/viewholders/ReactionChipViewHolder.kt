edf08e62d567b5ef1777a48fc8aebbb83f091911
Pierre Nicolas <pierre.nicolas@savoirfairelinux.com>
2023-04-18 11:00:45 -0400

    conversation: Implement adapter for reaction chips
    Will display reactions in a grid (line of 5) and make them call a remove interaction function at click.
    
    GitLab: #1236
    
    Change-Id: I67b7603cfc62fb4e3916318ca5c7efb8a1faf830

---
+++ jami-android/app/src/main/java/cx/ring/viewholders/ReactionChipViewHolder.kt
@@ -0,0 +1,8 @@
+package cx.ring.viewholders
+
+import androidx.recyclerview.widget.RecyclerView
+import cx.ring.databinding.ItemReactionChipBinding
+
+class ReactionChipViewHolder(
+    val binding: ItemReactionChipBinding
+) : RecyclerView.ViewHolder(binding.root)
