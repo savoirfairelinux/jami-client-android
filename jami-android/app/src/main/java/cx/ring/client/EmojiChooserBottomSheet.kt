c29f5a44fbbe80a3f055a1e1207d798b17314118
Pierre Nicolas <pierre.nicolas@savoirfairelinux.com>
2023-04-26 14:44:37 -0400

    configure "emoji chooser" bottomsheetdialog
    make it expanded and skip collapsed state (when component is hidden)
    
    GitLab: #1202
    Change-Id: Icc523f17ab029d456a91dda33b165a3776797f0d

--- jami-android/app/src/main/java/cx/ring/client/EmojiChooserBottomSheet.kt
+++ jami-android/app/src/main/java/cx/ring/client/EmojiChooserBottomSheet.kt
@@ -19,6 +19,7 @@
  */
 package cx.ring.client
 
+import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -26,6 +27,8 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.annotation.ArrayRes
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import cx.ring.R
 
@@ -58,4 +61,13 @@ class EmojiChooserBottomSheet(val onEmojiSelected: ((String?) -> Unit)? = null) 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View =
         (inflater.inflate(R.layout.frag_color_chooser, container) as RecyclerView)
             .apply { adapter = ColorAdapter(R.array.conversation_emojis) }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val dialog = super.onCreateDialog(savedInstanceState)
+        (dialog as BottomSheetDialog).behavior.apply {
+            state = BottomSheetBehavior.STATE_EXPANDED
+            skipCollapsed = true
+        }
+        return dialog
+    }
 }
