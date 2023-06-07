32d5ec28fdee0755f58ea77384fde8e8d3d8ce1a
Pierre Nicolas <pierre.nicolas@savoirfairelinux.com>
2023-06-07 11:22:22 -0400

    wip create asscociated fragment
    Change-Id: I3c8d4f1887b2c1717a9ae2301ca41eb987837f67

---
+++ jami-android/app/src/main/java/cx/ring/welcomejami/WelcomeJamiFragment.kt
@@ -0,0 +1,31 @@
+package cx.ring.welcomejami
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import cx.ring.databinding.WelcomeJamiCardBinding
+
+class WelcomeJamiFragment() : Fragment() {
+    private var binding: WelcomeJamiCardBinding? = null
+    private var jamiId: String = ""
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View =
+        WelcomeJamiCardBinding.inflate(inflater, container, false).apply {
+            jamiIdCard.editButton.setOnClickListener { }
+            jamiIdCard.copyButton.setOnClickListener { }
+            jamiIdCard.shareButton.setOnClickListener { }
+            jamiIdCard.jamiIdTextView.text = jamiId
+            binding = this
+        }.root
+
+    fun setJamiId(newJamiId: String) {
+        jamiId = newJamiId
+        binding?.let { it.jamiIdCard.jamiIdTextView.text = jamiId }
+    }
+}
