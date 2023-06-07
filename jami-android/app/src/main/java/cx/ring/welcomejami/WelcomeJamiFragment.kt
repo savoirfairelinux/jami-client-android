90a156d0c8eef066a97ff95648233811916942fc
Pierre Nicolas <pierre.nicolas@savoirfairelinux.com>
2023-06-07 11:22:34 -0400

    Integrate WelcomeJamiFragment to Home Activity
    - Manage OnRestore method
    - Manage OnBackPressed method
    - Manage device with and without slideable panel
    
    GitLab: #1292
    
    Change-Id: Ie32de9daf9be308682526c54e877f1f777a000ea

--- jami-android/app/src/main/java/cx/ring/welcomejami/WelcomeJamiFragment.kt
+++ jami-android/app/src/main/java/cx/ring/welcomejami/WelcomeJamiFragment.kt
@@ -9,11 +9,10 @@ import cx.ring.databinding.WelcomeJamiCardBinding
 import cx.ring.utils.ActionHelper.copyAndShow
 import cx.ring.utils.ActionHelper.shareAccount
 
-class WelcomeJamiFragment(
-    private val jamiId: String = "",
-    private val isJamiAccount: Boolean = true,
-) : Fragment() {
+class WelcomeJamiFragment() : Fragment() {
     private var binding: WelcomeJamiCardBinding? = null
+    private var jamiId: String = ""
+    private var isJamiAccount: Boolean = true
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,6 +20,10 @@ class WelcomeJamiFragment(
         savedInstanceState: Bundle?,
     ): View =
         WelcomeJamiCardBinding.inflate(inflater, container, false).apply {
+            requireArguments().let {
+                jamiId = it.getString(ARG_JAMI_ID, "")
+                isJamiAccount = it.getBoolean(ARG_IS_JAMI_ACCOUNT, true)
+            }
 //            jamiIdCard.editButton.setOnClickListener { }
             jamiIdCard.copyButton.setOnClickListener {
                 copyAndShow(requireContext(), binding!!.root, jamiId)
@@ -37,4 +40,17 @@ class WelcomeJamiFragment(
             jamiIdCard.jamiIdTextView.text = jamiId
             binding = this
         }.root
+
+    companion object {
+        private const val ARG_JAMI_ID = "jami_id"
+        private const val ARG_IS_JAMI_ACCOUNT = "is_jami_account"
+
+        fun newInstance(jamiId: String, isJamiAccount: Boolean): WelcomeJamiFragment =
+            WelcomeJamiFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_JAMI_ID, jamiId)
+                    putBoolean(ARG_IS_JAMI_ACCOUNT, isJamiAccount)
+                }
+            }
+    }
 }
