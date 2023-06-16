911f4f7dea4c582d7c43d9f7dba12b092289e637
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2023-06-16 14:28:43 -0400

    misc: update copyright date
    Change-Id: Id17fe3f95bf917d40e85c03e707f47878d4fb290
    GitLab: #1287

--- jami-android/app/src/main/java/cx/ring/fragments/HomeFragment.kt
+++ jami-android/app/src/main/java/cx/ring/fragments/HomeFragment.kt
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2004-2022 Savoir-faire Linux Inc.
+ *  Copyright (C) 2004-2023 Savoir-faire Linux Inc.
  *
  *  Author: Aline Bonnet <aline.bonnet@savoirfairelinux.com>
  *
--- jami-android/libjamiclient/src/main/kotlin/net/jami/utils/SwigNativeConverter.kt
+++ jami-android/libjamiclient/src/main/kotlin/net/jami/utils/SwigNativeConverter.kt
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2004-2022 Savoir-faire Linux Inc.
+ *  Copyright (C) 2004-2023 Savoir-faire Linux Inc.
  *
  *  Author: Alexandre Lision <alexandre.lision@savoirfairelinux.com>
  *
