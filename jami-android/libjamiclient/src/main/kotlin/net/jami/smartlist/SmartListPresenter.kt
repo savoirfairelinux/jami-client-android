2ca370d803ff0eb25df6151b0dec76188cddd1d9
Pierre Nicolas <pierre.nicolas@savoirfairelinux.com>
2023-06-28 15:42:56 -0400

    home: implement new home fragment design (step2)
    Implement design using coordinatorlayout and appbarlayout
    
    GitLab: #1293
    Change-Id: I73b1659e6ebef026156e8a9d4bfa06bdfb78a4e1

--- jami-android/libjamiclient/src/main/kotlin/net/jami/smartlist/SmartListPresenter.kt
+++ jami-android/libjamiclient/src/main/kotlin/net/jami/smartlist/SmartListPresenter.kt
@@ -78,10 +78,6 @@ class SmartListPresenter @Inject constructor(
         view?.displayConversationDialog(conversationItemViewModel)
     }
 
-    fun fabButtonClicked() {
-        view?.displayMenuItem()
-    }
-
     private fun startConversation(accountId: String, conversationUri: Uri?) {
         Log.w(TAG, "startConversation $accountId $conversationUri")
         val view = view
