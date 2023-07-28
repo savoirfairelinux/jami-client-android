625b95678e82d5203d8c11a167663c6afb223980
Pierre Nicolas <pierre.nicolas@savoirfairelinux.com>
2023-07-28 16:56:12 -0400

    Create JamiId fragment
    GitLab: #1292
    Change-Id: I653e7a98acc2a1d8778ec118f886e6daea1332ef

---
+++ jami-android/app/src/main/java/cx/ring/fragments/JamiIdFragment.kt
@@ -0,0 +1,66 @@
+package cx.ring.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import cx.ring.databinding.JamiIdLayoutBinding
+import cx.ring.utils.ActionHelper.copyAndShow
+import cx.ring.utils.ActionHelper.shareAccount
+
+class JamiIdFragment : Fragment() {
+
+    private var binding: JamiIdLayoutBinding? = null
+    private var jamiId: String = ""
+
+    companion object {
+        private const val ARG_JAMI_ID = "jami_id"
+
+        fun newInstance() =
+            JamiIdFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_JAMI_ID, jamiId)
+                }
+            }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? =
+        JamiIdLayoutBinding.inflate(inflater, container, false).apply {
+            requireArguments().let {
+                jamiId = it.getString(ARG_JAMI_ID, "")
+            }
+
+            binding?.jamiIdTextView?.text = jamiId
+
+            binding?.jamiIdTextView?.textSize =
+                if (jamiId.length > 16) {
+                    context?.resources?.getDimensionPixelSize(
+                        cx.ring.R.dimen.jami_id_small_font_size
+                    )!!.toFloat()
+                } else {
+                    context?.resources?.getDimensionPixelSize(
+                        cx.ring.R.dimen.jami_id_regular_font_size
+                    )!!.toFloat()
+                }
+
+            jamiIdCopyButton.setOnClickListener {
+                copyAndShow(requireContext(), binding!!.root, jamiId)
+            }
+            jamiIdShareButton.setOnClickListener {
+                shareAccount(requireContext(), jamiId)
+            }
+            binding = this
+        }.root
+
+    /**
+     * Set the Jami ID to display (adapt the text size depending on the text length)
+     */
+    private fun setJamiId(jamiId: String) {
+        this.jamiId = jamiId
+    }
+
+}
