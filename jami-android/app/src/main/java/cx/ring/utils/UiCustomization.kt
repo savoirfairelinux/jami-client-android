9bae1c09ec4afa1b31ff2dff53f1f13d13b75e24
Pierre Nicolas <pierre.nicolas@savoirfairelinux.com>
2023-08-16 14:56:45 -0400

    Create UiCustomization data class
    Add a loader method from Json format
    
    Change-Id: I1aa08e6580b1caad40fca152d10bf252e8bdf7a7

---
+++ jami-android/app/src/main/java/cx/ring/utils/UiCustomization.kt
@@ -0,0 +1,83 @@
+package cx.ring.utils
+
+import android.graphics.Color
+import net.jami.utils.Log
+import org.json.JSONObject
+
+private val tag = UiCustomization::class.simpleName!!
+
+enum class BackgroundType {
+    COLOR,
+    IMAGE,
+    DEFAULT
+}
+
+data class UiCustomization(
+    val title: String?,
+    val description: String?,
+    val areTipsEnabled: Boolean,
+    val backgroundType: BackgroundType,
+    val backgroundColor: Int?,
+    val backgroundUrl: String?,
+    val tipBoxAndIdColor: Int?,
+    val mainBoxColor: Int?,
+    val logoUrl: String?,
+    val logoSize: Int?, // The logo size from 0 to 100%.
+)
+
+/**
+ * Get the UiCustomization from the configuration JSON
+ * @param configurationJson The JSON object containing the configuration
+ * @param managerUri The URI of the manager (base url)
+ */
+fun getUiCustomizationFromConfigJson(
+    configurationJson: JSONObject,
+    managerUri: String,
+): UiCustomization? {
+    val logoUrl = if (configurationJson.has("logoUrl"))
+        configurationJson.optString("logoUrl") else null
+    val backgroundUrl = if (configurationJson.has("backgroundColorOrUrl"))
+        configurationJson.optString("backgroundColorOrUrl") else null
+
+    return try {
+        UiCustomization(
+            title = if (configurationJson.has("title"))
+                configurationJson.optString("title") else null,
+            description = if (configurationJson.has("description"))
+                configurationJson.optString("description") else null,
+            areTipsEnabled = configurationJson.optBoolean("areTipsEnabled", true),
+            backgroundType = BackgroundType.valueOf(
+                configurationJson.optString(
+                    "backgroundType",
+                    "default"
+                )
+            ),
+            backgroundColor =
+            try {
+                Color.parseColor(configurationJson.optString("backgroundColorOrUrl"))
+            } catch (e: Exception) {
+                null
+            },
+            backgroundUrl = if (backgroundUrl != null) managerUri + backgroundUrl else null,
+            tipBoxAndIdColor =
+            try {
+                Color.parseColor(configurationJson.optString("tipBoxAndIdColor"))
+            } catch (e: Exception) {
+                null
+            },
+            mainBoxColor =
+            try {
+                Color.parseColor(configurationJson.optString("mainBoxColor"))
+            } catch (e: Exception) {
+                null
+            },
+            logoUrl = if (logoUrl != null) managerUri + logoUrl else null,
+            logoSize =
+            if (configurationJson.isNull("logoSize")) null
+            else configurationJson.getInt("logoSize"),
+        )
+    } catch (e: Exception) {
+        Log.e(tag, "Error while parsing the configuration JSON")
+        null
+    }
+}
