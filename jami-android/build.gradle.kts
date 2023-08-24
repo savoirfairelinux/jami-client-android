b266b6bb2a87c9f9578b03052f97c2aa5280f2c0
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2023-08-24 13:45:28 -0400

    update gradle to 8.1.1
    Change-Id: Ib48d19f663115e11fa3ecabf5b5e115496c29d39

--- jami-android/build.gradle.kts
+++ jami-android/build.gradle.kts
@@ -9,7 +9,7 @@ buildscript {
     val hilt_version by extra { "2.47" }
 
     dependencies {
-        classpath ("com.android.tools.build:gradle:8.1.0")
+        classpath ("com.android.tools.build:gradle:8.1.1")
         classpath ("com.google.gms:google-services:4.3.15")
         classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
         classpath ("com.google.dagger:hilt-android-gradle-plugin:$hilt_version")
