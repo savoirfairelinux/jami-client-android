d3e3db36b230bbb69f2e009bf7b1c077d0476b74
Emma Falkiewitz <emma.falkiewitz@savoirfairelinux.com>
2023-09-14 10:43:27 -0400

    ShareFragment: updated depreciated code
    Change-Id: I8e5e6b2669e2f8fb69ed6dfa46502213e6741ff7

--- jami-android/app/src/main/java/cx/ring/share/ShareFragment.kt
+++ jami-android/app/src/main/java/cx/ring/share/ShareFragment.kt
@@ -64,7 +64,11 @@ class ShareFragment : BaseSupportFragment<SharePresenter, GenericView<ShareViewM
 
     override fun showViewModel(viewModel: ShareViewModel) {
         if (binding == null) return
-        val qrCodeData = viewModel.getAccountQRCodeData(resources.getColor(R.color.color_primary_dark), resources.getColor(R.color.transparent))
+        val qrCodeData = viewModel.getAccountQRCodeData(
+            requireContext().getColor(R.color.color_primary_dark),
+            requireContext().getColor(R.color.transparent)
+        )
+
         if (qrCodeData == null) {
             binding!!.qrImage.visibility = View.INVISIBLE
             binding!!.shareInstruction.setText(R.string.share_message_no_account)
