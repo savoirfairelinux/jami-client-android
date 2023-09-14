4e161b6bbaa504d0d96f1ebd1bc7630469b4e51b
Alexander Lussier-Cullen <alexander.lussier-cullen@savoirfairelinux.com>
2023-09-14 10:25:02 -0400

    HomeFragment: add rx calls to disposables
    Change-Id: I9a63dc7f848b54eb806d27145f67aa688643cefb

--- jami-android/app/src/main/java/cx/ring/fragments/HomeFragment.kt
+++ jami-android/app/src/main/java/cx/ring/fragments/HomeFragment.kt
@@ -373,13 +373,14 @@ class HomeFragment : BaseSupportFragment<HomePresenter, HomeView>(),
         binding.appBarContainer.fitsSystemWindows = false
 
         // Update AppBarLayoutBottom padding.
-        mAccountService.currentAccountSubject
+        mDisposable.add(mAccountService.currentAccountSubject
             .switchMap { account ->
                 account.getPendingSubject()
             }
             .firstOrError().subscribe { list ->
                 updateAppBarLayoutBottomPadding(hasInvites = list.isNotEmpty())
             }
+        )
 
         // Make the invitation card wrap content (not take all space available anymore).
         binding.appBar.updateLayoutParams {
@@ -606,13 +607,14 @@ class HomeFragment : BaseSupportFragment<HomePresenter, HomeView>(),
         mBinding!!.invitationCard.invitationGroup.isVisible = isVisible
 
         // Update padding AppBarLayoutBottom and conversation list.
-        mAccountService.currentAccountSubject
+        mDisposable.add(mAccountService.currentAccountSubject
             .switchMap { account ->
                 account.getPendingSubject()
             }
             .firstOrError().subscribe { list ->
                 updateAppBarLayoutBottomPadding(hasInvites = list.isNotEmpty())
             }
+        )
     }
 
     companion object {
