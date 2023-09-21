ce5f9ebf030c95152947892b058e0dc2b5e438f4
Emma Falkiewitz <emma.falkiewitz@savoirfairelinux.com>
2023-09-21 17:31:11 -0400

    chatView: changed contact event message
    Delete the avatar in the contact event message.
    Put the name, event, date and time in one single line.
    Add a coma and a space after the contact event message (someone has joined, left the conversation etc).
    So it's like "Bob was invited, OCT 6, 09:54". Screnshots are available in the ticket Gitlab.
    
    GitLab: #1382
    Change-Id: I055e132382fb8cd2b06b499689b04952da62fafe

--- jami-android/app/src/main/java/cx/ring/adapters/ConversationAdapter.kt
+++ jami-android/app/src/main/java/cx/ring/adapters/ConversationAdapter.kt
@@ -1249,13 +1249,14 @@ class ConversationAdapter(
                         .withContact(vm)
                         .withCircleCrop(true)
                         .build(viewHolder.itemView.context))
-                    viewHolder.mMsgTxt?.text = viewHolder.itemView.context.getString(when (event.event) {
+                    val eventString = viewHolder.itemView.context.getString(when (event.event) {
                         ContactEvent.Event.ADDED -> R.string.conversation_contact_added
                         ContactEvent.Event.INVITED -> R.string.conversation_contact_invited
                         ContactEvent.Event.REMOVED -> R.string.conversation_contact_left
                         ContactEvent.Event.BANNED -> R.string.conversation_contact_banned
                         else -> R.string.hist_contact_added
                     }, vm.displayName)
+                    viewHolder.mMsgTxt?.text = "$eventString, "
                 })
             /*viewHolder.compositeDisposable.add(Observable.combineLatest(
                 presenter.contactService.observeContact(event.account!!, event.contact!!, false),
@@ -1277,14 +1278,15 @@ class ConversationAdapter(
                 })*/
         } else {
             viewHolder.mImage?.setImageResource(R.drawable.baseline_person_24)
-            viewHolder.mMsgTxt?.setText(when (event.event) {
+            val eventString = when (event.event) {
                 ContactEvent.Event.ADDED -> R.string.hist_contact_added
                 ContactEvent.Event.INVITED -> R.string.hist_contact_invited
                 ContactEvent.Event.REMOVED -> R.string.hist_contact_left
                 ContactEvent.Event.BANNED -> R.string.hist_contact_banned
                 ContactEvent.Event.INCOMING_REQUEST -> R.string.hist_invitation_received
                 else -> R.string.hist_contact_added
-            })
+            }
+            viewHolder.mMsgTxt?.text = "$eventString, "
         }
     }
 
