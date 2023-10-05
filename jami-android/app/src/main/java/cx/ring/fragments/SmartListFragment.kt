64c79e8e9eb676dbf467498a552f917d37b4ed12
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2023-10-05 01:11:46 -0400

    smartlist: remove message on copy
    Change-Id: I9a1a04a413d01658f202efb89167b591fc1750f1

--- jami-android/app/src/main/java/cx/ring/fragments/SmartListFragment.kt
+++ jami-android/app/src/main/java/cx/ring/fragments/SmartListFragment.kt
@@ -99,9 +99,6 @@ class SmartListFragment : BaseSupportFragment<SmartListPresenter, SmartListView>
 
     override fun copyContactNumberToClipboard(contactNumber: String) {
         TextUtils.copyToClipboard(requireContext(), contactNumber)
-        val snackbarText = getString(R.string.conversation_action_copied_peer_number_clipboard,
-            TextUtils.getShortenedNumber(contactNumber))
-        Snackbar.make(binding!!.listCoordinator, snackbarText, Snackbar.LENGTH_LONG).show()
     }
 
     override fun displayChooseNumberDialog(numbers: Array<CharSequence>) {
