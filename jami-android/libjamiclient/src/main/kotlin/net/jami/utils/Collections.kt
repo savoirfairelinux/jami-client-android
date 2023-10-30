1f43d743fe8b68a99fb41947a1a9abb9d252137c
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2023-10-29 22:26:28 -0400

    update license header
    Change-Id: Iefff4a47095b0376ccec14d3599d0c5bd7a38e08

--- jami-android/libjamiclient/src/main/kotlin/net/jami/utils/Collections.kt
+++ jami-android/libjamiclient/src/main/kotlin/net/jami/utils/Collections.kt
@@ -1,6 +1,22 @@
+/*
+ *  Copyright (C) 2004-2023 Savoir-faire Linux Inc.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
 package net.jami.utils
 
-public fun <T> Iterable<T>.takeFirstWhile(n: Int, predicate: (T) -> Boolean): List<T> {
+fun <T> Iterable<T>.takeFirstWhile(n: Int, predicate: (T) -> Boolean): List<T> {
     require(n >= 0) { "Requested element count $n is less than zero." }
     if (n == 0) return emptyList()
     var count = 0
