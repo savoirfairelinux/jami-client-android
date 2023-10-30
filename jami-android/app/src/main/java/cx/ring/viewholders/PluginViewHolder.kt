e5c5566cba574a901354e0487034cceee87bc9a9
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2023-10-29 22:26:28 -0400

    update license header
    Change-Id: Iefff4a47095b0376ccec14d3599d0c5bd7a38e08

--- jami-android/app/src/main/java/cx/ring/viewholders/PluginViewHolder.kt
+++ jami-android/app/src/main/java/cx/ring/viewholders/PluginViewHolder.kt
@@ -1,18 +1,29 @@
+/*
+ *  Copyright (C) 2004-2023 Savoir-faire Linux Inc.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
 package cx.ring.viewholders
 
-import android.util.Log
 import android.view.View
 import android.widget.CheckBox
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import cx.ring.R
 import cx.ring.settings.pluginssettings.PluginDetails
 import cx.ring.adapters.PluginsAdapter.PluginListItemListener
-import cx.ring.fragments.CallFragment
-import net.jami.daemon.JamiService
-import java.util.*
 
 class PluginViewHolder(itemView: View, listener: PluginListItemListener) : RecyclerView.ViewHolder(itemView) {
     private val pluginIcon: ImageView = itemView.findViewById(R.id.plugin_item_icon)
--- jami-android/app/src/main/java/cx/ring/views/ParticipantView.kt
+++ jami-android/app/src/main/java/cx/ring/views/ParticipantView.kt
@@ -1,14 +1,24 @@
+/*
+ *  Copyright (C) 2004-2023 Savoir-faire Linux Inc.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
 package cx.ring.views
 
 import androidx.recyclerview.widget.RecyclerView
 import cx.ring.databinding.ItemConferenceParticipantBinding
 import io.reactivex.rxjava3.disposables.Disposable
-
-/*
-class ParticipantView(val participantBinding: ItemConferenceParticipantBinding? = null, val addBinding: ItemConferenceAddBinding? = null)
-    : RecyclerView.ViewHolder(participantBinding?.root ?: addBinding!!.root) {
-    var disposable: Disposable? = null
-}*/
 
 class ParticipantView(val participantBinding: ItemConferenceParticipantBinding): RecyclerView.ViewHolder(participantBinding.root) {
     var disposable: Disposable? = null
