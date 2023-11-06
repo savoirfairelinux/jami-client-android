fb33a72794c96ec4e640b717ee11adbcf5d4e01c
Emma Falkiewitz <emma.falkiewitz@savoirfairelinux.com>
2023-11-06 15:48:05 -0500

    chatView: change chooser color
    Page 2 of the suggestions
    GitLab: #1382
    
    Change-Id: I575f3a8ac45ea172e82a1b8fc451564f3b2e0c43

--- jami-android/app/src/main/java/cx/ring/client/ColorChooserBottomSheet.kt
+++ jami-android/app/src/main/java/cx/ring/client/ColorChooserBottomSheet.kt
@@ -64,14 +64,14 @@ class ColorChooserBottomSheet(val onColorSelected: ((Int) -> Unit)? = null) : Bo
 
     companion object {
         private val colors = intArrayOf(
-            R.color.pink_500,
-            R.color.purple_500, R.color.deep_purple_500,
-            R.color.indigo_500, R.color.blue_500,
-            R.color.cyan_500, R.color.teal_500,
-            R.color.green_500, R.color.light_green_500,
-            R.color.grey_500, R.color.lime_500,
-            R.color.amber_500, R.color.deep_orange_500,
-            R.color.brown_500, R.color.blue_grey_500
+            R.color.pink,
+            R.color.purple, R.color.indigo,
+            R.color.dark_blue, R.color.blue,
+            R.color.blue_jean, R.color.cyan,
+            R.color.teal, R.color.green,
+            R.color.lime, R.color.yellow,
+            R.color.orange, R.color.red,
+            R.color.brown, R.color.grey
         )
     }
 }
