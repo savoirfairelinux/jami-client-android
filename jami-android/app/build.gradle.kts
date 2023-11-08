1e641953274c3dbe501b1e095a53b76005e7bbbb
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2023-11-08 15:03:41 -0500

    update version to 20231108-01 (391)
    Change-Id: I2c1ffafd528111bb5cdf633e3f51d09fa395801d

--- jami-android/app/build.gradle.kts
+++ jami-android/app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
     defaultConfig {
         minSdk = 24
         targetSdk = 34
-        versionCode = 390
-        versionName = "20231103-01"
+        versionCode = 391
+        versionName = "20231108-01"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         externalNativeBuild {
             cmake {
