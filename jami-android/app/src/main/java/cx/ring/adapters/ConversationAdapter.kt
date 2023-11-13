5a5c56316d8a573a56b44d630c99a3ae399b4fe7
Emma Falkiewitz <emma.falkiewitz@savoirfairelinux.com>
2023-11-13 16:34:53 -0500

    chatView: set the magnetization between text and call message
    Also for call started button
    GitLab: #1382
    
    Change-Id: Idf02cf928e67e08f0dde4eab9af766f742371e64

--- jami-android/app/src/main/java/cx/ring/adapters/ConversationAdapter.kt
+++ jami-android/app/src/main/java/cx/ring/adapters/ConversationAdapter.kt
@@ -1374,7 +1374,6 @@ class ConversationAdapter(
                     val textMessage = lastElement as Call
                     val isTimeShown = hasPermanentTimeString(textMessage, position)
                     val msgSequenceType = getMsgSequencing(position, isTimeShown)
-
                     val callInfoLayout = convViewHolder.mCallInfoLayout ?: return@subscribe
                     callInfoLayout.background?.setTintList(null)
                     val callIcon = convViewHolder.mIcon ?: return@subscribe
@@ -1383,6 +1382,11 @@ class ConversationAdapter(
                     val detailCall = convViewHolder.mHistDetailTxt ?: return@subscribe
                     val resIndex: Int
                     val typeCallTxt : String
+
+                    val endOfSeq =
+                        msgSequenceType == SequenceType.LAST || msgSequenceType == SequenceType.SINGLE
+                    // Apply a bottom margin to the global layout if end of sequence needed.
+                    convViewHolder.mItem?.let { setBottomMargin(it, if (endOfSeq) 8 else 0) }
 
                     // Manage the update of the timestamp
                     if (isTimeShown) {
@@ -1497,8 +1501,12 @@ class ConversationAdapter(
      * @return the previous TextMessage if any, null otherwise
      */
     private fun getPreviousMessageFromPosition(position: Int): Interaction? =
-        if (mInteractions.isNotEmpty() && position > 0) mInteractions[position - 1]
-        else null
+        if (mInteractions.isNotEmpty() && position > 0) {
+            if (mInteractions[position - 1].type == Interaction.InteractionType.INVALID) {
+                // Recursive function to ignore invalid interactions.
+                getPreviousMessageFromPosition(position - 1)
+            } else mInteractions[position - 1]
+        } else null
 
     /**
      * Helper method to return the next TextMessage relative to an initial position.
@@ -1507,9 +1515,12 @@ class ConversationAdapter(
      * @return the next TextMessage if any, null otherwise
      */
     private fun getNextMessageFromPosition(position: Int): Interaction? =
-        if (mInteractions.isNotEmpty() && position < mInteractions.size - 1)
-            mInteractions[position + 1]
-        else null
+        if (mInteractions.isNotEmpty() && position < mInteractions.size - 1){
+            if (mInteractions[position + 1].type == Interaction.InteractionType.INVALID){
+                // Recursive function to ignore invalid interactions.
+                getNextMessageFromPosition(position + 1)
+            } else mInteractions[position + 1]
+        } else null
 
     /**
      * Returns a SequenceType object which tell what type is the Interaction.
@@ -1520,7 +1531,6 @@ class ConversationAdapter(
      */
     private fun getMsgSequencing(i: Int, isTimeShown: Boolean): SequenceType {
         val msg = mInteractions[i]
-
         // Manage specific interaction which are always single (ex : emoji).
         if (isAlwaysSingleMsg(msg)) {
             return SequenceType.SINGLE
@@ -1545,18 +1555,16 @@ class ConversationAdapter(
                     SequenceType.FIRST
                 }
             }
-        } else if (mInteractions.size == i + 1) { // If this is the last interaction.
+        } else if (mInteractions.size == i + 1) {
+            // If this is the last interaction.
             // Get the previous interaction and if exists check if sequence break needed.
             val prevMsg = getPreviousMessageFromPosition(i)
             if (prevMsg != null) {
                 return if (isSeqBreak(prevMsg, msg) || isTimeShown) {
                     SequenceType.SINGLE
-                } else {
-                    SequenceType.LAST
-                }
+                } else SequenceType.LAST
             }
         }
-
         // If not the first, nor the last and if there is not only one interaction.
         // Get the next and previous interactions and if exists check if sequence break needed.
         val prevMsg = getPreviousMessageFromPosition(i)
@@ -1570,6 +1578,7 @@ class ConversationAdapter(
             } else if (!isSeqBreak(prevMsg, msg) && !isTimeShown && isSeqBreak(msg, nextMsg)) {
                 SequenceType.LAST
             } else if (!isSeqBreak(prevMsg, msg) && !isTimeShown && !isSeqBreak(msg, nextMsg)) {
+                // todo probleme ici lorsque call en dernier sur la conv
                 if (nextMsgHasTime) SequenceType.LAST else SequenceType.MIDDLE
             } else {
                 SequenceType.SINGLE
