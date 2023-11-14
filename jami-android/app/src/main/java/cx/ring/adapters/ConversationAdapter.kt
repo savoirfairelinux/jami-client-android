a90a716254e99be0ff12a1ded17e5a20604f71f2
Emma Falkiewitz <emma.falkiewitz@savoirfairelinux.com>
2023-11-14 13:52:52 -0500

    chatView: show the avatar in group and hide it in one-to-one conversation
    Only for text and call message.
    Add avatar in call message.
    Update item_conv_msg_peer to have the margin respected when the avatar is hidden.
    GitLab: #1382
    
    Change-Id: Iacc99a4aa8b3665470cd4a8f570a62b7fccb5108

--- jami-android/app/src/main/java/cx/ring/adapters/ConversationAdapter.kt
+++ jami-android/app/src/main/java/cx/ring/adapters/ConversationAdapter.kt
@@ -1090,6 +1090,9 @@ class ConversationAdapter(
                 val msgSequenceType = getMsgSequencing(position, isTimeShown)
                 val peerDisplayName = convViewHolder.mPeerDisplayName
 
+                // Do not show the avatar if it is a one to one conversation.
+                // todo faire pareil avec call, file, image et audio
+                convViewHolder.mAvatar?.visibility = View.GONE
                 // Manage deleted message.
                 if (isDeleted) {
                     msgTxt.text = context.getString(R.string.conversation_message_deleted)
@@ -1189,37 +1192,6 @@ class ConversationAdapter(
                 }
                 msgTxt.movementMethod = LinkMovementMethod.getInstance()
                 msgTxt.text = markwon.toMarkdown(message)
-                val endOfSeq =
-                    msgSequenceType == SequenceType.LAST || msgSequenceType == SequenceType.SINGLE
-
-                // Manage animation for avatar doing ???.
-                if (textMessage.isIncoming) {
-                    val avatar = convViewHolder.mAvatar ?: return@subscribe
-                    if (endOfSeq) {
-                        avatar.setImageDrawable(conversationFragment.getConversationAvatar(contact.primaryNumber))
-                        avatar.visibility = View.VISIBLE
-                    } else {
-                        if (position == lastMsgPos - 1) {
-                            avatar.startAnimation(
-                                AnimationUtils.loadAnimation(avatar.context, R.anim.fade_out).apply {
-                                    setAnimationListener(object : Animation.AnimationListener {
-                                        override fun onAnimationStart(arg0: Animation) {}
-                                        override fun onAnimationRepeat(arg0: Animation) {}
-                                        override fun onAnimationEnd(arg0: Animation) {
-                                            avatar.setImageBitmap(null)
-                                            avatar.visibility = View.INVISIBLE
-                                        }
-                                    })
-                                })
-                        } else {
-                            avatar.setImageBitmap(null)
-                            avatar.visibility = View.INVISIBLE
-                        }
-                    }
-                }
-
-                // Apply a bottom margin to the global layout if end of sequence needed.
-                convViewHolder.mItem?.let { setBottomMargin(it, if (endOfSeq) 8 else 0) }
 
                 // Manage the update of the timestamp and the fact than we can expend/hide it.
                 if (isTimeShown) {
@@ -1271,6 +1243,39 @@ class ConversationAdapter(
                             text = null
                         }
                     }
+                }
+                // Only show the peer avatar if it is a group conversation
+                if (presenter.isGroup()){
+                    val endOfSeq =
+                        msgSequenceType == SequenceType.LAST || msgSequenceType == SequenceType.SINGLE
+
+                    // Manage animation for avatar doing ???.
+                    if (textMessage.isIncoming) {
+                        val avatar = convViewHolder.mAvatar ?: return@subscribe
+                        if (endOfSeq) {
+                            avatar.setImageDrawable(conversationFragment.getConversationAvatar(contact.primaryNumber))
+                            avatar.visibility = View.VISIBLE
+                        } else {
+                            if (position == lastMsgPos - 1) {
+                                avatar.startAnimation(
+                                    AnimationUtils.loadAnimation(avatar.context, R.anim.fade_out).apply {
+                                        setAnimationListener(object : Animation.AnimationListener {
+                                            override fun onAnimationStart(arg0: Animation) {}
+                                            override fun onAnimationRepeat(arg0: Animation) {}
+                                            override fun onAnimationEnd(arg0: Animation) {
+                                                avatar.setImageBitmap(null)
+                                                avatar.visibility = View.INVISIBLE
+                                            }
+                                        })
+                                    })
+                            } else {
+                                avatar.setImageBitmap(null)
+                                avatar.visibility = View.INVISIBLE
+                            }
+                        }
+                    }
+                    // Apply a bottom margin to the global layout if end of sequence needed.
+                    convViewHolder.mItem?.let { setBottomMargin(it, if (endOfSeq) 8 else 0) }
                 }
             })
     }
@@ -1383,6 +1388,14 @@ class ConversationAdapter(
                     val detailCall = convViewHolder.mHistDetailTxt ?: return@subscribe
                     val resIndex: Int
                     val typeCallTxt : String
+                    val callLayout = convViewHolder.mCallLayout
+                    val contact = textMessage.contact ?: return@subscribe
+                    val avatar = convViewHolder.mAvatar ?: return@subscribe
+
+                    val endOfSeq =
+                        msgSequenceType == SequenceType.LAST || msgSequenceType == SequenceType.SINGLE
+                    // Apply a bottom margin to the global layout if end of sequence needed.
+                    convViewHolder.mItem?.let { setBottomMargin(it, if (endOfSeq) 8 else 0) }
 
                     // Manage the update of the timestamp
                     if (isTimeShown) {
@@ -1417,7 +1430,8 @@ class ConversationAdapter(
                             typeCallTxt = context.getString(R.string.notif_incoming_call)
                         }
                         // Put the message to the left because it is incoming.
-                        convViewHolder.mCallLayout?.gravity = Gravity.START
+                        callLayout?.gravity = Gravity.START
+
                     } else {
                         if (call.isMissed) { // Outgoing call missed.
                             resIndex = msgSequenceType.ordinal + 16
@@ -1442,7 +1456,7 @@ class ConversationAdapter(
                             typeCallTxt = context.getString(R.string.notif_outgoing_call)
                         }
                         // Put the message to the right because it is outgoing.
-                        convViewHolder.mCallLayout?.gravity = Gravity.END
+                        callLayout?.gravity = Gravity.END
                     }
                     callInfoLayout.background =
                         ContextCompat.getDrawable(context, msgBGLayouts[resIndex])
@@ -1470,6 +1484,46 @@ class ConversationAdapter(
                             convViewHolder.itemView.context.getColor(R.color.colorOnSurface)
                         )
                     }
+                    // Only show the peer avatar if it is a group conversation
+                    if (presenter.isGroup()) {
+                        val endOfSeq =
+                            msgSequenceType == SequenceType.LAST || msgSequenceType == SequenceType.SINGLE
+
+                        // Manage animation for avatar doing ???.
+                        if (textMessage.isIncoming) {
+                            if (endOfSeq) {
+                                avatar.setImageDrawable(
+                                    conversationFragment.getConversationAvatar(
+                                        contact.primaryNumber
+                                    )
+                                )
+                                avatar.visibility = View.VISIBLE
+                            } else {
+                                if (position == lastMsgPos - 1) {
+                                    avatar.startAnimation(
+                                        AnimationUtils.loadAnimation(
+                                            avatar.context,
+                                            R.anim.fade_out
+                                        ).apply {
+                                            setAnimationListener(object :
+                                                Animation.AnimationListener {
+                                                override fun onAnimationStart(arg0: Animation) {}
+                                                override fun onAnimationRepeat(arg0: Animation) {}
+                                                override fun onAnimationEnd(arg0: Animation) {
+                                                    avatar.setImageBitmap(null)
+                                                    avatar.visibility = View.INVISIBLE
+                                                }
+                                            })
+                                        })
+                                } else {
+                                    avatar.setImageBitmap(null)
+                                    avatar.visibility = View.INVISIBLE
+                                }
+                            }
+                        } else avatar.visibility = View.GONE
+                        // Apply a bottom margin to the global layout if end of sequence needed.
+                        convViewHolder.mItem?.let { setBottomMargin(it, if (endOfSeq) 8 else 0) }
+                    } else avatar.visibility = View.GONE
                 })
     }
     private fun configureSearchResult(convViewHolder: ConversationViewHolder, interaction: Interaction) {
