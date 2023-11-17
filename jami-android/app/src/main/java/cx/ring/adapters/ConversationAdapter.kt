22838a762f25f3ac8cdfae407b4c60350e022149
Emma Falkiewitz <emma.falkiewitz@savoirfairelinux.com>
2023-11-17 13:53:36 -0500

    chatView: add contact name above messages (only swarm conv)
    GitLab: #1447
    Change-Id: I4e0bf01cb12417ae7a7f3d2126203c79460ca0dc

--- jami-android/app/src/main/java/cx/ring/adapters/ConversationAdapter.kt
+++ jami-android/app/src/main/java/cx/ring/adapters/ConversationAdapter.kt
@@ -1272,10 +1272,7 @@ class ConversationAdapter(
                                         text = it.displayName
                                     }
                             )
-                        } else {
-                            visibility = View.GONE
-                            text = null
-                        }
+                        } else visibility = View.GONE
                     }
                 }
             })
@@ -1477,7 +1474,6 @@ class ConversationAdapter(
                     val textMessage = lastElement as Call
                     val isTimeShown = hasPermanentTimeString(textMessage, position)
                     val msgSequenceType = getMsgSequencing(position, isTimeShown)
-
                     val callInfoLayout = convViewHolder.mCallInfoLayout ?: return@subscribe
                     callInfoLayout.background?.setTintList(null)
                     val callIcon = convViewHolder.mIcon ?: return@subscribe
@@ -1486,6 +1482,9 @@ class ConversationAdapter(
                     val detailCall = convViewHolder.mHistDetailTxt ?: return@subscribe
                     val resIndex: Int
                     val typeCallTxt : String
+                    val peerDisplayName = convViewHolder.mPeerDisplayName
+                    val account = interaction.account ?: return@subscribe
+                    val contact = textMessage.contact ?: return@subscribe
 
                     val endOfSeq = msgSequenceType == SequenceType.LAST
                             || msgSequenceType == SequenceType.SINGLE
@@ -1577,6 +1576,22 @@ class ConversationAdapter(
                         detailCall.setTextColor(
                             convViewHolder.itemView.context.getColor(R.color.colorOnSurface)
                         )
+                    }
+                    // Show the name of the contact if it is a group conversation
+                    peerDisplayName?.apply {
+                        if (presenter.isGroup() && (msgSequenceType == SequenceType.SINGLE ||
+                                    msgSequenceType == SequenceType.FIRST)
+                        ) {
+                            visibility = View.VISIBLE
+                            convViewHolder.compositeDisposable.add(
+                                presenter.contactService
+                                    .observeContact(account, contact, false)
+                                    .observeOn(DeviceUtils.uiScheduler)
+                                    .subscribe {
+                                        text = it.displayName
+                                    }
+                            )
+                        } else visibility = View.GONE
                     }
                 })
     }
