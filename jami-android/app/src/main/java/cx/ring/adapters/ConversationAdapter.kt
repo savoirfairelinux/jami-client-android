643198c78fca023bf35d2673accd7704450d582f
Emma Falkiewitz <emma.falkiewitz@savoirfairelinux.com>
2024-01-04 15:30:56 -0500

    cleanup
    renamed dimens
    
    Change-Id: I35a3f02953f15414dbb81658a1125453aeb7780c

--- jami-android/app/src/main/java/cx/ring/adapters/ConversationAdapter.kt
+++ jami-android/app/src/main/java/cx/ring/adapters/ConversationAdapter.kt
@@ -1197,7 +1197,7 @@ class ConversationAdapter(
             )
         } else {
             layoutParams.setMargins(
-                res.getDimensionPixelSize(R.dimen.margin_without_avatar), 0,
+                res.getDimensionPixelSize(R.dimen.base_left_conversation_margin), 0,
                 0, 0
             )
         }
@@ -1257,7 +1257,7 @@ class ConversationAdapter(
         isGroup: Boolean, isIncoming: Boolean
     ) {
         if (isGroup && isIncoming) {
-            context.resources.getDimensionPixelSize(R.dimen.margin_no_group).let {
+            context.resources.getDimensionPixelSize(R.dimen.conditional_left_conversation_margin).let {
                 (messageBubbleBorder.layoutParams as MarginLayoutParams)
                     .apply { leftMargin = it }
                 replyBubble?.let { replyBubble ->
@@ -1266,7 +1266,7 @@ class ConversationAdapter(
                 }
             }
         } else {
-            context.resources.getDimensionPixelSize(R.dimen.margin_without_avatar).let {
+            context.resources.getDimensionPixelSize(R.dimen.base_left_conversation_margin).let {
                 (messageBubbleBorder.layoutParams as MarginLayoutParams)
                     .apply { leftMargin = it }
                 replyBubble?.let { replyBubble ->
