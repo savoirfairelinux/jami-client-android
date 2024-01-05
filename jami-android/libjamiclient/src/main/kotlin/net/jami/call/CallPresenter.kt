e1b0e605902740b8eb67dc4bac49623a507c8953
Pierre Nicolas <pierre.nicolas@savoirfairelinux.com>
2024-01-05 13:30:21 -0500

    call: fix audio call crash when using plugin
    No video media when an audio call, which cause the media collection filter to return an error.
    GitLab: #1526
    
    Change-Id: I1348a089ee42d0ae49a0ea9df5c5bcb03411d3c8

--- jami-android/libjamiclient/src/main/kotlin/net/jami/call/CallPresenter.kt
+++ jami-android/libjamiclient/src/main/kotlin/net/jami/call/CallPresenter.kt
@@ -669,11 +669,10 @@ class CallPresenter @Inject constructor(
         mHardwareService.startMediaHandler(mediaHandlerId)
         val conference = mConference ?: return
         val media = conference.getMediaList() ?: return
-        val source = media.first {
-            it.mediaType == Media.MediaType.MEDIA_TYPE_VIDEO &&
-            it.source != "camera://desktop"
-        }.source ?: return
-        mHardwareService.switchInput(
+        val source = media.firstOrNull {
+            it.mediaType == Media.MediaType.MEDIA_TYPE_VIDEO && it.source != "camera://desktop"
+        }?.source ?: return
+        mHardwareService.switchInput( // Plugins doesn't support hardware acceleration.
             conference.accountId,
             conference.id,
             source
@@ -684,10 +683,10 @@ class CallPresenter @Inject constructor(
         mHardwareService.stopMediaHandler()
         val conference = mConference ?: return
         val media = conference.getMediaList() ?: return
-        val source = media.first {
+        val source = media.firstOrNull {
             it.mediaType == Media.MediaType.MEDIA_TYPE_VIDEO &&
                     it.source != "camera://desktop"
-        }.source ?: return
+        }?.source ?: return
         mHardwareService.switchInput(
             conference.accountId,
             conference.id,
