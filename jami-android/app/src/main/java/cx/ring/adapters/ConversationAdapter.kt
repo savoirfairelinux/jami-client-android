a93dee91dba41d64ed5924f87743e6d7f45a8ced
Emma Falkiewitz <emma.falkiewitz@savoirfairelinux.com>
2024-01-10 10:26:21 -0500

    chatview: update date in contact event
    Just put the time and not the date.
    
    Change-Id: I7b196e6bd92a9049d39a490c70321e5b02ad0869

--- jami-android/app/src/main/java/cx/ring/adapters/ConversationAdapter.kt
+++ jami-android/app/src/main/java/cx/ring/adapters/ConversationAdapter.kt
@@ -1501,7 +1501,7 @@ class ConversationAdapter(
             "configureForContactEvent ${event.account} ${event.event} ${event.contact} ${event.author} "
         )
         val timestamp =
-            TextUtils.timestampToDetailString(context, formatter, event.timestamp)
+            TextUtils.timestampInMessage(context, formatter, event.timestamp)
 
         if (interaction.isSwarm) {
             viewHolder.compositeDisposable.add(
