c466e17ee8db691253b8d04bbdc911fa1cb301e2
Emma Falkiewitz <emma.falkiewitz@savoirfairelinux.com>
2024-01-10 15:57:21 -0500

    chatView: update the date info between messages.
    Display the date and not the hour for every new day.
    
    Change-Id: I24c00eae64e52138df9926532c1f1815d0d3f166

--- jami-android/app/src/main/java/cx/ring/adapters/ConversationAdapter.kt
+++ jami-android/app/src/main/java/cx/ring/adapters/ConversationAdapter.kt
@@ -1108,7 +1108,7 @@ class ConversationAdapter(
         if (hasPermanentDateString(file, position)) {
             viewHolder.compositeDisposable.add(timestampUpdateTimer.subscribe {
                 viewHolder.mMsgDetailTxtPerm?.text =
-                    TextUtils.timestampToDetailString(context, formatter, file.timestamp)
+                    TextUtils.timestampToDate(context, formatter, file.timestamp)
             })
             viewHolder.mMsgDetailTxtPerm?.visibility = View.VISIBLE
         } else {
@@ -1350,7 +1350,7 @@ class ConversationAdapter(
                 if (isDateShown) {
                     convViewHolder.compositeDisposable.add(timestampUpdateTimer.subscribe {
                         timePermanent?.text = TextUtils
-                            .timestampToDetailString(context, formatter, textMessage.timestamp)
+                            .timestampToDate(context, formatter, textMessage.timestamp)
                     })
                     convViewHolder.mMsgDetailTxtPerm?.visibility = View.VISIBLE
                 } else convViewHolder.mMsgDetailTxtPerm?.visibility = View.GONE
@@ -1517,7 +1517,7 @@ class ConversationAdapter(
             viewHolder.mMsgDetailTxt?.apply {
                 if (isDateShown || getPreviousInteractionFromPosition(position) == null) {
                     visibility = View.VISIBLE
-                    text = TextUtils.timestampToDetailString(context, formatter, event.timestamp)
+                    text = TextUtils.timestampToDate(context, formatter, event.timestamp)
                 } else {
                     visibility = View.GONE
                 }
@@ -1655,7 +1655,7 @@ class ConversationAdapter(
                     if (isDateShown) {
                         convViewHolder.compositeDisposable.add(timestampUpdateTimer.subscribe {
                             timePermanent?.text = TextUtils
-                                .timestampToDetailString(context, formatter, call.timestamp)
+                                .timestampToDate(context, formatter, call.timestamp)
                         })
                         convViewHolder.mMsgDetailTxtPerm?.visibility = View.VISIBLE
                     } else convViewHolder.mMsgDetailTxtPerm?.visibility = View.GONE
