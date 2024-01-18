cccb0ac21c8ab974c3d9a9407e58852e4e08b03a
Pierre Nicolas <pierre.nicolas@savoirfairelinux.com>
2024-01-18 14:33:38 -0500

    donation: update end donation timestamp
    End of the donation is now the 31 March 2024
    
    Change-Id: I289afbb852b1546ce8a18fd97fcf9078db1b86c3

--- jami-android/libjamiclient/src/main/kotlin/net/jami/utils/DonationUtils.kt
+++ jami-android/libjamiclient/src/main/kotlin/net/jami/utils/DonationUtils.kt
@@ -4,8 +4,8 @@ object DonationUtils {
     // First millisecond of 27 November 2023 GMT
     const val startDonationTimeMillis = 1701043200000
 
-    // Last millisecond of 31 January 2024 GMT
-    const val endDonationTimeMillis = 1706745599999
+    // Last millisecond of 31 March 2024 GMT
+    const val endDonationTimeMillis = 1711929599999
 
     fun isDonationPeriod(): Boolean {
         return System.currentTimeMillis() in startDonationTimeMillis until endDonationTimeMillis
