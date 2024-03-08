3f15131351ca9951ddcf423f367b15423a6e53c4
Pierre Nicolas <pierre.nicolas@savoirfairelinux.com>
2024-03-08 17:03:31 -0500

    update version to 20240308-01 (410)
    Change-Id: I9e7596d30c4d35291c8d5bc87913614443a17235

--- jami-android/app/build.gradle.kts
+++ jami-android/app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
     defaultConfig {
         minSdk = 24
         targetSdk = 34
-        versionCode = 409
-        versionName = "20240305-01"
+        versionCode = 410
+        versionName = "20240308-01"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         externalNativeBuild {
             cmake {
