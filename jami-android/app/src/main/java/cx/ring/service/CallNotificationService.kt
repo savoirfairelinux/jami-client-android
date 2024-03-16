37f84481e286c3129ec1a5c9cc2152498c8370bb
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2024-03-16 19:57:22 -0400

    CallNotificationService: handle non-granted permissions
    Change-Id: Ic41387f2e9ad55e15ae438a4e6cf3d2cd3db3cc8

--- jami-android/app/src/main/java/cx/ring/service/CallNotificationService.kt
+++ jami-android/app/src/main/java/cx/ring/service/CallNotificationService.kt
@@ -16,9 +16,11 @@
  */
 package cx.ring.service
 
+import android.Manifest
 import android.app.Notification
 import android.app.Service
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.content.pm.ServiceInfo
 import android.os.Build
 import android.os.IBinder
@@ -42,23 +44,34 @@ class CallNotificationService : Service() {
             if (notification != null) {
                 // Since API 34, screen sharing (media projection)
                 // should not be specified before user grants permission.
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE && !startScreenshare)
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+                    val pm = packageManager
+                    val name = packageName
+                    val cameraServiceType = if (pm.checkPermission(Manifest.permission.FOREGROUND_SERVICE_CAMERA, name) ==
+                            PackageManager.PERMISSION_GRANTED) ServiceInfo.FOREGROUND_SERVICE_TYPE_CAMERA else 0
+                    val microphoneServiceType = if (pm.checkPermission(Manifest.permission.FOREGROUND_SERVICE_MICROPHONE, name) ==
+                            PackageManager.PERMISSION_GRANTED) ServiceInfo.FOREGROUND_SERVICE_TYPE_MICROPHONE else 0
+                    val callServiceType = if (pm.checkPermission(Manifest.permission.FOREGROUND_SERVICE_PHONE_CALL, name) ==
+                            PackageManager.PERMISSION_GRANTED) ServiceInfo.FOREGROUND_SERVICE_TYPE_PHONE_CALL else 0
+                    val screenShareType = if (startScreenshare && pm.checkPermission(Manifest.permission.FOREGROUND_SERVICE_MEDIA_PROJECTION, name) ==
+                        PackageManager.PERMISSION_GRANTED) ServiceInfo.FOREGROUND_SERVICE_TYPE_MEDIA_PROJECTION else 0
                     startForeground(
                         NotificationServiceImpl.NOTIF_CALL_ID,
                         notification,
-                        ServiceInfo.FOREGROUND_SERVICE_TYPE_PHONE_CALL
-                                or ServiceInfo.FOREGROUND_SERVICE_TYPE_MICROPHONE
-                                or ServiceInfo.FOREGROUND_SERVICE_TYPE_CAMERA
+                        callServiceType
+                                or microphoneServiceType
+                                or cameraServiceType
+                                or screenShareType
                     )
-                // Since API 30, microphone and camera should be specified for app to use them.
-                else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R)
+                    // Since API 30, microphone and camera should be specified for app to use them.
+                } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R)
                     startForeground(
                         NotificationServiceImpl.NOTIF_CALL_ID,
                         notification,
                         ServiceInfo.FOREGROUND_SERVICE_TYPE_PHONE_CALL
                                 or ServiceInfo.FOREGROUND_SERVICE_TYPE_MICROPHONE
                                 or ServiceInfo.FOREGROUND_SERVICE_TYPE_CAMERA
-                                or ServiceInfo.FOREGROUND_SERVICE_TYPE_MEDIA_PROJECTION
+                                or (if (startScreenshare) ServiceInfo.FOREGROUND_SERVICE_TYPE_MEDIA_PROJECTION else 0)
                     )
                 // Since API 29, should specify foreground service type.
                 else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q)
