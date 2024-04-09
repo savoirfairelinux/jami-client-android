0c8f3405550229f974f34128dec11b980905f8a0
Vitalii <vitalii.nikitchyn@savoirfairelinux.com>
2024-04-09 15:59:00 -0400

    update version to 20240409-02 (414)
    Change-Id: I84ca87ef6a4e6f636327eb6eab9e89ecc50e00d0

--- jami-android/app/build.gradle.kts
+++ jami-android/app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
     defaultConfig {
         minSdk = 24
         targetSdk = 34
-        versionCode = 412
-        versionName = "20240404-01"
+        versionCode = 414
+        versionName = "20240409-02"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         externalNativeBuild {
             cmake {
