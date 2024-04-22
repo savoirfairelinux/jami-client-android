d171849ceb064d120b839cae30505d0f566ada11
Pierre Nicolas <pierre.nicolas@savoirfairelinux.com>
2024-04-22 14:53:41 -0400

    improve test script
    Change-Id: I5b9c5ef08d3894ec07e368a8cb8e055ba5548f82

--- test.sh
+++ test.sh
@@ -1,3 +1,11 @@
-#/jami-client-android/start_emu_headless.sh
-
 /opt/java/openjdk/bin/java -jar /spoon-runner.jar --apk ./jami-android/app/build/outputs/apk/noPush/debug/app-noPush-debug.apk --test-apk ./jami-android/app/build/outputs/apk/androidTest/noPush/debug/app-noPush-debug-androidTest.apk --sdk /opt/android/ --fail-on-failure
+
+# Capture the exit code
+exit_code=$?
+
+# Check the exit code and display appropriate logs
+if [ $exit_code -eq 0 ]; then
+    echo "Jami UI test failure. More info on 'spoon-output' directory."
+else
+    echo "Jami UI test success. More info on 'spoon-output' directory."
+fi
