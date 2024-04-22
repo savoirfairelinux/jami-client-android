93711053f28631713bbc849e0864fc6abe445fbc
Pierre Nicolas <pierre.nicolas@savoirfairelinux.com>
2024-04-22 12:46:47 -0400

    fix java path
    Change-Id: I9649087e185f72a54fa6c48322f6d5212e266585

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #/jami-client-android/start_emu_headless.sh
 
-java -jar /spoon-runner.jar --apk ./app/build/outputs/apk/noPush/debug/app-noPush-debug.apk --test-apk ./app/build/outputs/apk/androidTest/noPush/debug/app-noPush-debug-androidTest.apk --sdk /opt/android/
+/opt/java/openjdk/bin/java -jar /spoon-runner.jar --apk ./app/build/outputs/apk/noPush/debug/app-noPush-debug.apk --test-apk ./app/build/outputs/apk/androidTest/noPush/debug/app-noPush-debug-androidTest.apk --sdk /opt/android/
