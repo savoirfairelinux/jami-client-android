a372e55ab8fca1d3010dda6afc4c9ad5c0ac8b6e
Pierre Nicolas <pierre.nicolas@savoirfairelinux.com>
2024-04-22 14:28:49 -0400

    makes test stop pipeline
    Change-Id: I1911246e83a31309ff5004b60ad4cabbbe1a8006

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #/jami-client-android/start_emu_headless.sh
 
-/opt/java/openjdk/bin/java -jar /spoon-runner.jar --apk ./jami-android/app/build/outputs/apk/noPush/debug/app-noPush-debug.apk --test-apk ./jami-android/app/build/outputs/apk/androidTest/noPush/debug/app-noPush-debug-androidTest.apk --sdk /opt/android/
+/opt/java/openjdk/bin/java -jar /spoon-runner.jar --apk ./jami-android/app/build/outputs/apk/noPush/debug/app-noPush-debug.apk --test-apk ./jami-android/app/build/outputs/apk/androidTest/noPush/debug/app-noPush-debug-androidTest.apk --sdk /opt/android/ --fail-on-failure
