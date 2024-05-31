d751fbadc47ff4756d44e1d2ca048065ef8658bf
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2024-05-31 10:27:02 -0400

    update firebase messaging
    Change-Id: I20d3d245718b2103f5ea6894bd907f99f96c17d5

--- jami-android/build.gradle.kts
+++ jami-android/build.gradle.kts
@@ -14,7 +14,7 @@ buildscript {
 
     dependencies {
         classpath ("com.android.tools.build:gradle:8.4.1")
-        classpath ("com.google.gms:google-services:4.4.1")
+        classpath ("com.google.gms:google-services:4.4.2")
         classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
         classpath ("com.google.dagger:hilt-android-gradle-plugin:$hilt_version")
     }
