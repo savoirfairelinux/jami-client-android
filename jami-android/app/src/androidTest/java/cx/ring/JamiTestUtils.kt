a921a4bc0d14bda72f21a79f80301059285876bc
Pierre Nicolas <pierre.nicolas@savoirfairelinux.com>
2024-08-01 14:46:24 -0400

    test: add accountsettings test
    Change-Id: If26ce0f6ed5dac27260e468eb2a777f6ee77fe04

--- jami-android/app/src/androidTest/java/cx/ring/JamiTestUtils.kt
+++ jami-android/app/src/androidTest/java/cx/ring/JamiTestUtils.kt
@@ -27,47 +27,38 @@ object AccountUtils {
 
     private const val NAME_SERVER_ADDRESS = "https://ns-test.jami.net"
 
-    /**
-     * Create n accounts and register them.
-     * This function is blocking.
-     *
-     * @param accountService The account service to use.
-     * @param count The number of accounts to create.
-     * @return The list of registered account names.
-     */
-    fun createAccountAndRegister(accountService: AccountService, count: Int): List<Account> {
-
+    fun createAccount(accountService: AccountService, count: Int): List<Account> {
         val baseUsername = "jamitest"
         val time = System.currentTimeMillis()
 
         val accountObservableList = (0..<count).map { accountCount ->
             val username = "${baseUsername}_${time}_${accountCount}"
-            net.jami.utils.Log.d(TAG, "Account username: $username...")
+            Log.d(TAG, "Account username: $username...")
             accountService.getAccountTemplate(AccountConfig.ACCOUNT_TYPE_JAMI)
                 .map { accountDetails: HashMap<String, String> ->
                     accountDetails[ConfigKey.ACCOUNT_ALIAS.key] = "Jami account $accountCount"
                     accountDetails[ConfigKey.RINGNS_HOST.key] = NAME_SERVER_ADDRESS
                     accountDetails
                 }.flatMapObservable { details ->
-                    net.jami.utils.Log.d(TAG, "Adding account ...")
+                    Log.d(TAG, "Adding account ...")
                     accountService.addAccount(details)
                 }
                 .filter { account: Account ->
                     account.registrationState != AccountConfig.RegistrationState.INITIALIZING
                 }
                 .firstOrError()
-                .map { account: Account ->
-                    net.jami.utils.Log.d(TAG, "Registering account ...")
-                    accountService.registerName(
-                        account, username, AccountService.ACCOUNT_SCHEME_PASSWORD, ""
-                    )
-                    account
-                }
         }
 
         // Wait for all accounts to be created.
-        val accountList: List<Account> =
-            Single.zip(accountObservableList) { it.filterIsInstance<Account>() }.blockingGet()
+        return Single.zip(accountObservableList) { it.filterIsInstance<Account>() }.blockingGet()
+    }
+
+    fun registerAccount(accountService: AccountService, accountList: List<Account>) {
+        Log.d(TAG, "Registering account ...")
+        for (account in accountList) {
+            accountService
+                .registerName(account, account.alias!!, AccountService.ACCOUNT_SCHEME_PASSWORD, "")
+        }
 
         // Wait for all accounts to be registered.
         Single.zip(
@@ -78,9 +69,18 @@ object AccountUtils {
                     }.firstOrError()
             }
         ) { it }.blockingSubscribe()
-
-        return accountList
     }
+
+    /**
+     * Create n accounts and register them.
+     * This function is blocking.
+     *
+     * @param accountService The account service to use.
+     * @param count The number of accounts to create.
+     * @return The list of registered account names.
+     */
+    fun createAccountAndRegister(accountService: AccountService, count: Int): List<Account> =
+        createAccount(accountService, count).apply { registerAccount(accountService, this) }
 
     /**
      * Remove all accounts.
