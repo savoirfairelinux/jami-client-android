ab47cb17330bbe398f0e7a548896d3b138240416
Vitalii <vitalii.nikitchyn@savoirfairelinux.com>
2024-08-02 14:30:27 -0400

    tests: unify synchronisation for async views
    Change-Id: I6d7233037b3da3bb8ebe053a53e8e22caf795cae

--- jami-android/app/src/androidTest/java/cx/ring/EspressoExtensions.kt
+++ jami-android/app/src/androidTest/java/cx/ring/EspressoExtensions.kt
@@ -1,104 +1,35 @@
 package cx.ring
 
 import android.view.View
-import androidx.test.espresso.Espresso
-import androidx.test.espresso.NoMatchingViewException
-import androidx.test.espresso.UiController
-import androidx.test.espresso.ViewAction
-import androidx.test.espresso.ViewAssertion
-import androidx.test.espresso.ViewInteraction
-import androidx.test.espresso.matcher.ViewMatchers.isRoot
-import androidx.test.espresso.util.TreeIterables
+import android.widget.TextView
+import androidx.annotation.StringRes
+import androidx.test.espresso.Root
+import com.google.android.material.textfield.TextInputLayout
+import org.hamcrest.Description
 import org.hamcrest.Matcher
+import org.hamcrest.TypeSafeMatcher
 
 
-/**
- * Perform action of waiting for a certain view within a single root view
- * @param matcher Generic Matcher used to find our view
- */
-fun searchFor(matcher: Matcher<View>): ViewAction {
+fun hasTextInputLayoutError(expectedErrorText: String): Matcher<View> =
+    object : TypeSafeMatcher<View>() {
 
-    return object : ViewAction {
+        override fun describeTo(description: Description?) {}
 
-        override fun getConstraints(): Matcher<View> {
-            return isRoot()
-        }
+        override fun matchesSafely(item: View?): Boolean =
+            if (item !is TextInputLayout) false
+            else if (item.error == null) false
+            else expectedErrorText == item.error.toString()
+    }
 
-        override fun getDescription(): String {
-            return "searching for view $matcher in the root view"
-        }
+fun isDialogWithTitle(@StringRes dialogTitle: Int): Matcher<Root> =
+    object : TypeSafeMatcher<Root>() {
 
-        override fun perform(uiController: UiController, view: View) {
+        override fun describeTo(description: Description) {}
 
-            var tries = 0
-            val childViews: Iterable<View> = TreeIterables.breadthFirstViewTraversal(view)
-
-            // Look for the match in the tree of child views
-            childViews.forEach {
-                tries++
-                if (matcher.matches(it)) {
-                    // found the view
-                    return
-                }
-            }
-
-            throw NoMatchingViewException.Builder()
-                .withRootView(view)
-                .withViewMatcher(matcher)
-                .build()
+        override fun matchesSafely(root: Root): Boolean {
+            val rootView: View = root.decorView
+            val str = rootView.context.getString(dialogTitle)
+            val tv = rootView.findViewById<TextView>(androidx.appcompat.R.id.alertTitle)
+            return if (tv == null) false else tv.text == str
         }
     }
-}
-
-fun doOnView(matcher: Matcher<View>, vararg actions: ViewAction) {
-    actions.forEach {
-        waitForView(matcher).perform(it)
-    }
-}
-
-fun assertOnView(matcher: Matcher<View>, vararg assertions: ViewAssertion) {
-    assertions.forEach {
-        waitForView(matcher).check(it)
-    }
-}
-
-/**
- * Perform action of implicitly waiting for a certain view.
- * This differs from EspressoExtensions.searchFor in that,
- * upon failure to locate an element, it will fetch a new root view
- * in which to traverse searching for our @param match
- *
- * @param viewMatcher ViewMatcher used to find our view
- */
-fun waitForView(
-    viewMatcher: Matcher<View>,
-    waitMillis: Int = 5000,
-    waitMillisPerTry: Long = 100,
-): ViewInteraction {
-
-    // Derive the max tries
-    val maxTries = waitMillis / waitMillisPerTry.toInt()
-
-    var tries = 0
-
-    for (i in 0..maxTries)
-        try {
-            // Track the amount of times we've tried
-            tries++
-
-            // Search the root for the view
-            Espresso.onView(isRoot()).perform(searchFor(viewMatcher))
-
-            // If we're here, we found our view. Now return it
-            return Espresso.onView(viewMatcher)
-
-        } catch (e: Exception) {
-
-            if (tries == maxTries) {
-                throw e
-            }
-            Thread.sleep(waitMillisPerTry)
-        }
-
-    throw Exception("Error finding a view matching $viewMatcher")
-}
