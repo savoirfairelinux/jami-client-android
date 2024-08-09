04d8091f53681e4e65cf8dfb2a370c663f61b031
Pierre Nicolas <pierre.nicolas@savoirfairelinux.com>
2024-08-09 17:23:42 -0400

    test: download images
    Will be used to set avatar profile and send pictures.
    
    Change-Id: Ied3121104ead5f15e5f4de638d6e694212b0f984

--- jami-android/app/src/androidTest/java/cx/ring/JamiTestUtils.kt
+++ jami-android/app/src/androidTest/java/cx/ring/JamiTestUtils.kt
@@ -1,10 +1,23 @@
 package cx.ring
 
+import android.content.Context
+import android.net.Uri
 import io.reactivex.rxjava3.core.Single
+import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import net.jami.model.Account
 import net.jami.model.AccountConfig
 import net.jami.model.ConfigKey
 import net.jami.services.AccountService
+import net.jami.utils.Log
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import java.io.File
+import java.io.FileOutputStream
+import java.io.InputStream
 import java.util.HashMap
 
 
@@ -77,6 +90,81 @@ object AccountUtils {
     fun removeAllAccounts(accountService: AccountService) {
         accountService.observableAccountList.blockingFirst().forEach {
             accountService.removeAccount(it.accountId)
+        }
+    }
+}
+
+class ImageProvider {
+
+    /**
+     * Download images from a list of URLs and save them to the cache directory.
+     * @param context The context.
+     * @param count The number of images to download (Max=2).
+     */
+    fun downloadImagesToUri(context: Context, count: Int): List<Uri> {
+        if (count > imagesUrlList.size)
+            throw Exception("Count is greater than the number of images available.")
+
+        runBlocking {
+            val deferredResult = CompletableDeferred<Boolean>()
+            CoroutineScope(Dispatchers.IO).launch {
+                imagesUrlList.forEach {
+                    downloadImageToUri(it, context)?.let { uri ->
+                        Log.w(TAG, "Image downloaded to: $uri")
+                        downloadedImagesUri.add(uri)
+                    } ?: throw Exception("Image download failed.")
+                }
+                deferredResult.complete(true)
+            }
+            // This will block the test until the image is downloaded.
+            deferredResult.await()
+        }
+
+        return downloadedImagesUri
+    }
+
+    private val imagesUrlList = listOf(
+        "https://jami.net/content/images/2024/05/pexels-los-muertos-crew-7205804.jpg",
+        "https://jami.net/content/images/2024/06/pexels-tkirkgoz-11423986.jpg"
+    )
+
+    private var downloadedImagesUri: MutableList<Uri> = mutableListOf()
+
+    private var TAG = ImageProvider::class.java.simpleName
+
+    /**
+     * Download an image from a URL.
+     */
+    private fun downloadImageToUri(imageUrl: String, context: Context): Uri? {
+        val client = OkHttpClient()
+        val request = Request.Builder().url(imageUrl).build()
+
+        return try {
+            // Send the request and get the response.
+            val response = client.newCall(request).execute()
+            val inputStream: InputStream? = response.body?.byteStream()
+
+            // Save the image to a file.
+            inputStream?.let {
+                val file = File(context.cacheDir, "${System.currentTimeMillis()}.jpg")
+                val outputStream = FileOutputStream(file)
+
+                val buffer = ByteArray(1024)
+                var bytesRead: Int
+
+                while (inputStream.read(buffer).also { bytesRead = it } != -1) {
+                    outputStream.write(buffer, 0, bytesRead)
+                }
+
+                outputStream.close()
+                inputStream.close()
+
+                // Returns the URI.
+                Uri.fromFile(file)
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            null
         }
     }
 }
