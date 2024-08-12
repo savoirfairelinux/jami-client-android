aea0c9fdec286090f5bb3ba269e9d223c337e7f6
Pierre Nicolas <pierre.nicolas@savoirfairelinux.com>
2024-08-12 14:39:39 -0400

    test: use resourceId instead of text
    Change-Id: I971e8cf6af55abd88fa7f340a953f665abc9f4a5

--- jami-android/app/src/androidTest/java/cx/ring/EspressoExtensions.kt
+++ jami-android/app/src/androidTest/java/cx/ring/EspressoExtensions.kt
@@ -1,23 +1,30 @@
 package cx.ring
 
 import android.view.View
+import androidx.annotation.StringRes
 import com.google.android.material.textfield.TextInputLayout
 import org.hamcrest.Description
 import org.hamcrest.Matcher
 import org.hamcrest.TypeSafeMatcher
 
 
-fun hasTextInputLayoutError(expectedErrorText: String): Matcher<View> = object : TypeSafeMatcher<View>() {
+/**
+ * Check if a TextInputLayout has an error with the expected text
+ * @param resourceId Resource id of the expected error. If null, check if there is no error.
+ */
+fun hasTextInputLayoutError(@StringRes resourceId: Int?): Matcher<View> =
+    object : TypeSafeMatcher<View>() {
 
-    override fun describeTo(description: Description?) { }
+        override fun describeTo(description: Description?) {}
 
-    override fun matchesSafely(item: View?): Boolean {
-        if (item !is TextInputLayout) {
-            return false
+        override fun matchesSafely(item: View?): Boolean {
+            if (item !is TextInputLayout) {
+                return false
+            }
+            // If resourceId is null, we just check if there is no error
+            if (resourceId == null) return item.error == null
+            // Else we check if the error is the expected one
+            val expectedErrorText = item.context.getString(resourceId)
+            return expectedErrorText == item.error.toString()
         }
-
-        val error = item.error ?: return false
-        return expectedErrorText == error.toString()
     }
-
-}
