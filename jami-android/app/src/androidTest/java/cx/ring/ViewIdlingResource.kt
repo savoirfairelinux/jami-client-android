5a23c340dbb352f9e2d9558fca87e5cd5f46e4ce
Pierre Nicolas <pierre.nicolas@savoirfairelinux.com>
2024-08-12 15:04:08 -0400

    test: restore waitForView
    Don't know why but unable to wait for dialogs with waitUntil.
    
    GitLab: #1739
    Change-Id: Ie13cfdde6afd0d31a6cd7c291f732de2b99d6af1

--- jami-android/app/src/androidTest/java/cx/ring/ViewIdlingResource.kt
+++ jami-android/app/src/androidTest/java/cx/ring/ViewIdlingResource.kt
@@ -19,10 +19,15 @@ package cx.ring
 
 import android.view.View
 import android.view.ViewTreeObserver
+import androidx.test.espresso.Espresso
 import androidx.test.espresso.IdlingRegistry
 import androidx.test.espresso.IdlingResource
+import androidx.test.espresso.NoMatchingViewException
 import androidx.test.espresso.UiController
 import androidx.test.espresso.ViewAction
+import androidx.test.espresso.ViewInteraction
+import androidx.test.espresso.matcher.ViewMatchers.isRoot
+import androidx.test.espresso.util.TreeIterables
 import org.hamcrest.Matcher
 import org.hamcrest.Matchers.any
 import org.hamcrest.StringDescription
@@ -79,4 +84,72 @@ fun waitUntil(matcher: Matcher<View>) = object : ViewAction {
             }
         }
     }
+}
+
+/**
+ * Code from: https://stackoverflow.com/a/56499223/12911704
+ * Perform action of waiting for a certain view within a single root view
+ * @param matcher Generic Matcher used to find our view
+ */
+fun searchFor(matcher: Matcher<View>): ViewAction {
+    return object : ViewAction {
+        override fun getConstraints(): Matcher<View> {
+            return isRoot()
+        }
+
+        override fun getDescription(): String {
+            return "searching for view $matcher in the root view"
+        }
+
+        override fun perform(uiController: UiController, view: View) {
+            var tries = 0
+            val childViews: Iterable<View> = TreeIterables.breadthFirstViewTraversal(view)
+            // Look for the match in the tree of child views
+            childViews.forEach {
+                tries++
+                if (matcher.matches(it)) {
+                    // found the view
+                    return
+                }
+            }
+            throw NoMatchingViewException.Builder()
+                .withRootView(view)
+                .withViewMatcher(matcher)
+                .build()
+        }
+    }
+}
+
+/**
+ * Code from: https://stackoverflow.com/a/56499223/12911704
+ * Perform action of implicitly waiting for a certain view.
+ * This differs from EspressoExtensions.searchFor in that,
+ * upon failure to locate an element, it will fetch a new root view
+ * in which to traverse searching for our @param match
+ *
+ * @param viewMatcher ViewMatcher used to find our view
+ */
+fun waitForView(
+    viewMatcher: Matcher<View>,
+    waitMillis: Int = 5000,
+    waitMillisPerTry: Long = 100,
+): ViewInteraction {
+    // Derive the max tries
+    val maxTries = waitMillis / waitMillisPerTry.toInt()
+    var tries = 0
+    for (i in 0..maxTries)
+        try {
+            // Track the amount of times we've tried
+            tries++
+            // Search the root for the view
+            Espresso.onView(isRoot()).perform(searchFor(viewMatcher))
+            // If we're here, we found our view. Now return it
+            return Espresso.onView(viewMatcher)
+        } catch (e: Exception) {
+            if (tries == maxTries) {
+                throw e
+            }
+            Thread.sleep(waitMillisPerTry)
+        }
+    throw Exception("Error finding a view matching $viewMatcher")
 }
