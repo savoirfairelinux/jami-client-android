0f1d0af77f9c7b183814b2cbddc1656a9a06ea58
Pierre NICOLAS <pierre.nicolas@savoirfairelinux.com>
2024-08-15 07:10:14 -0400

    test: add accountsettings test
    GitLab: #1739
    
    Change-Id: If26ce0f6ed5dac27260e468eb2a777f6ee77fe04

--- jami-android/app/src/androidTest/java/cx/ring/EspressoExtensions.kt
+++ jami-android/app/src/androidTest/java/cx/ring/EspressoExtensions.kt
@@ -1,14 +1,21 @@
 package cx.ring
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.view.View
 import android.widget.TextView
+import android.widget.ImageView
 import androidx.annotation.StringRes
 import androidx.test.espresso.Root
 import com.google.android.material.textfield.TextInputLayout
+import cx.ring.views.AvatarDrawable
+import net.jami.utils.Log
 import org.hamcrest.Description
 import org.hamcrest.Matcher
 import org.hamcrest.TypeSafeMatcher
-
+import java.io.InputStream
 
 /**
  * Check if a TextInputLayout has an error with the expected text
@@ -44,4 +51,52 @@ fun isDialogWithTitle(@StringRes dialogTitle: Int): Matcher<Root> = object : Typ
         return if (tv == null) false else tv.text == str
     }
 
+}
+
+fun withImageUri(uri: Uri): Matcher<View> {
+    return object : TypeSafeMatcher<View>() {
+
+        private val TAG = "EspressoExtensions:withImageUri"
+
+        override fun describeTo(description: Description) {
+            description.appendText("with image from URI: `$uri`")
+        }
+
+        override fun matchesSafely(view: View): Boolean {
+            if (view !is ImageView) {
+                Log.e(TAG, "View is not an ImageView.")
+                return false
+            }
+            if (view.drawable == null) {
+                Log.e(TAG, "View has no drawable.")
+                return false
+            }
+            if (view.drawable !is AvatarDrawable) {
+                Log.e(TAG, "Drawable is not an AvatarDrawable.")
+                return false
+            }
+
+            val drawable = view.drawable as AvatarDrawable
+            val drawableBitmaps = drawable.getBitmap()
+
+            if (drawableBitmaps == null) {
+                Log.e(TAG, "Drawable has no bitmap.")
+                return false
+            }
+            if (drawableBitmaps.isEmpty()) {
+                Log.e(TAG, "Drawable has no photo.")
+                return false
+            }
+
+            val actualBitmap = drawableBitmaps[0]
+            val expectedBitmap = loadBitmapFromUri(view.context, uri)
+
+            return expectedBitmap.sameAs(actualBitmap)
+        }
+
+        private fun loadBitmapFromUri(context: Context, uri: Uri): Bitmap {
+            val inputStream: InputStream = context.contentResolver.openInputStream(uri)!!
+            return BitmapFactory.decodeStream(inputStream)
+        }
+    }
 }
