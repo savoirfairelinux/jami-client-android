af84c16ef2ce9d68bb6c6775383e1bc40e765562
Pierre Nicolas <pierre.nicolas@savoirfairelinux.com>
2024-08-23 14:31:47 -0400

    update version to 20240823-01 (428)
    Change-Id: Ifd5da7fadc7d11a65add3cb11d0d83ad94105eb3

--- jami-android/app/build.gradle.kts
+++ jami-android/app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
     defaultConfig {
         minSdk = 24
         targetSdk = 34
-        versionCode = 427
-        versionName = "20240802-01"
+        versionCode = 428
+        versionName = "20240823-01"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         externalNativeBuild {
             cmake {
