aa25fc32edd7af2c41a48745f45f39c435b079c2
Pierre Nicolas <pierre.nicolas@savoirfairelinux.com>
2024-08-26 15:17:07 -0400

    misc: unify terminology
    Change-Id: I576d948617433603eb42c2192cc77703a40fc5e3
    GitLab: #1702

--- jami-android/app/src/main/java/cx/ring/extensions/RecyclerPicker/RecyclerPicker.java
+++ jami-android/app/src/main/java/cx/ring/extensions/RecyclerPicker/RecyclerPicker.java
@@ -14,7 +14,7 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
-package cx.ring.plugins.RecyclerPicker;
+package cx.ring.extensions.RecyclerPicker;
 
 import android.content.Context;
 import android.graphics.drawable.Drawable;
