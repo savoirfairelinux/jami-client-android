930d49c77c9861c1e4f9637cd2d489c592da6d15
Pierre Nicolas <pierre.nicolas@savoirfairelinux.com>
2024-08-29 15:39:29 -0400

    test: implement messaging tests
    Change-Id: Ia3458c8b0366ec1470b118897f535a5f3ccefde7

--- jami-android/app/build.gradle.kts
+++ jami-android/app/build.gradle.kts
@@ -132,7 +132,9 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit-ktx:1.2.1")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    androidTestImplementation("androidx.test.espresso:espresso-contrib:3.6.1")
     androidTestImplementation("androidx.test:rules:1.6.1")
+    androidTestImplementation("androidx.test.espresso:espresso-intents:3.6.1")
 
     // Glide
     implementation ("com.github.bumptech.glide:glide:4.16.0")
