66cda08f607dc050e93dd62a54b793848ccf6412
Pierre Nicolas <pierre.nicolas@savoirfairelinux.com>
2024-11-19 13:58:01 -0500

    linkdevice: add ui for import side
    Change-Id: Iffd5236f3bd9349623104d80822985cadaea2dde

---
+++ jami-android/libjamiclient/src/main/kotlin/net/jami/linkdevice/presenter/ImportSidePresenter.kt
@@ -0,0 +1,66 @@
+package net.jami.linkdevice.presenter
+
+import io.reactivex.rxjava3.core.Scheduler
+import net.jami.linkdevice.view.ImportSideResult
+import net.jami.linkdevice.view.ImportSideView
+import net.jami.mvp.RootPresenter
+import net.jami.services.AccountService
+import javax.inject.Inject
+import javax.inject.Named
+
+
+class ImportSidePresenter @Inject constructor(
+    private val mAccountService: AccountService,
+    @Suppress("unused") @param:Named("UiScheduler") private val mUiScheduler: Scheduler
+) : JamiDaemonListener, RootPresenter<ImportSideView>() {
+    private var _currentState = LinkDeviceState.NONE
+    val currentState: LinkDeviceState
+        get() = _currentState
+
+    fun onAuthentication(password: String) {
+        // Todo: Send the password to Jami-daemon.
+    }
+
+    fun onCancel() {
+        // Todo: Cancel the operation.
+    }
+
+    override fun onNoneSignal() {
+        _currentState = LinkDeviceState.NONE
+        view?.showAuthenticationUri(null)
+    }
+
+    override fun onTokenAvailableSignal(token: String) {
+        _currentState = LinkDeviceState.TOKEN_AVAIL
+        view?.showAuthenticationUri(token)
+    }
+
+    override fun onConnectingSignal(ip: String) {
+        _currentState = LinkDeviceState.CONNECTING
+        view?.showActionRequired()
+    }
+
+    override fun onAuthenticatingSignal(
+        needPassword: Boolean,
+        jamiId: String,
+        registeredName: String?
+    ) {
+        _currentState = LinkDeviceState.AUTHENTICATION
+        view?.showAuthentication(needPassword, jamiId, registeredName)
+    }
+
+    override fun onImportingSignal() {
+        _currentState = LinkDeviceState.IMPORTING
+        view?.showResult(result = ImportSideResult.IN_PROGRESS)
+    }
+
+    override fun onDoneSignal() {
+        _currentState = LinkDeviceState.DONE
+        view?.showResult(result = ImportSideResult.SUCCESS)
+    }
+
+    override fun onErrorSignal() {
+        throw UnsupportedOperationException()
+    }
+
+}
