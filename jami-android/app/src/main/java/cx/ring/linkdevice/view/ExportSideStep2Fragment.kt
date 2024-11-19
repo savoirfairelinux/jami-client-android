37ad0fa8b9d52b33828970e0ac0a4ddee043014a
Pierre Nicolas <pierre.nicolas@savoirfairelinux.com>
2024-11-19 13:05:04 -0500

    linkdevice: add ui for export side
    Change-Id: I53f49adee70d094ec83768f240cd925f3e570fc6

---
+++ jami-android/app/src/main/java/cx/ring/linkdevice/view/ExportSideStep2Fragment.kt
@@ -0,0 +1,108 @@
+/*
+ *  Copyright (C) 2004-2024 Savoir-faire Linux Inc.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+package cx.ring.linkdevice.view
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import cx.ring.R
+import cx.ring.databinding.FragmentExportSideStep2Binding
+
+class ExportSideStep2Fragment : Fragment() {
+    private var _binding: FragmentExportSideStep2Binding? = null
+    private val binding get() = _binding!!
+    private var _callback: OnReviewCallback? = null
+    private val callback get() = _callback!!
+
+    interface OnReviewCallback {
+        fun onIdentityConfirmation(confirm: Boolean)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (requireActivity() is OnReviewCallback) {
+            _callback = requireActivity() as OnReviewCallback
+        } else {
+            throw RuntimeException("Parent fragment must implement ${OnReviewCallback::class.java.simpleName}")
+        }
+    }
+
+    // Silencing the warning about accessibility. We just want to disable the view.
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = FragmentExportSideStep2Binding.inflate(inflater, container, false)
+        .apply { _binding = this }.root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.cancel.setOnClickListener {
+            MaterialAlertDialogBuilder(requireContext())
+                .setTitle(resources.getString(R.string.link_device_dialog_exit_title))
+                .setMessage(resources.getString(R.string.link_device_dialog_exit_export_body_1))
+                .setPositiveButton(android.R.string.ok) { _, _ ->
+                    Log.i(TAG, "Cancel button clicked.")
+                    callback.onIdentityConfirmation(false)
+                }
+                .setNegativeButton(android.R.string.cancel) { _, _ -> }
+                .show()
+        }
+
+        binding.confirm.setOnClickListener {
+            Log.i(TAG, "Confirm button clicked.")
+            showLoading()
+            callback.onIdentityConfirmation(true)
+        }
+    }
+
+    fun showIP(ip: String) {
+        Log.i(TAG, "Showing IP: $ip")
+        binding.passwordContainer.visibility = View.GONE
+        binding.confirm.visibility = View.VISIBLE
+        binding.cancel.visibility = View.VISIBLE
+        binding.progress.visibility = View.INVISIBLE
+        binding.locationContainer.visibility = View.VISIBLE
+        binding.advice.text =
+            getString(R.string.export_side_step2_advice_ip_only)
+        binding.location.text =
+            getString(R.string.export_side_step2_ip_only, ip)
+    }
+
+    fun showPasswordProtection() {
+        Log.i(TAG, "Showing password protection.")
+        binding.passwordContainer.visibility = View.VISIBLE
+        binding.locationContainer.visibility = View.GONE
+    }
+
+    private fun showLoading() {
+        binding.confirm.visibility = View.INVISIBLE
+        binding.cancel.visibility = View.INVISIBLE
+        binding.progress.visibility = View.VISIBLE
+    }
+
+    companion object {
+        private val TAG = ExportSideStep2Fragment::class.java.simpleName
+    }
+}
---
+++ jami-android/app/src/main/java/cx/ring/linkdevice/viewmodel/ExportSideViewModel.kt
@@ -0,0 +1,164 @@
+/*
+ *  Copyright (C) 2004-2024 Savoir-faire Linux Inc.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+package cx.ring.linkdevice.viewmodel
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import io.reactivex.rxjava3.core.Scheduler
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import net.jami.services.AccountService
+import net.jami.services.AccountService.AuthState
+import net.jami.services.AccountService.AuthResult
+import net.jami.utils.Log
+import javax.inject.Inject
+import javax.inject.Named
+
+
+sealed class AddDeviceExportState {
+    data class Init(val error: ExportSideInputError? = null) : AddDeviceExportState()
+    data object TokenAvailable : AddDeviceExportState()
+    data object Connecting : AddDeviceExportState()
+    data class Authenticating(val peerAddress: String?) : AddDeviceExportState()
+    data object InProgress : AddDeviceExportState()
+    data class Done(val error: AuthError? = null) : AddDeviceExportState()
+}
+
+enum class ExportSideInputError {
+    INVALID_INPUT,
+}
+
+@HiltViewModel
+class ExportSideViewModel @Inject constructor(
+    private val accountService: AccountService,
+    @param:Named("UiScheduler") private val mUiScheduler: Scheduler
+) : AuthStateListener, ViewModel() {
+
+    // Expose screen UI state
+    private val _uiState = MutableStateFlow<AddDeviceExportState>(AddDeviceExportState.Init())
+    val uiState: StateFlow<AddDeviceExportState> = _uiState.asStateFlow()
+
+    private var operationId: Long? = null
+
+    private var compositeDisposable = CompositeDisposable()
+
+    fun onAuthenticationUri(jamiAuthentication: String) {
+        // Verify the input.
+        if (jamiAuthentication.isEmpty()
+            || !jamiAuthentication.startsWith("jami-auth://")
+            || (jamiAuthentication.length != 59)
+        ) {
+            Log.w(TAG, "Invalid input: $jamiAuthentication")
+            _uiState.value = AddDeviceExportState.Init(ExportSideInputError.INVALID_INPUT)
+            return
+        }
+
+        operationId = accountService
+            .addDevice(accountService.currentAccount!!.accountId, jamiAuthentication)
+
+        assert(compositeDisposable.size() == 0) // Should not have any subscription.
+
+        accountService.authResultObservable
+            .filter {
+                it.accountId == accountService.currentAccount?.accountId
+                        && it.operationId == operationId
+            }
+            .observeOn(mUiScheduler)
+            .subscribe { it: AuthResult ->
+                updateAuthState(it)
+            }.apply { compositeDisposable.add(this) }
+    }
+
+    fun onIdentityConfirmation() {
+        accountService.confirmAddDevice(accountService.currentAccount!!.accountId, operationId!!)
+    }
+
+    fun onCancel() {
+        val operationId = operationId ?: return
+        accountService.cancelAddDevice(accountService.currentAccount!!.accountId, operationId)
+    }
+
+    // There is no token to be received in export side.
+    override fun onTokenAvailableSignal(details: Map<String, String>) {
+        throw UnsupportedOperationException()
+    }
+
+    override fun onConnectingSignal() {
+        // Nothing to do here.
+        _uiState.value = AddDeviceExportState.Connecting
+    }
+
+    override fun onAuthenticatingSignal(details: Map<String, String>) {
+        val peerAddress = details[EXPORT_PEER_ADDRESS_KEY]
+        _uiState.value = AddDeviceExportState.Authenticating(peerAddress)
+    }
+
+    override fun onInProgressSignal() {
+        _uiState.value = AddDeviceExportState.InProgress
+    }
+
+    override fun onDoneSignal(details: Map<String, String>) {
+        val error = details[EXPORT_ERROR_KEY]
+            ?.let { if (it.isEmpty() || it == "none") null else AuthError.fromString(it) }
+        _uiState.value = AddDeviceExportState.Done(error)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.clear()
+        if (_uiState.value !is AddDeviceExportState.Done) onCancel()
+    }
+
+    private fun updateAuthState(authenticationResult: AuthResult) {
+        Log.d(TAG, "Processing signal: ${authenticationResult.accountId}:${authenticationResult.operationId}:${authenticationResult.state} ${authenticationResult.details}")
+        if (!checkNewStateValidity(authenticationResult.state)) {
+            Log.e(TAG, "Invalid state transition: ${_uiState.value}->${authenticationResult.state}")
+            throw IllegalStateException("Invalid state transition")
+        }
+        val details = authenticationResult.details
+        when (authenticationResult.state) {
+            AuthState.INIT -> onInitSignal()
+            AuthState.TOKEN_AVAILABLE -> throw UnsupportedOperationException()
+            AuthState.CONNECTING -> onConnectingSignal()
+            AuthState.AUTHENTICATING -> onAuthenticatingSignal(details)
+            AuthState.IN_PROGRESS -> onInProgressSignal()
+            AuthState.DONE -> onDoneSignal(details)
+        }
+    }
+
+    /**
+     * Check if the new state is valid from the current state.
+     */
+    private fun checkNewStateValidity(newState: AuthState): Boolean {
+        return newState in when (_uiState.value) {
+            is AddDeviceExportState.Init -> listOf(AuthState.CONNECTING, AuthState.DONE)
+            is AddDeviceExportState.TokenAvailable -> emptyList()
+            is AddDeviceExportState.Connecting -> listOf(AuthState.AUTHENTICATING, AuthState.DONE)
+            is AddDeviceExportState.Authenticating -> listOf(AuthState.IN_PROGRESS, AuthState.DONE)
+            is AddDeviceExportState.InProgress -> listOf(AuthState.DONE, AuthState.DONE)
+            is AddDeviceExportState.Done -> listOf(AuthState.DONE)
+        }
+    }
+
+    companion object {
+        private val TAG = ExportSideViewModel::class.simpleName!!
+        const val EXPORT_PEER_ADDRESS_KEY = "peer_address"
+        const val EXPORT_ERROR_KEY = "error"
+    }
+}
