8aed04c609ce8440b2abe88c177d1f0925c89b5c
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2025-02-20 14:43:48 -0500

    update to API 35
    Gitlab: #1790
    Change-Id: I6d474361145ce0cf4926daf75c0e0f9e34abc55c

--- jami-android/build.gradle.kts
+++ jami-android/build.gradle.kts
@@ -13,7 +13,7 @@ buildscript {
     val hilt_version by extra { "2.52" }
 
     dependencies {
-        classpath ("com.android.tools.build:gradle:8.5.2")
+        classpath ("com.android.tools.build:gradle:8.8.1")
         classpath ("com.google.gms:google-services:4.4.2")
         classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
         classpath ("com.google.dagger:hilt-android-gradle-plugin:$hilt_version")
