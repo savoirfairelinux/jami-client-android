a0034f51db963d56b62b2040d582d8f2481595f0
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2025-03-10 16:02:28 -0400

    update version to 20250310-01 (450)
    Change-Id: I561d1bec5309c5dd142615594a947b2c8e8b107b

--- jami-android/app/build.gradle.kts
+++ jami-android/app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
     defaultConfig {
         minSdk = 24
         targetSdk = 35
-        versionCode = 449
-        versionName = "20250307-01"
+        versionCode = 450
+        versionName = "20250310-01"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         externalNativeBuild {
             cmake {
