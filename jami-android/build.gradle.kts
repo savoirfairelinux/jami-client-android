6c1940915d89413432762e3acb30d271720eeb27
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2025-04-10 13:39:53 -0400

    build: update to Kotlin 2.1
    Change-Id: I36619b441d934a39c40b9dc15f5cfb4bf8b2e0bb

--- jami-android/build.gradle.kts
+++ jami-android/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.google.devtools.ksp") version "1.9.23-1.0.19" apply false
+    id("com.google.devtools.ksp") version "2.1.20-2.0.0" apply false
 }
 
 buildscript {
@@ -9,11 +9,11 @@ buildscript {
         mavenCentral()
     }
 
-    val kotlin_version by extra { "1.9.23" }
+    val kotlin_version by extra { "2.1.20" }
     val hilt_version by extra { "2.56.1" }
 
     dependencies {
-        classpath ("com.android.tools.build:gradle:8.8.1")
+        classpath ("com.android.tools.build:gradle:8.9.1")
         classpath ("com.google.gms:google-services:4.4.2")
         classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
         classpath ("com.google.dagger:hilt-android-gradle-plugin:$hilt_version")
