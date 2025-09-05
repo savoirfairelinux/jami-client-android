ef760a9469c725dea813bd847252085fad0b9510
pknellore <pavankoushik.nellore@savoirfairelinux.com>
2025-09-05 15:01:13 -0400

    update donation timestamps
    change the end date to 15 november
    
    Change-Id: I27e7d9f7166b1f77d1e5adf33aff902eef7d4b3e

--- jami-android/libjamiclient/src/main/kotlin/net/jami/utils/DonationUtils.kt
+++ jami-android/libjamiclient/src/main/kotlin/net/jami/utils/DonationUtils.kt
@@ -4,8 +4,8 @@ object DonationUtils {
     // First millisecond of 8 September 2025 GMT
     const val startDonationTimeMillis = 1757304000000
 
-    // First millisecond of 31 October 2025 GMT
-    const val endDonationTimeMillis = 1761969599999
+    // Last millisecond of 15 November 2025 GMT
+    const val endDonationTimeMillis = 1763269199999
 
     fun isDonationPeriod(): Boolean {
         return System.currentTimeMillis() in startDonationTimeMillis until endDonationTimeMillis
