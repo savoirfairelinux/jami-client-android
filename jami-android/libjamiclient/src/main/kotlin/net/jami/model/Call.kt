2ff1e21a29db4e1ac0f6f5787fc080ae4149af90
ovari123 <ovari123@zoho.com>
2025-11-04 21:24:00 -0400

    Call.kt: update unhold to resume
    GitLab: https://git.jami.net/savoirfairelinux/jami-daemon/-/issues/1154
    Change-Id: I42f74536c5b2ace7f107c44a758d3b1310f0775e

--- jami-android/libjamiclient/src/main/kotlin/net/jami/model/Call.kt
+++ jami-android/libjamiclient/src/main/kotlin/net/jami/model/Call.kt
@@ -1,19 +1,20 @@
 /*
- *  Copyright (C) 2004-2025 Savoir-faire Linux Inc.
+ * Copyright (C) 2004-2025 Savoir-faire Linux Inc.
  *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- *  GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this program. If not, see <https://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
+
 package net.jami.model
 
 import io.reactivex.rxjava3.core.Observable
@@ -156,12 +157,12 @@ class Call(
     }
 
     enum class CallStatus {
-        NONE, SEARCHING, CONNECTING, RINGING, CURRENT, HUNGUP, BUSY, FAILURE, HOLD, UNHOLD, INACTIVE, OVER;
+        NONE, SEARCHING, CONNECTING, RINGING, CURRENT, HUNGUP, BUSY, FAILURE, HOLD, RESUME, INACTIVE, OVER;
 
         val isRinging: Boolean
             get() = this == CONNECTING || this == RINGING || this == NONE || this == SEARCHING
         val isOnGoing: Boolean
-            get() = this == CURRENT || this == HOLD || this == UNHOLD
+            get() = this == CURRENT || this == HOLD || this == RESUME
         val isOver: Boolean
             get() = this == HUNGUP || this == BUSY || this == FAILURE || this == OVER
 
@@ -176,7 +177,7 @@ class Call(
                     "BUSY" -> BUSY
                     "FAILURE" -> FAILURE
                     "HOLD" -> HOLD
-                    "UNHOLD" -> UNHOLD
+                    "RESUME" -> RESUME
                     "INACTIVE" -> INACTIVE
                     "OVER" -> OVER
                     "NONE" -> NONE
