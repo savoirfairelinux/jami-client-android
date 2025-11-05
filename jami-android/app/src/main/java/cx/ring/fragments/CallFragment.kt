8f32fd0ab0a4f10eecb9d4565a74127b9e1862a1
ovari123 <ovari123@zoho.com>
2025-11-04 21:04:34 -0400

    CallFragment.kt: update unhold to resume
    GitLab: https://git.jami.net/savoirfairelinux/jami-daemon/-/issues/1154
    Change-Id: I86bd493aa38189fa49c7f07ce9809f461ca7ae8f

--- jami-android/app/src/main/java/cx/ring/fragments/CallFragment.kt
+++ jami-android/app/src/main/java/cx/ring/fragments/CallFragment.kt
@@ -1,19 +1,20 @@
 /*
- *  Copyright (C) 2004-2025 Savoir-faire Linux Inc.
+ * Copyright (C) 2004-2025 Savoir-faire Linux Inc.
  *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- *  GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this program. If not, see <https://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
+
 package cx.ring.fragments
 
 import android.Manifest
@@ -1416,7 +1417,7 @@ class CallFragment : BaseSupportFragment<CallPresenter, CallView>(), CallView,
             CallStatus.BUSY -> R.string.call_human_state_busy
             CallStatus.FAILURE -> R.string.call_human_state_failure
             CallStatus.HOLD -> R.string.call_human_state_hold
-            CallStatus.UNHOLD -> R.string.call_human_state_unhold
+            CallStatus.RESUME -> R.string.call_human_state_resume
             CallStatus.OVER -> R.string.call_human_state_over
             CallStatus.NONE -> R.string.call_human_state_none
             else -> R.string.call_human_state_none
