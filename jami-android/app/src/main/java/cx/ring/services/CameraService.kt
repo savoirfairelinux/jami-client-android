74c8dab7b248c9452ac3e4ccc99a10e10c8715de
pknellore <pavankoushik.nellore@savoirfairelinux.com>
2026-01-19 10:58:50 -0500

    camera: set default buffersize
    set default buffer size before opening
    the camera.
    
    Change-Id: Ib1d0d3f38e6cdc03ad3d2f874258c58960c6fe98

--- jami-android/app/src/main/java/cx/ring/services/CameraService.kt
+++ jami-android/app/src/main/java/cx/ring/services/CameraService.kt
@@ -648,6 +648,7 @@ class CameraService internal constructor(c: Context) {
             Log.d(TAG, "Selected preview size: " + previewSize + ", fps range: " + fpsRange + " rate: " + videoParams.rate)
             view.setAspectRatio(previewSize.height, previewSize.width)
             val texture = view.surfaceTexture ?: throw IllegalStateException()
+            texture.setDefaultBufferSize(previewSize.width, previewSize.height)
             val previewSurface = Surface(texture)
             val codec = if (hw_accel)
                 openEncoder(videoParams, videoParams.getAndroidCodec(), handler, resolution, bitrate)
